ed7bde4fe995b100e90a7ca46600cfd20d0db842
Alan Runyan <alan@enfoldsystems.com>
2002-03-14 15:42:43 +0000

    really lame hack to get topics owrking
    svn path=/CMFPlone/trunk/; revision=164

--- skins/plone_scripts/showEditableBorder.py
+++ skins/plone_scripts/showEditableBorder.py
@@ -21,5 +21,4 @@ if anonymous and not contextEditable:
 if template_id=='folder_contents' or isWorkflowable :
     return 1
 
-if contextEditable:
-    return 1
+if contextEditable and context.getTypeInfo().Type()=='Topic': return 1
