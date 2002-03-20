0b715cad79b3e2596ee239911bf6b158a895ddb9
Lalo Martins <lalo@laranja.org>
2002-03-20 03:29:55 +0000

    ...and now I checked in a version that had typos. Sigh. * lalo wishes for unit tests, and begs for forgiveness
    svn path=/CMFPlone/trunk/; revision=179

--- skins/plone_scripts/showEditableBorder.py
+++ skins/plone_scripts/showEditableBorder.py
@@ -7,12 +7,11 @@
 ##parameters=template_id
 ##title=returns whether or not current template displays *editable* border
 ##
-actions=context.portal_actions.listFilteredActionsFor(context)
-anonymous=membership.isAnonymousUser()
+actions=container.portal_actions.listFilteredActionsFor(context)
 wf_actions=actions.get('workflow', ())
 obj_actions=actions.get('object', ())
 
 # no special cases; just check if we have tabs to show
 # listFilteredActionsFor does all the rest for us
 
-return wf_actions or len(wf_actions) > 1
+return wf_actions or len(obj_actions) > 1
