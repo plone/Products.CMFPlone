c097f1f0dc8192e7f51978b56dbf4ee197949e45
Alan Runyan <alan@enfoldsystems.com>
2002-03-26 06:49:38 +0000

    strips off syndication tab if found on folder types
    svn path=/CMFPlone/trunk/; revision=195

--- Extensions/migrateFolders.py
+++ Extensions/migrateFolders.py
@@ -10,25 +10,33 @@ def migrateFolders(self):
     root=getToolByName(self, 'portal_url').getPortalObject()
     old_folder='Folder'
     new_folder='Plone Folder'
+    folder_types = ['Portal Folder', 'Plone Folder']
 
     def migrate(f):
-	log('inside migrate for ' + f.absolute_url() + '\n')
-	log(f.isPrincipiaFolderish)
 	target=None
-        if f.isPrincipiaFolderish and f.meta_type!='Plone Folder':
+        if f.isPrincipiaFolderish and f.meta_type in (folder_types):
 	   id, tmp_id = f.getId(), 'tmp__'+f.getId()
 	   parent=f.aq_parent
 	   if not hasattr(parent, 'portal_type'): return
-	   parent.invokeFactory(id=tmp_id, type_name=new_folder)
+	   parent.invokeFactory(id=tmp_id, type_name='Folder')
 	   src,target=getattr(parent, f.getId()), getattr(parent, tmp_id)
 	   target.manage_pasteObjects( src.manage_copyObjects( src.objectIds()))
 	   parent.manage_delObjects(id)
 	   parent.manage_renameObjects( (tmp_id, ), (id, ) )
-	for o in target.objectValues('Portal Folder'):
+	for o in target.objectValues(folder_types):
 	       migrate(o)
 	       
-    for f in root.objectValues('Portal Folder'):
+    for f in root.objectValues(folder_types):
         migrate(f)
 
+    #take off syndication tab if its on Folder Type
+    folder_type = getToolByName(self, 'portal_types').getTypeInfo('Folder')
+    types_tool = getToolByName(self, 'portal_types')
+    actions = []
+    for idx in range( len(folder_type._actions)):
+	action=folder_type._actions[idx]
+        if action['id']!='syndication':
+            actions.append(action)
+    setattr( getattr(types_tool, 'Folder'), '_actions', actions)
     return 'finished migration'
 
