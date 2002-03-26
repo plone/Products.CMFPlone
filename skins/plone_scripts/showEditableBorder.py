10991be31fba2c84742d7a46db0c899f421cfcd6
Alan Runyan <alan@enfoldsystems.com>
2002-03-26 18:33:49 +0000

    hopefully fixed green edit border... someone changed it w/o understanding its impact **grr**
    svn path=/CMFPlone/trunk/; revision=204

--- skins/plone_scripts/showEditableBorder.py
+++ skins/plone_scripts/showEditableBorder.py
@@ -1,25 +1,40 @@
-## Script (Python) "showEditableBorder"
 ##bind container=container
 ##bind context=context
 ##bind namespace=
 ##bind script=script
 ##bind subpath=traverse_subpath
-##parameters=template_id
+##parameters=template_id=None
 ##title=returns whether or not current template displays *editable* border
 ##
+if template_id is None:
+    REQUEST=context.REQUEST
+    template_id=REQUEST['PUBLISHED'].getId()
+
+show_border=0
 actions=container.portal_actions.listFilteredActionsFor(context)
 wf_actions=actions.get('workflow', ())
 obj_actions=actions.get('object', ())
 folder_actions=actions.get('folder', ())
 
+def idInActions(seq, action_id):
+    for a in seq:
+        if a.get('id','name').lower()==action_id:
+            return 1
+    return None
+
 # no special cases; just check if we have tabs to show
 # listFilteredActionsFor does all the rest for us
+# XXX - runyaga; I wish it could be this simple ;(
+# show_border = wf_actions or len(obj_actions) > 1 #in CMF1.2 'log in' is a wf_action!
 
-show_border = wf_actions or len(obj_actions) > 1
-
-if template_id=='folder_contents' and container.portal_membership.checkPermission( 'List folder contents', context):
-    show_border = 1
-if template_id=='folder_listing' and container.portal_membership.checkPermission( 'List folder contents', context):
-    show_border = 1
+if context.isPrincipiaFolderish:
+    if idInActions(folder_actions, 'edit'): 
+        return 1
+    if (template_id=='folder_contents' or template_id=='folder_listing') \
+        and container.portal_membership.checkPermission( 'List folder contents', context):
+        show_border = 1
+else:
+    if idInActions(obj_actions, 'edit'): 
+        return 1
 
 return show_border
