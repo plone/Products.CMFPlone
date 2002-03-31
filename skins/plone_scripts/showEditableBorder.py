62352c419ad3252ef0231236e5a5ca375da93d3f
Alan Runyan <alan@enfoldsystems.com>
2002-03-31 22:17:58 +0000

    another stab at editable border ;(
    svn path=/CMFPlone/trunk/; revision=220

--- skins/plone_scripts/showEditableBorder.py
+++ skins/plone_scripts/showEditableBorder.py
@@ -3,20 +3,30 @@
 ##bind namespace=
 ##bind script=script
 ##bind subpath=traverse_subpath
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
 ##parameters=template_id=None
 ##title=returns whether or not current template displays *editable* border
 ##
-REQUEST=context.REQUEST
-actions=[]
+""" assumes the object has a edit property """
 
-if REQUEST.get('filtered_actions', ''): #short circuit listFilteredActionsFor
-    actions=REQUEST['filtered_actions']
-else:
-    actions=container.portal_actions.listFilteredActionsFor(context)
+REQUEST=context.REQUEST
+actions=REQUEST.get('filtered_actions', container.portal_actions.listFilteredActionsFor(context))
+
+if template_id is None:
+    template_id=REQUEST['PUBLISHED'].getId()
 
 if ( getattr(context, 'isPortalContent', 0) and \
      'edit' in [ o.get('id', '') for o in actions.get('object', ()) ] ) or \
      'edit' in [ f.get('id', '') for f in actions.get('folder', ()) ] :
-    return 1
+   
+    if ( getattr(context, 'isPortalContent', 0) and \
+         template_id in [ o.get('action', '') for o in actions.get('object', ()) ] ) or \
+         template_id in [ f.get('action', '') for f in actions.get('folder', ()) ] or \
+         template_id in ['folder_contents', 'folder_listing'] :
+        return 1
 
 return 0
