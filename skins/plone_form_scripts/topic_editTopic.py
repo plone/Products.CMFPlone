18ab899f5c6516f1f874903287d8263f89d22018
Alan Runyan <alan@enfoldsystems.com>
2002-03-31 23:44:18 +0000

    moved topic_view into plone_contents for consistency touched up editableBorder--- again.. ;( main_template reformat
    added topic_edit_form so it uses Plone template and gave it form validation
    this is preliminary Topic support.  ultimately all of the topic xxx_forms need
    to be 'plonized'
    
    svn path=/CMFPlone/trunk/; revision=222

---
+++ skins/plone_form_scripts/topic_editTopic.py
@@ -0,0 +1,36 @@
+## Script (Python) "topic_editTopic"
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
+##parameters=REQUEST, RESPONSE, acquireCriteria, field_id, field_title=None, field_description=None
+##title=
+##
+id, title, description = field_id, field_title, field_description
+errors=context.validate_topic_edit()
+
+if errors:
+    edit_form=getattr(context, context.getTypeInfo().getActionById( 'edit'))
+    return edit_form()
+
+context.edit(acquireCriteria=acquireCriteria,
+             title=title,
+             description=description)
+
+qst='portal_status_message=Topic+changed.'
+target_action = context.getTypeInfo().getActionById( 'view' )
+
+#this needs to be factored into renameAndViewObject and taken out of extrneded_edit
+if id!=context.getId():
+    container.manage_renameObjects( (context.getId(), ), (id, ))
+    url='%s/%s?%s' % ( REQUEST['URL2']
+                     , id +'/'+target_action
+                     , 'portal_status_message='+qst )
+    return REQUEST.RESPONSE.redirect(url)
+
+context.REQUEST.RESPONSE.redirect( '%s/%s?%s' % ( context.absolute_url()
+                                                , target_action
+                                                , qst) )
+
+#RESPONSE.redirect('%s/topic_view' % context.absolute_url())
--- skins/plone_scripts/showEditableBorder.py
+++ skins/plone_scripts/showEditableBorder.py
@@ -16,17 +16,15 @@
 REQUEST=context.REQUEST
 actions=REQUEST.get('filtered_actions', container.portal_actions.listFilteredActionsFor(context))
 
-if template_id is None:
+if template_id is None and hasattr(REQUEST['PUBLISHED'], 'getId'):
     template_id=REQUEST['PUBLISHED'].getId()
 
-if ( getattr(context, 'isPortalContent', 0) and \
-     'edit' in [ o.get('id', '') for o in actions.get('object', ()) ] ) or \
-     'edit' in [ f.get('id', '') for f in actions.get('folder', ()) ] :
+if ( 'edit' in [ o.get('id', '') for o in actions.get('object', ()) ] or \
+     'edit' in [ f.get('id', '') for f in actions.get('folder', ()) ] ) :
    
-    if ( getattr(context, 'isPortalContent', 0) and \
-         template_id in [ o.get('action', '') for o in actions.get('object', ()) ] ) or \
+    if ( template_id in [ o.get('action', '') for o in actions.get('object', ()) ] or \
          template_id in [ f.get('action', '') for f in actions.get('folder', ()) ] or \
-         template_id in ['folder_contents', 'folder_listing'] :
+         template_id in ['folder_contents', 'folder_listing'] ) :
         return 1
 
 return 0
