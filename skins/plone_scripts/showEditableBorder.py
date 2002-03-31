5d25cc4c2eea5565f213b59ac7018c9fa7db7218
Alan Runyan <alan@enfoldsystems.com>
2002-03-31 01:38:41 +0000

    attempting to simplfy it
    svn path=/CMFPlone/trunk/; revision=210

--- skins/plone_scripts/showEditableBorder.py
+++ skins/plone_scripts/showEditableBorder.py
@@ -6,37 +6,24 @@
 ##parameters=template_id=None
 ##title=returns whether or not current template displays *editable* border
 ##
-if template_id is None:
-    REQUEST=context.REQUEST
-    template_id=REQUEST['PUBLISHED'].getId()
+REQUEST=context.REQUEST
+actions=[]
 
-actions=container.portal_actions.listFilteredActionsFor(context)
-wf_actions=actions.get('workflow', ())
+# lets attempt to short circuit listFilteredActionsFor, which is expensive!
+if REQUEST.get('filtered_actions', ''): 
+    actions=REQUEST['filtered_actions']
+else:
+    actions=container.portal_actions.listFilteredActionsFor(context)
+
 obj_actions=actions.get('object', ())
 folder_actions=actions.get('folder', ())
 
-def idInActions(seq, action_id):
-    for a in seq:
-        if a.get('id','name').lower()==action_id:
-            return 1
-    return None
+objectActionIds = [ o.get('id', '') for o in obj_actions ]
+if getattr(context, 'isPortalContent', 0) and 'edit' in objectActionIds: 
+    return 1
 
-# no special cases; just check if we have tabs to show
-# listFilteredActionsFor does all the rest for us
-# XXX - runyaga; I wish it could be this simple ;(
-# show_border = wf_actions or len(obj_actions) > 1 #in CMF1.2 'log in' is a wf_action!
+folderActionsIds = [ f.get('id', '') for f in folder_actions]
+if 'edit' in folderActionsIds:
+    return 1
 
-show_border=0
-isPortalContent = getattr(context, 'isPortalContent', 0)
-checkPermission = container.portal_membership.checkPermission
-
-#if folder_actions and \
-#   template_id in ['folder_listing', 'folder_contents'] and \
-#   checkPermission( 'List folder contents', context):
-#    show_border = 1
-if isPortalContent and idInActions(obj_actions, 'edit'): 
-    show_border = 1 
-elif idInActions(folder_actions, 'edit'): #if you can edit the folder
-    show_border = 1
-
-return show_border
+return 0
