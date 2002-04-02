af14c09eb774252910d470abdcfbd0194484f896
Alan Runyan <alan@enfoldsystems.com>
2002-04-02 09:47:53 +0000

    *** empty log message ***
    svn path=/CMFPlone/trunk/; revision=234

--- skins/plone_scripts/my_worklist.py
+++ skins/plone_scripts/my_worklist.py
@@ -20,7 +20,7 @@ for wlist_map_sequence in wf_wlist_map.values():
         permission=wlist_map['guard_permissions']
         catalog_vars=wlist_map['catalog_vars']
         for result in catalog.searchResults(catalog_vars):
-	    o = getObject()
+	    o = results.getObject()
 	    if context.portal_membership.checkPermission(permission, o) and \
                 o.absolute_url() not in [a.absolute_url() for a in avail_objs] :
                 avail_objs.append(o)
