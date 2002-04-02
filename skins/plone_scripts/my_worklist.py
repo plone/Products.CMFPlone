f3fce216d8458cf6e7e12ef8d620c9749666f376
andyd_ <andyd_@localhost>
2002-04-02 15:18:27 +0000

    fixed 2 workflow bugs.  One nameerror on Results.. was a simple typeo should have been result.  and fixed bug when event has no Title
    svn path=/CMFPlone/trunk/; revision=239

--- skins/plone_scripts/my_worklist.py
+++ skins/plone_scripts/my_worklist.py
@@ -19,9 +19,10 @@ for wlist_map_sequence in wf_wlist_map.values():
     for wlist_map in wlist_map_sequence:
         permission=wlist_map['guard_permissions']
         catalog_vars=wlist_map['catalog_vars']
+
         for result in catalog.searchResults(catalog_vars):
-	    o = results.getObject()
-	    if context.portal_membership.checkPermission(permission, o) and \
+            o = result.getObject()
+            if context.portal_membership.checkPermission(permission, o) and \
                 o.absolute_url() not in [a.absolute_url() for a in avail_objs] :
                 avail_objs.append(o)
 
