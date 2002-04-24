cce8463b282d34198b259b4ade1d6e92d867a840
Alan Runyan <alan@enfoldsystems.com>
2002-04-24 00:36:12 +0000

    dont goto 'folder_contents' unless already there (editor mode)
    svn path=/CMFPlone/trunk/; revision=305

--- skins/plone_scripts/breadcrumbs.py
+++ skins/plone_scripts/breadcrumbs.py
@@ -16,14 +16,16 @@ relative_ids = context.portal_url.getRelativeContentPath( obj)
 
 path_seq = ( ('home', o.absolute_url()), )
 
+template_id = context.REQUEST.get('PUBLISHED', None)
+
 for id in relative_ids:
-    
     try:        
         o=o.restrictedTraverse(id)
         if o.getId() in ('talkback', ): # I'm sorry ;(
             raise 'talkbacks would clutter our precious breadcrumbs'
         if o.isPrincipiaFolderish and \
-           context.portal_membership.checkPermission('List folder contents', o):
+           context.portal_membership.checkPermission('List folder contents', o) and \
+	   (template_id is not None and template_id.getId()=='folder_contents'):
             path_seq+=( (o.title_or_id(), o.absolute_url()+'/folder_contents'), )
         else:
             path_seq+=( (o.title_or_id(), o.absolute_url()), )
