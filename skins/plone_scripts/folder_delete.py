a27cc79e30857964b62abbf95e22e2537f6c64b9
Alan Runyan <alan@enfoldsystems.com>
2002-05-20 13:52:51 +0000

    mistype
    svn path=/CMFPlone/trunk/; revision=558

--- skins/plone_scripts/folder_delete.py
+++ skins/plone_scripts/folder_delete.py
@@ -14,4 +14,4 @@ if REQUEST.has_key('ids'):
   context.manage_delObjects(REQUEST['ids'], REQUEST)
   return REQUEST.RESPONSE.redirect(context.absolute_url() + '/folder_contents?portal_status_message=Deleted.')
 else:
-  return REQUEST.RESPONSE.redirect(context.absolute_url() + '/folder_contents?portal_status_message=Please+select+one+or+more+items+first.'
+  return REQUEST.RESPONSE.redirect(context.absolute_url() + '/folder_contents?portal_status_message=Please+select+one+or+more+items+first.')
