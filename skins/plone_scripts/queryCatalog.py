58914591910a1b382be69e9f7dcbaf949352f531
Alan Runyan <alan@enfoldsystems.com>
2002-05-31 20:51:11 +0000

    wrapped catalog queries with some logic, most form based catalog queries can go through queryCatalog() fixed search_form.pt made search.pt use queryCatalog()
    svn path=/CMFPlone/trunk/; revision=596

---
+++ skins/plone_scripts/queryCatalog.py
@@ -0,0 +1,29 @@
+## Script (Python) "queryCatalog"
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
+##parameters=
+##title=wraps the portal_catalog with a rules qualified query
+##
+results=[]
+REQUEST=context.REQUEST
+catalog=context.portal_catalog
+indexes=catalog.indexes()
+query={}
+
+for i in indexes:
+    v=REQUEST.get(i, None)
+    if v:
+        query.update({i:v})
+
+notRawIndexFields=[k for k in REQUEST.form.keys() if k not in query.keys()]
+if notRawIndexFields:
+    for k in notRawIndexFields:
+        if k.endswith('_usage'): 
+            query.update({k:REQUEST.get(k)})
+
+if query:
+    results=catalog(query)
+return results
