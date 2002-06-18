36e2ab506d390fa00208f3c24bf657a9087a2f33
hhamish <hhamish@users.sourceforge.net>
2002-06-18 20:07:57 +0000

    event_edit_form uses calendar popup
    svn path=/CMFPlone/trunk/; revision=689

--- skins/plone_scripts/form_scripts/event_edit.py
+++ skins/plone_scripts/form_scripts/event_edit.py
@@ -4,7 +4,7 @@
 ##bind namespace=
 ##bind script=script
 ##bind subpath=traverse_subpath
-##parameters=REQUEST, RESPONSE, field_title=None, field_description=None, event_type=None, effectiveDay=None, effectiveMo=None, effectiveYear=None, expirationDay=None, expirationMo=None, expirationYear=None, start_time=None, startAMPM=None, stop_time=None, stopAMPM=None, location=None, contact_name=None, contact_email=None, contact_phone=None, event_url=None, field_id='' 
+##parameters=REQUEST, RESPONSE, field_title=None, field_description=None, event_type=None, start_date=None, end_date=None, location=None, contact_name=None, contact_email=None, contact_phone=None, event_url=None, field_id='' 
 ##title=
 ##
 from Products.CMFPlone import transaction_note
@@ -16,20 +16,27 @@ if not field_id:
 
 id, title, description=field_id, field_title, field_description
 
+# need to parse date string *before* passing to Event.edit since
+# it expects bite sized chunks....
+from DateTime import DateTime
+
+dt_start = DateTime( start_date )
+dt_end = DateTime( end_date )
+
 try:
     context.edit(title=title
              , description=description
              , eventType=event_type
-             , effectiveDay=effectiveYear
-             , effectiveMo=effectiveMo
-             , effectiveYear=effectiveDay
-             , expirationDay=expirationYear
-             , expirationMo=expirationMo
-             , expirationYear=expirationDay
-             , start_time=start_time
-             , startAMPM=startAMPM
-             , stopAMPM=stopAMPM
-             , stop_time=stop_time
+             , effectiveDay=dt_start.year()
+             , effectiveMo=dt_start.month()
+             , effectiveYear=dt_start.day()
+             , expirationDay=dt_end.year()
+             , expirationMo=dt_end.month()
+             , expirationYear=dt_end.day()
+             , start_time='%2.2d:%2.2d'%(dt_start.h_24(), dt_start.minute())
+             , startAMPM=dt_start.ampm()
+             , stopAMPM=dt_start.ampm()
+             , stop_time='%2.2d:%2.2d'%(dt_end.h_24(), dt_end.minute())
              , location=location
              , contact_name=contact_name
              , contact_email=contact_email
