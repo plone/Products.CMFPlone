2dc08407a8a8e44db5c0b275f2545c04a0ea5b92
Alan Runyan <alan@enfoldsystems.com>
2002-07-25 22:40:01 +0000

    Issue 71 now you can rename both ID's and Titles at the same time in a folder_rename_form We need text to go along with this form.  As well as the 'optional ids'
    svn path=/CMFPlone/trunk/; revision=796

--- skins/plone_scripts/folder_rename.py
+++ skins/plone_scripts/folder_rename.py
@@ -7,14 +7,25 @@
 ##parameters=
 ##title=Rename Object
 ##
+from Products.CMFPlone import transaction_note
 REQUEST=context.REQUEST
-changed=0
-for id in REQUEST['new_ids']:
-    if id:
-        changed=1
-if not changed: 
+x=0
+new_ids=REQUEST['new_ids']
+old_ids=REQUEST['ids']
+new_titles=REQUEST['new_titles']
+
+for id in new_ids:
+    old_id=old_ids[x]
+    new_title=new_titles[x]
+    o=getattr(context,old_id)
+    if o.Title()!=new_title:
+        o.setTitle(new_title)
+    x=x+1
+    
+if not new_ids: 
     return REQUEST.RESPONSE.redirect(context.absolute_url() + '/folder_contents?portal_status_message=No+Item(s)+Marked+For+Renaming.')
 
-
 context.manage_renameObjects(REQUEST['ids'], REQUEST['new_ids'], REQUEST)
+transaction_note( str(REQUEST['ids']) + 'have been renamed' )
+
 return REQUEST.RESPONSE.redirect(context.absolute_url() + '/folder_contents?portal_status_message=Item(s)+Renamed.')
