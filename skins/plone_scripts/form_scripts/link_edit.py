9fa054c656ebcb3bc4cadafa4ddaffde81012b14
Geoff Davis <geoff@geoffdavis.net>
2002-08-17 18:50:45 +0000

    refactored to work with FormTool
    svn path=/CMFPlone/trunk/; revision=964

--- skins/plone_scripts/form_scripts/link_edit.py
+++ skins/plone_scripts/form_scripts/link_edit.py
@@ -8,21 +8,8 @@
 ##title=Edit a link
 ##
 
-errors = context.portal_form_validation.validate(context, 'validate_link_edit')
-if errors:
-    edit_form=context.portal_navigation.getNextPageFor( context
-                                                , script.getId()
-                                                , 'failure')
-    return edit_form()
-    
 context.edit(remote_url=remote_url)
-
 context.plone_utils.contentEdit( context
                                , id=id
                                , description=description)
-
-return context.portal_navigation.getNextRequestFor( context
-                                            , script.getId()
-                                            , 'success'
-                                            , portal_status_message='Link changed.')
-
+return 'success'
