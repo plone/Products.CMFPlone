5b9bcfd09f6a74567bf07b4d53b5b6af30de916b
Geoff Davis <geoff@geoffdavis.net>
2002-08-17 18:49:03 +0000

    brand new FormTool, lots of enhancements to NavigationTool
    svn path=/CMFPlone/trunk/; revision=963

---
+++ FormTool.py
@@ -0,0 +1,259 @@
+from Products.CMFCore.utils import UniqueObject
+from Globals import InitializeClass
+from AccessControl import ClassSecurityInfo
+from OFS.SimpleItem import SimpleItem
+from OFS.Folder import Folder
+from OFS.Traversable import Traversable
+from Acquisition import aq_parent, aq_base
+from FormulatorTool import CMFForm
+from FactoryTool import PendingCreate
+from OFS.SimpleItem import Item
+from OFS.ObjectManager import bad_id
+
+#from AccessControl.SecurityManagement import getSecurityManager
+from Acquisition import Implicit
+from AccessControl import Role
+
+validator_cache = {}  # a place to stash cached validators
+# (we don't want to persist them in the ZODB since that would make debugging a big pain)
+
+debug = 1  # enable/disable logging
+
+class FormTool(UniqueObject, SimpleItem):
+    id = 'portal_form'
+    meta_type= 'Plone Form Tool'
+    security = ClassSecurityInfo()
+
+    # special-purpose items used in the REQUEST
+    error_key = 'errors'                    # a dictionary used for storing form validation errors
+    form_submitted_key = 'form_submitted'   # supplies the name of the form submitted
+    id_key = 'id'                           # the id for the object being operated on (can be None)
+    form_action_key = 'form_action'         # the action for the form
+
+
+    def setValidator(self, form, validator):
+        """Register a form validator"""
+        property_tool = getattr(self, 'portal_properties')
+        formprops = getattr(property_tool, 'form_properties')
+        if formprops.hasProperty(form):
+            form_props._updateProperty(form, validator)
+        else:
+            formprops._setProperty(form, validator)
+
+
+    security.declarePublic('getValidator')
+    def getValidator(self, form):
+        """Get the validator registered for a given form"""
+        property_tool = getattr(self, 'portal_properties')
+        form_props = getattr(property_tool, 'form_properties')
+        validator = form_props.getProperty(form, None)
+        if validator:
+            validator = validator.strip()
+        return validator
+
+
+    security.declarePublic('validate')
+    def validate(self, context, REQUEST, validator=None):
+        """Convenience method for form handlers.  Calls an optional external validator,
+           checks for ID collisions when objects are renamed, then sets up the REQUEST.
+           Places a dictionary of errors in the REQUEST that is accessed via REQUEST[FormTool.error_key]
+           Returns a status of 'success' or 'failure'.
+        """
+        # validate is a convenience method that is typically called by a form handling
+        # script, e.g. a myobject_edit.py external method in skins/plone_scripts/form_scripts.
+        #
+        # validate first calls the validation script specified by validator.  Next, it
+        # does a basic check on the id parameter: if the id has changed, it makes sure there
+        # isn't aready an object with the new id in the current object's container.  Finally,
+        # it sets up the REQUEST by calling validate_setupRequest.
+        #
+        # *** Form handlers should always call validate even if they require no additional
+        # validation to check for id collisions and to set up the REQUEST ***
+        errors = {}
+        if validator and validator.strip() != '':
+            errors = apply(getattr(context, validator), ())
+            # make sure errors is a dictionary
+            if not type(errors) == type({}):
+                errors = {}
+
+        messages = context.errorMessages()
+        # do some basic id validation
+        id = REQUEST.get(self.id_key, None)
+        if id:
+            if bad_id(id):
+                # id is bad
+                errors[self.id_key] = messages['illegal_id']
+            else:
+                # id is good; make sure we have no id collisions
+                
+                if context.__class__ == PendingCreate:
+                    # always check for collisions if we are creating a new object
+                    checkForCollision = 1
+                    container = context.getParentNode().getParentNode()
+                else:
+                    # if we have an existing object, only check for collisions if we are changing the id
+                    checkForCollision = (context.getId() != id)
+                    container = context.getParentNode()
+
+                # perform the actual check
+                if checkForCollision:
+                    if id in container.objectIds():
+                        errors[self.id_key] = messages['id_exists']
+
+        # set a status message indicating errors
+        if errors:
+            self.REQUEST.set('portal_status_message', messages['error_exists'])
+
+        self.REQUEST.set(self.error_key, errors)
+        if errors:
+            return 'failure'
+        else:
+            return 'success'
+
+
+    security.declarePublic('createForm')
+    def createForm(self):
+        """Returns a CMFForm object"""
+        # CMFForm wraps Formulator.BasicForm and provides some convenience methods that
+        # make BasicForms easier to work with from external methods.
+        portalRootObject=getToolByName(self, 'portal_url').getPortalObject()
+        form = CMFForm()
+        return form.__of__(portalRootObject)
+
+
+    security.declarePublic('cacheValidator')
+    def cacheValidator(self, key, validator):
+        """Cache a validator for later use"""
+        global validator_cache
+        validator_cache[key] = validator
+
+
+    security.declarePublic('getValidator')
+    def getCachedValidator(self, key):
+        """Get a validator from the cache"""
+        global validator_cache
+        return validator_cache.get(key, None)
+
+
+    def __bobo_traverse__(self, REQUEST, name):
+        # We intercept traversal when a form needs validation.  We insert a FormValidator object
+        # into the traversal stack which, when published, does the proper validation and then
+        # hands off to the navigation tool to determine the correct object to publish.
+
+        # see if we are handling validation for this form
+        validator = self.getValidator(name)
+        if not validator:
+            # no -- do normal traversal
+            target = getattr(aq_parent(self), name, None)
+            if name.endswith('.js'):
+                self.log('name: %s, target: <Javascript>, context: %s' % (name, str(aq_parent(self))), '_traverseTo')
+            else:
+                self.log('name: %s, target: %s, context: %s' % (name, target, str(aq_parent(self))), '_traverseTo')
+            if target:
+                return target
+            else:
+                return REQUEST.RESPONSE.notFoundError("%s\n" % (name))
+
+        # There are three potential points of entry to a form:
+        # 1) The form is accessed directly from a URL, e.g. http://plone/portal_form/link_edit:
+        #       In this case we have REQUEST.errors = None, REQUEST.form_submitted = None
+        # 2) The form is filled out and submitted.
+        #       In this case we have REQUEST.errors = None, REQUEST.form_submitted = 1
+        # 3) A form validation error occurs, and the form is invoked by the navigation tool
+        #       In this case we have REQUEST.errors != None, REQUEST.form_submitted = 1
+        #
+        # We only need to invoke the validator for case (2)
+    
+        # see if we need to invoke validation
+        errors = REQUEST.get(self.error_key, None)
+        # Make sure errors is something generated by us, not something submitted via POST or GET
+        if not type(errors) != type({}):
+            errors = None
+        form_submitted = REQUEST.get(FormTool.form_submitted_key, None)
+        self.log('errors = '+str(errors) + ', form_submitted = ' + str(form_submitted), '__bobo_traverse__')
+
+        
+        # We wrap the object in the acquisition layer of the parent of the FormTool
+        # so that subsequent forms will operate on the FormTool's context and not
+        # on the FormTool.
+        do_validate = form_submitted and not errors
+        if not do_validate:
+            self.log('would go to %s.%s' % (str(aq_parent(self)), name))
+        self.log('returning FormValidator(%s,%s,%s)' % (name, validator, do_validate), '__bobo_traverse__')
+        self.log(REQUEST.URL)
+        return FormValidator(name, validator, do_validate).__of__(aq_parent(self)) # wrap in acquisition layer
+
+  
+    security.declarePublic('log')
+    def log(self, msg, loc=None):
+        """ """
+        if not debug:
+            return
+        import sys
+        prefix = 'FormTool'
+        if loc:
+            prefix = prefix + '. ' + str(loc)
+        sys.stdout.write(prefix+': '+str(msg)+'\n')
+
+InitializeClass(FormTool)
+
+
+class FormValidator(SimpleItem):
+    """ """
+    # When a form needs validation, we publish a FormValidator object in place of the next form.
+    # The FormValidator calls a validator method on the variables in the REQUEST and then
+    # hands off to a page determined by the NavigationTool
+    security = ClassSecurityInfo()
+
+    def __init__(self, form, validator, do_validate):
+        self.form = form
+        self.validator = validator
+        self.do_validate = do_validate
+
+
+    security.declarePublic('__call__')
+    def __call__(self, REQUEST, **kw):
+        """ """
+        self.log('validator[%s] = \'%s\'' % (self.form, self.validator), '__call__')
+        REQUEST.set(FormTool.form_action_key, REQUEST.get('URL0'))
+
+        if self.do_validate:
+            context = self.aq_parent
+            form_tool = context.portal_form
+            self.log('invoking validation, status = '+str(form_tool.validate(context, REQUEST, self.validator)))
+            # invoke validation
+            status = form_tool.validate(context, REQUEST, self.validator)
+
+            # check for validation errors
+            if status == 'success':
+                # if no errors, create a new object if creation is pending and change the context
+                if context.__class__ == PendingCreate:
+                    self.log("new id = " + REQUEST[FormTool.id_key])
+                    context = context.invokeFactory(REQUEST[FormTool.id_key], )
+
+            return context.portal_navigation.getNext(context, self.form, status, **kw)
+        else:
+            self.log('instead going to to %s.%s' % (str(aq_parent(self)), self.form))
+            target = getattr(aq_parent(self), self.form, None)
+            return target(REQUEST, **kw)
+
+
+    index_html = None  # call __call__, not index_html
+#    security.declarePublic('index_html')
+#    def index_html(self, REQUEST, **kw):
+#        """ """
+#        return self.__call__(REQUEST, **kw)
+
+
+    security.declarePublic('log')
+    def log(self, msg, loc=None):
+        """ """
+        if not debug:
+            return
+        import sys
+        prefix = 'FormValidator'
+        if loc:
+            prefix = prefix + '. ' + str(loc)
+        sys.stdout.write(prefix+': '+str(msg)+'\n')
+
+InitializeClass(FormValidator)
--- NavigationTool.py
+++ NavigationTool.py
@@ -1,3 +1,5 @@
+from ZPublisher.mapply import mapply
+from ZPublisher.Publish import call_object, missing_name, dont_publish_class
 from Products.CMFCore.utils import UniqueObject
 from Products.CMFCore.utils import _checkPermission, _getAuthenticatedUser, limitGrantedRoles
 from Products.CMFCore.utils import getToolByName, _dtmldir
@@ -9,6 +11,9 @@ from types import TupleType
 from urllib import urlencode
 from cgi import parse_qs
 import re
+from FactoryTool import PendingCreate
+
+debug = 1  # enable/disable logging
 
 class NavigationTool (UniqueObject, SimpleItem):
     """ provides navigation related utilities """
@@ -16,6 +21,104 @@ class NavigationTool (UniqueObject, SimpleItem):
     meta_type= 'CMF Navigation Tool'
     security = ClassSecurityInfo()
     plone_tool = 1
+
+
+    security.declarePublic('getNext')
+    def getNext(self, context, action, status, **kwargs):
+        """ Perform the next action specified by in portal_properties.navigation_properties.
+
+            context - the current context
+
+            action - the script currently being called
+
+            status - 'success' or 'failure' strings used in calculating destination
+
+            kwargs - additional keyword arguments are passed to subsequent pages either in
+                the REQUEST or as GET parameters if a redirection needs to be done
+        """
+        (transition_type, transition) = self.getNavigationTransistion(context,action,status)
+        self.log("%s.%s.%s(%s) -> %s:%s" % (context, action, status, str(kwargs), transition_type, transition), 'getNext')
+        if transition_type == 'action':
+            return self._dispatchAction(context, transition, **kwargs)
+        elif transition_type == 'url':
+            return self._dispatchUrl(context, transition, **kwargs)
+        elif transition_type == 'script':
+            return self._dispatchScript(context, transition, **kwargs)
+        else:
+            return self._dispatchPage(context, transition, **kwargs)
+
+
+    def addTransitionFor(self, content, action, status, destination):
+        """ adds a transition 
+
+            content - is a object or a TypeInfo that you would like to
+                      register a None content object will register Default values.
+
+            action - script that is used by content edit form
+                     XXX this kinda hokey and error prone
+
+            status - SUCCESS or FAILURE strings used in calculating destination
+
+            destination - is an action registed on the TypeInfo or a free-form script
+                          that would be appended to the url of the content
+
+            Destinations are specified as follows:
+                action:ACTION_NAME invokes the action ACTION_NAME on the current context
+                script:SCRIPT_NAME invokes the python script SCRIPT_NAME on the current context.
+                        The script should return a tuple containing a status code (either 'success'
+                        or 'failure') and optional kwargs.
+                        getNext() will be called using the return code to determine the next page
+                        to load.
+                url:URL redirects to the url specified by URL
+                PAGE invokes the page PAGE on the current context
+        """
+
+        property_tool = getattr(self, 'portal_properties')
+        navprops = getattr(property_tool, 'navigation_properties') #propertymanager that holds data
+        status = status.lower()
+        action = action.lower()
+
+        if status not in self._availableStatus():
+            raise KeyError, '%s status is not supported' % status
+
+        content=self._getContentFrom(content)
+
+        transition = '%s.%s.%s'%( content
+                                , action
+                                , status )
+
+        if navprops.hasProperty(transition):
+            navprops._updateProperty(transition, destination)
+        else:
+            navprops._setProperty(transition, destination)
+
+
+    security.declarePrivate('removeTransitionFor')
+    def removeTransitionFrom(self, content, action=None, status=None):
+        """ removes everything regarding a content/action combination """
+        property_tool = getattr(self, 'portal_properties')
+        navprops = getattr(property_tool, 'navigation_properties') #propertymanager that holds data
+        transition = self._getContentFrom(content)
+        if action is not None:
+            transition += '.'+action
+        if status is not None:
+            transition += '.'+status
+        for prop in navprops.propertyIds():
+            if prop.startswith(transition):
+                navprops._delProperty(prop)
+
+
+    def _getContentFrom(self, content):
+        """ returns the internal representation of content type """
+        if content is None:
+            content = 'Default'
+        if hasattr(content, '_isPortalContent'): #XXX Contentish xface
+            content = content.getTypeInfo()
+        if hasattr(content, '_isTypeInformation'): #XXX use a xface
+            content = content.getId()
+        content = ''.join(content.split(' ')).lower() #normalize
+        return content
+
 
     # Transitions are determined by a set of actions listed in a properties file.
     # One can include information from the REQUEST into a transition by using 
@@ -55,25 +158,128 @@ class NavigationTool (UniqueObject, SimpleItem):
         action2 = action2.replace('\]', ']')
         return action2
 
-    def getNavigationTransistion(self, context, action, status):
-        navprops = getattr(self, 'navigation_properties')
-        fixedTypeName = ''.join(context.getTypeInfo().getId().lower().split(' '))
-        navTransition = fixedTypeName+'.'+action+'.'+status
-        action_id = getattr(navprops.aq_explicit, navTransition, None)
-        if action_id is None:
-            navTransition='%s.%s.%s' % ('default',action,status)
-            action_id = getattr(navprops.aq_explicit, navTransition, None)
-        if action_id is None:
-            navTransition='%s.%s.%s' % ('default','default',status)
-            action_id = getattr(navprops.aq_explicit, navTransition, '')
-        return self._transitionSubstitute(action_id, self.REQUEST)
+    def _parseTransition(self, transition):
+        type_list = ['action', 'url', 'script']
 
+        for t in type_list:
+            if transition.startswith(t+':'):
+                return (t, transition[len(t)+1:].strip())
+        return (None, transition.strip())
+
+    def getNavigationTransistion(self, context, action, status):
+        property_tool = getattr(self, 'portal_properties')
+        navprops = getattr(property_tool, 'navigation_properties')
+
+        self.log(str(context.__class__))
+        from FactoryTool import PendingCreate
+        if context.__class__ == PendingCreate:
+            type = context.getPendingCreateType()
+        else:
+            type = context.getTypeInfo().getId()
+        fixedTypeName = ''.join(type.lower().split(' '))
+        self.log('FIXEDTYPENAME = ' + fixedTypeName)
+        
+        transition_key = fixedTypeName+'.'+action+'.'+status
+        transition = getattr(navprops.aq_explicit, transition_key, None)
+        
+        if transition is None:
+            transition_key='%s.%s.%s' % ('default',action,status)
+            transition = getattr(navprops.aq_explicit, transition_key, None)
+
+            if transition is None:
+                transition_key='%s.%s.%s' % ('default','default',status)
+                transition = getattr(navprops.aq_explicit, transition_key, '')
+
+        transition = self._transitionSubstitute(transition, self.REQUEST)
+        return self._parseTransition(transition)
+
+    def _dispatchPage(self, context, page, **kwargs):
+        # If any query parameters have been specified in the transition,
+        # stick them into the request before calling getActionById()
+        queryIndex = page.find('?')
+        if queryIndex > -1:
+            query = parse_qs(page[queryIndex+1:])
+            for key in query.keys():
+                if len(query[key]) == 1:
+                    self.REQUEST[key] = query[key][0]
+                else:
+                    self.REQUEST[key] = query[key]
+            page = page[0:queryIndex]
+
+        self.log("page = " + str(page) + ", context = " + str(context), '_dispatchPage')
+        if page is not None:
+            return apply(context.restrictedTraverse(page), (context, context.REQUEST), kwargs)
+        raise Exception, 'Argh! could not find the transition, ' + page
+
+    def _dispatchScript(self, context, script, **kwargs):
+        self.log('calling ' + script, '_dispatchScript')
+
+        request = {}
+        for key in self.REQUEST.keys():
+            request[key] = self.REQUEST[key]
+        request.update(kwargs)
+
+        script_object = getattr(context, script)
+
+        status = mapply(script_object, self.REQUEST.args, request,
+                        call_object, 1, missing_name, dont_publish_class,
+                        self.REQUEST, bind=1)
+
+        self.log('status = ' + str(status), '_dispatchScript')
+        if type(status) == type(()):
+            (status, kwargs) = status
+        else:
+            kwargs = {}
+        return self.getNext(context, script, status, **kwargs)
+    
+    def _dispatchRedirect(self, context, url, **kwargs):
+        url = self._addUrlArgs(url, kwargs)
+        self.log('url = ' + str(url), '_dispatchRedirect')
+        return self.REQUEST.RESPONSE.redirect(url)
+
+    def _dispatchAction(self, context, action_id, **kwargs):
+        next_action = context.getTypeInfo().getActionById(action_id)
+        url = self._addUrlArgs(next_action, kwargs)
+        self.log('url = ' + str(url), '_dispatchAction')
+        return self.REQUEST.RESPONSE.redirect('%s/%s' % (context.absolute_url(), url))
+
+    def _addUrlArgs(self, base, kwargs):
+        url_params=urlencode(kwargs)
+        if base.find('?') >= 0:
+            separator = '&'
+        else:
+            separator = '?'
+            if len(url_params) == 0:
+                separator = ''
+        return base+separator+url_params 
+
+
+    def _availableStatus(self):
+        return ('success', 'failure')
+
+
+    security.declarePublic('log')
+    def log(self, msg, loc=None):
+        """ """
+        if not debug:
+            return
+        import sys
+        if not loc:
+            loc = 'NavigationTool'
+        else:
+            loc = 'NavigationTool.'+loc
+        sys.stdout.write(loc+': '+str(msg)+'\n')
+
+
+    # DEPRECATED -- FOR BACKWARDS COMPATIBILITY WITH PLONE 1.0 ALPHA 2 ONLY
+    # USE GETNEXT() INSTEAD AND UPDATE THE NAVIGATION PROPERTIES FILE
     security.declarePublic('getNextPageFor')
     def getNextPageFor(self, context, action, status, **kwargs):
         """ given a object, action_id and status we can fetch the next action
             for this object 
         """
-        action_id=self.getNavigationTransistion(context,action,status)
+        (transition_type, action_id) = self.getNavigationTransistion(context,action,status)
+        
         # If any query parameters have been specified in the transition,
         # stick them into the request before calling getActionById()
         queryIndex = action_id.find('?')
@@ -96,11 +302,13 @@ class NavigationTool (UniqueObject, SimpleItem):
         if next_action is not None:
             return context.restrictedTraverse(next_action)
         raise Exception, 'Argh! could not find the transition, ' + navTransition
-            
+
+    # DEPRECATED -- FOR BACKWARDS COMPATIBILITY WITH PLONE 1.0 ALPHA 2 ONLY
+    # USE GETNEXT() INSTEAD AND UPDATE THE NAVIGATION PROPERTIES FILE
     security.declarePublic('getNextRequestFor')
     def getNextRequestFor(self, context, action, status, **kwargs):
         """ takes object, action, and status and returns a RESPONSE redirect """
-        action_id=self.getNavigationTransistion(context,action,status) ###
+        (transition_type, action_id) = self.getNavigationTransistion(context,action,status) ###
         if action_id.find('?') >= 0:
             separator = '&'
         else:
@@ -119,66 +327,5 @@ class NavigationTool (UniqueObject, SimpleItem):
                                                              , next_action
                                                              , separator
                                                              , url_params) )
-
-    def _availableStatus(self):
-        return ('success', 'failure')
-
-    def getContentFrom(self, content):
-        """ returns the internal representation of content type """
-        if content is None:
-            content = 'Default'
-        if hasattr(content, '_isPortalContent'): #XXX Contentish xface
-            content = content.getTypeInfo()
-        if hasattr(content, '_isTypeInformation'): #XXX use a xface
-            content = content.getId()
-        content = ''.join(content.split(' ')).lower() #normalize
-        return content
-
-    def addTransitionFor(self, content, action, status, destination):
-        """ adds a transition 
-
-            content - is a object or a TypeInfo that you would like to
-                      register a None content object will register Default values.
-
-            action - script that is used by content edit form
-                     XXX this kinda hokey and error prone
-
-            status - SUCCESS or FAILURE strings used in calculating destination
-
-            destination - is an action registed on the TypeInfo or a free-form script
-                          that would be appended to the url of the content
-        """
-
-        navprops = getattr(self, 'navigation_properties') #propertymanager that holds data
-        status = status.lower()
-        action = action.lower()
-
-        if status not in self._availableStatus():
-            raise KeyError, '%s status is not supported' % status
-
-        content=self.getContentFrom(content)
-
-        transition = '%s.%s.%s'%( content
-                                , action
-                                , status )
-
-        if navprops.hasProperty(transition):
-            navprops._updateProperty(transition, destination)
-        else:
-            navprops._setProperty(transition, destination)
-
-    security.declarePrivate('removeTransitionFor')
-    def removeTransitionFrom(self, content, action=None, status=None):
-        """ removes everything regarding a content/action combination """
-        navprops = getattr(self, 'navigation_properties') #propertymanager that holds data
-        transition = self.getContentFrom(content)
-        if action is not None:
-            transition += '.'+action
-        if status is not None:
-            transition += '.'+status
-        for prop in navprops.propertyIds():
-            if prop.startswith(transition):
-                navprops._delProperty(prop)
-
-            
+              
 InitializeClass(NavigationTool)
