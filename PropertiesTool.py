04b561ad555a93c43c62d478736d9686bfab91d8
Alan Runyan <alan@enfoldsystems.com>
2002-08-19 05:10:56 +0000

    fixed it so xfering properties works
    svn path=/CMFPlone/trunk/; revision=972

--- PropertiesTool.py
+++ PropertiesTool.py
@@ -20,13 +20,11 @@ class PropertiesTool(Folder, BaseTool):
     manage_options = ( (Folder.manage_options[0], ) +
                         BaseTool.manage_options  )
 
-    manage_addPropertySheetForm = PageTemplateFile('www/addPropertySheet',globals())
-
-    def manage_addPropertySheet(self, id, title='', propertysheet=None, REQUEST=None):
-        """ adds a instance of a Property Sheet 
-            if handed a propertysheet put the
-            properties into new propertysheet.
-        """
+    manage_addPropertySheetForm = PageTemplateFile( 'www/addPropertySheet'
+                                                  , globals() )
+    
+    def addPropertySheet(id, title='', propertysheet=None):
+        """ add a new PropertySheet """
         o=SimpleItemWithProperties(id, title)
         self._setObject(id, o)
         o=self._getOb(id)
@@ -39,9 +37,16 @@ class PropertiesTool(Folder, BaseTool):
             for property in propertysheet.propertyMap():
                 pid=property.get('id')
                 ptype=property.get('type')
-                pvalue=property.getProperty(pid)
+                pvalue=propertysheet.getProperty(pid)
                 o._setProperty(pid, pvalue, ptype)
             
+    def manage_addPropertySheet(self, id, title='', propertysheet=None, REQUEST=None):
+        """ adds a instance of a Property Sheet 
+            if handed a propertysheet put the
+            properties into new propertysheet.
+        """
+        self.addPropertySheet(id, title, propertysheet)
+        
         if REQUEST is not None:
             return self.manage_main()
 
