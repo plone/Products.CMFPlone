f9a6ce8eace1af8ae3c4a25ab7226aa7a7889259
Alan Runyan <alan@enfoldsystems.com>
2002-08-19 18:07:30 +0000

    finish lame packager ;0
    svn path=/CMFPlone/trunk/; revision=976

--- Extensions/package_plone.py
+++ Extensions/package_plone.py
@@ -1,12 +1,30 @@
 import os
 
 release='1.0alpha3'
+releasename='CMFPlone-%s' % release
+releasetar='CMFPlone%s.tar' % release
 
 sh_cleanmisc = """find . | grep '~' | xargs rm -rf"""
 sh_cleanpyc = """find . | grep 'pyc' | xargs rm -rf"""
+sh_cleancvs = """ find . | grep 'CVS' | xargs rm -rf"""
 
-os.system(sh_cleanmisc)
-os.system(sh_cleanpyc)
+os.system("cd ..; rm -rf " + releasetar)
+#os.system(sh_cleanmisc)
+#os.system(sh_cleanpyc)
+#os.system(sh_cleancvs)
 
-os.system("cd .. ; tar -cvf CMFPlone%s.tar CMFPlone" % release)
+os.system("cd .. ; mkdir " + releasename)
+os.system("cd .. ; cp -rf CMFPlone " + releasename)
+os.system("cd .. ; cp -rf Formulator " + releasename)
+os.system("cd .. ; cp -rf DCWorkflow " + releasename)
+
+os.system("cd ../" + releasename+";" + sh_cleanmisc)
+os.system("cd ../" + releasename+";" + sh_cleanpyc)
+os.system("cd ../" + releasename+";" + sh_cleancvs)
+
+os.system("cd .. ; tar -cvf %s %s" % ( releasetar 
+                                     , releasename ) )
+os.system("cd .. ; gzip %s " % releasetar )
+
+os.system("cd .. ; rm -rf " + releasename )
 
