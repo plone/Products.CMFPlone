431e7a082079acae5a0924d8fc1f592dae2d278e
Alan Runyan <alan@enfoldsystems.com>
2002-08-19 03:39:53 +0000

    refactored MembershipTool for CMF1.3
    svn path=/CMFPlone/trunk/; revision=970

--- MembershipTool.py
+++ MembershipTool.py
@@ -1,62 +1,19 @@
-from Products.CMFDefault.MembershipTool import MembershipTool as DefaultMembershipTool
-from Products.CMFDefault import Document
-from Products.CMFCore.utils import getToolByName
-import PloneFolder 
+from Products.CMFDefault.MembershipTool import MembershipTool as BaseTool
 
-default_member_content = '''Default page for %s
-
-  This is the default document created for you when 
-  you joined this community.
-
-  To change the content just select "Edit"
-  in the tabs along the top of this form.
-'''
-
-class MembershipTool(DefaultMembershipTool):
+class MembershipTool( BaseTool ):
     """ Plone customized Membership Tool """
     meta_type='Plone Membership Tool'
     plone_tool = 1
 
-    memberareaCreationFlag = 1
-
     def createMemberarea(self, member_id):
         """
-        create a member area with a workflow enabled homepage
+        provides some extra features
         """
-        parent=self.aq_inner.aq_parent
-        members=getattr(parent, 'Members', None)
-    
-        if members is not None and not hasattr(members, member_id):
-            f_title = "%s's Home" % member_id
-            #addPloneFolder(members, id=member_id )
-            if members.meta_type=='Plone Folder':
-                members.manage_addPloneFolder( member_id, f_title )
-            else:
-                members.manage_addPortalFolder( member_id, f_title)
-
-            f=getattr(members, member_id)
-    
-            # Grant ownership to Member
-            acl_users = self.__getPUS()
-            user = acl_users.getUser(member_id).__of__(acl_users)
-            f.changeOwnership(user)
-            f.manage_setLocalRoles(member_id, ['Owner'])
-    
-            # the below skips workflow                          
-            Document.addDocument( f
-                                , 'index_html'
-                                , member_id+"'s Home"
-                                , member_id+"'s front page"
-                                , "structured-text"
-                                , (default_member_content % member_id)
-                                )
-    
-            wf_tool=getToolByName(self, 'portal_workflow')
-            homepage=getattr(f, 'index_html')
-	    wf_tool.notifyCreated(homepage)
-    
-            f.index_html._setPortalTypeName( 'Document' )
-            # Overcome an apparent catalog bug.
-            f.index_html.reindexObject()
-            #Add .personal folder for Portraits and Personal content
-            f.manage_addPortalFolder(id='.personal', title='Personal Items')
+        BaseTool.createMemberarea(self, member_id)
+        member_folder=self.getHomeFolder(member_id)
+        member_folder.setDescription('Home page area that contains the items created and collected by %s' % member_id)
+        
+        member_folder.invokeFactory(id='.personal', type_name='Folder')
+        personal=getattr(member_folder, '.personal')
+        personal.setTitle('Personal Items')
+        personal.setDescription("contains personal workarea items for %s" % member_id)
