4ec5954c2c478e0efad870be85cfd0174269c6da
Alan Runyan <alan@enfoldsystems.com>
2002-08-23 17:56:47 +0000

    removed base class, maybe cause for FreeBSD to freakout
    svn path=/CMFPlone/trunk/; revision=1002

--- PloneUtilities.py
+++ PloneUtilities.py
@@ -1,15 +1,14 @@
-from ZTUtils import Iterator
-
-class IndexIterator(Iterator):
+class IndexIterator:
     __allow_access_to_unprotected_subobjects__ = 1 
 
     def __init__(self, upper=100):
-        self.seq=[x for x in xrange(0,upper)]
+        self.upper=upper
         self.pos=0
 
     def next(self):
-        if self.pos<len(self.seq):
-            self.pos+=1
-            return self.seq[self.pos]
+        if self.pos <= self.upper:
+            self.pos += 1
+            return self.pos
+        raise KeyError, 'Reached upper bounds'
 
 
