cf0bc9670340864275ca9b7387647d7d1d0fe7a4
Alan Runyan <alan@enfoldsystems.com>
2002-08-27 14:07:27 +0000

    fixed typo
    svn path=/CMFPlone/trunk/; revision=1044

--- skins/plone_scripts/form_scripts/validate_folder_edit.py
+++ skins/plone_scripts/form_scripts/validate_folder_edit.py
@@ -14,4 +14,4 @@ validator.addField('title', 'String', required=1)
 errors=validator.validate(context.REQUEST)
 if errors:
     return ('failure', errors, {'portal_status_message':'Please correct the indicated errors.'})
-return ('success', errors, {{'portal_status_message':'Your folder propeties have been saved.'})
+return ('success', errors, {'portal_status_message':'Your folder propeties have been saved.'})
