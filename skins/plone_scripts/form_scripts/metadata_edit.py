a3194d1c115d508790bf9f4498aadf34a30ac2cf
Alan Runyan <alan@enfoldsystems.com>
2002-08-28 15:26:42 +0000

    strip null keywords (subjects)
    svn path=/CMFPlone/trunk/; revision=1077

--- skins/plone_scripts/form_scripts/metadata_edit.py
+++ skins/plone_scripts/form_scripts/metadata_edit.py
@@ -6,7 +6,7 @@
 ##bind subpath=traverse_subpath
 ##title=Update Content Metadata
 ##parameters=allowDiscussion=None,title=None,subject=None,description=None,contributors=None,effective_date=None,expiration_date=None,format=None,language=None,rights=None
-
+subject=[s for s in subject if s]
 context.plone_utils.editMetadata(context,
                                  allowDiscussion=allowDiscussion,
                                  title=title,
