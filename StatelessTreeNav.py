09bdf1e34f4b41741f76081f934ac51520974a58
zworkb <zworkb@localhost>
2002-09-10 22:51:37 +0000

    added support for batched display added some more options added comments into navigation_tree_builder.py documenting the possible options
    svn path=/CMFPlone/trunk/; revision=1226

--- StatelessTreeNav.py
+++ StatelessTreeNav.py
@@ -1,16 +1,14 @@
 #Stateless Tree Navigation
-#(c) Philipp Auersperg phil@bluedynamics.com 05.09.2002
+#(c) Philipp Auersperg phil@bluedynamics.com 10.09.2002
 
 import string
 from Globals import HTML
 from AccessControl import ClassSecurityInfo,ModuleSecurityInfo,allow_class,allow_module
 
+# to be able to import this stuff into python scripts
 ModuleSecurityInfo('Products.CMFPlone').declarePublic('StatelessTreeNav')
 ModuleSecurityInfo('Products.CMFPlone.StatelessTreeNav').declarePublic('StatelessTreeBuilder')
 allow_module('Products.CMFPlone.StatelessTreeNav')
-
-import pprint
-pp=pprint.PrettyPrinter(indent=4)
 
 def wrap_obj(o,parent):
     return o.__of__(parent)
@@ -22,7 +20,8 @@ class StatelessTreeBuilder:
     
     def __init__(self, object, topObject=None, topMetaType='CMF Site',
             maxcount=65535,includeTop=0,topLevel=0,listSiblings=1,
-            childFinder=None,showFolderishSiblingsOnly=0,showFolderishChildrenOnly=0):
+            childFinder=None,showFolderishSiblingsOnly=0,showFolderishChildrenOnly=0,
+            showNonFolderishObject=0):
         self.object=object
         self.topObject=topObject
         self.topMetaType=topMetaType
@@ -33,6 +32,7 @@ class StatelessTreeBuilder:
         self.childFinder=childFinder
         self.showFolderishSiblingsOnly=showFolderishSiblingsOnly
         self.showFolderishChildrenOnly=showFolderishChildrenOnly
+        self.showNonFolderishObject=showNonFolderishObject
         
     security.declarePublic('getLevel')
     def getLevel (self,object=None):
@@ -109,6 +109,8 @@ class StatelessTreeBuilder:
         path=self.getParentObjects()
         
         count=0
+        p=None
+
         for p in path:
             
            r={'object':p,'level':None,'siblings':[],'title':p.title_or_id(),'url':p.absolute_url()}
@@ -119,65 +121,94 @@ class StatelessTreeBuilder:
               pass
         
            if self.listSiblings == 1:
-              r['siblings']=self.getChildObjects(p.aq_parent,self.showFolderishSiblingsOnly)
-              r['siblingtitles']=map(lambda x:x.title_or_id(),r['siblings'])
+               if self.includeTop and p==path[0]:
+                   r['siblings']=[p]
+               else:
+                   r['siblings']=self.getChildObjects(p.aq_parent,self.showFolderishSiblingsOnly)
+                   r['siblingtitles']=map(lambda x:x.title_or_id(),r['siblings'])
         
            res.append(r)
            itemcount=itemcount+1
            if len(r['siblings']):
                itemcount=itemcount+len(r['siblings'])-1
            count=count+1
-        else:
-           p=None
         
         if p:
-            siblings=self.getChildObjects(p,folderishOnly=self.showFolderishChildrenOnly)
-            #print 'finally p siblings:',map(lambda x:x.title_or_id(),siblings)
-            res.append({'object':None,'siblings':siblings,'level':r['level']+1,'siblingtitles':map(lambda x:x.title_or_id(),siblings)})    
-            itemcount=itemcount+len(siblings,self.showFolderishChildrenOnly)
+            o=self.object
+            level=r['level']+1
         else:
             o=self.object
-            if self.showFolderishChildrenOnly==0 and  not o.isPrincipiaFolderish:
-                o=o.aq_parent
-                
-            siblings=self.getChildObjects(o,folderishOnly=self.showFolderishChildrenOnly)
-            #print 'finally no p siblings:',map(lambda x:x.title_or_id(),siblings)
-            res.append({'object':None,'siblings':siblings,'level':self.getLevel(self.object)+1,'siblingtitles':map(lambda x:x.title_or_id(),siblings)})    
-            itemcount=itemcount+len(siblings)
+            level=self.getLevel(self.object)+1
+
+        addItself=None
+        if not o.isPrincipiaFolderish:
+            if self.showFolderishChildrenOnly and self.showNonFolderishObject:
+                addItself=o
+
+            o=o.aq_parent
             
-        #pp.pprint(res)    
+        siblings=self.getChildObjects(o,folderishOnly=self.showFolderishChildrenOnly)
+        if addItself:
+            siblings.append(addItself)
+            
+        res.append({'object':None,'siblings':siblings,'level':level,'siblingtitles':map(lambda x:x.title_or_id(),siblings)})    
+        itemcount=itemcount+len(siblings)
+        
         return res,itemcount
 
     
     security.declarePublic('buildFlatMenuStructure')
-    def buildFlatMenuStructure (self ):
+    def buildFlatMenuStructure (self, batchSize=65535, batchStart=0 ):
         """
         Constructs a Menu
         """
         
         list,itemtotal = self.buildMenuStructure()
-        res = []
+        
+        l=[]
+        res = {'list':l,'batchSize':batchSize,'batchStart':batchStart,'next':0,'prev':0}
+        
+        if batchStart:
+            res['prev']=1
+            res['prevBatchStart']=max(0,batchStart-batchSize)
         
         itemcounter=0
         menucounter=0
         # Opening
         for item in list :
-          menucounter=menucounter+1
-          menuparams={'sibling':item['object'],'menucounter':menucounter,'target':item['object'],'itemtotal':itemtotal}  
-          #res.append({'level':item['level'],'open':1,'object':item['object']})
           act = 0
+          current=0
         
           for sibling in item['siblings'] :
-            itemcounter=itemcounter+1
+
+            current = 0
+
             if (sibling == item['object']) :
-              act = 1
+                act = 1
+
+                if item==list[-2] and self.showFolderishChildrenOnly and not self.showNonFolderishObject:
+                    # deepest level
+                        current=1
+
             else :
-              act = 0
+                act = 0
+                current=0
+
+            if sibling == self.object and (self.showNonFolderishObject or not self.showFolderishChildrenOnly):
+                current=1
+            #print item['object'],map(lambda x:x['object'],list)
               
-            params={'sibling':sibling,'itemcounter':itemcounter,'menucounter':menucounter,'target':sibling,'itemtotal':itemtotal}
+            r={'level':self.getLevel(sibling),'open':act,'object':sibling,'title':sibling.title_or_id(),'iscurrent':current}  
+            itemcounter = itemcounter + 1
+
+            if itemcounter > batchStart:
+                l.append(r)
+                
+            if 0 and itemcounter >= batchStart+batchSize :
+                res['nextBatchStart']=itemcounter
+                res['next']=1
+                return res
             
-            res.append({'level':self.getLevel(sibling),'open':act,'object':sibling,'title':sibling.title_or_id()})
-        
             if (act == 1) :
               break
         
@@ -186,28 +217,30 @@ class StatelessTreeBuilder:
         
         for item in list :
           act = 0  
-          menucounter=menucounter-1
-          menuparams={'sibling':item['object'],'menucounter':menucounter,'target':item['object'],'itemtotal':itemtotal}  
         
           for sibling in item['siblings'] :
-            
+
             if (sibling == item['object']) :
               act = 1
               continue
           
-        
             if (act == 1) :
-              itemcounter=itemcounter+1
-              params={'sibling':sibling,'itemcounter':itemcounter,'menucounter':menucounter,'target':sibling,'itemtotal':itemtotal}
+              itemcounter = itemcounter + 1
         
+              r={'level':self.getLevel(sibling),'open':0,'object':sibling}
+
               if sibling:
-                  res.append({'level':self.getLevel(sibling),'open':0,'object':sibling,'title':sibling.title_or_id()})
-              else:
-                  res.append({'level':self.getLevel(sibling),'open':0,'object':sibling})
+                  r.update({'title':sibling.title_or_id()})
                   
-        #print '--------------------------------'
+              if itemcounter > batchStart:
+                  l.append(r)
+
+              if itemcounter >= batchStart+batchSize :
+                  res['nextBatchStart']=itemcounter
+                  res['next']=1
+                  return res
+                  
         #pp.pprint(res)    
         return res
-    
 
 allow_class(StatelessTreeBuilder)
--- skins/plone_templates/ui_slots/navigation_tree_builder.py
+++ skins/plone_templates/ui_slots/navigation_tree_builder.py
@@ -1,36 +1,58 @@
 ## Script (Python) "navigation_tree_builder"
-##parameters=tree_root
+##parameters=tree_root,navBatchStart=0
 ##title=Standard Tree
 ##
+#Stateless Tree Navigation
+#(c) Philipp Auersperg phil@bluedynamics.com 10.09.2002
+
 
 from Products.CMFPlone.StatelessTreeNav import StatelessTreeBuilder
 from Products.CMFPlone.StatelessTreeNav import wrap_obj
 from Products.CMFCore.utils import getToolByName
 
+#default function that finds the children out of a folderish object
 def childFinder(obj,folderishOnly=1):
     if obj.meta_type == 'Portal Topic':
+        # to traverse through Portal Topics
         cat = getToolByName( obj, 'portal_catalog' )
+        
+        #folderishOnly=0 #in order to view all topic results in the tree 
 
         res=obj.listFolderContents()
         subs=obj.queryCatalog()
+        
+        # get the objects out of the cat results
         for s in subs:
             o=context.restrictedTraverse(cat.getpath(s.data_record_id_))
             res.append(wrap_obj(o,obj))
         
     else:    
+        #traversal to all 'CMFish' folders
         if hasattr(obj.aq_explicit,'listFolderContents'):
             res=obj.listFolderContents()
+            #raise 'res:',res
         else:
+            #... and all other folders
             res=obj.objectValues()
 
+    # if wanted just keep folderish objects
     if folderishOnly:
         return filter(lambda x: hasattr(x.aq_explicit,'isPrincipiaFolderish') and x.aq_explicit.isPrincipiaFolderish,res)
     else:
         return res
-    
 
-tb=StatelessTreeBuilder(context,childFinder=childFinder,includeTop=0,
-showFolderishSiblingsOnly=1,showFolderishChildrenOnly=1)
+tb=StatelessTreeBuilder(context,childFinder=childFinder,
+        includeTop=1, #if set, the top object itself is included in the tree
+        showFolderishSiblingsOnly=1, #in the hierarchy above the leaf object
+                                     #just folders should be displayed
+        showFolderishChildrenOnly=1, #list only folders below the leaf object
+        showNonFolderishObject=0)    #if the leaf object is not a folder 
+                                     #and showFolderishChildrenOnly the leaf is
+                                     # displayed in any case, but not its siblings
 
-return tb.buildFlatMenuStructure()
+return tb.buildFlatMenuStructure(
+    batchSize=30, # how long should one batch be
+                  # per definition it stops not before the leaf object is reached
+    batchStart=int(navBatchStart) #from where to start? is called automatically by the .pt
+    )
 
