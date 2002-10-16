7974d658a39eadaa8e548dec33b39399b537fe81
zopezen <andy@enfoldsystems.com >
2002-10-16 16:17:48 +0000

    Add homePageText as a skin so that people can change this in the skin. Putting this text here overrides the default text in Membership tool and closes #336
    svn path=/CMFPlone/trunk/; revision=1462

---
+++ skins/plone_scripts/homePageText.py
@@ -0,0 +1,22 @@
+## Script (Python) "homePageText"
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
+##parameters=
+##title=Home Page Text
+
+return """\
+Home page for %s
+
+  This page was automatically created for you when
+  you joined the community or when your account
+  was created. You are encouraged to customize
+  this page to your tastes.
+
+  To change the content just select the "Edit"
+  tab located directly on top of this page.
+"""
+
+
