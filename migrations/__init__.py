d94132550c88aed3992d5aaac61799434b18e448
Lalo Martins <lalo@laranja.org>
2002-10-30 23:01:02 +0000

    adding again files that got lost in the merge
    svn path=/CMFPlone/trunk/; revision=1660

---
+++ migrations/__init__.py
@@ -0,0 +1,21 @@
+try:
+    from Products.CMFPlone import MigrationTool
+    import beta2_beta3, beta1_beta2, alpha_beta
+except:
+    print "Ack.  MigrationTool could not be found"   
+
+def registerMigrations():
+    # so the basic concepts is you put  a bunch of migrations is here
+
+    MigrationTool.registerUpgradePath( '1.0alpha4' ,
+                                       '1.0beta1',
+                                       alpha_beta.migrate )
+                                       
+    MigrationTool.registerUpgradePath( '1.0beta1',
+                                       '1.0beta2',
+                                       beta1_beta2.onetwo )
+                                       
+    MigrationTool.registerUpgradePath( '1.0beta2',
+                                       '1.0beta3',
+                                       beta2_beta3.twothree )
+
---
+++ migrations/alpha_beta.py
@@ -0,0 +1,80 @@
+# alpha Plones to migrate tooling to a new site.
+# Its rather unfortunate that its a destructive process.
+# To overcome this will require us being very careful about
+# releasing stuff to public and promoting 'the one way' of doing things
+
+from Products.CMFCore.DirectoryView import createDirectoryView 
+from Products.CMFPlone import cmfplone_globals
+from Products.CMFPlone.Portal import PloneGenerator
+from Products.CMFPlone.CustomizationPolicy import DefaultCustomizationPolicy
+
+def bestEffortDirectoryViews(ob, name, _prefix):
+    '''
+    Adds a directory view for every subdirectory of the
+    given directory.
+    '''
+    from types import StringType
+    from os import path, listdir, stat
+    from Globals import HTMLFile, Persistent, package_home, DTMLFile
+    from Products.CMFCore.utils import expandpath, minimalpath
+    from Products.CMFCore.DirectoryView import _dirreg
+    # Meant to be called by filesystem-based code.
+    # Note that registerDirectory() still needs to be called
+    # by product initialization code to satisfy
+    # persistence demands.
+    if not isinstance(_prefix, StringType):
+        _prefix = package_home(_prefix)
+    fp = path.join(_prefix, name)
+    filepath = minimalpath(fp)
+    info = _dirreg.getDirectoryInfo(filepath)
+    if info is None:
+        raise ValueError('Not a registered directory: %s' % filepath)
+    for entry in info.getSubdirs():
+        filepath2 = path.join(filepath, entry)
+        try:
+            createDirectoryView(ob, filepath2, entry)
+        except:
+            pass
+
+def kidgloves(skintool):
+    """ treat the skintool with care
+        We will register paths that need registering.
+        We then will prune skinpaths.
+    """
+    skins=skintool.selections
+    rm_ids=('plone_calendar', 'plone_ie')
+    add_ids=('plone_ecmascript', 'plone_3rdParty/CMFCalendar')
+    for skinname in skins.keys():
+        skinpaths=skins[skinname]
+        paths=[elem.strip() for elem in skinpaths.split(',') if elem.strip() not in rm_ids]
+        for id in add_ids:
+            paths.insert(paths.index('custom'), id)
+        skins[skinname]=','.join(paths)
+    
+def migrate(self, portal):
+    addPloneTool=portal.manage_addProduct['CMFPlone'].manage_addTool
+    addCoreTool=portal.manage_addProduct['CMFCore'].manage_addTool
+    addDefaultTool=portal.manage_addProduct['CMFDefault'].manage_addTool
+
+    rm_ids=('portal_form_validation', 'plone_utils', 'portal_navigation',
+            'portal_migration')
+    for id in rm_ids:
+        if id in portal.objectIds():
+            portal.manage_delObjects(id)
+
+    if 'portal_registration' not in portal.objectIds():
+        addDefaultTool('Default Registration Tool')
+    
+    base_config=PloneGenerator()
+    base_config.customizePortalOptions(portal)
+    base_config.setupPloneWorkflow(portal)
+    base_config.setupForms(portal)
+
+    sk_tool=portal.portal_skins
+    bestEffortDirectoryViews( sk_tool, 'skins', cmfplone_globals )
+    kidgloves(sk_tool)
+
+    custom=DefaultCustomizationPolicy()
+    custom.customize(portal)
+    return 'fin'
+
