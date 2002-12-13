413a663438221c3e32a02376c2a8a8d516d9fce0
Alan Runyan <alan@enfoldsystems.com>
2002-12-13 00:30:12 +0000

    remove debug
    svn path=/CMFPlone/branches/Plone-1_0/; revision=1815

--- skins/plone_scripts/form_scripts/getFilenameFromRequest.py
+++ skins/plone_scripts/form_scripts/getFilenameFromRequest.py
@@ -7,12 +7,7 @@
 ##parameters=key
 ##title=
 ##
-context.plone_debug('inside getFilename')
-
 file=context.REQUEST.get(key, '')
 if file:
-    context.plone_debug(file.filename)
     return file.filename
-
 return ''
-
