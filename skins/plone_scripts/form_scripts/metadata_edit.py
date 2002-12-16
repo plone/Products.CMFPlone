b7c0b23ae742b125f38c5a0ff6f81d4e98262d4c
Alan Runyan <alan@enfoldsystems.com>
2002-12-16 04:45:33 +0000

    limi's keyword slector
    svn path=/CMFPlone/branches/Plone-1_0/; revision=1840

--- skins/plone_scripts/form_scripts/metadata_edit.py
+++ skins/plone_scripts/form_scripts/metadata_edit.py
@@ -5,8 +5,11 @@
 ##bind script=script
 ##bind subpath=traverse_subpath
 ##title=Update Content Metadata
-##parameters=allowDiscussion=None,title=None,subject=None,description=None,contributors=None,effective_date=None,expiration_date=None,format=None,language=None,rights=None
+##parameters=allowDiscussion=None,title=None,subject=None,description=None,contributors=None,effective_date=None,expiration_date=None,format=None,language=None,rights=None,predefined_subjects=None
 subject=[s for s in subject if s]
+if predefined_subjects:
+    subject=context.unique(list(subject)+list(predefined_subjects))
+
 if not effective_date:
    effective_date='None'
 if not expiration_date:
