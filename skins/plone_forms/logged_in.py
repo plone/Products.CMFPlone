6b16a93b93c6d05556e7efeed877b0ba629ce93a
Kapil Thangavelu <k_vertigo@users.sourceforge.net>
2003-01-03 17:52:28 +0000

    fix conditional check, compare login time to DateTime obj not string
    svn path=/CMFPlone/trunk/; revision=1875

--- skins/plone_forms/logged_in.py
+++ skins/plone_forms/logged_in.py
@@ -24,8 +24,8 @@ if isAnonymous:
     REQUEST.RESPONSE.expireCookie('__ac', path='/')
     pagetemplate=context.restrictedTraverse(login_failed)
 
-if pagetemplate is None and member.getProperty('login_time', None) == '2000/01/01' and \
-  properties_tool.validate_email:
+if pagetemplate is None and member.getProperty('login_time', None) == DateTime('2000/01/01')
+  and properties_tool.validate_email:
     pagetemplate=context.restrictedTraverse(login_changepassword)
 
 if pagetemplate is None and not(member.getProperty('login_time', None) == DateTime('2000/01/01') and properties_tool.validate_email):
