7d5d8d44f77cca7206eac4a1e3c84b2c247bddf6
zopezen <andy@enfoldsystems.com >
2003-01-27 05:04:39 +0000

    Whilst I remember seeing the check in, add this to the migration...
    svn path=/CMFPlone/branches/Plone-1_0/; revision=1973

---
+++ migrations/rc2_final.py
@@ -0,0 +1,24 @@
+from Products.CMFPlone import MigrationTool
+
+def rc2Final(portal):
+    """ Upgrade from Plone 1.0 RC2 to Final"""
+
+    p = portal.portal_properties
+    e = getattr(p, 'available_editors', [])
+
+    # if there is the old editor in there,
+    # change it to the new one
+    if 'XSDHTMLEditor' in e:
+        e[e.index('XSDHTMLEditor')] = 'Visual Editor'
+        p._setProperty('available_editors', e)
+
+def registerMigrations():
+    MigrationTool.registerUpgradePath(
+            '1.0RC2', 
+            '1.0', 
+            rc1Final
+            )
+
+if __name__=='__main__':
+    registerMigrations()
+
