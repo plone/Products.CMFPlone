c976c22378dcdf37c55bb40004e80afbf343dd65
Sidnei da Silva <sidnei.da.silva@gmail.com>
2003-02-12 21:52:16 +0000

    reverting changes
    svn path=/CMFPlone/branches/Plone-1_0/; revision=2025

--- Extensions/package_plone.py
+++ Extensions/package_plone.py
@@ -4,23 +4,20 @@ release='1.0.1'
 releasename='CMFPlone-%s' % release
 releasetar='CMFPlone%s.tar' % release
 
-sh_cleanmisc = """find . -name '*~' | xargs rm -rf"""
-sh_cleanpyc = """find . -name '*\.pyc' | xargs rm -rf"""
-sh_cleancvs = """ find . -name 'CVS' | xargs rm -rf"""
-sh_cleanhidden = """ find . -name '\.#*' | xargs rm -rf"""
+sh_cleanmisc = """find . | grep '~' | xargs rm -rf"""
+sh_cleanpyc = """find . | grep 'pyc' | xargs rm -rf"""
+sh_cleancvs = """ find . | grep 'CVS' | xargs rm -rf"""
 
 os.system("cd ..; rm -rf " + releasetar)
-os.system("cd ..; rm -rf " + releasename)
 
 os.system("cd .. ; mkdir " + releasename)
-os.system("cd .. ; cp -rfL CMFPlone " + releasename)
-os.system("cd .. ; cp -rfL Formulator " + releasename)
-os.system("cd .. ; cp -rfL DCWorkflow " + releasename)
+os.system("cd .. ; cp -rf CMFPlone " + releasename)
+os.system("cd .. ; cp -rf Formulator " + releasename)
+os.system("cd .. ; cp -rf DCWorkflow " + releasename)
 
 os.system("cd ../" + releasename+";" + sh_cleanmisc)
 os.system("cd ../" + releasename+";" + sh_cleanpyc)
 os.system("cd ../" + releasename+";" + sh_cleancvs)
-os.system("cd ../" + releasename+";" + sh_cleanhidden)
 
 os.system("cd .. ; tar -cvf %s %s" % ( releasetar 
                                      , releasename ) )
