02234e2d3abdad94fece4de04079ef3250e6e233
zopezen <andy@enfoldsystems.com >
2003-03-02 04:32:17 +0000

    Remove trailing <p> and >> that would occur on this and is annoying
    svn path=/CMFPlone/trunk/; revision=2069

--- skins/plone_scripts/aboveInThread.py
+++ skins/plone_scripts/aboveInThread.py
@@ -1,44 +1,19 @@
 ## Script (Python) "aboveInThread"
-
 ##bind container=container
-
 ##bind context=context
-
 ##bind namespace=
-
 ##bind script=script
-
 ##bind subpath=traverse_subpath
-
 ##parameters=
-
 ##title=Discussion parent breadcrumbs
-
-
-
 breadcrumbs = ''
-
+crumbs = []
 parents = context.parentsInThread()
 
-
-
 if parents:
-
-    breadcrumbs = ''
-
     for parent in parents:
-
-        p_str = '<a href="%s">%s</a> <strong>&raquo;</strong> ' % (parent.absolute_url(), parent.Title())
-
-        breadcrumbs = breadcrumbs + p_str
-
-
-
-    breadcrumbs = breadcrumbs[:-1] + '<p>'
-
+        crumbs.append('<a href="%s">%s</a> ' % (parent.absolute_url(), parent.Title()))
         
+    breadcrumbs = '<strong>&raquo;</strong> '.join(crumbs)
 
 return breadcrumbs
-
-
-
