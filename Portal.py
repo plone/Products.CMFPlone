013e37781b50b17f0b1f747ee39b6dc9edcfffde
Sidnei da Silva <sidnei.da.silva@gmail.com>
2003-04-01 20:26:23 +0000

    Create a object on the root of Plone Site to hold results of customization, if any.
    svn path=/CMFPlone/branches/Plone-1_0/; revision=2144

--- Portal.py
+++ Portal.py
@@ -296,8 +296,12 @@ def manage_addSite(self, id, title='Portal', description='',
     customization_policy=None
     if listPolicies() and custom_policy:
         customization_policy=custom_policies[custom_policy]
-        customization_policy.customize(p)
-    
+        # Save customization policy results on a object
+        result = customization_policy.customize(p)
+        if result:
+            p.invokeFactory(type_name='Document', id='CustomizationLog')
+            p.CustomizationLog.edit(text_format='plain', text=result)
+
     # reindex catalog and workflow settings
     p.portal_catalog.refreshCatalog()     
     p.portal_workflow.updateRoleMappings() 
