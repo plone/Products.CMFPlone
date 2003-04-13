6da1102279eba3503348e2e01fbcf60f246ab506
Alan Runyan <alan@enfoldsystems.com>
2003-04-13 18:10:59 +0000

    removed accidental call to member which would turn member into a string
    svn path=/CMFPlone/trunk/; revision=2192

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 cmfplone_globals=globals()
 custom_policies={} #stores the registered Policies
-import MigrationTool, migrations
+import migrations, MigrationTool
 from Products.CMFCore import CachingPolicyManager, DirectoryView, utils
 from AccessControl import ModuleSecurityInfo, ClassSecurityInfo, allow_module, allow_class, allow_type
 import MembershipTool, FormulatorTool, PloneTool, WorkflowTool
