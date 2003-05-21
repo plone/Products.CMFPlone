09536a6f9b9e26bfb80452b6a85be574dd5860c9
zopezen <andy@enfoldsystems.com >
2003-05-21 04:44:51 +0000

    And finally turn on migrations and the actual CacheManagers
    svn path=/CMFPlone/trunk/; revision=2276

--- migrations/upg_1_0_1_to_1_1.py
+++ migrations/upg_1_0_1_to_1_1.py
@@ -1,4 +1,5 @@
 from migration_util import safeEditProperty
+from Products.StandardCacheManagers import AcceleratedHTTPCacheManager, RAMCacheManager
 
 def upg_1_0_1_to_1_1(portal):
     """ Migrations from 1.0.1 to 1.1 """
@@ -13,5 +14,12 @@ def upg_1_0_1_to_1_1(portal):
     # change the action in portal_types for
     # viewing a folder
 
-    
+    # add in caches, HTTPCache and RamCache
+    meta_type = AcceleratedHTTPCacheManager.AcceleratedHTTPCacheManager.meta_type
+    if 'HTTPCache' not in portal.objectIds(meta_type):
+        AcceleratedHTTPCacheManager.manage_addAcceleratedHTTPCacheManager(portal, 'HTTPCache')
+
+    meta_type = RAMCacheManager.RAMCacheManager.meta_type
+    if 'RAMCache' not in portal.objectIds(meta_type):
+        RAMCacheManager.manage_addRAMCacheManager(portal, 'RAMCache')
     
