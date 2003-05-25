555da1fe78e3c137b42d86deb7f75a2cd05c3f10
Geoff Davis <geoff@geoffdavis.net>
2003-05-25 16:25:49 +0000

    Fixed  1208 and 1171
    svn path=/CMFPlone/branches/Plone-1_0/; revision=2347

--- migrations/one01_one02.py
+++ migrations/one01_one02.py
@@ -1,5 +1,6 @@
 from Products.CMFCore.utils import getToolByName
 from Products.CMFCore.Expression import Expression
+from Products.CMFCore import CMFCorePermissions
 
 def onezerotwo(portal):
     """ Upgrade from Plone 1.0.1 to Plone 1.0.2"""
@@ -7,6 +8,10 @@ def onezerotwo(portal):
     cookie_authentication = getToolByName(portal, 'cookie_authentication')
     cookie_authentication._updateProperty('auto_login_page', 'require_login')
     portal.portal_syndication.isAllowed=1
+
+    # give Owner the right to manage local roles
+    portal.manage_permission(CMFCorePermissions.ChangePermissions, ('Owner','Manager',), acquire=1)
+
 
 if __name__=='__main__':
     registerMigrations()
