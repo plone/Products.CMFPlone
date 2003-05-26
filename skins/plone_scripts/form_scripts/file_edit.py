66bdda8f45443e953f1354190609493d53d93652
Alan Runyan <alan@enfoldsystems.com>
2003-05-26 22:11:40 +0000

    moved contentEdit before edit (so when you call edit the file knows whats its new id)
    svn path=/CMFPlone/trunk/; revision=2355

--- skins/plone_scripts/form_scripts/file_edit.py
+++ skins/plone_scripts/form_scripts/file_edit.py
@@ -25,11 +25,14 @@ if not id:
     id = context.getId()
 
 new_context = context.portal_factory.doCreate(context, id)
-new_context.edit( precondition=precondition
-                , file=file )
+
 new_context.plone_utils.contentEdit( new_context
                                    , id=id
                                    , description=description )
 
-return ('success', new_context, {'portal_status_message':context.REQUEST.get('portal_status_message', 'File changes saved.')})
+new_context.edit( precondition=precondition, file=file )
+
+return ('success', new_context, 
+        {'portal_status_message' : context.REQUEST.get('portal_status_message', 
+                                                       'File changes saved.')})
 
