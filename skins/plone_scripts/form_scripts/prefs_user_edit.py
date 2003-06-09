7b1d17240c7213fa323c8cc3d29f2af3851c865b
Alan Runyan <alan@enfoldsystems.com>
2003-06-09 06:56:36 +0000

    individiual user edit form
    svn path=/CMFPlone/trunk/; revision=2429

---
+++ skins/plone_scripts/form_scripts/prefs_user_edit.py
@@ -0,0 +1,20 @@
+## Script (Python) "prefs_user_edit"
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
+##parameters=id
+##title=Edit user
+##
+REQUEST=context.REQUEST
+membership=context.portal_membership
+setMemberProperties=context.plone_utils
+member=membership.getMemberById(id)
+
+
+REFERER=REQUEST.HTTP_REFERER
+statusmsg=REFERER.find('portal_status_message')
+url='%s&%s' % (REFERER[:REFERER.find('portal_status_message')],
+               'portal_status_message=Changes+made.')
+return REQUEST.RESPONSE.redirect(url)
