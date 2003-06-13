4291961485a8c6407479734bb03ceb47f656b950
Geoff Davis <geoff@geoffdavis.net>
2003-06-13 17:53:14 +0000

    Fixed typo.  Cut-and-paste coding is evil!
    svn path=/CMFPlone/branches/Plone-1_0/; revision=2486

--- migrations/one02_one03.py
+++ migrations/one02_one03.py
@@ -4,7 +4,7 @@ from Products.CMFCore import CMFCorePermissions
 
 def onezerothree(portal):
     """ Upgrade from Plone 1.0.2 to Plone 1.0.3"""
-    props=getToolByName(self, 'portal_properties')
+    props=getToolByName(portal, 'portal_properties')
     sprops=getattr(props,'site_properties')
     if sprops is not None:
         if not sprops.hasProperty('invalid_ids'):
