baec2d5977535faaf8a4abb82cf60922748b71e6
Helge Tesdal <tesdal@jarn.com>
2003-06-17 14:02:12 +0000

    Changed wording.
    svn path=/CMFPlone/trunk/; revision=2506

--- dependencies.py
+++ dependencies.py
@@ -10,15 +10,15 @@ def log(message,summary='',severity=0):
 try:
     import Products.CMFQuickInstallerTool
 except ImportError:
-    log("CMFQuickInstallerTool not found.  Please download it from sf.net/projects/collective.")
+    log("CMFQuickInstallerTool not found.  Please download it from http://sf.net/projects/collective")
 
 try:
     import Products.CMFActionIcons
 except ImportError:
-    log("CMFActionIcons not found.  Please download it from cvs.zope.org/CMF.")
+    log("CMFActionIcons not found.  Please download it from http://cvs.zope.org/CMF")
 
 try:
     import Products.ExternalEditor
 except ImportError:
-    log("ExternalEditor not found.  Please down it from http://www.zope.org/Members/Caseman/ExternalEditor")
+    log("ExternalEditor not found.  Please download it from http://www.zope.org/Members/Caseman/ExternalEditor")
 
