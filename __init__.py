b35ecf2040c88b9c3f8fde573d41e2334dc4f8f8
zopezen <andy@enfoldsystems.com >
2003-06-23 04:12:26 +0000

    Dont forget to import the setup widgets
    svn path=/CMFPlone/trunk/; revision=2532

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ import dependencies
 
 cmfplone_globals=globals()
 custom_policies={} #stores the registered Policies
-import migrations, MigrationTool
+import migrations, setup, MigrationTool
 from Products.CMFCore import CachingPolicyManager, DirectoryView, utils
 from AccessControl import ModuleSecurityInfo, ClassSecurityInfo, allow_module, allow_class, allow_type
 import MembershipTool, FormulatorTool, PloneTool, WorkflowTool
