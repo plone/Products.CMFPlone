c46f438d84bc153c76c9fa663e492d59bc77538e
Geoff Davis <geoff@geoffdavis.net>
2003-07-18 16:54:26 +0000

    Modified cookie testing code in logins to fix problems with IE 5.0.
    svn path=/CMFPlone/branches/Plone-1_0/; revision=2593

--- migrations/one03_one04.py
+++ migrations/one03_one04.py
@@ -6,9 +6,6 @@ def onezerofour(portal):
     """ Upgrade from Plone 1.0.3 to Plone 1.0.4"""
     props=getToolByName(portal, 'portal_properties')
     sprops=getattr(props,'site_properties')
-    if sprops is not None:
-        if not sprops.hasProperty('test_cookie_name'):
-            sprops.manage_addProperty('test_cookie_name', '', 'string')
             
 if __name__=='__main__':
     registerMigrations()
