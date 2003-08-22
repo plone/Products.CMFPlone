b757a8203531bb1ae8b41c4730cff5bb301f2ccf
zworkb <zworkb@localhost>
2003-08-22 17:46:24 +0000

    finally got rid of trashcan, there were some debris in folder_delete.py
    svn path=/CMFPlone/trunk/; revision=2722

--- skins/plone_scripts/folder_delete.py
+++ skins/plone_scripts/folder_delete.py
@@ -5,55 +5,23 @@
 ##bind script=script
 ##bind subpath=traverse_subpath
 ##parameters=
-##title=
+##title=Delete objects from a folder
 ##
 from Products.CMFPlone import transaction_note
 ids=context.REQUEST.get('ids', None)
 
-# Get homefolder of user
 
-homeFolder = context.portal_membership.getHomeFolder()
-
-# Create Trashcan if it not exists
-
-if homeFolder and '.trashcan' not in homeFolder.objectIds():
-    homeFolder.invokeFactory(id='.trashcan', title='Trashcan', type_name='Folder')
-    homeFolder.folder_publish(workflow_action='hide', ids=['.trashcan'])
-
-# Get Trashcan
 
 status='failure'
 message='Please select one or more items to delete.'
 
 if ids:
-    if homeFolder:
-        Trashcan = getattr(homeFolder.aq_explicit, '.trashcan')
-    
-    # If current Folder is Trashcan, then delete objects.
 
-    if homeFolder is None or context == Trashcan:
-        status='success'
-        message=', '.join(ids)+' has been deleted.'
-        transaction_note(message)        
-        context.manage_delObjects(ids)
+    status='success'
+    message=', '.join(ids)+' has been deleted.'
+    transaction_note(message)        
+    context.manage_delObjects(ids)
 
-    # If not, move objects to Trashcan
-
-    else:
-        status='success'
-        message=', '.join(ids)+' has been moved to Trashcan.'
-        transaction_note(message)
-        
-        # First we have to retract all objects...
-        
-        context.folder_publish(workflow_action='retract', ids=ids, include_subfolders=1)
-        
-        # ... then hide them => All objects will be private after that operations.
-        
-        context.folder_publish(workflow_action='hide', ids=ids, include_subfolders=1)
-        
-        cp=context.manage_cutObjects(ids)
-        Trashcan.manage_pasteObjects(cb_copy_data=cp)
 
 return context.portal_navigation.getNext(
                       context,
