4093248479ce33979fa7aefe6b514758992c53fa
zopezen <andy@enfoldsystems.com >
2003-09-05 15:57:24 +0000

    Setup 1.0.5
    svn path=/CMFPlone/branches/Plone-1_0/; revision=2754

---
+++ migrations/one04_one05.py
@@ -0,0 +1,10 @@
+from Products.CMFCore.utils import getToolByName
+from Products.CMFCore.Expression import Expression
+from Products.CMFCore import CMFCorePermissions
+
+def onezerofive(portal):
+    """ Upgrade from Plone 1.0.4 to Plone 1.0.4"""
+    pass
+            
+if __name__=='__main__':
+    registerMigrations()
