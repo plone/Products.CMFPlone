59e695bcc64642b5dc468226dd12e59bd82421dc
ronnix <ronnix@localhost>
2003-09-07 15:46:04 +0000

    Hopefully fix base tag issues forever.
    svn path=/CMFPlone/trunk/; revision=2761

--- skins/plone_scripts/renderBase.py
+++ skins/plone_scripts/renderBase.py
@@ -2,8 +2,4 @@
 if getattr(context.aq_explicit, 'isPrincipiaFolderish', 0):
     return context.absolute_url()+'/'
 else:
-    path = '/'.join(context.portal_url.getRelativeContentPath(context)[:-1])
-    appendix = ''
-    if len(path) > 0:
-        appendix = '/'
-    return context.portal_url() + '/' + path + appendix
+    return context.absolute_url()
