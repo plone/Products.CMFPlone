77ad7dff1320137317d0a2924af2e60eec85feef
Stefan H. Holek <stefan@epy.co.at>
2003-09-13 14:48:21 +0000

    * Most of the stuff that has been done on import of the CMPlone module   is now done in the initialize() method. This should not make a difference   for most users, but speeds up import of CMFPlone considerably for test   authors.
    svn path=/CMFPlone/trunk/; revision=2817

--- __init__.py
+++ __init__.py
@@ -1,123 +1,151 @@
-#custom_policies stories the available 'Customization Policies'
-cmfplone_globals=globals()
-custom_policies={} 
-
-import migrations, setup, MigrationTool
-from Products.CMFCore import CachingPolicyManager, DirectoryView, utils
-from AccessControl import ModuleSecurityInfo, ClassSecurityInfo, allow_module, allow_class, allow_type
-
-#CMFCore and CMFDefault Tools
-import MembershipTool, WorkflowTool, URLTool, MetadataTool, RegistrationTool
-import PropertiesTool, ActionsTool, TypesTool, UndoTool
-
-#Plone Tools
-import FormulatorTool, PloneTool, NavigationTool, FactoryTool, FormTool, InterfaceTool
-
-import PloneFolder, Portal, PloneWorkflow, FolderWorkflow
-import PloneControlPanel
-
-try:
-    import LargePloneFolder
-except ImportError:
-    LargePloneFolder=None
-
 import sys
-import StatelessTreeNav
 import Globals
 from os import path
 
+cmfplone_globals=globals()
+this_module = sys.modules[ __name__ ]
+
+# Stores the available 'Customization Policies'
+custom_policies={} 
+
 ADD_CONTENT_PERMISSION = 'Add portal content'
 
-#for plone_debug method
+misc_ = {'plone_icon': Globals.ImageFile(path.join('skins','plone_images','logoIcon.gif'), cmfplone_globals)}
+
+# For plone_debug method
 import zLOG
 def log(message,summary='',severity=0):
     zLOG.LOG('MyDebugLog',severity,summary,message)
 
-ModuleSecurityInfo('zLOG').declarePublic('LOG')
-ModuleSecurityInfo('zLOG').declarePublic('INFO')
-ModuleSecurityInfo('Products.CMFPlone.Portal').declarePublic('listPolicies')
-
-# for content_status_modify
-from Products.CMFCore.WorkflowCore import ObjectMoved, ObjectDeleted, \
-  WorkflowException
-
-from StatelessTree import NavigationTreeViewBuilder
-allow_class(NavigationTreeViewBuilder)
-
-ModuleSecurityInfo('WorkflowCore').declarePublic('ObjectMoved')
-ModuleSecurityInfo('WorkflowCore').declarePublic('ObjectDeleted')
-ModuleSecurityInfo('WorkflowCore').declarePublic('WorkflowException')
-allow_class(ObjectMoved)
-allow_class(ObjectDeleted)
-allow_class(WorkflowException)
-
-#for form validation bits
-from PloneUtilities import IndexIterator
-allow_class(IndexIterator)
-
-from PloneBatch import Batch
-allow_class(Batch)
-
-from StringIO import StringIO
-allow_class(StringIO)
-
-ModuleSecurityInfo('Products.Formulator').declarePublic('StringField','EmailField')
-ModuleSecurityInfo('Products.Formulator.Form').declarePublic('FormValidationError', 'BasicForm')
-
-from Products.Formulator.StandardFields import StringField, EmailField
-from Products.Formulator.Form import FormValidationError, BasicForm
-allow_class(StringField)
-allow_class(EmailField)
-allow_class(FormValidationError)
-allow_class(BasicForm)
-
 def transaction_note(note):
-    """ write human legible note """
+    """ Write human legible note """
     T=get_transaction()
     T.note(str(note))
 
-ModuleSecurityInfo('Products.CMFPlone').declarePublic('transaction_note')
-
-tools = ( MembershipTool.MembershipTool,
-          FormulatorTool.FormulatorTool,
-          PloneTool.PloneTool,
-          WorkflowTool.WorkflowTool,
-          CachingPolicyManager.CachingPolicyManager,
-          NavigationTool.NavigationTool,
-          FactoryTool.FactoryTool,
-          FormTool.FormTool,
-          PropertiesTool.PropertiesTool,
-          MigrationTool.MigrationTool,
-          InterfaceTool.InterfaceTool,
-          PloneControlPanel.PloneControlPanel,
-          RegistrationTool.RegistrationTool,
-          URLTool.URLTool,
-          MetadataTool.MetadataTool,
-          ActionsTool.ActionsTool,
-          TypesTool.TypesTool,
-          UndoTool.UndoTool,
-        )
-
-contentClasses = ( PloneFolder.PloneFolder , )
-contentConstructors = ( PloneFolder.addPloneFolder, )
-ftis = (PloneFolder.factory_type_information, )
-
-if LargePloneFolder is not None:
-    contentClasses += ( LargePloneFolder.LargePloneFolder, )
-    contentConstructors += ( LargePloneFolder.addLargePloneFolder,)
-    ftis += (LargePloneFolder.factory_type_information, )
-
-DirectoryView.registerDirectory('skins', cmfplone_globals)
-this_module = sys.modules[ __name__ ]
-z_bases = utils.initializeBasesPhase1(contentClasses, this_module)
-
-misc_ = {'plone_icon': Globals.ImageFile(path.join('skins','plone_images','logoIcon.gif'), cmfplone_globals)}
 
 def initialize(context):
+
+    # Stuff has been moved from module level to this method for a
+    # better separation of import and installation.
+    # For the general user this change does not make a difference.
+    # For test authors (and people who use parts of Plone only)
+    # it does speed up import *significantly*.
+
+    from AccessControl import ModuleSecurityInfo, ClassSecurityInfo
+    from AccessControl import allow_module, allow_class, allow_type
+
+    ModuleSecurityInfo('zLOG').declarePublic('LOG')
+    ModuleSecurityInfo('zLOG').declarePublic('INFO')
+
+    import StatelessTreeNav
+    from StatelessTree import NavigationTreeViewBuilder
+    allow_class(NavigationTreeViewBuilder)
+
+    # For form validation bits
+    from PloneUtilities import IndexIterator
+    allow_class(IndexIterator)
+
+    # Make IndexIterator available at module level
+    this_module.IndexIterator = IndexIterator
+
+    # For content_status_modify
+    from Products.CMFCore.WorkflowCore import ObjectMoved, ObjectDeleted, WorkflowException
+    ModuleSecurityInfo('WorkflowCore').declarePublic('ObjectMoved')
+    ModuleSecurityInfo('WorkflowCore').declarePublic('ObjectDeleted')
+    ModuleSecurityInfo('WorkflowCore').declarePublic('WorkflowException')
+    allow_class(ObjectMoved)
+    allow_class(ObjectDeleted)
+    allow_class(WorkflowException)
+
+    from PloneBatch import Batch
+    allow_class(Batch)
+
+    # Make Batch available at module level
+    this_module.Batch = Batch
+
+    from StringIO import StringIO
+    allow_class(StringIO)
+
+    ModuleSecurityInfo('Products.CMFPlone').declarePublic('transaction_note')
+    ModuleSecurityInfo('Products.CMFPlone.Portal').declarePublic('listPolicies')
+
+    ModuleSecurityInfo('Products.Formulator').declarePublic('StringField','EmailField')
+    ModuleSecurityInfo('Products.Formulator.Form').declarePublic('FormValidationError', 'BasicForm')
+
+    from Products.Formulator.StandardFields import StringField, EmailField
+    from Products.Formulator.Form import FormValidationError, BasicForm
+    allow_class(StringField)
+    allow_class(EmailField)
+    allow_class(FormValidationError)
+    allow_class(BasicForm)
+
+    # Setup ZODB if needed
+    import PloneInitialize
+
+    # Setup migrations
+    import migrations
+    migrations.registerMigrations()
+
+    import setup
+    import imagePatch
+
+    from Products.CMFCore import DirectoryView
+    DirectoryView.registerDirectory('skins', cmfplone_globals)
+
+    import PloneFolder, PloneWorkflow, FolderWorkflow
+
+    contentClasses = ( PloneFolder.PloneFolder , )
+    contentConstructors = ( PloneFolder.addPloneFolder, )
+    ftis = (PloneFolder.factory_type_information, )
+
+    try:
+        import LargePloneFolder
+    except ImportError:
+        pass
+    else:
+        contentClasses += ( LargePloneFolder.LargePloneFolder, )
+        contentConstructors += ( LargePloneFolder.addLargePloneFolder,)
+        ftis += (LargePloneFolder.factory_type_information, )
+
+    # CMFCore and CMFDefault Tools
+    from Products.CMFCore import CachingPolicyManager
+    import MembershipTool, WorkflowTool, URLTool, MetadataTool, RegistrationTool
+    import PropertiesTool, ActionsTool, TypesTool, UndoTool
+
+    # Plone Tools
+    import FormulatorTool, PloneTool, NavigationTool, FactoryTool, FormTool, \
+           InterfaceTool, MigrationTool, PloneControlPanel
+
+    tools = ( MembershipTool.MembershipTool,
+              FormulatorTool.FormulatorTool,
+              PloneTool.PloneTool,
+              WorkflowTool.WorkflowTool,
+              CachingPolicyManager.CachingPolicyManager,
+              NavigationTool.NavigationTool,
+              FactoryTool.FactoryTool,
+              FormTool.FormTool,
+              PropertiesTool.PropertiesTool,
+              MigrationTool.MigrationTool,
+              InterfaceTool.InterfaceTool,
+              PloneControlPanel.PloneControlPanel,
+              RegistrationTool.RegistrationTool,
+              URLTool.URLTool,
+              MetadataTool.MetadataTool,
+              ActionsTool.ActionsTool,
+              TypesTool.TypesTool,
+              UndoTool.UndoTool,
+            )
+
+    from Products.CMFCore import utils
+    import Portal
+
+    z_bases = utils.initializeBasesPhase1(contentClasses, this_module)
     utils.initializeBasesPhase2( z_bases, context )
+
     utils.ToolInit('Plone Tool', tools=tools,
                    product_name='CMFPlone', icon='tool.gif',
                    ).initialize( context )
+
     utils.ContentInit( 'Plone Content'
                      , content_types=contentClasses
                      , permission=ADD_CONTENT_PERMISSION
@@ -133,10 +161,3 @@ def initialize(context):
     CustomizationPolicy.register(context, cmfplone_globals)
     PrivateSitePolicy.register(context, cmfplone_globals)
 
-# setup ZODB if needed
-import PloneInitialize
-
-# setup migrations
-migrations.registerMigrations()
-
-import imagePatch
