93ced0016122055bcbd8274c74ff95efd079e0a6
Stefan H. Holek <stefan@epy.co.at>
2003-09-15 12:02:01 +0000

    * Transcribed Sidnei's tests to the new style
    svn path=/CMFPlone/trunk/; revision=2897

---
+++ tests/testInterfaceTool.py
@@ -0,0 +1,116 @@
+#
+# InterfaceTool tests
+#
+
+import os, sys
+if __name__ == '__main__':
+    execfile(os.path.join(sys.path[0], 'framework.py'))
+
+from Testing import ZopeTestCase
+from Products.CMFPlone.tests import PloneTestCase
+
+import unittest
+
+# Create a Plone site in the test (demo-) storage
+app = ZopeTestCase.app()
+PloneTestCase.setupPloneSite(app, id='portal')
+ZopeTestCase.close(app)
+
+
+from Products.CMFCore.interfaces.DublinCore import DublinCore
+from Products.CMFCore.interfaces.Contentish import Contentish
+from Products.CMFCore.PortalContent import PortalContent
+from Products.CMFDefault.DublinCore import DefaultDublinCoreImpl
+from Products.CMFDefault.Document import Document
+
+from Products.CMFPlone.InterfaceTool import resolveInterface, getDottedName
+from Products.CMFPlone.InterfaceTool import InterfaceTool, InterfaceFinder
+
+
+class MyPortalContent(Contentish): pass
+
+class A(PortalContent, DefaultDublinCoreImpl):
+    __implements__ = PortalContent.__implements__, \
+                     DefaultDublinCoreImpl.__implements__
+
+class B(PortalContent, DefaultDublinCoreImpl):
+    __implements__ = MyPortalContent, \
+                     DefaultDublinCoreImpl.__implements__
+
+
+class TestInterfaceResolution(unittest.TestCase):
+
+    def testResolveDublinCore(self):
+        '''DublinCore should be resolved'''
+        dotted_name = getDottedName(DublinCore)
+        self.assertEqual(resolveInterface(dotted_name), DublinCore)
+
+    def testResolveContentish(self):
+        '''Contentish should be resolved'''
+        dotted_name = getDottedName(Contentish)
+        self.assertEqual(resolveInterface(dotted_name), Contentish)
+
+    def testResolveNonInterface(self):
+        '''Should raise ValueError when called with non-Interface'''
+        dotted_name = getDottedName(PortalContent)
+        self.assertRaises(ValueError, resolveInterface, dotted_name)
+
+
+class TestInterfaceFinder(unittest.TestCase):
+    
+    def testAvailableInterfaces(self):
+        '''Should find available interfaces'''
+        from Products.CMFPlone.interfaces import InterfaceTool
+        ifs = InterfaceFinder().findInterfaces(module=InterfaceTool)
+        self.assertEqual(ifs, [getDottedName(InterfaceTool.IInterfaceTool)])
+
+
+class TestInterfaceTool(PloneTestCase.PloneTestCase):
+
+    def afterSetUp(self):
+        self.pi = self.portal.portal_interface
+
+    def testContentImplements(self):
+        content = PortalContent()
+        self.failUnless(self.pi.objectImplements(content, getDottedName(Contentish)))
+
+    def testDocumentImplements(self):
+        document = Document(id='foo')
+        self.failUnless(self.pi.objectImplements(document, getDottedName(Contentish)))
+        self.failUnless(self.pi.objectImplements(document, getDottedName(DublinCore)))
+                       
+    def testDCImplements(self):
+        dc = DefaultDublinCoreImpl()
+        self.failUnless(self.pi.objectImplements(dc, getDottedName(DublinCore)))
+
+    def testAImplements(self):
+        a = A()
+        self.failUnless(self.pi.objectImplements(a, getDottedName(Contentish)))
+        self.failUnless(self.pi.objectImplements(a, getDottedName(DublinCore)))
+        self.failIf(self.pi.objectImplements(a, getDottedName(MyPortalContent)))
+
+    def testBImplements(self):
+        b = B()
+        self.failUnless(self.pi.objectImplements(b, getDottedName(Contentish)))
+        self.failUnless(self.pi.objectImplements(b, getDottedName(DublinCore)))
+        self.failUnless(self.pi.objectImplements(b, getDottedName(MyPortalContent)))
+
+    def testNamesAndDescriptions(self):
+        from Products.CMFPlone.interfaces.InterfaceTool import IInterfaceTool
+        nd = self.pi.namesAndDescriptions(getDottedName(IInterfaceTool))
+        nd2 = IInterfaceTool.namesAndDescriptions()
+        nd2 = [(n, d.getDoc()) for n, d in nd2]
+        self.assertEquals(nd, nd2)
+
+    
+if __name__ == '__main__':
+    framework()
+else:
+    import unittest
+    def test_suite():
+        suite = unittest.TestSuite()
+        suite.addTest(unittest.makeSuite(TestInterfaceResolution))
+        suite.addTest(unittest.makeSuite(TestInterfaceFinder))
+        suite.addTest(unittest.makeSuite(TestInterfaceTool))
+        return suite
+
