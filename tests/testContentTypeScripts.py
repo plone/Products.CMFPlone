0e8cad2c41b4dfae925b3275b82d9f496bd45b05
Stefan H. Holek <stefan@epy.co.at>
2003-09-19 20:35:23 +0000

    Add tests for Favorite, DiscussionItem, and LargePloneFolder's edit scripts.
    svn path=/CMFPlone/trunk/; revision=2964

--- tests/testContentTypeScripts.py
+++ tests/testContentTypeScripts.py
@@ -11,6 +11,7 @@ from Products.CMFPlone.tests import PloneTestCase
 
 AddPortalTopics = 'Add portal topics'
 from DateTime import DateTime
+from Products.CMFPlone import LargePloneFolder
 
 
 # Fake upload object
@@ -26,11 +27,26 @@ class TestContentTypeScripts(PloneTestCase.PloneTestCase):
 
     def afterSetUp(self):
         perms = self.getPermissionsOfRole('Member')
-        self.setPermissions(perms + [AddPortalTopics])
+        self.setPermissions(perms + [AddPortalTopics], 'Member')
+        self.discussion = self.portal.portal_discussion
+        self.member = self.portal.portal_membership.getAuthenticatedMember()
+        self.request = self.app.REQUEST
 
     def getPermissionsOfRole(self, role):
         perms = self.portal.permissionsOfRole(role)
         return [p['name'] for p in perms if p['selected']]
+
+    def testDiscussionReply(self):
+        self.folder.invokeFactory('Document', id='doc')
+        # Create the talkback object
+        self.discussion.overrideDiscussionFor(self.folder.doc, 1)
+        self.discussion.getDiscussionFor(self.folder.doc)
+        # Now test it
+        self.folder.doc.talkback.discussion_reply('Foo', 'Blah', self.member)
+        talkback = self.discussion.getDiscussionFor(self.folder.doc)
+        reply = talkback.objectValues()[0]
+        assert reply.Title() == 'Foo'
+        assert reply.EditableBody() == 'Blah'
 
     def testDocumentEdit(self):
         self.folder.invokeFactory('Document', id='doc')
@@ -45,6 +61,12 @@ class TestContentTypeScripts(PloneTestCase.PloneTestCase):
         assert self.folder.event.Title() == 'Foo'
         assert self.folder.event.start().ISO() == '2003-09-18 00:00:00'
         assert self.folder.event.end().ISO() == '2003-09-19 00:00:00'
+
+    def testFavoriteEdit(self):
+        self.folder.invokeFactory('Favorite', id='favorite')
+        self.folder.favorite.link_edit('bar/baz.html')
+        assert self.folder.favorite.getRemoteUrl() == \
+                '%s/bar/baz.html' % self.portal.portal_url()
 
     def testFileEdit(self):
         self.folder.invokeFactory('File', id='file')
@@ -61,6 +83,12 @@ class TestContentTypeScripts(PloneTestCase.PloneTestCase):
         self.folder.invokeFactory('Image', id='image')
         self.folder.image.image_edit(file=File())
         assert str(self.folder.image.data) == 'file_contents'
+
+    def testLargePloneFolderEdit(self):
+        LargePloneFolder.addLargePloneFolder(self.folder, 'lpf')
+        self.folder.lpf.folder_edit('Foo', 'Bar')
+        assert self.folder.lpf.Title() == 'Foo'
+        assert self.folder.lpf.Description() == 'Bar'
 
     def testLinkEdit(self):
         self.folder.invokeFactory('Link', id='link')
@@ -81,7 +109,7 @@ class TestContentTypeScripts(PloneTestCase.PloneTestCase):
         self.folder.invokeFactory('Topic', id='topic')
         # TODO: Analyze that funky data structure
 
-            
+
 if __name__ == '__main__':
     framework()
 else:
