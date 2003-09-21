5cb441c2a2b15821a0b278bb39abd96f09ba6e86
Stefan H. Holek <stefan@epy.co.at>
2003-09-21 00:09:32 +0000

    Learned more about ZChecker; this test now actually reports errors...
    svn path=/CMFPlone/trunk/; revision=2980

--- tests/zcheck.py
+++ tests/zcheck.py
@@ -13,6 +13,8 @@ from Products.CMFPlone.tests import PloneTestCase
 
 ZopeTestCase.installProduct('ZChecker')
 
+_print = ZopeTestCase._print
+
 
 class TestSkins(PloneTestCase.PloneTestCase):
 
@@ -24,7 +26,25 @@ class TestSkins(PloneTestCase.PloneTestCase):
         '''Runs the ZChecker on skins'''
         dirs = self.portal.portal_skins.objectValues()
         for dir in dirs:
-            self.portal.zchecker.checkObjects(dir.objectValues())
+            results = self.portal.zchecker.checkObjects(dir.objectValues())
+            for result in results:
+                self._report(result)
+        _print('\n')
+
+    def _report(self, result):
+        msg = result['msg']
+        obj = result['obj']
+        if msg:
+            _print('\n------\n%s\n' %self._skinpath(obj))
+            for line in msg:
+                _print('%s\n' %line)
+        else:
+            _print('.')
+
+    def _skinpath(self, obj):
+        path = obj.absolute_url(1)
+        path = path.split('/')
+        return '/'.join(path[1:])
             
 
 if __name__ == '__main__':
