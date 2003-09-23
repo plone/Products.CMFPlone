88a9b3d20b412dcf506e79f1641675275c539127
J. Cameron Cooper <jccooper@gmail.com>
2003-09-23 04:12:57 +0000

    making changes to control panel users/groups stuff per limi http://plone.org/Members/limi/users-groups-todo points 3 and 4. Probably some control flow and style stuff left, but it works.
    svn path=/CMFPlone/trunk/; revision=3031

--- skins/plone_prefs/prefs_group_edit.py
+++ skins/plone_prefs/prefs_group_edit.py
@@ -4,13 +4,18 @@
 ##bind namespace=
 ##bind script=script
 ##bind subpath=traverse_subpath
-##parameters=groupname
+##parameters=addname=None, groupname=None
 ##title=Edit user
 ##
 from Products.PythonScripts.standard import url_quote
 
 REQUEST=context.REQUEST
-group=context.portal_groups.getGroupById(groupname)
+
+if addname:
+  context.portal_groups.addGroup(addname,"",(),())
+  group=context.portal_groups.getGroupById(addname)
+else:
+  group=context.portal_groups.getGroupById(groupname)
 
 processed={}
 for id, property in context.portal_groupdata.propertyItems():
@@ -21,15 +26,8 @@ msg = 'Changes made.'
 if group:
     # for what reason ever, the very first group created does not exist
     group.setGroupProperties(processed)
-
-    #REFERER=REQUEST.HTTP_REFERER
-    #if REFERER.find('portal_status_message')!=-1:
-    #    REFERER=REFERER[:REFERER.find('portal_status_message')]
-    #url='%s&%s' % (REFERER, 'portal_status_message=Changes+made.')
-    #return REQUEST.RESPONSE.redirect(url)
 else:
     msg = 'No Changes made.'
-
 
 url='%s?%s=%s' % (context.prefs_groups_overview.absolute_url(),
     url_quote('portal_status_message'),
---
+++ skins/plone_scripts/returnNone.py
@@ -0,0 +1,11 @@
+## Script (Python) "returnNone"
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
+##parameters=a=0,b=0,c=0,d=0,e=0,f=0,g=0,h=0,i=0,j=0,k=0,l=0,m=0,n=0
+##title=Return None
+# a simple script to give back 'None'. Useful to masqurade as some other method
+# that doesn't really exist, but must be callable
+return None
