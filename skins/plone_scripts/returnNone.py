d6465119f9029d54774961bae5c763588e94aff7
J. Cameron Cooper <jccooper@gmail.com>
2003-09-24 22:07:14 +0000

    fixing the crazy parameters I put in, replacing them with a more sober (and useful) **kw parameter. Ah, the clear light of day...
    svn path=/CMFPlone/branches/Plone-2_0-branch/; revision=3076

--- skins/plone_scripts/returnNone.py
+++ skins/plone_scripts/returnNone.py
@@ -4,8 +4,8 @@
 ##bind namespace=
 ##bind script=script
 ##bind subpath=traverse_subpath
-##parameters=a=0,b=0,c=0,d=0,e=0,f=0,g=0,h=0,i=0,j=0,k=0,l=0,m=0,n=0
+##parameters=kw**
 ##title=Return None
 # a simple script to give back 'None'. Useful to masqurade as some other method
-# that doesn't really exist, but must be callable
+# that doesn't really exist, but must be callable.
 return None
