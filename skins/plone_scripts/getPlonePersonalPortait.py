b01328056dcb719ce83365c84ad531b82691c015
redcor <robert@redcor.ch>
2003-09-24 09:33:01 +0000

    adaptedt to the fact that portrait now lives in memberdatatool
    svn path=/CMFPlone/branches/Plone-2_0-branch/; revision=3067

--- skins/plone_scripts/getPlonePersonalPortait.py
+++ skins/plone_scripts/getPlonePersonalPortait.py
@@ -7,12 +7,14 @@
 ##parameters=member=None
 ##title=
 ##
-try:
-  home=context.portal_membership.getHomeFolder(member)
-  personal=getattr(home, '.personal', None) 
-  if personal:
-    portrait=getattr(personal, 'MyPortrait', None)
-    if portrait:         
-        return portrait
-except:
-  pass
+# portrait does not reside in the .personal folder anymore
+# try:
+#   home=context.portal_membership.getHomeFolder(member)
+#   personal=getattr(home, '.personal', None)
+#   if personal:
+#     portrait=getattr(personal, 'MyPortrait', None)
+#     if portrait:
+#         return portrait
+# except:
+#   pass
+return context.portal_membership.getPersonalPortrait(member)
