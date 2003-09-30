1a9ae54db88de2fff8ca056109acbec829f135aa
Stefan H. Holek <stefan@epy.co.at>
2003-09-30 22:00:40 +0000

    Refactored tests contributed by Robert Rottermann and gave them their own TestCase
    svn path=/CMFPlone/branches/Plone-2_0-branch/; revision=3144

--- tests/testCatalogTool.py
+++ tests/testCatalogTool.py
@@ -11,33 +11,8 @@ from Products.CMFPlone.tests import PloneTestCase
 
 from Acquisition import aq_base
 
-_user_name = ZopeTestCase._user_name
+user1 = ZopeTestCase._user_name
 user2 = 'u2'
-
-def addLocalGroupAndUser(self):
-    # utility for
-    uf = self.portal.acl_users
-    prefix = uf.getGroupPrefix()
-    groupname = 'extranet' #% prefix
-    groups = self.portal.portal_groups
-    groups.groupWorkspacesCreationFlag = 0
-    groups.addGroup(groupname, None, [], ())
-    assert len( uf.getGroups()) == 1
-    group=groups.getGroupById(groupname)
-    group.addMember(user2)
-    assert(group.getGroupMembers()[0].getUserName() == user2)
-    # mind you, do not get the user before she was added to the group, or she will not have any groups
-    user = uf.getUser(user2)
-    assert user.getGroups()[0] == '%s%s' % (prefix,groupname)
-    return '%s%s' % (prefix,groupname)
-
-def addUser2AndDocument(self):
-    uf = self.portal.acl_users
-    uf._doAddUser(user2, 'secret', (), (), (), )
-    self.portal.portal_types.constructContent('Document', self.folder, 'testdocument', None, )
-    document = self.folder.testdocument
-    document.edit(document.text_format, text='user u2 added some nonsense')
-    self.portal.portal_workflow.doActionFor(document, 'hide', comment='')
 
 
 class TestCatalogTool(PloneTestCase.PloneTestCase):
@@ -66,32 +41,66 @@ class TestCatalogTool(PloneTestCase.PloneTestCase):
         self.app.manage_pasteObjects(cb)
         self.failUnless(hasattr(self.app, 'copy_of_portal'))
 
+
+class TestCatalogSearch(PloneTestCase.PloneTestCase):
+
+    def afterSetUp(self):
+        self.catalog = self.portal.portal_catalog
+        self.workflow = self.portal.portal_workflow
+
+    def addLocalGroupAndUser(self):
+        # utility for
+        uf = self.portal.acl_users
+        prefix = uf.getGroupPrefix()
+        groupname = 'extranet' #% prefix
+        groups = self.portal.portal_groups
+        groups.groupWorkspacesCreationFlag = 0
+        groups.addGroup(groupname, None, [], ())
+        assert len( uf.getGroups()) == 1
+        group=groups.getGroupById(groupname)
+        group.addMember(user2)
+        assert(group.getGroupMembers()[0].getUserName() == user2)
+        # mind you, do not get the user before she was added to the group, 
+        # or she will not have any groups
+        user = uf.getUser(user2)
+        assert user.getGroups()[0] == '%s%s' % (prefix,groupname)
+        return '%s%s' % (prefix,groupname)
+
+    def addUser2AndDocument(self):
+        uf = self.portal.acl_users
+        uf._doAddUser(user2, 'secret', (), (), (), )
+        self.folder.invokeFactory('Document', id='testdocument', text='nonsense')
+        self.workflow.doActionFor(self.folder.testdocument, 'hide', comment='')
+
     def test_listAllowedRolesAndUsers(self):
         # should add group to list of allowed users
         uf = self.portal.acl_users
         uf._doAddUser(user2, 'secret', (), (), (), )
-        groupname = addLocalGroupAndUser(self)
-        assert ('user:%s' % groupname) in self.catalog._listAllowedRolesAndUsers(self.portal.acl_users.getUser(user2))
+        groupname = self.addLocalGroupAndUser()
+        self.failUnless(('user:%s' % groupname) in 
+                self.catalog._listAllowedRolesAndUsers(uf.getUser(user2)))
 
     def testSearchReturnsDocument(self):
         # document should be found when owner does a search
-        addUser2AndDocument(self)
-        assert self.catalog({'SearchableText':'nonsense'})[0].getObject().getId() == 'testdocument'
+        self.addUser2AndDocument()
+        self.assertEqual(self.catalog({'SearchableText':'nonsense'})[0].id,
+                         'testdocument')
 
     def testSearchDoesNotReturnDocument(self):
         # document should not be found when user 2 does a search
-        addUser2AndDocument(self)
+        self.addUser2AndDocument()
         self.login(user2)
-        assert len(self.catalog({'SearchableText':'nonsense'})) == 0
+        self.assertEqual(len(self.catalog({'SearchableText':'nonsense'})), 0)
 
     def testSearchReturnsDocumentWhenPermissionIsTroughLocalRole2(self):
         # after adding a local group with access rights and adding user2 search must find document
-        addUser2AndDocument(self)
-        groupname = addLocalGroupAndUser(self)
+        self.addUser2AndDocument()
+        groupname = self.addLocalGroupAndUser()
         self.folder.folder_localrole_edit('add', [groupname], 'Owner')
         # login again as user 2. a search now must get the document
         self.login(user2)
-        assert self.catalog({'SearchableText':'nonsense'})[0].getObject().getId() == 'testdocument'
+        self.assertEqual(self.catalog({'SearchableText':'nonsense'})[0].id,
+                         'testdocument')
 
 
 if __name__ == '__main__':
@@ -101,5 +110,6 @@ else:
     def test_suite():
         suite = unittest.TestSuite()
         suite.addTest(unittest.makeSuite(TestCatalogTool))
+        suite.addTest(unittest.makeSuite(TestCatalogSearch))
         return suite
 
