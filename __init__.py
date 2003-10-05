1a5d17c1158a26ab5ea6cafad081f390cdf4cec4
Alan Runyan <alan@enfoldsystems.com>
2003-10-05 01:48:17 +0000

    comment and put back the plone id headers andy forgot to checkin *wink*
    svn path=/CMFPlone/branches/Plone-2_0-branch/; revision=3162

--- __init__.py
+++ __init__.py
@@ -94,8 +94,9 @@ def initialize(context):
     import migrations
     migrations.registerMigrations()
 
-    import setup
-    import imagePatch
+    import setup           # Configuration Machinery - Andy we need to fix this at some point
+    import imagePatch      # WAII and 508 we need more properties on image objects 
+    import zserverPatch    # identify Plone in HTTP Headers - netcraft here we come!
     import UnicodeSplitter # registers unicode splitter w/ zctextindex pipeline registry
 
     from Products.CMFCore import DirectoryView
