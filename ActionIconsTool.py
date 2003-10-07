331e36e8dbae91b2252e2845a1adb12c1c3e9baa
Alan Runyan <alan@enfoldsystems.com>
2003-10-07 02:58:37 +0000

    add renderActionIcon method, which returns the actual image instance to ActionIcons and exposed it in the contentviews (green border where 'add new item' pulldown). you can create an action whose category is 'object_actions' and associate an icon with it by using the same category and id.
    added unit tests and fixed css so everything looks sane.
    
    svn path=/CMFPlone/branches/Plone-2_0-branch/; revision=3196

--- ActionIconsTool.py
+++ ActionIconsTool.py
@@ -1,4 +1,6 @@
 from Products.CMFActionIcons.ActionIconsTool import ActionIconsTool as BaseTool
+from Products.CMFActionIcons.permissions import View
+from Products.CMFCore.utils import getToolByName
 from Products.CMFPlone import ToolNames
 from AccessControl import ClassSecurityInfo
 from Globals import InitializeClass
@@ -8,6 +10,27 @@ class ActionIconsTool(BaseTool):
     meta_type = ToolNames.ActionIconsTool
     security = ClassSecurityInfo()
 
+    security.declareProtected(View, 'renderActionIcon')
+    def renderActionIcon( self, 
+                          category,
+                          action_id,
+                          default=None,
+                          context=None ):
+        """ Returns the actual object for the icon.  If you
+            pass in a path elements in default it will attempt
+            to traverse to that path.  Otherwise it will return
+            None
+        """ 
+        icon = self.queryActionIcon( category,
+                                     action_id,
+                                     default=default,
+                                     context=context )
+        if icon is not None:
+            portal=getToolByName(self, 'portal_url').getPortalObject()
+            return portal.restrictedTraverse(icon)
+
+        return default     
+               
 ActionIconsTool.__doc__ = BaseTool.__doc__
 
 InitializeClass(ActionIconsTool)
