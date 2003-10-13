ece73ed6d29b82329de4c9d4984607e4cb788f69
zopezen <andy@enfoldsystems.com >
2003-10-13 20:50:15 +0000

    Removing plone 1_1
    svn path=/CMFPlone/branches/Plone-2_0-branch/; revision=3245

---
+++ migrations/one03_one04.py
@@ -0,0 +1,10 @@
+from Products.CMFCore.utils import getToolByName
+from Products.CMFCore.Expression import Expression
+from Products.CMFCore import CMFCorePermissions
+
+def onezerofour(portal):
+    """ Upgrade from Plone 1.0.3 to Plone 1.0.4"""
+    pass
+            
+if __name__=='__main__':
+    registerMigrations()
