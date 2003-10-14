1591da1be8ee2e3ce28aadf4c872d9c5cdf97eb8
bmh <bmh@localhost>
2003-10-14 17:15:29 +0000

    Added security tests for content creation/deletion
    svn path=/CMFPlone/branches/Plone-2_0-branch/; revision=3249

---
+++ tests/testContentSecurity.py
@@ -0,0 +1,73 @@
+#
+# Tests the default workflow
+#
+
+import os, sys
+if __name__ == '__main__':
+    execfile(os.path.join(sys.path[0], 'framework.py'))
+
+from Testing import ZopeTestCase
+from Products.CMFPlone.tests import PloneTestCase
+
+from AccessControl import Unauthorized
+from Acquisition import aq_base
+
+_user_name = ZopeTestCase._user_name
+
+
+class TestContentSecurity(PloneTestCase.PloneTestCase):
+
+    def afterSetUp(self):
+        self.portal.acl_users._doAddUser('user1', 'secret', ['Member'], [])
+        self.portal.acl_users._doAddUser('user2', 'secret', ['Member'], [])
+        self.membership = self.portal.portal_membership
+        self.membership.getMemberById('user1')
+        self.membership.getMemberById('user2')
+
+    def testCreateMemberContent(self):
+        self.login('user1')
+        folder = self.membership.getHomeFolder('user1')
+        folder.invokeFactory('Document', id='new')
+        self.failUnless(hasattr(aq_base(folder), 'new'))
+        self.logout()
+
+    def testCreateOtherMemberContentFails(self):
+        self.login('user1')
+        folder = self.membership.getHomeFolder('user2')
+        self.assertRaises(Unauthorized, folder.invokeFactory, 'Document', 'new')
+        self.logout()
+
+    def testCreateRootContentFails(self):
+        self.login('user1')
+        self.assertRaises(Unauthorized, self.portal.invokeFactory, 'Document', 'new')
+        self.logout()
+
+    def testDeleteMemberContent(self):
+        self.login('user1')
+        folder = self.membership.getHomeFolder('user1')
+        folder.invokeFactory('Document', id='new')
+        folder.manage_delObjects(['new'])
+        self.failIf(hasattr(aq_base(folder), 'new'))
+        self.logout()
+
+    def testDeleteOtherMemberContent(self):
+        self.login('user1')
+        folder = self.membership.getHomeFolder('user1')
+        folder.invokeFactory('Document', id='new')
+        self.logout()
+
+        self.login('user2')
+        folder = self.membership.getHomeFolder('user1')
+        self.assertRaises(Unauthorized, folder.manage_delObjects, ['new'])
+        self.logout()
+
+
+if __name__ == '__main__':
+    framework()
+else:
+    import unittest
+    def test_suite():
+        suite = unittest.TestSuite()
+        suite.addTest(unittest.makeSuite(TestContentSecurity))
+        return suite
+
