bb5966179ce668d0acd8fce8e0821ee7947d4317
zopezen <andy@enfoldsystems.com >
2003-11-01 23:43:02 +0000

    Removing crufty files (I believe)
    Adding in experimental setupSite.py that needs to be called manually by the installer to create the correct sort of Plone site.
    
    svn path=/CMFPlone/branches/Plone-2_0-branch/; revision=3445

---
+++ Extensions/setupSite.py
@@ -0,0 +1,49 @@
+import os
+import sys
+
+def process(name, ihome, swhome):
+    admin_username = name
+    os.environ['INSTANCE_HOME'] = ihome
+
+    if sys.platform == 'win32':
+        print swhome
+        sys.path.insert(0, '%s/Zope/lib/python' % swhome)
+        sys.path.insert(1, '%s/Python/lib' % swhome)
+        sys.path.insert(2, '%s' % swhome)
+    else:    
+        os.environ['SOFTWARE_HOME'] = swhome
+        
+    # have to set up env first
+    from App import FindHomes
+    import Zope
+
+    app = Zope.app()
+    
+    from OFS.Application import initialize
+    initialize(app)
+
+    from Testing import makerequest
+    app = makerequest.makerequest(app)
+
+    from Products.CMFPlone.PloneInitialize import create
+    out = create(app, admin_username)
+    return out
+
+if __name__=='__main__':
+    # sys.argv[0] is script name
+    if len(sys.argv) < 4:
+        print """
+setupSite.py username swhome ihome
+        
+username: the username to setup the site with
+swhome: the software home
+ihome: the instance home
+
+note: win32 does this slightly differently
+"""
+        sys.exit()
+
+    # for example the following is how i run it on win32
+    # "c:\Program Files\Plone\python\python" setupSite.py admin "c:\Program Files\Plone\Data" "c:\Program Files\Plone"
+    name, swhome, ihome = sys.argv[1:]
+    process(name, swhome, ihome)
