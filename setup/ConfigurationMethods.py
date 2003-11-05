3ca2deeb0a1eaf25beeea74b913bb51a26e25d6e
Alan Runyan <alan@enfoldsystems.com>
2003-11-05 21:46:44 +0000

    fix error in diff
    svn path=/CMFPlone/branches/Plone-2_0-branch/; revision=3484

--- setup/ConfigurationMethods.py
+++ setup/ConfigurationMethods.py
@@ -345,14 +345,8 @@ these functions do not have a uninstall function."""
     def addItems(self, fns):   
         out = []         
         for fn in fns:
-<<<<<<< ConfigurationMethods.py
-            functions[fn](self, self.portal)
-            #print self.portal._p_jar._storage
-            #get_transaction().commit(1) #XXX This is throwing stones
-=======
             portal=getToolByName(self, 'portal_url').getPortalObject()
             functions[fn](self, portal)
->>>>>>> 1.16.2.15
             out.append(('Function %s has been applied' % fn, INFO))
         return out
 
