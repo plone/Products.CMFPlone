a2fb9aff443869493787535e1ce0a38c5e1e26e1
Alexander Limi <limi@plone.org>
2003-11-06 00:26:31 +0000

    Added the files for the style selector.
    svn path=/CMFPlone/branches/Plone-2_0-branch/; revision=3490

--- skins/plone_ecmascript/plone_javascripts.js
+++ skins/plone_ecmascript/plone_javascripts.js
@@ -417,4 +417,53 @@ function highlightSearchTerm() {
 if (window.addEventListener) window.addEventListener("load",highlightSearchTerm,false);
 else if (window.attachEvent) window.attachEvent("onload",highlightSearchTerm);
 
+<!--
 
+// ----------------------------------------------
+// StyleSwitcher functions written by Paul Sowden
+// http://www.idontsmoke.co.uk/ss/
+// - - - - - - - - - - - - - - - - - - - - - - -
+// For the details, visit ALA:
+// http://www.alistapart.com/stories/alternate/
+// ----------------------------------------------
+
+function setActiveStyleSheet(title, reset) {
+  var i, a, main;
+  for(i=0; (a = document.getElementsByTagName("link")[i]); i++) {
+    if(a.getAttribute("rel").indexOf("style") != -1 && a.getAttribute("title")) {
+      a.disabled = true;
+      if(a.getAttribute("title") == title) a.disabled = false;
+    }
+  }
+  if (reset == 1) {
+  createCookie("wstyle", title, 365);
+  }
+}
+
+function setStyle() {
+var style = readCookie("wstyle");
+if (style != null) {
+setActiveStyleSheet(style, 0);
+}
+}
+
+function createCookie(name,value,days) {
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime()+(days*24*60*60*1000));
+    var expires = "; expires="+date.toGMTString();
+  }
+  else expires = "";
+  document.cookie = name+"="+value+expires+"; path=/;";
+}
+
+function readCookie(name) {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(';');
+  for(var i=0;i < ca.length;i++) {
+    var c = ca[i];
+    while (c.charAt(0)==' ') c = c.substring(1,c.length);
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+  }
+  return null;
+}
