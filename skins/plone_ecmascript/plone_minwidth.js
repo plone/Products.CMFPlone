213d4f92355e030fd397c3dbbe1b0f6ec6007a6d
Alexander Limi <limi@plone.org>
2003-12-02 16:48:40 +0000

    Added the new minwidth.js script and removed the old plone_minmax script. Thanks to Geir and Michael for refactoring!
    svn path=/CMFPlone/branches/Plone-2_0-branch/; revision=3668

---
+++ skins/plone_ecmascript/plone_minwidth.js
@@ -0,0 +1,51 @@
+/* 
+Plone sizing fix for broke IE min-width.
+This script will make sure things float correctly with the
+tableless layout, even in broken IE. All other browsers ignore
+this script.
+By Geir Bækholt - Plone Solutions http://www.plonesolutions.com
+*/
+
+
+
+/*@cc_on
+@if (@_win32 && @_jscript_version>4)
+
+
+
+var rulersCreated = 0
+    
+function fixwidth() {
+    var el;
+    el=document.getElementById('visual-portal-wrapper');
+    if (!rulersCreated){
+        ruler = document.createElement('div');
+        ruler.style.width="58em";
+        ruler.style.position="absolute";
+        ruler.style.top="-10px"
+        ruler.style.visibility="hidden";
+        document.body.insertBefore(ruler, document.body.firstChild)
+        
+        ruler2 = document.createElement('div');
+        ruler2.style.position="relative";
+        ruler2.style.height="1px"
+        ruler2.style.visibility="hidden";
+        document.body.insertBefore(ruler2, document.body.firstChild)       
+        
+        rulersCreated=1
+    }
+    if (ruler2.offsetWidth < ruler.offsetWidth){
+        el.style.width="58em"
+    }else{
+        el.style.width=ruler2.offsetWidth+'px'
+    }
+    //alert(""+ ruler2.offsetWidth + ":" + ruler.offsetWidth) //debugging info
+}
+
+window.attachEvent('onload', fixwidth);
+window.attachEvent('onresize', fixwidth);
+
+
+
+
+@end @*/
