2763f95add222425f4eb079a468354e2ef920aaa
Stefan H. Holek <stefan@epy.co.at>
2003-12-12 10:45:11 +0000

    Fixed a typo to make the tests pass again
    svn path=/CMFPlone/branches/Plone-2_0-branch/; revision=3730

--- migrations/twoBeta2_twoBeta3.py
+++ migrations/twoBeta2_twoBeta3.py
@@ -29,7 +29,7 @@ def twoBeta2_twoBeta3(portal):
 
     # and then in workflow remove any workflow from it
     wt = getToolByName(portal, "portal_workflow")
-    wt.setChainForPortalTypes(("Plone Site"), "")
+    wt.setChainForPortalTypes(("Plone Site",), "")
 
 
 if __name__=='__main__':
