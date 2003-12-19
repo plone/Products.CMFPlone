5f810ace250f968b16525f0599b8153be0334a6c
Fabiano Weimar dos Santos <xiru@xiru.org>
2003-12-19 21:00:42 +0000

    Added script to insert copyright messages on .pt and .py files (based on Andy McKay's script)
    svn path=/CMFPlone/branches/Plone-2_0-branch/; revision=3799

---
+++ Extensions/add_copyright.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+
+## script to add copyright messages on .py and .pt files
+## by Fabiano Weimar dos Santos <xiru@xiru.org>
+## based on Andy McKay ClearWind script.
+
+import os, sys
+
+def notPresent(file, add):
+    data = open(file, 'r').read()
+    if data.find(add) > -1:
+        print "Text already present in file", file
+        return
+    return data        
+
+def change_py(file):
+    add = """###################################
+## here goes the copyright text
+###################################
+"""
+    res = notPresent(file, add)
+    if res is not None:
+	res = add + res
+        open(file, 'w').write(res)
+        print "Changed", file
+
+def change_pt(file):
+    add = """<tal:comment replace="nothing">
+    here goes the copyright text
+</tal>
+"""
+    res = notPresent(file, add)
+    if res is not None:
+        lines = res.split('\n')
+        addlines = add.split('\n')
+	if file.endswith('main_template.pt'):
+	    res = lines[0:1] + addlines + lines[1:]
+	else:
+	    res = addlines + lines
+        res = '\n'.join(res)
+        open(file, 'w').write(res)
+        print "Changed", file
+
+def walker(ignore, dr, files):
+    for file in files:
+        file = os.path.join(dr, file)
+        if file.endswith('.py'):
+            change_py(file)
+        if file.endswith('.pt'):
+            change_pt(file)
+
+if __name__=='__main__':
+    os.path.walk(sys.argv[1], walker, [])
