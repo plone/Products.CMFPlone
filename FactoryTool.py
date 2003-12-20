6cc112ff11a2d0425a9be8f0fe94a1824a9deca3
Geoff Davis <geoff@geoffdavis.net>
2003-12-20 00:45:00 +0000

    Fix for 2072, 2073
    svn path=/CMFPlone/branches/Plone-2_0-branch/; revision=3818

--- FactoryTool.py
+++ FactoryTool.py
@@ -52,6 +52,10 @@ class TempFolder(TempFolderBase):
             obj = self._getOb(id).__of__(aq_parent(aq_parent(self)))
             obj.unindexObject()
             return obj
+    
+    # ignore rename requests since they don't do anything
+    def manage_renameObject(self, id1, id2):
+        pass
 
 
 
