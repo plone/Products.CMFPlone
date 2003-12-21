8d22d39c86e860a17f467912c5d88d39c1b14df4
Danny Bloemendaal <danny.bloemendaal@companion.nl>
2003-12-21 21:48:49 +0000

    Updated toggleSelect, selectAll and deselectAll to make it possible to switch groups of checkboxes with equal Names but in different form on the same page (used in folder_local_role.pt)
    svn path=/CMFPlone/branches/Plone-2_0-branch/; revision=3909

--- skins/plone_ecmascript/plone_javascripts.js
+++ skins/plone_ecmascript/plone_javascripts.js
@@ -263,24 +263,43 @@ function submitFilterAction() {
 
 // Functions for selecting all checkboxes in folder_contents/search_form view
 
-function selectAll(id) {
-  checkboxes = document.getElementsByName(id);
-  for (i = 0; i < checkboxes.length; i++)
-    checkboxes[i].checked = true ;
+function selectAll(id, formName) {
+  // get the elements. if formName is p rovided, get the elements inside the form
+  if (formName==null) {
+     checkboxes = document.getElementsByName(id)
+     for (i = 0; i < checkboxes.length; i++)
+         checkboxes[i].checked = true ;
+  } else {
+     for (i=0; i<document.forms[formName].elements.length;i++)
+	 {
+	   if (document.forms[formName].elements[i].name==id) 
+            document.forms[formName].elements[i].checked=true;
+	  }
+  }
 }
 
-function deselectAll(id) {
-  checkboxes = document.getElementsByName(id);
-  for (i = 0; i < checkboxes.length; i++)
-    checkboxes[i].checked = false ;
+function deselectAll(id, formName) {
+  if (formName==null) {
+     checkboxes = document.getElementsByName(id)
+     for (i = 0; i < checkboxes.length; i++)
+         checkboxes[i].checked = false ;
+  } else {
+     for (i=0; i<document.forms[formName].elements.length;i++)
+	 {
+	   if (document.forms[formName].elements[i].name==id) 
+            document.forms[formName].elements[i].checked=false;
+	  }
+  }
 }
 
-function toggleSelect(selectbutton, id, initialState) {
+function toggleSelect(selectbutton, id, initialState, formName) {
   // required selectbutton: you can pass any object that will function as a toggle
   // optional id: id of the the group of checkboxes that needs to be toggled (default=ids:list
   // optional initialState: initial state of the group. (default=false)
-  //                              e.g. folder_contents is false, search_form=true because the item boxes
-  //                              are checked initially.
+  //   e.g. folder_contents is false, search_form=true because the item boxes
+  //   are checked initially.
+  // optional formName: name of the form in which the boxes reside, use this if there are more
+  //   forms on the page with boxes with the same name
 
   id=id || 'ids:list'  // defaults to ids:list, this is the most common usage
 
@@ -295,12 +314,12 @@ function toggleSelect(selectbutton, id, initialState) {
   if (selectbutton.isSelected == false) {
     selectbutton.setAttribute('src', portal_url + '/select_none_icon.gif');
     selectbutton.isSelected=true;
-    return selectAll(id);
+    return selectAll(id, formName);
   }
   else {
     selectbutton.setAttribute('src',portal_url + '/select_all_icon.gif');
     selectbutton.isSelected=false;
-    return deselectAll(id);
+    return deselectAll(id, formName);
   }
 }
 
