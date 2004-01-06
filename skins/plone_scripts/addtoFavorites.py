f18491caad932faf19a7e33d53013da25e790d7d
Alan Runyan <alan@enfoldsystems.com>
2004-01-06 20:48:08 +0000

    stupid API
    svn path=/CMFPlone/branches/Plone-2_0-branch/; revision=4047

--- skins/plone_scripts/addtoFavorites.py
+++ skins/plone_scripts/addtoFavorites.py
@@ -8,7 +8,7 @@
 ##parameters=
 homeFolder=context.portal_membership.getHomeFolder()
 if not hasattr(homeFolder, 'Favorites'):
-    homeFolder.invokeFactory( 'Folder', id='Favorites', title='Favorites')
+    homeFolder.invokeFactory(id='Favorites', type_name='Folder')
 
 targetFolder = getattr( homeFolder, 'Favorites' )
 new_id='fav_' + str(int( context.ZopeTime()))
