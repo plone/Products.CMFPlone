f3f8912b34550fb964d3d0a2ec55be1b297e94e7
jimroepcke <jimroepcke@localhost>
2004-01-07 05:27:53 +0000

    make the body tag and/or its class customizable to enable a variety of css design patterns
    svn path=/CMFPlone/trunk/; revision=4055

---
+++ skins/plone_scripts/getBodyTagClass.py
@@ -0,0 +1,12 @@
+## Script (Python) "getBodyTagClass"
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
+##parameters=
+##title=
+##
+#from Products.PythonScripts.standard import html_quote
+#
+return None # html_quote('location-FrontPage')
