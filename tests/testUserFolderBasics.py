b14869300d1181bd42255922ded454bb86243235
Stefan H. Holek <stefan@epy.co.at>
2004-01-30 20:11:32 +0000

    Updated version of user folder tests to resemble those checked into Zope 2.6.4 and 2.7.0. Also see http://zope.org/Collectors/Zope/1210
    svn path=/CMFPlone/branches/Plone-2_0-branch/; revision=4340

--- tests/testUserFolderBasics.py
+++ tests/testUserFolderBasics.py
@@ -1,5 +1,5 @@
 #
-# Generic User Folder tests. Every User Folder implementation
+# Generic user folder tests. Every user folder implementation
 # must pass these.
 #
 
@@ -10,52 +10,40 @@ if __name__ == '__main__':
 from Testing import ZopeTestCase
 from Products.CMFPlone.tests import PloneTestCase
 
+import base64
+from AccessControl import Unauthorized
+
 default_user = ZopeTestCase.user_name
 user_perms   = ZopeTestCase.standard_permissions
 user_role    = 'Member'
 
-_pm = 'ThePublishedMethod'
 
-from AccessControl import Unauthorized
-
-
-class TestBase(PloneTestCase.PloneTestCase):
+class TestUserFolder(PloneTestCase.PloneTestCase):
 
     def afterSetUp(self):
         self.logout()
         self.uf = self.portal.acl_users
-
-    def _setupPublishedMethod(self):
-        self.folder.addDTMLMethod(_pm, file='some content')
-        pm = self.folder[_pm]
-        for p in user_perms:
-            pm.manage_permission(p, [user_role], acquire=0)
-
-    def _setupPublishedRequest(self):
-        request = self.app.REQUEST
-        request.set('PUBLISHED', self.folder[_pm])
-        request.set('PARENTS', [self.folder, self.app])
-        folder_path = self.folder.absolute_url(1).split('/')
-        request.steps = folder_path + [_pm]
-
-    def _basicAuth(self, name):
-        import base64
-        return 'Basic %s' % base64.encodestring('%s:%s' %(name, 'secret'))
-
-    def _call__roles__(self, object):
-        # From BaseRequest.traverse()
-        roles = ()
-        object = getattr(object, 'aq_base', object)
-        if hasattr(object, '__call__') and hasattr(object.__call__, '__roles__'):
-            roles = object.__call__.__roles__
-        return roles
-
-
-class TestUserFolderSecurity(TestBase):
-    '''Test UF is working'''
+        self.basic = 'Basic %s' % base64.encodestring('%s:secret' % default_user)
+        # Set up a published object accessible to the default user
+        self.folder.addDTMLMethod('doc', file='')
+        self.folder.doc.manage_permission('View', [user_role], acquire=0)
+        # Rig the REQUEST so it looks like we traversed to 'doc'
+        self.app.REQUEST.set('PUBLISHED', self.folder['doc'])
+        self.app.REQUEST.set('PARENTS', [self.folder, self.app])
+        folder_path = list(self.folder.getPhysicalPath())
+        self.app.REQUEST.steps = folder_path + ['doc']
 
     def testGetUser(self):
-        self.failIf(self.uf.getUser(default_user) is None)
+        self.failIfEqual(self.uf.getUser(default_user), None)
+
+    def testGetBadUser(self):
+        self.assertEqual(self.uf.getUser('user2'), None)
+
+    def testGetUserById(self):
+        self.failIfEqual(self.uf.getUserById(default_user), None)
+
+    def testGetBadUserById(self):
+        self.assertEqual(self.uf.getUserById('user2'), None)
 
     def testGetUsers(self):
         users = self.uf.getUsers()
@@ -68,11 +56,9 @@ class TestUserFolderSecurity(TestBase):
         self.assertEqual(names[0], default_user)
 
     def testIdentify(self):
-        auth = self._basicAuth(default_user)
-        name, password = self.uf.identify(auth)
-        self.failIf(name is None)
+        name, password = self.uf.identify(self.basic)
         self.assertEqual(name, default_user)
-        self.failIf(password is None)
+        self.assertEqual(password, 'secret')
 
     def testGetRoles(self):
         user = self.uf.getUser(default_user)
@@ -96,11 +82,12 @@ class TestUserFolderSecurity(TestBase):
 
     def testHasPermission(self):
         user = self.uf.getUser(default_user)
-        self.folder.manage_role(user_role, user_perms+['Add Folders'])
+        self.failUnless(user.has_permission('View', self.folder))
+        self.folder.manage_role(user_role, ['Add Folders'])
         self.login()   # !!! Fixed in Zope 2.6.2
         self.failUnless(user.has_permission('Add Folders', self.folder))
 
-    def testHasLocalPermission(self):
+    def testHasLocalRolePermission(self):
         user = self.uf.getUser(default_user)
         self.folder.manage_role('Owner', ['Add Folders'])
         self.folder.manage_addLocalRoles(default_user, ['Owner'])
@@ -111,77 +98,37 @@ class TestUserFolderSecurity(TestBase):
         user = self.uf.getUser(default_user)
         self.failUnless(user.authenticate('secret', self.app.REQUEST))
 
+    def testValidate(self):
+        user = self.uf.validate(self.app.REQUEST, self.basic, [user_role])
+        self.failIfEqual(user, None)
+        self.assertEqual(user.getUserName(), default_user)
 
-class TestUserFolderAccess(TestBase):
-    '''Test UF is protecting access'''
+    def testNotValidateWithoutAuth(self):
+        user = self.uf.validate(self.app.REQUEST, '', ['role1']) 
+        self.assertEqual(user, None)
 
-    def afterSetUp(self):
-        TestBase.afterSetUp(self)
-        self._setupPublishedMethod()
+    def testValidateWithoutRoles(self):
+        # Roles will be determined by looking at 'doc' itself
+        user = self.uf.validate(self.app.REQUEST, self.basic)
+        self.assertEqual(user.getUserName(), default_user)
 
-    def testAllowAccess(self):
+    def testNotValidateWithEmptyRoles(self):
+        user = self.uf.validate(self.app.REQUEST, self.basic, []) 
+        self.assertEqual(user, None)
+
+    def testNotValidateWithoutWrongRoles(self):
+        user = self.uf.validate(self.app.REQUEST, self.basic, ['Manager']) 
+        self.assertEqual(user, None)
+
+    def testAllowAccessToUser(self):
         self.login()
         try:
-            self.folder.restrictedTraverse(_pm)
+            self.folder.restrictedTraverse('doc')
         except Unauthorized:
             self.fail('Unauthorized')
 
-    def testDenyAccess(self):
-        self.assertRaises(Unauthorized, self.folder.restrictedTraverse, _pm)
-
-
-class TestUserFolderValidate(TestBase):
-    '''Test UF is authorizing us'''
-
-    def afterSetUp(self):
-        TestBase.afterSetUp(self)
-        self._setupPublishedMethod()
-        self._setupPublishedRequest()
-
-    def testAuthorize(self):
-        # Validate should log us in
-        request = self.app.REQUEST
-        auth = self._basicAuth(default_user)
-        user = self.uf.validate(request, auth, [user_role])
-        self.failIf(user is None)
-        self.assertEqual(user.getUserName(), default_user)
-
-    def testNotAuthorize(self):
-        # Validate should fail without auth
-        request = self.app.REQUEST
-        auth = ''
-        self.assertEqual(self.uf.validate(request, auth, [user_role]), None)
-
-    def testNotAuthorize2(self):
-        # Validate should fail without roles
-        request = self.app.REQUEST
-        auth = self._basicAuth(default_user)
-        self.assertEqual(self.uf.validate(request, auth), None)
-
-    def testNotAuthorize3(self):
-        # Validate should fail with wrong roles
-        request = self.app.REQUEST
-        auth = self._basicAuth(default_user)
-        self.assertEqual(self.uf.validate(request, auth, ['Manager']), None)
-
-    def testAuthorize2(self):
-        # Validate should allow us to call dm
-        request = self.app.REQUEST
-        auth = self._basicAuth(default_user)
-        roles = self._call__roles__(self.folder[_pm])
-        user = self.uf.validate(request, auth, roles)
-        self.failIf(user is None)
-        self.assertEqual(user.getUserName(), default_user)
-
-    def testNotAuthorize4(self):
-        # Validate should deny us to call dm
-        request = self.app.REQUEST
-        auth = self._basicAuth(default_user)
-        pm = self.folder[_pm]
-        for p in user_perms:
-            pm.manage_permission(p, [], acquire=0)
-        roles = self._call__roles__(pm)
-        self.assertEqual(self.uf.validate(request, auth, roles), None)
+    def testDenyAccessToAnonymous(self):
+        self.assertRaises(Unauthorized, self.folder.restrictedTraverse, 'doc')
 
 
 if __name__ == '__main__':
@@ -190,7 +137,5 @@ else:
     import unittest
     def test_suite():
         suite = unittest.TestSuite()
-        suite.addTest(unittest.makeSuite(TestUserFolderSecurity))
-        suite.addTest(unittest.makeSuite(TestUserFolderAccess))
-        suite.addTest(unittest.makeSuite(TestUserFolderValidate))
+        suite.addTest(unittest.makeSuite(TestUserFolder))
         return suite
