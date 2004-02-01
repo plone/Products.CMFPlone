27cd01ad6fe71ebf769146948fbaa99f53b65ddd
Alan Runyan <alan@enfoldsystems.com>
2004-02-01 21:04:26 +0000

    attempt to fix contents tab
    svn path=/CMFPlone/branches/Plone-2_0-branch/; revision=4363

--- skins/plone_scripts/displayContentsTab.py
+++ skins/plone_scripts/displayContentsTab.py
@@ -7,12 +7,25 @@
 ##parameters=
 ##title=
 ##
-
 pm=context.portal_membership
 checkPermission=pm.checkPermission
-for permission in ('List folder contents',
-                   'Modify portal content',
-                   'Copy or Move'):
+
+parent = context.aq_parent
+show = 1
+#We only want to show the 'contents' tab under the following conditions:
+# - If you can DO SOMETHING in a folder_contents view. i.e.
+#   Copy or Move, or Modify portal content, or Add portal content.
+# - If you can not do that in the current context, check the container
+#   to see if you can do SOMETHING
+for permission in ('Copy or Move',
+                   'List folder contents',
+                   'Modify portal content'):
     if not checkPermission(permission, context):
-        return 0
+        show = 0
+        break
+
+if not show and (not checkPermission('Modify portal content', parent) or \
+                 not checkPermission('Copy or Move', parent)):
+    return 0
+
 return 1
