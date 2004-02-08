8dbc9a955df9ac7ac64d8b5061f465f41ffc27cf
Alan Runyan <alan@enfoldsystems.com>
2004-02-08 20:52:25 +0000

    prep for 2.0 release in a few minutes
    svn path=/CMFPlone/branches/Plone-2_0-branch/; revision=4416

--- Extensions/package_plone.py
+++ Extensions/package_plone.py
@@ -22,7 +22,8 @@ os.system("cd .. ; mkdir %s" % releasename)
 # copy products
 for product in ('CMFPlone', 'Formulator', 'CMFActionIcons', 'CMFQuickInstallerTool',
                 'BTreeFolder2', 'GroupUserFolder', 'CMFFormController',
-                ' PlacelessTranslationService',
+                'PlacelessTranslationService', 'Archetypes', 'generator', 'validation',
+                'PortalTransforms', )
                ):
     os.system("cd .. ; cp -rfL %s %s" % (product, releasename))
 
