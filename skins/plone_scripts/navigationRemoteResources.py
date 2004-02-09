35b9b3820a7fc6da8b6a23cff7e7c390f1761404
Geoff Davis <geoff@geoffdavis.net>
2004-02-09 00:09:21 +0000

    * add try block to handle edge case uncovered in CMFMember
    svn path=/CMFPlone/branches/Plone-2_0-branch/; revision=4418

--- skins/plone_scripts/navigationRemoteResources.py
+++ skins/plone_scripts/navigationRemoteResources.py
@@ -15,11 +15,14 @@ if obj is None:
     if not obj.isPrincipiaFolderish:
         obj=obj.aq_parent
 
-for o in obj.contentValues('Link'):
-    try:
-        listing.append( (o.title_or_id(),
-                         o.getRemoteUrl()) )
-    except (Unauthorized,AttributeError):
-        pass
+try:
+    for o in obj.contentValues('Link'):
+        try:
+            listing.append( (o.title_or_id(),
+                             o.getRemoteUrl()) )
+        except (Unauthorized,AttributeError):
+            pass
+except Unauthorized:
+    pass
 
 return listing
