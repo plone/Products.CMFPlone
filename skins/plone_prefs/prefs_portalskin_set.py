e6cf8b671dab1a9c42e8734f1b3bf37a0d844456
Jochen Knuth <jok-zope@kumbi.org>
2004-02-12 19:50:58 +0000

    Second pass in fixing the clearing of the cookie name for the plone skin. i had to get the existing value and reapply it to the portal_skins.
    svn path=/CMFPlone/branches/Plone-2_0-branch/; revision=4462

--- skins/plone_prefs/prefs_portalskin_set.py
+++ skins/plone_prefs/prefs_portalskin_set.py
@@ -12,9 +12,13 @@ REQUEST=context.REQUEST
 
 ps = context.portal_skins
 
+#get cookie name to set it again, if not cookie name is cleared
+rv=ps.request_varname
+
 ps.manage_properties(default_skin=default_skin,
                      allow_any=allow_any,
-                     cookie_persistence=cookie_persistence)
+                     cookie_persistence=cookie_persistence
+		     request_varname=rv)
 
 msg = 'Portal skin updated'
 RESPONSE.redirect('prefs_portalskin_form?portal_status_message=' + msg)
