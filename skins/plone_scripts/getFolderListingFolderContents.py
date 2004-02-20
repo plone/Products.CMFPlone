ff36b9cd04af5f8a920e85077a03ca7ec5d61a05
jimroepcke <jimroepcke@localhost>
2004-02-20 18:44:48 +0000

    added aq_explicit to calls to listFolderContents and folderlistingFolderContents as suggested by tesdal and runyaga
    svn path=/CMFPlone/branches/Plone-2_0-branch/; revision=4516

--- skins/plone_scripts/getFolderListingFolderContents.py
+++ skins/plone_scripts/getFolderListingFolderContents.py
@@ -24,16 +24,16 @@
 
 contents = None
 try:
-    contents = context.folderlistingFolderContents(contentFilter=contentFilter, suppressHiddenFiles=suppressHiddenFiles)
+    contents = context.aq_explicit.folderlistingFolderContents(contentFilter=contentFilter, suppressHiddenFiles=suppressHiddenFiles)
 except AttributeError:
     try:
-        contents = context.listFolderContents(contentFilter=contentFilter, suppressHiddenFiles=suppressHiddenFiles)
+        contents = context.aq_explicit.listFolderContents(contentFilter=contentFilter, suppressHiddenFiles=suppressHiddenFiles)
     except TypeError:
         #XXX Manually do suppression
         context.plone_log('Manual fall back in getFolderContents - your Folder.listFolderContents method does not ' \
                           'support suppressHiddenFiles')
         contents = [obj
-                    for obj in context.listFolderContents(contentFilter=contentFilter)
+                    for obj in context.aq_explicit.listFolderContents(contentFilter=contentFilter)
                     if not obj.getId().startswith('.')
                    ]
 except TypeError:
@@ -41,7 +41,7 @@ except TypeError:
     context.plone_log('Manual fall back in getFolderContents - your Folder.listFolderContents method does not ' \
                       'support suppressHiddenFiles')
     contents = [obj
-                for obj in context.listFolderContents(contentFilter=contentFilter)
+                for obj in context.aq_explicit.listFolderContents(contentFilter=contentFilter)
                 if not obj.getId().startswith('.')
                ]
 return contents
