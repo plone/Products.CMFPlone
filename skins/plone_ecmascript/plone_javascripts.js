012379ec9b50ae8cd21b990465f93ff91b9ee0d2
Alexander Limi <limi@plone.org>
2004-02-20 01:25:47 +0000

    Added cookies to fix the "can't log in after switching font" bug. Thanks to Seb Potter :)
    svn path=/CMFPlone/branches/Plone-2_0-branch/; revision=4503

--- skins/plone_ecmascript/plone_javascripts.js
+++ skins/plone_ecmascript/plone_javascripts.js
@@ -522,7 +522,7 @@ function createCookie(name,value,days) {
     var expires = "; expires="+date.toGMTString();
   }
   else expires = "";
-  document.cookie = name+"="+value+expires+"; path=/;";
+  document.cookie = name+"="+escape(value)+expires+"; path=/;";
 }
 
 function readCookie(name) {
@@ -531,7 +531,7 @@ function readCookie(name) {
   for(var i=0;i < ca.length;i++) {
     var c = ca[i];
     while (c.charAt(0)==' ') c = c.substring(1,c.length);
-    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    if (c.indexOf(nameEQ) == 0) return unescape(c.substring(nameEQ.length,c.length));
   }
   return null;
 }
