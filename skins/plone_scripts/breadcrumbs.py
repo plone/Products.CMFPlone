fcf0c3991eed534fe6b8e526de3942f28fd3fc40
Geoff Davis <geoff@geoffdavis.net>
2004-03-04 19:02:26 +0000

    breadcrumbs.py had bugs in which (1) a terminal object was sometimes displayed twice, and (2) terminal non-content non-view objects (e.g. page templates) were not displayed at all.  The bugs have been fixed and breadcrumbs.py has been refactored a bit to make the source easier to understand and maintain
    svn path=/CMFPlone/branches/Plone-2_0-branch/; revision=4639

--- skins/plone_scripts/breadcrumbs.py
+++ skins/plone_scripts/breadcrumbs.py
@@ -11,9 +11,6 @@ from Products.CMFCore.utils import getToolByName
 from AccessControl import Unauthorized
 dynamic_type = "Products.CMFCore.interfaces.Dynamic.DynamicType"
 
-if obj is not None:
-    context = obj
-
 # Some variables
 url_tool = getToolByName(context, 'portal_url')
 # required for login into a not migrated plone1 site
@@ -22,8 +19,6 @@ try:
 except AttributeError:
     iface_tool = None
 
-published = context.REQUEST.get('PUBLISHED', None)
-published_id = None
 checkPermission=context.portal_membership.checkPermission
 dont_show_metatypes = ['TempFolder', # Metatypes of objects we wont show
                        'Plone Factory Tool',
@@ -34,89 +29,120 @@ dont_show = ['talkback'] # Objects we won't show.
 
 portal = url_tool.getPortalObject()
 
+
+def get_parent(obj, portal):
+    if obj == portal:
+        return None
+    # If the user doesn't have 'Access contents information'
+    # at some level, then it will append '...' to breadcrumbs
+    # and not present a link to that item.
+    try:
+        parent = obj.getParentNode()
+        # Try to access attribute to see if we have enough
+        # permissions to get info about this object.
+        getattr(obj, 'meta_type', None)
+        return parent
+    except Unauthorized:
+        # It wasn't possible to get at the parent
+        # or at it's attributes. We probably dont
+        # have the required permissions, so let's
+        # try to skip this object on to the next one.
+        subpath = list(url_tool.getRelativeContentPath(current))[:-1]
+        while subpath:
+            try:
+                parent = portal.restrictedTraverse(subpath)
+                # Try to access attribute to see if we have enough
+                # permissions to get info about this object.
+                getattr(parent, 'meta_type', None)
+                return parent
+            except Unauthorized:
+                # Unable to get info. Use '...' for name
+                # and dont generate a link.
+                path_seq.append(('...', None))
+                subpath.pop()
+            else:
+                break
+        if parent == obj:
+            # Uhm. We are still at the same object,
+            # so trying to get a parent failed. 
+            return None
+
+
+published = context.REQUEST.get('PUBLISHED', None)
+published_id = None
 if published is not None and hasattr(published, 'getId'):
     published_id = published.getId()
 
 currentlyViewingFolderContents = (published_id in ['folder_contents',
                                                    'folderContents'])
 path_seq = []
-subpath = []
 
-#o = current = published or context
-o = current = context
-first_object = 1
+if obj is not None:
+    context = obj
+
+first_item = 1
+if published != context and published_id:
+    current = published
+    parent = context
+    id = published_id
+else:
+    current = context
+    parent = get_parent(current, portal)
+    id = None
+    if hasattr(current, 'getId'):
+        id = current.getId()
 
 # Add breadcrumbs for directories between the root and the published object.
-while current and (first_object or current is not portal):
-    if not first_object:
-        # If the user doesn't have 'Access contents information'
-        # at some level, then it will append '...' to breadcrumbs
-        # and not present a link to that item.
+while 1:
+    if first_item:
+        first_item = 0
+        if id is None:
+            continue
+        # see if the first item in the stack is folder_contents
+        if currentlyViewingFolderContents:
+            continue
+        # see if the first item in the stack is a view
         try:
-            o = current.getParentNode()
-            # Try to access attribute to see if we have enough
-            # permissions to get info about this object.
-            getattr(o, 'meta_type', None)
+            if parent is not None:
+                if getattr(parent, 'isPrincipiaFolderish', 0):
+                    if id in portal.plone_utils.browserDefault(parent)[1]:
+                        continue
+                if hasattr(parent, 'getTypeInfo'):
+                    if id == parent.getTypeInfo().getActionById('view'):
+                        continue
         except Unauthorized:
-            # It wasn't possible to get at the parent
-            # or at it's attributes. We probably dont
-            # have the required permissions, so let's
-            # try to skip this object on to the next one.
-            subpath = list(url_tool.getRelativeContentPath(current))[:-1]
-            while subpath:
-                try:
-                    o = portal.restrictedTraverse(subpath)
-                    # Try to access attribute to see if we have enough
-                    # permissions to get info about this object.
-                    getattr(o, 'meta_type', None)
-                except Unauthorized:
-                    # Unable to get info. Use '...' for name
-                    # and dont generate a link.
-                    path_seq.append(('...', None))
-                    subpath.pop()
-                else:
-                    break
-            if o is current:
-                # Uhm. We are still at the same object,
-                # so trying to get a parent failed. Break
-                # the loop.
-                break
-        current = o
+            if id in ['view', 'index_html', 'folder_listing']:
+                continue
     else:
-        first_object = 0
-
-    if hasattr(o, 'getId'):
-        id = o.getId()
-        if id in dont_show:
+        current = parent
+        if current is None:
+            break
+        parent = get_parent(current, portal)
+        if not hasattr(current, 'getId'):
+            continue
+        id = current.getId()
+        
+        # Content objects inherit from DynamicType (except for the portal itself)
+        # Make sure the object is either a content object or the portal
+        # (Don't do this test on the first object because it could be a page template)
+        if current != portal and iface_tool and not iface_tool.objectImplements(current, dynamic_type):
             continue
 
+    if not hasattr(current, 'title_or_id'):
+        continue
+    
+    if id in dont_show:
+        continue
+
     # don't show temporary folders or other excluded types
-    if getattr(o, 'meta_type', None) in dont_show_metatypes:
+    if getattr(current, 'meta_type', None) in dont_show_metatypes:
         continue
 
-    # Content objects inherit from DynamicType (except for the portal itself)
-    # Make sure the object is either a content object or the portal
-    if current != portal and iface_tool and not iface_tool.objectImplements(o, dynamic_type):
-        continue
-
-    # Special treatment for the context object of the breadcrumb script:
-    # We don't want to show a crumb if the current object is the default
-    # view for a folder
-    if current == context:
-        try:
-            parent = current.getParentNode()
-            if parent.isPrincipiaFolderish:
-                if current.getId() in portal.plone_utils.browserDefault(parent)[1]:
-                    continue
-        except Unauthorized:
-            if current.getId() in ['index_html', 'folder_listing']:
-                continue
-
-    url = o.absolute_url()
-    title = o.title_or_id()
-    if o.isPrincipiaFolderish:
+    url = current.absolute_url()
+    title = current.title_or_id()
+    if current.isPrincipiaFolderish:
         if (currentlyViewingFolderContents and
-            checkPermission('List folder contents', o)):
+            checkPermission('List folder contents', current)):
             path_seq.append((title, url+'/folder_contents'))
         else:
             path_seq.append((title, url+'/'))
@@ -132,16 +158,5 @@ path_seq.reverse()
 # If the published object was not added to breadcrumbs above and
 #    it is not a view template, add a breadcrumb for it
 
-# Under some circunstances (read ZWiki :P) 'published' may
-# be a method. A method doesn't have title_or_id, and even if it had
-# it would not be accessible from here.
-
-if published != o and iface_tool and \
-   iface_tool.objectImplements(published, dynamic_type) and not \
-    currentlyViewingFolderContents and \
-    published_id not in  ('view', 'index_html') and \
-    hasattr(published, 'title_or_id'):
-    url = published.absolute_url() + '/view'
-    path_seq.append( (published.title_or_id(), url) )
-
+context.plone_log(path_seq)
 return path_seq
