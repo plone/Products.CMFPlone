05b690279870b9c335b5aa1755460af0cd9653ec
Daniel Nouri <dpunktnpunkt@users.sourceforge.net>
2004-03-23 13:30:29 +0000

    Smarter default handling.
    svn path=/CMFPlone/branches/Plone-2_0-branch/; revision=4727

--- skins/plone_scripts/translate.py
+++ skins/plone_scripts/translate.py
@@ -8,8 +8,17 @@
 
 from Products.CMFPlone.PloneUtilities import translate_wrapper
 
-return translate_wrapper(domain,
-                         msgid,
-                         mapping,
-                         context=context,
-                         default=default)
+value = translate_wrapper(domain,
+                          msgid,
+                          mapping,
+                          context=context,
+                          default=default)
+
+if not value and default is None:
+    value = msgid
+
+    for k, v in mapping.items():
+        value = value.replace('${%s}' % k, v)
+
+return value
+    
