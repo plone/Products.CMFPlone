626c2c2ae76f72ce3b256814a33e79c0a445c126
zopezen <andy@enfoldsystems.com >
2004-04-14 16:45:39 +0000

    Correct my own typo and point out the siteRoot object
    svn path=/CMFPlone/branches/Plone-2_0-branch/; revision=4797

--- Extensions/accessRule.py
+++ Extensions/accessRule.py
@@ -8,19 +8,25 @@
 # select 'Set Access Rule' to disable the access rule
 # (yes that is a little odd).
 #
+# If you are turning this off, also go into your Plone site and remove the
+# SiteRoot object. It should be benign with this access rule turned off, but
+# you don't need it any more.
+#
 # The installer sets up a default Plone site for you, called Plone (this
-# is assigned to siteObj variable). To all you to get to the root this access
+# is assigned to siteObj variable). To allow you to get to the root this access
 # rule will accept an environment variable called HTTP_MANAGE to point to the
 # value of the manager port. This value is normally 8080 and is set by the 
 # zope.conf file.
 #
-# The Plone installer adds this in as an External Method point to accessRule.
-# If a request comes in and its not the HTTP_MANAGE port then its going to 
-# append Plone into the path.
+# The Plone installer adds this in as an External Method pointing to
+# the accessRule function, below. If a request comes in and its not 
+# the HTTP_MANAGE port then its going to append Plone into the path and voila
+# you see your Plone site correctly.
 # 
 # This will work with FTP, WebDAV and all other protocols.
 
 import os
+
 HTTP_MANAGE = os.environ.get('HTTP_MANAGE', '')
 siteObj = 'Plone'
 
