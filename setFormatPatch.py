33a9ae423c38c3947acf5fa9bcb4d00858beb7bb
Stefan H. Holek <stefan@epy.co.at>
2004-04-15 18:22:27 +0000

    Repair indentation accident.
    svn path=/CMFPlone/trunk/; revision=4810

--- setFormatPatch.py
+++ setFormatPatch.py
@@ -4,14 +4,14 @@
 from Acquisition import aq_base
 
 def setFormat( self, format ):
-        """
-            Dublin Core element - resource format
-        """
-        self.__old_setFormat(format)
-        # Update the content_type property if present
-        if (getattr(aq_base(self), 'hasProperty', None) and
-            self.hasProperty('content_type')):
-            self.manage_changeProperties(content_type=format)
+    """
+        Dublin Core element - resource format
+    """
+    self.__old_setFormat(format)
+    # Update the content_type property if present
+    if (hasattr(aq_base(self), 'hasProperty') and
+        self.hasProperty('content_type')):
+        self.manage_changeProperties(content_type=format)
 
 from Products.CMFDefault.DublinCore import DefaultDublinCoreImpl
 DefaultDublinCoreImpl.__old_setFormat = DefaultDublinCoreImpl.setFormat
