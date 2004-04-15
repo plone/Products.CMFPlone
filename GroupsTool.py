1fc55092d8d405044531f405010dbc3f9cff2431
zopezen <andy@enfoldsystems.com >
2004-04-15 01:11:46 +0000

    Allow the ability to change the icon for each tool in the ZMI. This is done because ToolInit only excepts one icon for the whole Plone Tool set. So without hacking the CMFCore this easily lets you alter what you see in the ZMI.
    To use:
    
    - look at the GroupsTool.py example
    - add in the ToolImageOverride class
    - define an iconlist variable that is the list of icons
    
    svn path=/CMFPlone/branches/Plone-2_0-branch/; revision=4803

--- GroupsTool.py
+++ GroupsTool.py
@@ -3,11 +3,13 @@ from Products.GroupUserFolder.GroupsTool import GroupsTool as BaseTool
 from Products.CMFPlone import ToolNames
 from AccessControl import ClassSecurityInfo
 from Globals import InitializeClass
+from Products.CMFPlone.PloneUtilities import ToolIconOverride
 
-class GroupsTool(BaseTool):
+class GroupsTool(BaseTool, ToolIconOverride):
 
     meta_type = ToolNames.GroupsTool
     security = ClassSecurityInfo()
+    iconlist = ["group.gif",]
 
 GroupsTool.__doc__ = BaseTool.__doc__
 
