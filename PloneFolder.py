933fbf3b5234d6671a3d17a87902da2d64c8171b
Christian 'Tiran' Heimes <lists@cheimes.de>
2004-04-15 20:17:26 +0000

    New moveObjectsByDelta method based on the new interface with subset_ids
    svn path=/CMFPlone/trunk/; revision=4814

--- PloneFolder.py
+++ PloneFolder.py
@@ -1,7 +1,7 @@
 try: from zExceptions import NotFound
 except ImportError: NotFound = 'NotFound' # Zope < 2.7
 from Products.CMFCore.utils import _verifyActionPermissions, \
-     getToolByName, getActionContext, _checkPermission
+     getToolByName, getActionContext
 from Products.CMFCore.Skinnable import SkinnableObjectManager
 from OFS.Folder import Folder
 from Products.CMFCore.CMFCatalogAware import CMFCatalogAware
@@ -125,60 +125,61 @@ class OrderedContainer(Folder):
     # the 2.7 specific class OSF.OrderedContainer.OrderedContainer
 
     security.declareProtected(ModifyPortalContent, 'moveObjectsByDelta')
-    def moveObjectsByDelta(self, ids, delta):
+    def moveObjectsByDelta(self, ids, delta, subset_ids=None):
         """ Move specified sub-objects by delta.
         """
         if type(ids) is StringType:
             ids = (ids,)
         min_position = 0
-        #objects = list(self._objects)
-        obj_visible = []
-        obj_hidden =[]
-        obj_dict = {}
-
-        types_tool = getToolByName(self, 'portal_types')
-        types=types_tool.listContentTypes(by_metatype=1)
-
-        for obj in self._objects:
-            # sort out in portal visible and invisible objects in 2 lists
-            try:
-                types.index(obj['meta_type'])
-            except ValueError:
-                obj_hidden.append(obj)
-            else:
-                obj_dict[ obj['id'] ] = obj
-                obj_visible.append(obj)
-
-
+        objects = list(self._objects)
+        if subset_ids == None:
+            # OLD: subset_ids = [ obj['id'] for obj in objects ]
+            subset_ids = self.getCMFObjectsSubsetIds(objects)
+        else:
+            subset_ids = list(subset_ids)
         # unify moving direction
         if delta > 0:
             ids = list(ids)
             ids.reverse()
-            obj_visible.reverse()
+            subset_ids.reverse()
         counter = 0
 
         for id in ids:
-            try:
-                object = obj_dict[id]
-            except KeyError:
-                raise ValueError('The object with the id "%s" does not exist.'
-                                 % id)
-            old_position = obj_visible.index(object)
+            old_position = subset_ids.index(id)
             new_position = max( old_position - abs(delta), min_position )
             if new_position == min_position:
                 min_position += 1
             if not old_position == new_position:
-                obj_visible.remove(object)
-                obj_visible.insert(new_position, object)
+                subset_ids.remove(id)
+                subset_ids.insert(new_position, id)
                 counter += 1
 
         if counter > 0:
             if delta > 0:
-                obj_visible.reverse()
-            self._objects = tuple(obj_hidden + obj_visible)
+                subset_ids.reverse()
+            obj_dict = {}
+            for obj in objects:
+                obj_dict[ obj['id'] ] = obj
+            pos = 0
+            for i in range( len(objects) ):
+                if objects[i]['id'] in subset_ids:
+                    try:
+                        objects[i] = obj_dict[ subset_ids[pos] ]
+                        pos += 1
+                    except KeyError:
+                        raise ValueError('The object with the id "%s" does '
+                                         'not exist.' % subset_ids[pos])
+            self._objects = tuple(objects)
 
         return counter
 
+    security.declarePrivate('getCMFObjectsSubset')
+    def getCMFObjectsSubsetIds(self, objs):
+        """Get the ids of only cmf objects (used for moveObjectsByDelta)
+        """
+        ttool = getToolByName(self, 'portal_types')
+        cmf_meta_types = ttool.listContentTypes(by_metatype=1)
+        return [obj.getId() for obj in objs if obj['meta_type'] in cmf_meta_types ]
 
     security.declareProtected(ModifyPortalContent, 'getObjectPosition')
     def getObjectPosition(self, id):
@@ -347,91 +348,7 @@ class BasePloneFolder ( SkinnedFolder, DefaultDublinCoreImpl ):
             contents=[obj for obj in contents if obj.getId()[:1]!='.']
 
         return contents
-    
-    # XXX: Not sure this belongs here, it was only on HEAD before the merge!
-    def _verifyObjectPaste(self, object, validate_src=1):
-        # XXX This is just an extension of a copy of the 
-        # _verifyObjectPaste code from CMFCores PortalFolder. 
-        # Actually the missing check if a content type is allowed 
-        # to be pasted into another is a bug in CMFCore/PortalFolder.py.
-        # Once this is corrected in CMFCore one savely can 
-        # remove this method (done probably in CMF 1.5).
 
-        # This assists the version in OFS.CopySupport.
-        # It enables the clipboard to function correctly
-        # with objects created by a multi-factory.
-        securityChecksDone = 0
-        if (hasattr(object, '__factory_meta_type__') and
-            hasattr(self, 'all_meta_types')):
-            mt = object.__factory_meta_type__
-            method_name=None
-            permission_name = None
-            meta_types = self.all_meta_types
-            if callable(meta_types): meta_types = meta_types()
-            for d in meta_types:
-                if d['name']==mt:
-                    method_name=d['action']
-                    permission_name = d.get('permission', None)
-                    break
-
-            if permission_name is not None:
-                if _checkPermission(permission_name,self):
-                    if not validate_src:
-                        # We don't want to check the object on the clipboard
-                        securityChecksDone = 1
-                    else:
-                        try: parent = aq_parent(aq_inner(object))
-                        except: parent = None
-                        if getSecurityManager().validate(None, parent,
-                                                         None, object):
-                            # validation succeeded
-                            securityChecksDone = 1
-                        else:
-                            raise 'Unauthorized', object.getId()
-                else:
-                    raise 'Unauthorized', permission_name
-            #
-            # Old validation for objects that may not have registered 
-            # themselves in the proper fashion.
-            #
-            elif method_name is not None:
-                meth=self.unrestrictedTraverse(method_name)
-                if hasattr(meth, 'im_self'):
-                    parent = meth.im_self
-                else:
-                    try:    parent = aq_parent(aq_inner(meth))
-                    except: parent = None
-                if getSecurityManager().validate(None, parent, None, meth):
-                    # Ensure the user is allowed to access the object on the
-                    # clipboard.
-                    if not validate_src:
-                        securityChecksDone = 1
-                    else:
-                        try: parent = aq_parent(aq_inner(object))
-                        except: parent = None
-                        if getSecurityManager().validate(None, parent,
-                                                         None, object):
-                            securityChecksDone = 1
-                        else:
-                            id = object.getId()
-                            raise 'Unauthorized', id
-                else:
-                    raise 'Unauthorized', method_name
-        
-        # call OFS's _verifyObjectPaste if necessary
-        if not securityChecksDone:
-            PortalFolder.inheritedAttribute(
-                '_verifyObjectPaste')(self, object, validate_src)
-        
-        # check if CMF content type is allowed to be pasted
-        type_name = getattr(aq_base(object), 'portal_type', None)
-        if type_name is not None:
-            pt = getToolByName(self, 'portal_types')
-            myType = pt.getTypeInfo(self)
-            if myType is not None and not myType.allowType(type_name):
-                raise ValueError, \
-                      "Disallowed to paste subobject type '%s'." % type_name
-        
     security.declareProtected( AccessContentsInformation, 'folderlistingFolderContents')
     def folderlistingFolderContents( self, spec=None, contentFilter=None, suppressHiddenFiles=0 ):
         """
