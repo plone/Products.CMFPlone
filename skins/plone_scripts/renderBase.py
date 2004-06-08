ddd85ba92c08cb4e9c498af828e5077857c9948b
Jodok Batlogg <jodok@batlogg.com>
2004-06-08 12:28:09 +0000

    renderbase raised an error when accessing plone via webdav, now added try: except: pass around it
    svn path=/CMFPlone/branches/Plone-2_0-branch/; revision=5328

--- skins/plone_scripts/renderBase.py
+++ skins/plone_scripts/renderBase.py
@@ -1,5 +1,10 @@
 # returns correct base href
-if getattr(context.aq_explicit, 'isPrincipiaFolderish', 0):
-    return context.absolute_url()+'/'
-else:
-    return context.absolute_url()
+
+# when accessing via WEBDAV you're not allowed to access aq_explicit
+try:
+    if getattr(context.aq_explicit, 'isPrincipiaFolderish', 0):
+        return context.absolute_url()+'/'
+    else:
+        return context.absolute_url()
+except:
+    pass
