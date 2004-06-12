3e87c77c7facc24c4cf372ce337a530fb525fde4
Jodok Batlogg <jodok@batlogg.com>
2004-06-12 21:57:40 +0000

    by popular demand :) bare pass/excepts are evil
    svn path=/CMFPlone/branches/Plone-2_0-branch/; revision=5339

--- skins/plone_scripts/renderBase.py
+++ skins/plone_scripts/renderBase.py
@@ -1,10 +1,12 @@
 # returns correct base href
 
+from AccessControl import Unauthorized
+
 # when accessing via WEBDAV you're not allowed to access aq_explicit
 try:
     if getattr(context.aq_explicit, 'isPrincipiaFolderish', 0):
         return context.absolute_url()+'/'
     else:
         return context.absolute_url()
-except:
+except (Unauthorized, 'Unauthorized'):
     pass
