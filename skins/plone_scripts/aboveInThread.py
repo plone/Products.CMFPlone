0d6ee78fd0ab1dff84d691316fdd770c2b522925
Jean-Paul Ladage <j.ladage@zestsoftware.nl>
2004-10-07 10:38:23 +0000

    ATContentTypes compatibility, default action view for ATCT breadcrumb in aboveInThread.py
    svn path=/CMFPlone/trunk/; revision=5623

--- skins/plone_scripts/aboveInThread.py
+++ skins/plone_scripts/aboveInThread.py
@@ -12,7 +12,10 @@ parents = context.parentsInThread()
 
 if parents:
     for parent in parents:
-        crumbs.append('<a href="%s">%s</a> ' % (parent.absolute_url(), parent.Title()))
+        if parent.Title() == context.aq_parent.Title():
+            crumbs.append('<a href="%s/view">%s</a> ' % (parent.absolute_url(), parent.Title()))
+        else:
+            crumbs.append('<a href="%s">%s</a> ' % (parent.absolute_url(), parent.Title()))
 
     breadcrumbs = '<strong>&raquo;</strong> '.join(crumbs)
 
