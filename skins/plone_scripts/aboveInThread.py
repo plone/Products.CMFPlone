1af05013dd94ba2c2a1fb5e157ce859a2835aec7
Stefan H. Holek <stefan@epy.co.at>
2004-10-10 11:07:41 +0000

    Get rid of ATCT compatibility code.
    svn path=/CMFPlone/trunk/; revision=5633

--- skins/plone_scripts/aboveInThread.py
+++ skins/plone_scripts/aboveInThread.py
@@ -12,10 +12,7 @@ parents = context.parentsInThread()
 
 if parents:
     for parent in parents:
-        if parent.Title() == context.aq_parent.Title():
-            crumbs.append('<a href="%s/view">%s</a> ' % (parent.absolute_url(), parent.Title()))
-        else:
-            crumbs.append('<a href="%s">%s</a> ' % (parent.absolute_url(), parent.Title()))
+        crumbs.append('<a href="%s">%s</a> ' % (parent.absolute_url(), parent.Title()))
 
     breadcrumbs = '<strong>&raquo;</strong> '.join(crumbs)
 
