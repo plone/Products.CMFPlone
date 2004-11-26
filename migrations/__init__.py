47625fa05dab5c380bcc68ae25d135b5a399c64d
Stefan H. Holek <stefan@epy.co.at>
2004-11-26 15:27:37 +0000

    Back to normal.
    svn path=/CMFPlone/branches/Plone-2_0-branch/; revision=5794

--- migrations/__init__.py
+++ migrations/__init__.py
@@ -86,7 +86,7 @@ def registerMigrations():
                                       null)
 
     MigrationTool.registerUpgradePath('2.0.4',
-                                      '2.0.5-rc2',
+                                      '2.0.5 (SVN/UNRELEASED)',
                                       v2.two04_two05.two04_two05)
 
 
