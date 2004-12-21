56b68488701cc8b90a535aa5872bb72bd78ef85c
Stefan H. Holek <stefan@epy.co.at>
2004-12-21 16:46:26 +0000

    Reintroduced the full OrderedContainer implementation. We can't just swap it out for Zope's after all. - We use a different permission - We restrict the types of objects that can be ordered
    svn path=/CMFPlone/branches/2.1/; revision=5873

--- PloneFolder.py
+++ PloneFolder.py
@@ -3,7 +3,6 @@ except ImportError: NotFound = 'NotFound' # Zope < 2.7
 from Products.CMFCore.utils import _verifyActionPermissions, \
      getToolByName, getActionContext
 from OFS.Folder import Folder
-from OFS.OrderedFolder import OrderedFolder
 from Products.CMFCore.CMFCatalogAware import CMFCatalogAware
 from Products.CMFDefault.SkinnedFolder import SkinnedFolder
 from Products.CMFDefault.DublinCore import DefaultDublinCoreImpl
@@ -17,6 +16,15 @@ from webdav.WriteLockInterface import WriteLockInterface
 from webdav.NullResource import NullResource
 from types import StringType
 from DocumentTemplate.sequence import sort
+
+# this import can change with Zope 2.7 to
+try:
+    from OFS.IOrderSupport import IOrderedContainer as IZopeOrderedContainer
+    hasZopeOrderedSupport=1
+except ImportError:
+    hasZopeOrderedSupport=0
+# atm its safer defining an own
+from interfaces.OrderedContainer import IOrderedContainer
 
 from OFS.ObjectManager import REPLACEABLE
 from ComputedAttribute import ComputedAttribute
@@ -73,13 +81,164 @@ Plone folders can define custom 'view' actions, or will behave like directory li
                                 )
                              }
 
+#Portions of this class was copy/pasted from the OFS.Folder.OrderedFolder from
+#Zope2.7.  This class is licensed under the ZPL 2.0 as stated here:
+#http://www.zope.org/Resources/ZPL
+#Zope Public License (ZPL) Version 2.0
+#This software is Copyright (c) Zope Corporation (tm) and Contributors. All rights reserved.
 
-class OrderedContainer(OrderedFolder):
+class OrderedContainer(Folder):
     """Folder with subobject ordering support"""
   
-    __implements__ = OrderedFolder.__implements__
+    if hasZopeOrderedSupport:
+        # got the IOrderedContainer interface from zope 2.7, too
+        # make shure this implementation fullfilles both interfaces
+        __implements__  = (IOrderedContainer, IZopeOrderedContainer)
+    else:
+        __implements__  = (IOrderedContainer,)
 
     security = ClassSecurityInfo()
+
+    security.declareProtected(CMFCorePermissions.ModifyPortalContent, 'moveObject')
+    def moveObject(self, id, position):
+        obj_idx  = self.getObjectPosition(id)
+        if obj_idx == position:
+            return None
+        elif position < 0:
+            position = 0
+
+        metadata = list(self._objects)
+        obj_meta = metadata.pop(obj_idx)
+        metadata.insert(position, obj_meta)
+        self._objects = tuple(metadata)
+
+    # here the implementing of IOrderedContainer starts
+    # if plone sometime depends on zope 2.7 it should be replaced by mixing in
+    # the 2.7 specific class OSF.OrderedContainer.OrderedContainer
+
+    security.declareProtected(CMFCorePermissions.ModifyPortalContent, 'moveObjectsByDelta')
+    def moveObjectsByDelta(self, ids, delta, subset_ids=None):
+        """ Move specified sub-objects by delta.
+        """
+        if type(ids) is StringType:
+            ids = (ids,)
+        min_position = 0
+        objects = list(self._objects)
+        if subset_ids == None:
+            # OLD: subset_ids = [ obj['id'] for obj in objects ]
+            subset_ids = self.getCMFObjectsSubsetIds(objects)
+        else:
+            subset_ids = list(subset_ids)
+        # unify moving direction
+        if delta > 0:
+            ids = list(ids)
+            ids.reverse()
+            subset_ids.reverse()
+        counter = 0
+
+        for id in ids:
+            old_position = subset_ids.index(id)
+            new_position = max( old_position - abs(delta), min_position )
+            if new_position == min_position:
+                min_position += 1
+            if not old_position == new_position:
+                subset_ids.remove(id)
+                subset_ids.insert(new_position, id)
+                counter += 1
+
+        if counter > 0:
+            if delta > 0:
+                subset_ids.reverse()
+            obj_dict = {}
+            for obj in objects:
+                obj_dict[ obj['id'] ] = obj
+            pos = 0
+            for i in range( len(objects) ):
+                if objects[i]['id'] in subset_ids:
+                    try:
+                        objects[i] = obj_dict[ subset_ids[pos] ]
+                        pos += 1
+                    except KeyError:
+                        raise ValueError('The object with the id "%s" does '
+                                         'not exist.' % subset_ids[pos])
+            self._objects = tuple(objects)
+
+        return counter
+
+    security.declarePrivate('getCMFObjectsSubsetIds')
+    def getCMFObjectsSubsetIds(self, objs):
+        """Get the ids of only cmf objects (used for moveObjectsByDelta)
+        """
+        ttool = getToolByName(self, 'portal_types')
+        cmf_meta_types = ttool.listContentTypes(by_metatype=1)
+        return [obj['id'] for obj in objs if obj['meta_type'] in cmf_meta_types ]
+
+    security.declareProtected(CMFCorePermissions.ModifyPortalContent, 'getObjectPosition')
+    def getObjectPosition(self, id):
+
+        objs = list(self._objects)
+        om = [objs.index(om) for om in objs if om['id']==id ]
+
+        if om: # only 1 in list if any
+            return om[0]
+
+        raise NotFound, 'Object %s was not found' % str(id)
+
+    security.declareProtected(CMFCorePermissions.ModifyPortalContent, 'moveObjectsUp')
+    def moveObjectsUp(self, ids, delta=1, RESPONSE=None):
+        """ Move an object up """
+        self.moveObjectsByDelta(ids, -delta)
+        if RESPONSE is not None:
+            RESPONSE.redirect('manage_workspace')
+
+    security.declareProtected(CMFCorePermissions.ModifyPortalContent, 'moveObjectsDown')
+    def moveObjectsDown(self, ids, delta=1, RESPONSE=None):
+        """ move an object down """
+        self.moveObjectsByDelta(ids, delta)
+        if RESPONSE is not None:
+            RESPONSE.redirect('manage_workspace')
+
+    security.declareProtected(CMFCorePermissions.ModifyPortalContent, 'moveObjectsToTop')
+    def moveObjectsToTop(self, ids, RESPONSE=None):
+        """ move an object to the top """
+        self.moveObjectsByDelta( ids, -len(self._objects) )
+        if RESPONSE is not None:
+            RESPONSE.redirect('manage_workspace')
+
+    security.declareProtected(CMFCorePermissions.ModifyPortalContent, 'moveObjectsToBottom')
+    def moveObjectsToBottom(self, ids, RESPONSE=None):
+        """ move an object to the bottom """
+        self.moveObjectsByDelta( ids, len(self._objects) )
+        if RESPONSE is not None:
+            RESPONSE.redirect('manage_workspace')
+
+    security.declareProtected(CMFCorePermissions.ModifyPortalContent, 'moveObjectToPosition')
+    def moveObjectToPosition(self, id, position):
+        """ Move specified object to absolute position.
+        """
+        delta = position - self.getObjectPosition(id)
+        return self.moveObjectsByDelta(id, delta)
+
+    security.declareProtected(CMFCorePermissions.ModifyPortalContent, 'orderObjects')
+    def orderObjects(self, key, reverse=None):
+        """ Order sub-objects by key and direction.
+        """
+        ids = [ id for id, obj in sort( self.objectItems(),
+                                        ( (key, 'cmp', 'asc'), ) ) ]
+        if reverse:
+            ids.reverse()
+        return self.moveObjectsByDelta( ids, -len(self._objects) )
+
+    # here the implementing of IOrderedContainer ends
+
+    def manage_renameObject(self, id, new_id, REQUEST=None):
+        " "
+        objidx = self.getObjectPosition(id)
+        method = OrderedContainer.inheritedAttribute('manage_renameObject')
+        result = method(self, id, new_id, REQUEST)
+        self.moveObject(new_id, objidx)
+
+        return result
 
 InitializeClass(OrderedContainer)
 
