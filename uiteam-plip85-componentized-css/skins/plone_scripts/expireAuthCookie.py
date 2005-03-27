7d2a595d6fd4b4a6a6ae94f0d36ad1b86b5077fa
Alexander Limi <limi@plone.org>
2005-03-27 12:46:19 +0000

    Oops, copy/paste error made me create a branch inside 2.1 - sorry about that.
    /me loves svn mv ;)
    
    svn path=/CMFPlone/branches/2.1/; revision=6261

--- uiteam-plip85-componentized-css/skins/plone_scripts/expireAuthCookie.py
+++
@@ -1,5 +0,0 @@
-## Script (Python) "expireAuthCookie"
-##title=Expire Authentication Cookie
-##parameters=resp, cookie_name
-resp.expireCookie( cookie_name, path='/')
-resp.expireCookie( '__ac', path='/')
