0362007503de03d8e6778cade82b10a8e77cd9ba
Geir Baekholt <geir@baekholt.com>
2005-04-10 19:59:20 +0000

    remove that extra closing curly bracket that caused the menu to break.
    svn path=/CMFPlone/branches/2.1/; revision=6527

--- skins/plone_ecmascript/highlightsearchterms.js
+++ skins/plone_ecmascript/highlightsearchterms.js
@@ -1,11 +1,11 @@
 function climb(node, word){
-	 // traverse childnodes
+     // traverse childnodes
     if (! node){return false}
     if (node.hasChildNodes) {
-		var i;
-		for (i=0;i<node.childNodes.length;i++) {
+        var i;
+        for (i=0;i<node.childNodes.length;i++) {
             climb(node.childNodes[i],word);
-		}
+        }
         if (node.nodeType == 3){
             checkforhighlight(node, word);
            // check all textnodes. Feels inefficient, but works
@@ -15,26 +15,22 @@ function climb(node, word){
 
 function checkforhighlight(node,word) {
     ind = node.nodeValue.toLowerCase().indexOf(word.toLowerCase())
-	if (ind != -1) {
+    if (ind != -1) {
         if (node.parentNode.className != "highlightedSearchTerm"){
             par = node.parentNode;
             contents = node.nodeValue;
-		
+        
             // make 3 shiny new nodes
             hiword = document.createElement("span");
-			hiword.className = "highlightedSearchTerm";
-			hiword.appendChild(document.createTextNode(contents.substr(ind,word.length)));
-			
+            hiword.className = "highlightedSearchTerm";
+            hiword.appendChild(document.createTextNode(contents.substr(ind,word.length)));
             par.insertBefore(document.createTextNode(contents.substr(0,ind)),node);
-			par.insertBefore(hiword,node);
-			par.insertBefore(document.createTextNode(contents.substr(ind+word.length)),node);
-
+            par.insertBefore(hiword,node);
+            par.insertBefore(document.createTextNode(contents.substr(ind+word.length)),node);
             par.removeChild(node);
-	        }
-    	} 
-	}  
-}
-
+        }
+    } 
+}  
 function highlightSearchTerm() {
     // search-term-highlighter function --  Geir Bækholt
     query = window.location.search
@@ -55,7 +51,7 @@ function highlightSearchTerm() {
             queries = query.replace(/\+/g,' ').split(/\s+/)            
             // make sure we start the right place so we don't higlight menuitems or breadcrumb
             contentarea = getContentArea();
-			for (q=0;q<queries.length;q++) {
+            for (q=0;q<queries.length;q++) {
                 // don't highlight reserved catalog search terms
                 if (queries[q].toLowerCase() != 'not'
                     && queries[q].toLowerCase() != 'and'
