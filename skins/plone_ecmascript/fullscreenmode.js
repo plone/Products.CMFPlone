1791a4d00d119a52a4ca2aa11e0c0615971b0a6c
Geir Baekholt <geir@baekholt.com>
2005-04-11 09:29:23 +0000

    workaround IE6-brokenness for fullscreenmode. Thanks a lot to Hanno Schlichting for fix
    svn path=/CMFPlone/branches/2.1/; revision=6541

--- skins/plone_ecmascript/fullscreenmode.js
+++ skins/plone_ecmascript/fullscreenmode.js
@@ -6,27 +6,27 @@ function setDisplayMode(item, state) {
     }
 }
 function fullscreenMode() {
-    // toggle the display prop of divs none/block    
-    // XXX FIXME : this one needs fixing before 2.1 final
-    // IE cannot handle dinsplay:table-cell properly
-    // Mozilla shifts margins if we do display:block
-    // - We need a browser-capabilties-branching here
-    if (document.getElementById('portal-column-one').style.display == 'none') {        
-        //setDisplayMode('portal-top', 'block');
-        setDisplayMode('portal-column-one', 'table-cell');
-        setDisplayMode('portal-column-two', 'table-cell');
-        if(!document.getElementById('portal-column-one').style.display){
+    // toggle the display prop of the columns    
+    if (document.getElementById('portal-column-one').style.display == 'none') {
+        try {
+            setDisplayMode('portal-column-one', 'table-cell');
+            setDisplayMode('portal-column-two', 'table-cell');
+        } catch (e) {
             // silly IE does not understand display:table-cell, so we must workaround
-            setDisplayMode('portal-column-one', 'block');
-            setDisplayMode('portal-column-two', 'block');
+            // alert(e);
+            if (e == "[object Error]") {
+                setDisplayMode('portal-column-one', 'block');
+                setDisplayMode('portal-column-two', 'block');
+            } else {
+                throw e;
+            }
         }
-        // set cookie        
-        createCookie('fullscreenMode', '');        
-    }else {    
-        //setDisplayMode('portal-top', 'none');
+        // set cookie
+        createCookie('fullscreenMode', '');
+    } else {
         setDisplayMode('portal-column-one', 'none');
-        setDisplayMode('portal-column-two', 'none');        
-        createCookie('fullscreenMode', '1');    
+        setDisplayMode('portal-column-two', 'none');
+        createCookie('fullscreenMode', '1');
     }
 }
 
