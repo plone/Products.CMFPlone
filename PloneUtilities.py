f11b5f8ed5e5b81c8acecbde86b1ba65b70a7ffe
Stefan H. Holek <stefan@epy.co.at>
2005-04-18 07:55:43 +0000

    Stacklevel 2 makes for a much more useful warning message.
    svn path=/CMFPlone/branches/2.1/; revision=6616

--- PloneUtilities.py
+++ PloneUtilities.py
@@ -3,4 +3,4 @@ from Products.CMFPlone.utils import _createObjectByType
 
 from warnings import warn
 warn('PloneUtilities is deprecated, please import from '
-     'Products.CMFPlone.utils instead.', DeprecationWarning)
+     'Products.CMFPlone.utils instead.', DeprecationWarning, 2)
