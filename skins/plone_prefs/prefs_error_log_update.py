e27812a9d50480246662c060cdf30cb5abe67803
Rob Miller <ra@burningman.com>
2005-04-29 23:14:51 +0000

    Fixed http://members.plone.org/collector/3848/ - changed prefs_error_log_update.py to match the button submit values on prefs_error_log_form.
    svn path=/CMFPlone/branches/2.1/; revision=6741

--- skins/plone_prefs/prefs_error_log_update.py
+++ skins/plone_prefs/prefs_error_log_update.py
@@ -5,11 +5,11 @@ request=context.REQUEST
 membership_tool=context.portal_membership
 member=membership_tool.getAuthenticatedMember()
 
-if (request['submit']==" I'm up to date "):
-    member.setProperties(error_log_update=DateTime().timeTime())
-    return request.RESPONSE.redirect(context.absolute_url() +'/prefs_error_log_form?portal_status_message=%s' % ('Data+Updated'))
-elif (request['submit']==" Show all entries "):
+if (request['submit']=="Show all entries"):
     member.setProperties(error_log_update=0.0)
     return request.RESPONSE.redirect(context.absolute_url() +'/prefs_error_log_form?portal_status_message=%s' % ('Showing+all+entries'))
+elif (request['submit']=="Clear Displayed Entries"):
+    member.setProperties(error_log_update=DateTime().timeTime())
+    return request.RESPONSE.redirect(context.absolute_url() +'/prefs_error_log_form?portal_status_message=%s' % ('Entries+cleared'))
 else:
     return request.RESPONSE.redirect(context.absolute_url() +'/prefs_error_log_form')
