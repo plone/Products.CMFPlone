2e307f4b38cea1ad34d93a36176ce9f5772611f0
Alec Mitchell <apm13@columbia.edu>
2005-05-26 19:24:04 +0000

    Attempt to optimize listTypeInfo.
    svn path=/CMFPlone/branches/2.1/; revision=7007

--- TypesTool.py
+++ TypesTool.py
@@ -1,4 +1,6 @@
 from Products.CMFCore.TypesTool import TypesTool as BaseTool
+from Products.CMFCore.interfaces.portal_types import ContentTypeInformation
+from Products.CMFCore.CMFCorePermissions import AccessContentsInformation
 from Products.CMFPlone import ToolNames
 from AccessControl import ClassSecurityInfo
 from Globals import InitializeClass
@@ -12,6 +14,7 @@ class TypesTool(PloneBaseTool, BaseTool):
     
     __implements__ = (PloneBaseTool.__implements__, BaseTool.__implements__, )
 
+    security.declarePublic('listTypeTitles')
     def listTypeTitles(self, container=None):
         """ Return a dictionary of id/Title combinations """
         typenames = {}
@@ -21,6 +24,23 @@ class TypesTool(PloneBaseTool, BaseTool):
                 typenames[ name ] = t.title_or_id()
 
         return typenames
+
+    security.declareProtected(AccessContentsInformation, 'listTypeInfo')
+    def listTypeInfo( self, container=None ):
+        """
+            Remove silly security and getId checks from the CMFCore method
+        """
+        rval = []
+        for t in self.objectValues():
+            # Filter out things that aren't TypeInformation and
+            # types for which the user does not have adequate permission.
+            if not ContentTypeInformation.isImplementedBy(t):
+                continue
+            if container is not None:
+                if not t.isConstructionAllowed(container):
+                    continue
+            rval.append(t)
+        return rval
 
 TypesTool.__doc__ = BaseTool.__doc__
 
