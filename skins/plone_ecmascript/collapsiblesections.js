b85c89c2058e319a5ee509ffa1227812cb8e353a
Neil Kandalgaonkar <neilk@enfoldsystems.com>
2005-06-13 18:08:37 +0000

    fix for #4035, collapsible fieldsets take up too much space
    svn path=/CMFPlone/branches/2.1/; revision=7132

--- skins/plone_ecmascript/collapsiblesections.js
+++ skins/plone_ecmascript/collapsiblesections.js
@@ -1,4 +1,3 @@
-
 function activateCollapsibles(){
     /*
      * a script that searches for sections that can be (or are already) collapsed
@@ -22,94 +21,115 @@ function activateCollapsibles(){
     // we only do fieldsets for now. perhaps DIVs later is it is needed...
     collapsibles = contentarea.getElementsByTagName('fieldset');
 
-    for (i=0; i < collapsibles.length; i++){
-        if (collapsibles[i].className.indexOf('collapsible')== -1 ){
-            continue
-            }
-        legends = collapsibles[i].getElementsByTagName('LEGEND')
+    for (i=0; i < collapsibles.length; i++) {
+        if (collapsibles[i].className.indexOf('collapsible')== -1 ) {
+            continue;
+        }
+        
+        fieldset = collapsibles[i]
+        legends = fieldset.getElementsByTagName('LEGEND')
         /*
          * get the legend
          * if there is no legend, we do not touch the fieldset at all.
          * we assume that if there is a legend, there is only one.
          * nothing else makes any sense
          */
-        if (! legends.length){continue}
+        if (! legends.length) {
+            continue;
+        }
         legend = legends[0]
 
         // add the icon/button with its functionality to the legend
         icon = document.createElement('img');
         icon.setAttribute('src','treeExpanded.gif')
         icon.setAttribute('class','collapseIcon')
-        icon.setAttribute('heigth','9')
+        icon.setAttribute('height','9')
         icon.setAttribute('width','9')
-        icon.style.marginRight='1em';
 
-        //set up the legend to handle click-events
-        if (window.addEventListener) legend.addEventListener("click",toggleCollapsible,false);
-        else if (window.attachEvent) legend.attachEvent("onclick",toggleCollapsible);
-        legend.style.cursor = 'pointer';
-
-
+        
         // insert the icon icon at the start of the legend
         legend.insertBefore(icon,legend.firstChild)
+
         /*
-         * wrap the contents inside a div to make turning them on and off simpler.
-         * unless something very strange happens, this new div should always be the last childnode
-         * we'll give it a class to make sure.
+         * create a DOM fragment to represent the collapsed fieldset
+         * it's a div + span, very similar to fieldset + legend, but I couldn't
+         * make fieldset collapse to a single line no matter what I tried. -- NeilK.
          */
-        hiderWrapper = document.createElement('div');
-        hiderWrapper.setAttribute('class','collapseWrapper')
-
-        // also add a new div describing that the element is collapsed.
-        collapsedDescription = document.createElement('div');
-        collapsedDescription.setAttribute('class','collapsedText')
-        collapsedDescription.appendChild(document.createTextNode('Section is collapsed, click the [+] to expand'))
-        collapsedDescription.style.display = 'none'
-
-        // if the fieldset has the class of "collapsed", pre-collapse it. This can be used to preserve valuable UI-space
-        if (collapsibles[i].className.indexOf('collapsed')!= -1 ){
-            icon.setAttribute('src','treeCollapsed.gif')
-            collapsedDescription.style.display = 'block'
-            hiderWrapper.style.display = 'none';
-            }
-
-        // now we have the wrapper div.. Stuff all the contents inside it
-        nl = collapsibles[i].childNodes.length
-        for (j=0; j < nl; j++){
-            node = collapsibles[i].childNodes[0]
-            if ( node.nodeName == 'LEGEND'){
-                if (collapsibles[i].childNodes.length > 1 ){
-                    hiderWrapper.appendChild(collapsibles[i].childNodes[1])
-                    }
-            }else{
-                hiderWrapper.appendChild(collapsibles[i].childNodes[0])
-                }
-            }
-        // and add it to the document
-        collapsibles[i].appendChild(hiderWrapper)
-        collapsibles[i].insertBefore(collapsedDescription, hiderWrapper)
+        
+        cFieldset = document.createElement('div');
+        cFieldset.setAttribute('class','collapsedFieldset');
+        
+        cLegend = document.createElement('span');
+        cLegend.setAttribute('class', 'collapsedLegend');
+        var legendChildren = legend.childNodes;
+        for (i = 0; i < legendChildren.length; i++) {
+            elm = legendChildren[i].cloneNode(true);
+            if (i == 0 && i.nodeName == 'IMG') { // it's the icon
+                elm.setAttribute('src','treeCollapsed.gif');
+            }    
+            cLegend.appendChild(elm);
+        }
+        
+        cFieldset.appendChild(cLegend)
+     
+        new Collapsible();  // hack for some old browsers to wake it up.
+        c = new Collapsible(fieldset, cFieldset, legend, cLegend);
+        c.init();
     }
 }
+    
 registerPloneFunction(activateCollapsibles)
 
-function toggleCollapsible(e){
-    // this is the function that collapses/expands fieldsets.
-    var node = window.event ? window.event.srcElement : e.currentTarget;
-    // node should be the legend, but this can change later on.
-    if (node.nodeName == 'IMG'){
-        node = node.parentNode
-        }
-    var icon = node.firstChild
-    var legend = node
+function addHandler(target,eventName,obj,handlerName) { 
+    fn = function(e){obj[handlerName](e)};
+    if ( window.addEventListener ) { 
+         target.addEventListener(eventName, fn, false);
+    } else if ( window.attachEvent ) { 
+         target.attachEvent("on" + eventName, fn);
+    } 
+}
 
-    if( icon.getAttribute('src').indexOf('treeCollapsed.gif')!= -1 ){
-        // that was an ugly check, but IE rewrites image sources to absolute urls from some sick reason....
-        icon.setAttribute('src','treeExpanded.gif')
-        legend.parentNode.lastChild.style.display = 'block'
-        legend.parentNode.childNodes[1].style.display = 'none'
-    }else{
-        icon.setAttribute('src','treeCollapsed.gif')
-        legend.parentNode.lastChild.style.display = 'none'
-        legend.parentNode.childNodes[1].style.display = 'block'
-        }
+
+// defines two nodes that are siblings, clicking on one hides itself
+// and reveals the other.
+function Collapsible(expanded, collapsed, collapseButton, expandButton) {
+    this.expanded = expanded;
+    this.collapsed = collapsed;
+    this.expandButton = expandButton;
+    this.collapseButton = collapseButton;
+}
+
+
+
+Collapsible.prototype.init = function() {
+    
+    // add handlers to collapse/expand. 
+    addHandler(this.collapseButton, "click", this, "collapse")
+    addHandler(this.expandButton, "click", this, "expand")
+    
+//    addListener(this.collapseButton, closure(this.collapse) );
+//  addListener(this.expandButton, closure(this.expand) );
+    
+    /* by default, show expanded, 
+     * unless expanded class name also has 
+     * 'collapsed' in it, i.e. <fieldset class="collapsible collapsed">
+     */
+    if (this.expanded.className.indexOf('collapsed') == -1 ) {
+        this.collapse();
+    } else {
+        this.expand();
     }
++
+    // assumption: expanded is already in document, has parent node.
+    this.expanded.parentNode.insertBefore( this.collapsed, this.expanded );
+}
+
+Collapsible.prototype.collapse = function() {
+    this.expanded.style.display = 'none';
+    this.collapsed.style.display = 'block';
+}    
+
+Collapsible.prototype.expand = function() {
+    this.expanded.style.display = 'block';
+    this.collapsed.style.display = 'none';
+}    
