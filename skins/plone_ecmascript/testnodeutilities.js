17faaf4d973f8a999a6d2714fec22fff5e8f44e7
Florian Schulze <florian.schulze@gmx.net>
2005-06-14 17:38:46 +0000

    Better usage of TestCase functionality.
    svn path=/CMFPlone/branches/fschulze-dropdown-refactoring/; revision=7149

--- skins/plone_ecmascript/testnodeutilities.js
+++ skins/plone_ecmascript/testnodeutilities.js
@@ -2,29 +2,50 @@
 function WrapNodeTestCase(){
     this.name = 'WrapNodeTestCase';
 
-    this.testSimple = function(){
-        var sandbox = document.getElementById("testSandbox");
+    this.setUp = function() {
+        this.sandbox = document.getElementById("testSandbox");
+
         var node = document.createElement("span");
-        
         node.appendChild(document.createTextNode("Hallo, Welt!"));
-        sandbox.appendChild(node);
-        // check preconditions
+        this.sandbox.appendChild(node);
+    }
+
+    this.testPreconditions = function() {
+        var node = this.sandbox.firstChild;
         this.assert(node);
         this.assertEquals(node.tagName, 'SPAN');
         this.assert(!node.className);
-        // do it
+    }
+
+    this.testPostconditions = function() {
+        var node = this.sandbox.firstChild;
         wrapNode(node, 'div', 'foo');
-        // test node itself again
         this.assert(node);
         this.assertEquals(node.tagName, 'SPAN');
-        this.assert(!node.className);
-        // test the wrapper
-        wrapper = sandbox.firstChild;
+        this.assertFalse(node.className);
+    }
+
+    this.testWrapper = function() {
+        var node = this.sandbox.firstChild;
+        wrapNode(node, 'div', 'foo');
+        wrapper = this.sandbox.firstChild;
         this.assert(wrapper);
         this.assertEquals(wrapper.tagName, 'DIV');
         this.assertEquals(wrapper.className, 'foo');
+    }
 
-        clearChildNodes(sandbox);
+    this.testWrapped = function() {
+        var node = this.sandbox.firstChild;
+        wrapNode(node, 'div', 'foo');
+        wrapper = this.sandbox.firstChild;
+        wrapped = wrapper.firstChild;
+        this.assert(wrapped);
+        this.assertEquals(wrapped.tagName, 'SPAN');
+        this.assertFalse(wrapped.className);
+    }
+
+    this.tearDown = function() {
+        clearChildNodes(this.sandbox);
     }
 }
 WrapNodeTestCase.prototype = new TestCase;
