8d94ecf8fe34dad55f312aa8a86375735ee7a733
Geoff Davis <geoff@geoffdavis.net>
2005-06-20 01:17:40 +0000

    Moved require_login to plone_login, got rid of all redirects in the login sequence to keep vinsci happy.  Added border=None to metadata files and removed top_slot border disabling.
    svn path=/CMFPlone/branches/2.1/; revision=7217

---
+++ skins/plone_login/require_login.py
@@ -0,0 +1,26 @@
+## Script (Python) "require_login"
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
+##parameters=
+##title=Login
+##
+
+# needed for migration
+if context.portal_migration.isPloneOne():
+    login = 'failsafe_login'
+else:
+    login = 'login_form'
+
+portal = context.portal_url.getPortalObject()
+# if cookie crumbler did a traverse instead of a redirect,
+# this would be the way to get the value of came_from
+#url = portal.getCurrentUrl()
+#context.REQUEST.set('came_from', url)
+
+if context.portal_membership.isAnonymousUser():
+    return portal.restrictedTraverse(login)()
+else:
+    return portal.restrictedTraverse('insufficient_privileges')()
