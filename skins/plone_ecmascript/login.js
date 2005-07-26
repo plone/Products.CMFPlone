cc9b04560762f2917717fc143c235730cccf076c
Geoff Davis <geoff@geoffdavis.net>
2005-07-26 15:31:18 +0000

    * Made login_form validation more robust to screwy stuff in request * Made cookie tests lazy -- they now store their values in an input   field so they won't be called more than once per page
    svn path=/CMFPlone/branches/2.1/; revision=7628

--- skins/plone_ecmascript/login.js
+++ skins/plone_ecmascript/login.js
@@ -3,20 +3,38 @@
 function cookiesEnabled() {
   // Test whether cookies are enabled by attempting to set a cookie and then change its value
   // set test cookie
-  var c = "cookieTest=0";
+  var c = "areYourCookiesEnabled=0";
   document.cookie = c;
   var dc = document.cookie;
   // cookie not set?  fail
   if (dc.indexOf(c) == -1) return 0;
   // change test cookie
-  c = "cookieTest=1";
+  c = "areYourCookiesEnabled=1";
   document.cookie = c;
   dc = document.cookie;
   // cookie not changed?  fail
   if (dc.indexOf(c) == -1) return 0;
   // delete cookie
-  document.cookie = "cookieTest=; expires=Thu, 01-Jan-70 00:00:01 GMT";
+  document.cookie = "areYourCookiesEnabled=; expires=Thu, 01-Jan-70 00:00:01 GMT";
   return 1;
+}
+
+function lazyCookiesEnabled(id) {
+  // Test to see if cookies are enabled and store the result in element with given id.
+  // First check the specified element; only if it is empty, run the test
+  if (id) {
+     el = document.getElementById(id);
+     if (el.value == '') {
+        el.value = cookiesEnabled()
+     }
+     if (el.value=='1') {
+        return 1;
+     } else {
+        return 0;
+     }
+  } else {
+    return cookiesEnabled()
+  }
 }
 
 function setLoginVars(user_name_id, alt_user_name_id, password_id, empty_password_id, js_enabled_id, cookies_enabled_id) {
@@ -26,7 +44,7 @@ function setLoginVars(user_name_id, alt_user_name_id, password_id, empty_passwor
     document.getElementById(js_enabled_id).value = 1;
   }
   if (cookies_enabled_id) {
-    document.getElementById(cookies_enabled_id).value = cookiesEnabled();
+    lazyCookiesEnabled(cookies_enabled_id);
   }
   if (user_name_id && alt_user_name_id) {
     user_name = document.getElementById(user_name_id)
@@ -49,11 +67,11 @@ function setLoginVars(user_name_id, alt_user_name_id, password_id, empty_passwor
   return 1;
 }
 
-function showCookieMessage(id) {
+function showCookieMessage(msg_id, storage_id) {
   // Show the element with the given id if cookies are not enabled
-  msg = document.getElementById('enable_cookies_message')
+  msg = document.getElementById(msg_id)
   if (msg) {
-     if (!cookiesEnabled()) {
+     if (!lazyCookiesEnabled(storage_id)) {
         msg.style.display = 'block';
      }
   }
@@ -61,7 +79,7 @@ function showCookieMessage(id) {
 
 function showEnableCookiesMessage() {
   // Show the element with the id 'enable_cookies_message' if cookies are not enabled
-  showCookieMessage('enable_cookies_message')
+  showCookieMessage('enable_cookies_message', null)
 }
 // Call showEnableCookiesMessage after the page loads
 registerPloneFunction(showEnableCookiesMessage);
