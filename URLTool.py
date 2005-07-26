b463162105c4470feb70ddbd5fb20f548fa91257
Alec Mitchell <apm13@columbia.edu>
2005-07-26 03:35:14 +0000

    Fixed http://members.plone.org/collector/4335 added a method to URLTool to determine whther a URL is local to the portal.  This will be moved into CMFCore ASAP, pending write access.
    svn path=/CMFPlone/branches/2.1/; revision=7619

--- URLTool.py
+++ URLTool.py
@@ -4,6 +4,8 @@ from AccessControl import ClassSecurityInfo
 from Globals import InitializeClass
 from Products.CMFPlone.PloneBaseTool import PloneBaseTool
 
+from urlparse import urlparse
+
 class URLTool(PloneBaseTool, BaseTool):
 
     meta_type = ToolNames.URLTool
@@ -11,6 +13,18 @@ class URLTool(PloneBaseTool, BaseTool):
     toolicon = 'skins/plone_images/link_icon.gif'
     
     __implements__ = (PloneBaseTool.__implements__, BaseTool.__implements__, )
+
+    security.declarePublic('isURLInPortal')
+    def isURLInPortal(self, url):
+        """ Check if a given url is on the same host and contains the portal
+            path.  Used to ensure that login forms can determine relevant
+            referrers (i.e. in portal).
+        """
+        p_url = self()
+        p_host_path = urlparse(p_url)[1:3]
+        url_host_path = urlparse(url)[1:3]
+        return (p_host_path[0] == url_host_path[0] and
+                    url_host_path[1].startswith(p_host_path[1]))
 
 URLTool.__doc__ = BaseTool.__doc__
 
---
+++ tests/testURLTool.py
@@ -0,0 +1,42 @@
+#
+# Tests the workflow tool
+#
+
+import os, sys
+if __name__ == '__main__':
+    execfile(os.path.join(sys.path[0], 'framework.py'))
+
+from Testing import ZopeTestCase
+from Products.CMFPlone.tests import PloneTestCase
+
+
+
+class TestURLTool(PloneTestCase.PloneTestCase):
+
+    def afterSetUp(self):
+        self.url = self.portal.portal_url
+
+    def test_isURLInPortal(self):
+        url_tool = self.url
+        self.failUnless(url_tool.isURLInPortal(
+                                        'http://nohost/portal/foo'))
+        self.failUnless(url_tool.isURLInPortal(
+                                        'http://nohost/portal'))
+        self.failIf(url_tool.isURLInPortal(
+                                        'http://nohost2/portal/foo'))
+        self.failUnless(url_tool.isURLInPortal(
+                                        'https://nohost/portal/bar'))
+        self.failIf(url_tool.isURLInPortal(
+                                   'http://nohost:8080/portal/baz'))
+        self.failIf(url_tool.isURLInPortal(
+                                   'http://nohost/'))
+
+
+def test_suite():
+    from unittest import TestSuite, makeSuite
+    suite = TestSuite()
+    suite.addTest(makeSuite(TestURLTool))
+    return suite
+
+if __name__ == '__main__':
+    framework()
