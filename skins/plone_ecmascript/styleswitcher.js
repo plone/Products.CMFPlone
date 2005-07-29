3145d3e60d0ae08b9da1c08eae6970ad665eeff0
Florian Schulze <florian.schulze@gmx.net>
2005-07-29 10:56:07 +0000

    Fixed createCookie function when the days parameter is used. Made code style consistent.
    svn path=/CMFPlone/branches/2.1/; revision=7694

--- skins/plone_ecmascript/styleswitcher.js
+++ skins/plone_ecmascript/styleswitcher.js
@@ -5,22 +5,24 @@ function setActiveStyleSheet(title, reset) {
 
     var i, a, main;
     for(i=0; (a = document.getElementsByTagName("link")[i]); i++) {
-        if(a.getAttribute("rel").indexOf("style") != -1 && a.getAttribute("title")) {
+        if (a.getAttribute("rel").indexOf("style") != -1 && a.getAttribute("title")) {
             a.disabled = true;
-            if(a.getAttribute("title") == title) a.disabled = false;
+            if (a.getAttribute("title") == title) {
+                a.disabled = false;
+            }
         }
     }
     if (reset == 1) {
-    createCookie("wstyle", title, 365);
+        createCookie("wstyle", title, 365);
     }
-}
+};
 
 function setStyle() {
     var style = readCookie("wstyle");
     if (style != null) {
         setActiveStyleSheet(style, 0);
     }
-}
+};
 registerPloneFunction(setStyle);
 
 
