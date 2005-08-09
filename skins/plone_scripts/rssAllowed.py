4683c63ce29ae95bae1d760813a51c4324c8f701
Alec Mitchell <apm13@columbia.edu>
2005-08-09 02:08:43 +0000

    Only show search syndication when site syndication is enabled.
    svn path=/CMFPlone/branches/2.1/; revision=7844

--- skins/plone_scripts/rssAllowed.py
+++ skins/plone_scripts/rssAllowed.py
@@ -1,3 +1,12 @@
+## Script (Python) "rssAllowed"
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
+##parameters=site=False
+##title=RSS Allowed
+
 # this is a replacement for the old syndication allowed option
 # in this case we don't bother getting syndication on each folder
 # we just check globally
@@ -5,12 +14,14 @@ from Products.CMFCore.utils import getToolByName
 
 ps = getToolByName(context, "portal_syndication")
 
-
-# if you want the old behaviour, swap the two lines around...
-if not ps.isSyndicationAllowed(context):
-#if not ps.isSiteSyndicationAllowed():
+allowed = True
+if not site and not ps.isSyndicationAllowed(context):
+    allowed = False
+if site and not ps.isSiteSyndicationAllowed():
+    allowed = False
 # really we should be raising an HTTP error, something that
 # rss news readers would understand
+if not allowed:
     raise ValueError, "Site syndication via RSS feeds is not allowed. Ask the sites"\
     " system administrator to go to portal_syndication > Policies and enable syndication. Each folder"\
     " then needs to have syndication enabled."
