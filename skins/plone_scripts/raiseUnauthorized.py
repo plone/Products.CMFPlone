ab1fcfad33ca8ff571e1ad672e0d8e2cffbf029e
Rob Miller <ra@burningman.com>
2005-08-15 21:47:26 +0000

    * don't allow users w/o appropriate priveleges to get roles and groups information   by browsing to the users and/or groups preference pages
    svn path=/CMFPlone/branches/2.1/; revision=7937

---
+++ skins/plone_scripts/raiseUnauthorized.py
@@ -0,0 +1,11 @@
+## Script (Python) "raiseUnauthorized"
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
+##parameters=
+##title=
+##
+from AccessControl import Unauthorized
+raise Unauthorized
