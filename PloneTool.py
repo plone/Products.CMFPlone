c3efa9b1d5df6757e905258fbb013a4c230cbea9
Alec Mitchell <apm13@columbia.edu>
2005-10-07 23:15:18 +0000

    Refixed http://members.plone.org/collector/4639 by checking the presence of the property without acquisition and then using it only if it exists on the object, but allowing full acquisition if the property is there.
    svn path=/CMFPlone/branches/2.1/; revision=8155

--- PloneTool.py
+++ PloneTool.py
@@ -5,7 +5,7 @@ from types import TupleType, UnicodeType, StringType
 import urlparse
 
 from Products.CMFPlone.utils import safe_callable
-from Products.CMFPlone.utils import safe_hasattr
+from Products.CMFPlone.utils import base_hasattr
 from Products.CMFPlone.utils import log
 from Products.CMFPlone.utils import log_exc
 from Products.CMFPlone import transaction
@@ -1431,9 +1431,9 @@ class PloneTool(PloneBaseTool, UniqueObject, SimpleItem):
         of whether obj is a catalog brain or an object, but returning an
         empty title marker if the id is not set (i.e. it's auto-generated).
         """
-        if safe_hasattr(obj, 'aq_explicit'):
-            obj = obj.aq_explicit
-        title = getattr(obj, 'Title', None)
+        title = None
+        if base_hasattr(obj, 'Title'):
+            title = getattr(obj, 'Title', None)
         if safe_callable(title):
             title = title()
         if title:
