2f71d32a3e1af989c32c9c67c4964cbc9c725451
Alec Mitchell <apm13@columbia.edu>
2005-10-11 21:46:31 +0000

    Made the ExtendedPathIndex migration apply to all EPI from any ZCatalog instance in the portal root.  Should fix issues with migrating CMFMember instances.
    svn path=/CMFPlone/branches/2.1/; revision=8175

--- migrations/v2_1/final_two11.py
+++ migrations/v2_1/final_two11.py
@@ -15,17 +15,21 @@ def final_two11(portal):
 
 def reindexPathIndex(portal, out):
     """Rebuilds the path index."""
-    catalog = getToolByName(portal, 'portal_catalog', None)
-    if catalog is not None:
-        if ('path' in catalog.indexes() and
-           getattr(catalog.Indexes['path'], '_index_parents', None) is None):
-            # Reduce threshold for the reindex run
-            old_threshold = catalog.threshold
-            pg_threshold = getattr(catalog, 'pgthreshold', 0)
-            catalog.pgthreshold = 300
-            catalog.threshold = 2000
-            catalog.clearIndex('path')
-            catalog.manage_reindexIndex(ids=['path'])
-            catalog.threshold = old_threshold
-            catalog.pgthreshold = pg_threshold
-            out.append("Reindexed path index.")
+    from Products.ZCatalog.ZCatalog import ZCatalog
+    for catalog in portal.objectValues():
+        if not isinstance(catalog, ZCatalog):
+            # catalog is not really a catalog
+            continue
+        for name,index in catalog.Indexes.objectItems():
+            if (index.meta_type == "ExtendedPathIndex" and
+            getattr(catalog.Indexes['path'], '_index_parents', None) is None):
+                # Reduce threshold for the reindex run
+                old_threshold = catalog.threshold
+                pg_threshold = getattr(catalog, 'pgthreshold', 0)
+                catalog.pgthreshold = 300
+                catalog.threshold = 2000
+                catalog.clearIndex(name)
+                catalog.manage_reindexIndex(ids=[name])
+                catalog.threshold = old_threshold
+                catalog.pgthreshold = pg_threshold
+                out.append("Reindexed %s index in catalog %s."%(name, catalog.getId()))
