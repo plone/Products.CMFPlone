9fb06fb0860fa291324f440006a6814e8439ddba
panjunyong <panjunyong@localhost>
2005-10-11 10:54:43 +0000

    Plone's MemberData class is now actually used; in previous versions, it was always CMF class that was used.
    svn path=/CMFPlone/branches/2.1/; revision=8173

--- MemberDataTool.py
+++ MemberDataTool.py
@@ -170,36 +170,40 @@ class MemberDataTool(PloneBaseTool, BaseTool):
                     res.append(member)
         return res
 
-##    security.declarePrivate('wrapUser')
-##    def wrapUser(self, u):
-##        '''
-##        If possible, returns the Member object that corresponds
-##        to the given User object.
-##        We override this to ensure OUR MemberData class is used
-##        '''
-##        id = u.getId()
-##        members = self._members
-##        if not members.has_key(id):
-##            # Get a temporary member that might be
-##            # registered later via registerMemberData().
-##            temps = self._v_temps
-##            if temps is not None and temps.has_key(id):
-##                m = temps[id]
-##            else:
-##                base = aq_base(self)
-##                m = MemberData(base, id)
-##                if temps is None:
-##                    self._v_temps = {id:m}
-##                    if hasattr(self, 'REQUEST'):
-##                        # No REQUEST during tests.
-##                        self.REQUEST._hold(CleanupTemp(self))
-##                else:
-##                    temps[id] = m
-##        else:
-##            m = members[id]
-##        # Return a wrapper with self as containment and
-##        # the user as context.
-##        return m.__of__(self).__of__(u)
+    # This method is the same as CMFCore.MemberDataTool's. But don't comment it
+    # since the context is different. In the method, MemberData means Plone's 
+    # MemberData but not CMFCore's. Or, Plone will always create CMFCore's
+    # MemberData.
+    security.declarePrivate('wrapUser')
+    def wrapUser(self, u):
+        '''
+        If possible, returns the Member object that corresponds
+        to the given User object.
+        We override this to ensure OUR MemberData class is used
+        '''
+        id = u.getId()
+        members = self._members
+        if not members.has_key(id):
+            # Get a temporary member that might be
+            # registered later via registerMemberData().
+            temps = self._v_temps
+            if temps is not None and temps.has_key(id):
+                m = temps[id]
+            else:
+                base = aq_base(self)
+                m = MemberData(base, id)
+                if temps is None:
+                    self._v_temps = {id:m}
+                    if hasattr(self, 'REQUEST'):
+                        # No REQUEST during tests.
+                        self.REQUEST._hold(CleanupTemp(self))
+                else:
+                    temps[id] = m
+        else:
+            m = members[id]
+        # Return a wrapper with self as containment and
+        # the user as context.
+        return m.__of__(self).__of__(u)
 
 MemberDataTool.__doc__ = BaseTool.__doc__
 
