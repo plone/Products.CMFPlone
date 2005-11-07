8fb245904880f29c93aa50ff76b5cf7775ea5ec4
Michel Pelletier <michel@cignex.com>
2005-11-07 23:15:51 +0000

    test for plone global view
    svn path=/CMFPlone/branches/goldegg-phase-1/; revision=8399

---
+++ tests/testPloneView.py
@@ -0,0 +1,38 @@
+#
+# Test methods used to make ...
+#
+
+import os, sys
+if __name__ == '__main__':
+    execfile(os.path.join(sys.path[0], 'framework.py'))
+
+from zope.component import getView
+
+from Testing import ZopeTestCase
+from Products.CMFPlone.tests import PloneTestCase
+from Products.CMFPlone.tests import dummy
+
+from Products.CMFPlone.utils import _createObjectByType
+
+from Products.CMFPlone.URLTool import URLTool
+
+class TestNavigationView(PloneTestCase.PloneTestCase):
+    """Tests the ... """
+
+    def afterSetUp(self):
+        self.view = getView(self.portal, 'globals_view', self.app.REQUEST)
+
+    def testView(self):
+        view = self.view
+        assert isinstance(view.utool(), URLTool)
+        assert view.portal() is self.portal
+        assert view.portal_object() is self.portal
+
+def test_suite():
+    from unittest import TestSuite, makeSuite
+    suite = TestSuite()
+    suite.addTest(makeSuite(TestNavigationView))
+    return suite
+
+if __name__ == '__main__':
+    framework()
