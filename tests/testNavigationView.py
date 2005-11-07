33f08680f8b78ebc1eef68d63a99d8695f9346fe
Michel Pelletier <michel@cignex.com>
2005-11-07 21:29:29 +0000

    initial skeleton file for view component tests
    svn path=/CMFPlone/branches/goldegg-phase-1/; revision=8395

---
+++ tests/testNavigationView.py
@@ -0,0 +1,27 @@
+#
+# Test methods used to make ...
+#
+
+import os, sys
+if __name__ == '__main__':
+    execfile(os.path.join(sys.path[0], 'framework.py'))
+
+from Testing import ZopeTestCase
+from Products.CMFPlone.tests import PloneTestCase
+from Products.CMFPlone.tests import dummy
+
+from Products.CMFPlone.utils import _createObjectByType
+
+class TestNavigationView(PloneTestCase.PloneTestCase):
+    """Tests the ... """
+    pass
+
+
+def test_suite():
+    from unittest import TestSuite, makeSuite
+    suite = TestSuite()
+    suite.addTest(makeSuite(TestBrowserDefaultScripts))
+    return suite
+
+if __name__ == '__main__':
+    framework()
