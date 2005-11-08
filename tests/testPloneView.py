7ec8924f871cdcb651fdfc08f4518e42d975a3eb
Michel Pelletier <michel@cignex.com>
2005-11-08 00:44:33 +0000

    more plone view tests
    svn path=/CMFPlone/branches/goldegg-phase-1/; revision=8403

--- tests/testPloneView.py
+++ tests/testPloneView.py
@@ -15,20 +15,69 @@ from Products.CMFPlone.tests import dummy
 from Products.CMFPlone.utils import _createObjectByType
 
 from Products.CMFPlone.URLTool import URLTool
+from Products.CMFPlone.MembershipTool import MembershipTool
+from Products.CMFPlone.GroupsTool import GroupsTool
+from Products.CMFPlone.GroupDataTool import GroupDataTool
+from Products.CMFPlone.ActionsTool import ActionsTool
+from Products.CMFPlone.ActionIconsTool import ActionIconsTool
 
-class TestNavigationView(PloneTestCase.PloneTestCase):
+from Products.CMFCore.WorkflowTool import WorkflowTool
+
+from Products.CMFPlone.InterfaceTool import InterfaceTool
+from Products.CMFPlone.SyndicationTool import SyndicationTool
+
+class TestPloneView(PloneTestCase.PloneTestCase):
     """Tests the global plone view.  """
 
-    def testView(self):
-        view =  getView(self.portal, 'plone', self.app.REQUEST)
-        assert isinstance(view.utool(), URLTool)
-        assert view.portal() is self.portal
-        assert view.portal_object() is self.portal
+    def afterSetUp(self):
+        self.view = getView(self.portal, 'plone', self.app.REQUEST)
 
+    def testUTool(self):
+        assert isinstance(self.view.utool(), URLTool)
+
+    def testPortal(self):
+        assert self.view.portal() is self.portal
+        
+    def testPortalObject(self):
+        assert self.view.portal_object() is self.portal
+
+    def testPortalURL(self):
+        assert isinstance(self.view.portal_url(), type(''))
+
+    def testMTool(self):
+        assert isinstance(self.view.mtool(), MembershipTool)
+
+    def testGTool(self):
+        assert isinstance(self.view.gtool(), GroupsTool)
+
+    def testGDTool(self):
+        assert isinstance(self.view.gdtool(), GroupDataTool)
+
+    def testATool(self):
+        assert isinstance(self.view.atool(), ActionsTool)
+
+    def testAITool(self):
+        assert isinstance(self.view.aitool(), ActionIconsTool)
+
+    def testPUtils(self):
+        pass
+
+    def testWTool(self):
+        assert isinstance(self.view.wtool(), WorkflowTool)
+
+    def testIFaceTool(self):
+        assert isinstance(self.view.ifacetool(), InterfaceTool)
+
+    def testSynTool(self):
+        assert isinstance(self.view.syntool(), SyndicationTool)
+
+    def testPortalTitle(self):
+        pass
+    
 def test_suite():
     from unittest import TestSuite, makeSuite
     suite = TestSuite()
-    suite.addTest(makeSuite(TestNavigationView))
+    suite.addTest(makeSuite(TestPloneView))
     return suite
 
 if __name__ == '__main__':
