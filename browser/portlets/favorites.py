4951f98ff70f2b59d0ad06ddc65d98dce3377ff6
David Siedband <technique@oceanicsky.com>
2005-11-28 04:50:55 +0000

    fixing favorites view and testNewsPortlet
    svn path=/CMFPlone/branches/goldegg-phase-1/; revision=8495

--- browser/portlets/favorites.py
+++ browser/portlets/favorites.py
@@ -1,27 +1,12 @@
-from zope.component import getView
-from zope.interface import implements
-
-from Products.CMFPlone import utils
 from Products.CMFPlone.browser.interfaces import IFavoritesPortlet
+
+from zope.interface import implements
+from zope.component import getView
+from Products.Five import BrowserView
 
 class FavoritesPortlet(BrowserView):
     implements(IFavoritesPortlet)
 
-    def favorites(self):
-        context = utils.context(self)
-        g = getView(context, 'plone', self.request)
-        folder = getattr(portal_membership.getHomeFolder(), 'Favorites', None)
-        limit = 10
-        over_limit = len(favorites)>limit
-        if folder is not None
-            return self.folder.getFolderContents({'portal_type': 'Favorite'})[:limit] 
-                                                          
-    def all_favorites_link(self):
-        context = utils.context(self)
-        g = getView(context, 'plone', self.request)
-        portal = g.portal()
-        folder = getattr(portal_membership.getHomeFolder(), 'Favorites', None)
-        
-        if folder is not None
-            folder_url = folder.absolute_url()
-            return '%s/folder_listing' % folder_url
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
