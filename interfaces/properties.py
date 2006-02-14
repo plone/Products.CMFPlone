9c8f65f6a5359856c7eb7fdb7056f358e2bb4472
Rob Miller <ra@burningman.com>
2006-02-14 22:24:44 +0000

    * removing unused _marker variable declaration
    svn path=/CMFPlone/trunk/; revision=9122

--- interfaces/properties.py
+++ interfaces/properties.py
@@ -6,8 +6,6 @@ $Id: _tools.py 38762 2005-10-05 10:44:00Z yuppie $
 from zope.interface import Interface
 from zope.interface import Attribute
 
-_marker = object()
-
 #
 #   Site Properties tool interface
 #
