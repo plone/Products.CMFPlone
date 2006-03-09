023814132b38cab6a94f356001ce58f82cd24c45
Hanno Schlichting <hanno@hannosch.eu>
2006-03-09 14:05:38 +0000

    Fixed erroneous form actions in prefs_error_log_form. Made it work even if the button labels were translated.
    svn path=/CMFPlone/branches/2.1/; revision=9305

--- skins/plone_prefs/prefs_error_log_update.py
+++ skins/plone_prefs/prefs_error_log_update.py
@@ -5,10 +5,10 @@ request=context.REQUEST
 membership_tool=context.portal_membership
 member=membership_tool.getAuthenticatedMember()
 
-if (request['submit']=="Show all entries"):
+if hasattr(request, 'form.button.showall'):
     member.setProperties(error_log_update=0.0)
     return request.RESPONSE.redirect(context.absolute_url() +'/prefs_error_log_form?portal_status_message=%s' % ('Showing+all+entries'))
-elif (request['submit']=="Clear Displayed Entries"):
+elif hasattr(request, 'form.button.clear'):
     member.setProperties(error_log_update=DateTime().timeTime())
     return request.RESPONSE.redirect(context.absolute_url() +'/prefs_error_log_form?portal_status_message=%s' % ('Entries+cleared'))
 else:
