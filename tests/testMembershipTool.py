ff999329fe7b758b8d0fab6ddfd9d523b1841c7b
Stefan H. Holek <stefan@epy.co.at>
2006-04-10 18:22:19 +0000

    Checked in tests for https://dev.plone.org/plone/ticket/5432.
    svn path=/CMFPlone/branches/2.0/; revision=9504

--- tests/testMembershipTool.py
+++ tests/testMembershipTool.py
@@ -11,6 +11,7 @@ from Products.CMFPlone.tests import PloneTestCase
 from Products.CMFPlone.tests import dummy
 
 from AccessControl.User import nobody
+from AccessControl import Unauthorized
 from Acquisition import aq_base
 from DateTime import DateTime
 
@@ -460,6 +461,35 @@ class TestSearchForMembers(PloneTestCase.PloneTestCase):
         self.assertEqual(len(search(roles=['Member'], last_login_time=DateTime('2003-01-01'))), 1)
 
 
+class TestMethodProtection(PloneTestCase.PloneTestCase):
+    # MembershipTool is missing security declarations
+    # http://dev.plone.org/plone/ticket/5432
+
+    _unprotected = (
+        'changeMemberPortrait',
+        'deletePersonalPortrait',
+        'testCurrentPassword',
+    )
+
+    def afterSetUp(self):
+        self.membership = self.portal.portal_membership
+
+    def assertUnprotected(self, object, method):
+        self.logout()
+        object.restrictedTraverse(method)
+
+    def assertProtected(self, object, method):
+        self.logout()
+        self.assertRaises(Unauthorized, object.restrictedTraverse, method)
+
+    for method in _unprotected:
+        exec "def testUnprotected_%s(self):" \
+             "    self.assertProtected(self.membership, '%s')" % (method, method)
+
+        exec "def testMemberAccessible_%s(self):" \
+             "    self.membership.restrictedTraverse('%s')" % (method, method)
+
+
 def test_suite():
     from unittest import TestSuite, makeSuite
     suite = TestSuite()
@@ -467,6 +497,7 @@ def test_suite():
     suite.addTest(makeSuite(TestCreateMemberarea))
     suite.addTest(makeSuite(TestMemberareaSetup))
     suite.addTest(makeSuite(TestSearchForMembers))
+    suite.addTest(makeSuite(TestMethodProtection))
     return suite
 
 if __name__ == '__main__':
