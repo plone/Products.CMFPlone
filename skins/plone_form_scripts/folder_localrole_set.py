4dc2bd14cf329e5b217ed7482a87d9c6b6954c6c
Wichert Akkerman <wichert@wiggy.net>
2006-04-16 15:22:27 +0000

    Remove never used use_portrait handling from folder_localrole_set
    svn path=/CMFPlone/trunk/; revision=9544

--- skins/plone_form_scripts/folder_localrole_set.py
+++ skins/plone_form_scripts/folder_localrole_set.py
@@ -1,5 +1,5 @@
 ## Script (Python) "folder_localrole_set"
-##parameters=use_acquisition=0, use_portraits=0
+##parameters=use_acquisition=0
 ##title=set acquitision
 ##
 
@@ -12,14 +12,6 @@ if use_acquisition:
     msg1=_(u'Role Acquisition is now turned on.')
 else:
     msg1=_(u'Role Acquisition is now turned off.')
-
-if use_portraits:
-    msg2=_(u'Portraits turned on.')
-else:
-    msg2=_(u'Portraits turned off.')
-
-# set the portrait cookie
-context.REQUEST.RESPONSE.setCookie('usePortraits',use_portraits,path="/")
 
 transaction_note('Modified acquisition settings for folder %s at %s' % (context.title_or_id(), context.absolute_url()))
 context.plone_utils.addPortalMessage(msg1)
