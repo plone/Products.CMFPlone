7df6554965121c6a1b2c93827ffaef45093f7e8c
Alec Mitchell <apm13@columbia.edu>
2006-05-10 21:35:13 +0000

    The imagePatch is obsolete
    svn path=/CMFPlone/branches/2.5/; revision=9748

--- patches/__init__.py
+++ patches/__init__.py
@@ -1,7 +1,4 @@
 
-import imagePatch               # WAII and 508 we need more properties on image
-                                # objects
-
 import zserverPatch             # Identify Plone in HTTP Headers - netcraft
                                 # here we come!
 
