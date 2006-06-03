d8492121ae1b52228f833a18c1890f8a157f9b08
Alec Mitchell <apm13@columbia.edu>
2006-06-03 03:11:14 +0000

    Prepare RC1
    svn path=/CMFPlone/branches/2.5/; revision=9957

--- migrations/__init__.py
+++ migrations/__init__.py
@@ -172,7 +172,3 @@ def registerMigrations():
     MigrationTool.registerUpgradePath('2.5-beta2',
                                       '2.5-rc1',
                                       v2_5.rcs.beta2_rc1)
-
-    MigrationTool.registerUpgradePath('2.5-rc1',
-                                      '2.5.0 (SVN/UNRELEASED)',
-                                      null)
