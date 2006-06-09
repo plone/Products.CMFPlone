e4c2a817071cc0d6466afad175eb9f256af79bba
Hanno Schlichting <hanno@hannosch.eu>
2006-06-09 23:47:24 +0000

    Removed deprecated scripts from the plone_deprecated skins layer.
    svn path=/CMFPlone/trunk/; revision=10023

--- skins/plone_deprecated/clearCookie.py
+++
@@ -1,9 +0,0 @@
-## Script (Python) "clearCookie.py $Revision$"
-##parameters=
-##title=clear browser cookie
-##
-context.plone_log("The clearCookie script is deprecated and will be "
-                  "removed in plone 3.0.")
-REQUEST=context.REQUEST
-REQUEST.RESPONSE.expireCookie('folderfilter', path='/')
-REQUEST.RESPONSE.redirect( context.absolute_url() + '/folder_contents?portal_status_message=Filter+cleared.')
--- skins/plone_deprecated/unauthRedirect.py
+++
@@ -1,8 +0,0 @@
-## Script (Python) "unauthRedirect.py $Revision$"
-##parameters=
-##title=clear browser cookie
-##
-context.plone_log("The unauthRedirect script is deprecated and will be "
-                  "removed in plone 3.0.")
-REQUEST=context.REQUEST
-REQUEST.RESPONSE.redirect( context.absolute_url())
