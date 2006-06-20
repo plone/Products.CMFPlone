4255a777958d610dfb546e719bbfed322771d30e
Wichert Akkerman <wichert@wiggy.net>
2006-06-20 23:01:04 +0000

    Add tests for the new migration step, and fix the bugs it found
    svn path=/CMFPlone/trunk/; revision=10139

--- migrations/v3_0/alphas.py
+++ migrations/v3_0/alphas.py
@@ -15,9 +15,9 @@ def addInterfaceIndices(portal, out):
     """Add indexes for interfaces to the portal catalog.
     """
 
-    catalog=getToolByName(self, 'portal_catalog')
-    indeces=catalog.indexes()
-    for index in [ 'object_iplements', 'object_adapts_to' ]:
+    catalog=getToolByName(portal, 'portal_catalog')
+    indices=catalog.indexes()
+    for index in [ 'object_implements', 'object_adapts_to' ]:
         if index not in indices:
             catalog.addIndex(index, 'KeywordIndex')
             out.append('Added %s index to portal_catalog' % index)
