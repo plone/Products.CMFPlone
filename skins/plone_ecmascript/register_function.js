f38e64aca8e54cceabc66565d6e728d000e4d8fe
Florian Schulze <florian.schulze@gmx.net>
2006-07-05 22:15:54 +0000

    Use Dean Edwards' addEvent function when available. Use domload instead of load for registerPloneFunction when available.
    svn path=/CMFPlone/trunk/; revision=10199

--- skins/plone_ecmascript/register_function.js
+++ skins/plone_ecmascript/register_function.js
@@ -16,39 +16,63 @@ var W3CDOM = (!bugRiddenCrashPronePieceOfJunk &&
                document.createElement);
 
 // cross browser function for registering event handlers
-function registerEventListener(elem, event, func) {
-    if (elem.addEventListener) {
+if (typeof addEvent != 'undefined') {
+    // use Dean Edwards' function if available
+    function registerEventListener(elem, event, func) {
+        addEvent(elem, event, func);
+        return true;
+    }
+} else if (window.addEventListener) {
+    function registerEventListener(elem, event, func) {
         elem.addEventListener(event, func, false);
         return true;
-    } else if (elem.attachEvent) {
+    }
+} else if (window.attachEvent) {
+    function registerEventListener(elem, event, func) {
         var result = elem.attachEvent("on"+event, func);
         return result;
     }
-    // maybe we could implement something with an array
-    return false;
+} else {
+    function registerEventListener(elem, event, func) {
+        // maybe we could implement something with an array
+        return false;
+    }
 }
 
 // cross browser function for unregistering event handlers
-function unRegisterEventListener(elem, event, func) {
-    if (elem.removeEventListener) {
+if (typeof removeEvent != 'undefined') {
+    // use Dean Edwards' function if available
+    function unRegisterEventListener(elem, event, func) {
+        removeEvent(element, event, func);
+        return true;
+    }
+} else if (window.removeEventListener) {
+    function unRegisterEventListener(elem, event, func) {
         elem.removeEventListener(event, func, false);
         return true;
-    } else if (elem.detachEvent) {
+    }
+} else if (window.detachEvent) {
+    function unRegisterEventListener(elem, event, func) {
         var result = elem.detachEvent("on"+event, func);
         return result;
     }
-    // maybe we could implement something with an array
-    return false;
+} else {
+    function unRegisterEventListener(elem, event, func) {
+        // maybe we could implement something with an array
+        return false;
+    }
 }
 
-function registerPloneFunction(func) {
-    // registers a function to fire onload.
-    registerEventListener(window, "load", func);
-}
-
-function unRegisterPloneFunction(func) {
-    // unregisters a function so it does not fire onload.
-    unRegisterEventListener(window, "load", func);
+if (typeof addDOMLoadEvent != 'undefined') {
+    function registerPloneFunction(func) {
+        // registers a function to fire onload.
+        registerEventListener(window, "domload", func);
+    }
+} else {
+    function registerPloneFunction(func) {
+        // registers a function to fire onload.
+        registerEventListener(window, "load", func);
+    }
 }
 
 function getContentArea() {
