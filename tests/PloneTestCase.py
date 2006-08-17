2a77f1712ac928d738a055c4fb85eb4f3f7d7ffa
Hanno Schlichting <hanno@hannosch.eu>
2006-08-17 00:17:15 +0000

    Made the usage of the PTS mixed encoding/unicode hack explicit. So far it had been pulled in by various tests implicitly. As we need it anyways, we can simply make it obvious as well. This fixes weird test failures when testCalendarView was run standalone.
    svn path=/CMFPlone/branches/2.5/; revision=10457

--- tests/PloneTestCase.py
+++ tests/PloneTestCase.py
@@ -8,6 +8,10 @@ from Products.PloneTestCase.ptc import *
 
 setupPloneSite()
 
+# BBB once we don't have to deal with non-unicode anymore
+# For the transition phase of mixed Unicode and encoded string usage applying
+# this patch is mandatory or we will see endless UnicodeDecodeErrors
+from Products.PlacelessTranslationService import PatchStringIO
 
 class PloneTestCase(PloneTestCase):
     """This is a stub now, but in case you want to try
