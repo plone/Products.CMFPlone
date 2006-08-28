cbf47e918a0b0ee92358cbaaeeb45477660834a1
Whit Morriss <whit@openplans.org>
2006-08-28 04:31:55 +0000

    adding zcml layer to tests that indirectly depend on PloneTestCase
    svn path=/CMFPlone/branches/2.5/; revision=10667

--- exportimport/tests/base.py
+++ exportimport/tests/base.py
@@ -1,11 +1,14 @@
 
 from Products.GenericSetup.testing import BodyAdapterTestCase
 from Products.GenericSetup.testing import NodeAdapterTestCase
+from Products.PloneTestCase.layer import ZCMLLayer
 
 
 # Disable PlacelessSetup as PTC does not like it
 
 class BodyAdapterTestCase(BodyAdapterTestCase):
+
+    layer = ZCMLLayer
 
     def setUp(self):
         pass
@@ -15,6 +18,8 @@ class BodyAdapterTestCase(BodyAdapterTestCase):
 
 
 class NodeAdapterTestCase(NodeAdapterTestCase):
+
+    layer = ZCMLLayer
 
     def setUp(self):
         pass
