f2622ecffe42778c696ab03a4d3d4af8e288ffcc
Stefan H. Holek <stefan@epy.co.at>
2006-09-02 16:35:30 +0000

    Merged r10752:10754 from 2.1 branch.
    svn path=/CMFPlone/branches/2.5/; revision=10755

--- tests/test_doctests.py
+++ tests/test_doctests.py
@@ -8,12 +8,10 @@ if __name__ == '__main__':
     execfile(os.path.join(sys.path[0], 'framework.py'))
 
 from unittest import TestSuite
-from zope.testing.doctestunit import DocTestSuite
-from Testing.ZopeTestCase import FunctionalDocFileSuite
 from Testing.ZopeTestCase import ZopeDocTestSuite
 from Products.CMFPlone.tests import PloneTestCase
 from Products.PloneTestCase.layer import ZCMLLayer
-import Products.PloneTestCase.setup as setup
+from Products.PloneTestCase import setup
 
 
 def test_suite():
@@ -26,7 +24,7 @@ def test_suite():
                          test_class=PloneTestCase.FunctionalTestCase),
         ZopeDocTestSuite('Products.CMFPlone.CalendarTool',
                          test_class=PloneTestCase.FunctionalTestCase),
-        DocTestSuite('Products.CMFPlone.utils'),
+        ZopeDocTestSuite('Products.CMFPlone.utils'),
         )
 
     if setup.USELAYER:
