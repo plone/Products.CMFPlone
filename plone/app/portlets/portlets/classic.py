cf1cdc0f4d99bd68e59cabed82ef44d51a8b3db6
Geir Baekholt <geir@baekholt.com>
2006-09-02 22:37:41 +0000

    whitespace
    svn path=/plone.app.portlets/trunk/; revision=10765

--- plone/app/portlets/portlets/classic.py
+++ plone/app/portlets/portlets/classic.py
@@ -26,43 +26,43 @@ from Products.CMFPlone import PloneMessageFactory as _
 class IClassicPortlet(IPortletDataProvider):
     """A portlet which can render a classic Plone portlet macro
     """
-    
+
     template = schema.TextLine(title=_(u'Template'),
                                description=_(u'The template containing the portlet'),
                                required=True)
-                               
+
     macro = schema.TextLine(title=_(u'Macro'),
                                description=_(u'The macro containing the portlet. Leave blank if there is no macro.'),
                                default=u'portlet',
                                required=True)
-                               
+
 class ClassicPortletAssignment(Implicit, Contained):
     implements(IClassicPortlet, IPortletAssignment)
 
     def __init__(self, template=u'', macro=u''):
         self.template = template
         self.macro = macro
-        
+
     @property
     def title(self):
         return self.template
-        
+
     @property
     def available(self):
         return True
-        
+
     @property
     def data(self):
         return self
-        
+
     def __repr__(self):
         return '<ClassicPortlet rendering %s : %s>' % (self.template, self.macro,)
 
 class ClassicPortletRenderer(Explicit):
     implements(IPortletRenderer)
-    adapts(Interface, IBrowserRequest, IBrowserView, 
+    adapts(Interface, IBrowserRequest, IBrowserView,
             IPortletManager, IClassicPortlet)
-            
+
     def __init__(self, context, request, view, manager, data):
         self.context = context
         self.data = data
@@ -72,7 +72,7 @@ class ClassicPortletRenderer(Explicit):
 
     def use_macro(self):
         return bool(self.data.macro)
-        
+
     def path_expression(self):
         expr = 'context/%s' % self.data.template
         if self.use_macro():
