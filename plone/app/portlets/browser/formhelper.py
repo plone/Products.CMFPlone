04e26485261f0bea51205277ff89a30ada8fcfac
Martin Aspeli <optilude@gmail.com>
2006-09-10 22:32:30 +0000

    Make add forms return to where they really came from - thanks Jurgen Kartnaller
    svn path=/plone.app.portlets/trunk/; revision=10855

--- plone/app/portlets/browser/formhelper.py
+++ plone/app/portlets/browser/formhelper.py
@@ -5,6 +5,7 @@ import zope.lifecycleevent
 
 from Acquisition import aq_parent, aq_inner
 from Products.Five.formlib.formbase import AddFormBase, EditFormBase
+from Products.Five.browser.pagetemplatefile import ZopeTwoPageTemplateFile
 
 class AddForm(AddFormBase):
     """A base add form for portlets.
@@ -12,11 +13,21 @@ class AddForm(AddFormBase):
     Sub-classes should define create() and set the form_fields class variable.
     """
     
+    base_template = AddFormBase.template
+    template = ZopeTwoPageTemplateFile('templates/portlets-pageform.pt') 
+    
+    def referer(self):
+        return self.request.form.get('referer') or self.request.get('HTTP_REFERER', '')
+
     def nextURL(self):
-        addview = aq_parent(aq_inner(self.context))
-        context = aq_parent(aq_inner(addview))
-        url = str(getMultiAdapter((context, self.request), name=u"absolute_url"))
-        return url + '/@@manage-portlets'
+        referer = self.request.form.get('referer', None)
+        if referer is not None:
+            return referer
+        else:
+            addview = aq_parent(aq_inner(self.context))
+            context = aq_parent(aq_inner(addview))
+            url = str(getMultiAdapter((context, self.request), name=u"absolute_url"))
+            return url + '/@@manage-portlets'
     
     @form.action("Save")
     def handle_save_action(self, action, data):
@@ -27,17 +38,27 @@ class AddForm(AddFormBase):
         nextURL = self.nextURL()
         if nextURL:
             self.request.response.redirect(self.nextURL())
-            return ''
+        return ''
 
 class EditForm(EditFormBase):
     """An edit form for portlets.
     """
     
+    base_template = EditFormBase.template
+    template = ZopeTwoPageTemplateFile('templates/portlets-pageform.pt') 
+    
+    def referer(self):
+        return self.request.form.get('referer') or self.request.get('HTTP_REFERER', '')
+
     def nextURL(self):
-        portlet = aq_inner(self.context)
-        context = aq_parent(portlet)
-        url = str(getMultiAdapter((context, self.request), name=u"absolute_url"))
-        return url + '/@@manage-portlets'
+        referer = self.request.form.get('referer', None)
+        if referer is not None:
+            return referer
+        else:    
+            portlet = aq_inner(self.context)
+            context = aq_parent(portlet)
+            url = str(getMultiAdapter((context, self.request), name=u"absolute_url"))
+            return url + '/@@manage-portlets'
     
     @form.action("Save", condition=form.haveInputWidgets)
     def handle_save_action(self, action, data):
@@ -50,11 +71,11 @@ class EditForm(EditFormBase):
         nextURL = self.nextURL()
         if nextURL:
             self.request.response.redirect(self.nextURL())
-            return ''
+        return ''
             
     @form.action("Cancel", validator=lambda *args, **kwargs: None)
     def handle_cancel_action(self, action, data):
         nextURL = self.nextURL()
         if nextURL:
             self.request.response.redirect(self.nextURL())
-            return ''
+        return ''
