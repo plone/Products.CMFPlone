5b6856dabe0d3e6c443ce9aa914faca551dda201
Alec Mitchell <apm13@columbia.edu>
2006-09-12 16:40:11 +0000

    Make the branch aware of the rc
    svn path=/CMFPlone/branches/2.5/; revision=10888

--- migrations/__init__.py
+++ migrations/__init__.py
@@ -186,9 +186,9 @@ def registerMigrations():
                                       null)
 
     MigrationTool.registerUpgradePath('2.5',
-                                      '2.5.1',
+                                      '2.5.1-rc1',
                                       v2_5.final_two51.final_two51)
 
-    MigrationTool.registerUpgradePath('2.5.1',
-                                      '2.5.2 (SVN/UNRELEASED)',
+    MigrationTool.registerUpgradePath('2.5.1-rc1',
+                                      '2.5.1 (SVN/UNRELEASED)',
                                       null)
