600d2809068195604a0ff8e4f7d2567d606a51ca
Hanno Schlichting <hanno@hannosch.eu>
2006-09-25 21:52:12 +0000

    Fixed some Acquisition related problems in the actions migrations. Added ATCT tool migration step.
    svn path=/CMFPlone/branches/plip148-cmf21/; revision=10999

--- migrations/v3_0/alphas.py
+++ migrations/v3_0/alphas.py
@@ -2,6 +2,9 @@ from zope.app.component.interfaces import ISite
 from zope.component.globalregistry import base
 from zope.component.persistentregistry import PersistentComponents
 
+from Acquisition import aq_base
+
+from Products.ATContentTypes.migration.v1_2 import upgradeATCTTool
 from Products.CMFCore.ActionInformation import Action
 from Products.CMFCore.ActionInformation import ActionCategory
 from Products.CMFCore.utils import getToolByName
@@ -30,6 +33,10 @@ def three0_alpha1(portal):
     # description are returned as Messages
     updateFTII18NDomain(portal, out)
 
+    # The ATCT tool has lost all type migration functionality and quite some
+    # metadata and index information stored on it needs to be updated.
+    upgradeATCTTool(portal, out)
+
     return out
 
 
@@ -44,36 +51,47 @@ def enableZope3Site(portal, out):
         out.append('Made the portal a Zope3 site.')
 
 def migrateOldActions(portal, out):
-    new_providers = portal.portal_actions.listActionProviders()
+    special_providers = ['portal_controlpanel',
+                         'portal_types',
+                         'portal_workflow']
     # We don't need to operate on the providers that are still valid and
     # should ignore the control panel as well
     providers = [obj for obj in portal.objectValues()
-                     if hasattr(obj, '_actions') and obj.getId() not in
-                     new_providers and obj.getId() != 'portal_controlpanel']
+                     if hasattr(obj, '_actions') and
+                     obj.getId() not in special_providers]
     non_empty_providers = [p for p in providers if len(p._actions) > 0]
     for provider in non_empty_providers:
         for action in provider._actions:
             category = action.category
             # check if the category already exists, otherwise create it
-            new_category = getattr(portal.portal_actions, category, None)
+            new_category = getattr(aq_base(portal.portal_actions), category, None)
             if new_category is None:
-                portal.portal_actions[category] = ActionCategory(id=category)
+                portal.portal_actions._setObject(category, ActionCategory(id=category))
                 new_category = portal.portal_actions[category]
+
+            # Special handling for Expressions
+            url_expr = ''
+            if action.action:
+                url_expr = action.action.text
+            available_expr = ''
+            if action.condition:
+                available_expr = action.condition.text
 
             new_action = Action(action.id,
                 title=action.title,
                 description=action.description,
-                url_expr=action.action.text,
-                available_expr=action.condition.text,
+                url_expr=url_expr,
+                available_expr=available_expr,
                 permissions=action.permissions,
-                visible = action.visible
-            )
+                visible = action.visible)
+                
             # Only add an action if there isn't one with that name already
             if getattr(new_category, action.id, None) is None:
-                new_category[action.id] = new_action
+                new_category._setObject(action.id, new_action)
 
         # Remove old actions from migrated providers
         provider._actions = ()
+    out.append('Migrated old actions to new actions stored in portal_actions.')
 
 def addNewCSSFiles(portal, out):
     # add new css files to the portal_css registries
@@ -97,9 +115,11 @@ def updateActionsI18NDomain(portal, out):
     domainless_actions = [a for a in actions if not a.i18n_domain]
     for action in domainless_actions:
         action.i18n_domain = 'plone'
+    out.append('Updated actions i18n domain attribute.')
 
 def updateFTII18NDomain(portal, out):
     types = portal.portal_types.listTypeInfo()
     domainless_types = [fti for fti in types if not fti.i18n_domain]
     for fti in domainless_types:
         fti.i18n_domain = 'plone'
+    out.append('Updated type informations i18n domain attribute.')
