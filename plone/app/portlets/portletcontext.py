ce3433d37b384a49e427dfeae66839b928cea1ac
Geir Baekholt <geir@baekholt.com>
2006-10-20 22:07:37 +0000

    remove whitespace
    svn path=/plone.app.portlets/trunk/; revision=11198

--- plone/app/portlets/portletcontext.py
+++ plone/app/portlets/portletcontext.py
@@ -15,20 +15,20 @@ from plone.portlets.constants import CONTENT_TYPE_CATEGORY
 
 class ContentContext(object):
     """A portlet context for regular content items.
-    
+
     Note - we register this for Interface so that it can also work for
     tools and other non-content items. This may hijack the context in non-CMF
     contexts, but that is doubtfully going to be an issue.
     """
     implements(IPortletContext)
     adapts(Interface)
-    
+
     def __init__(self, context):
         self.context = context
-    
+
     def getParent(self):
         return aq_parent(aq_inner(self.context))
-    
+
     def globalPortletCategories(self, placeless=False):
         cats = []
         if not placeless:
@@ -41,14 +41,14 @@ class ContentContext(object):
         for g in self._getGroupIds():
             cats.append((GROUP_CATEGORY, g,))
         return cats
-    
+
     def _getUserId(self):
         membership = getToolByName(self.context, 'portal_membership', None)
-        
+
         member = membership.getAuthenticatedMember()
         if not member:
             return None
-        
+
         try:
             memberId = member.getUserId()
         except AttributeError:
@@ -59,29 +59,29 @@ class ContentContext(object):
 
         if not memberId:
             return None
-        
+
         return memberId
-                             
+
     def _getGroupIds(self):
         membership = getToolByName(self.context, 'portal_membership', None)
         if membership is None or membership.isAnonymousUser():
             return ()
-        
+
         member = membership.getAuthenticatedMember()
         if not member:
             return ()
-            
+
         groups = member.getGroups()
-        
+
         # Ensure we get the list of ids - getGroups() suffers some acquision
         # ambiguity - the Plone member-data version returns ids.
-        
+
         for group in groups:
             if type(group) not in StringTypes:
                 return ()
-                
+
         return sorted(groups)
-        
+
     def _getContentType(self):
         typeInfo = getattr(aq_base(self.context), 'getTypeInfo', None)
         if typeInfo is not None:
@@ -92,16 +92,16 @@ class ContentContext(object):
         if portal_type is not None:
             return portal_type
         return None
-    
+
 class PortalRootContext(ContentContext):
     """A portlet context for the site root.
     """
 
     implements(IPortletContext)
     adapts(ISiteRoot)
-    
+
     def __init__(self, context):
         self.context = context
-        
+
     def getParent(self):
         return None
