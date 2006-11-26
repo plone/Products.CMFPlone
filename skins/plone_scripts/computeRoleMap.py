2616c2078ef304fc30f6b035016cce717ca5025a
Hanno Schlichting <hanno@hannosch.eu>
2006-11-26 10:53:27 +0000

    Fixed the sharing page to handle groups which come from non-traditional PAS sources. This closes #5727.
    svn path=/CMFPlone/branches/2.1/; revision=11409

--- skins/plone_scripts/computeRoleMap.py
+++ skins/plone_scripts/computeRoleMap.py
@@ -15,7 +15,7 @@ for name, roles, type, id in acquired_roles:
     #We prefer the fullname
     if not id.startswith('group_'):
         member = mtool.getMemberInfo(name)
-        if not member['fullname'] == '':
+        if member is not None and not member['fullname'] == '':
             name = name + ' (' + member['fullname'] + ')'
     result1[id]={
        'id'            : id,
@@ -34,16 +34,16 @@ for name, roles, type, id in local_roles:
         #We prefer the fullname
         if not id.startswith('group_'):
             member = mtool.getMemberInfo(name)
-            if not member['fullname'] == '':
+            if member is not None and not member['fullname'] == '':
                 name = name + ' (' + member['fullname'] + ')'
-	result1[id]={
-	    'id'                : id,
-	    'name'              : name,
-	    'type'              : type,
-	    'global'            : [],
-	    'acquired'	        : [],
-	    'local'		: roles 
-	}
+        result1[id]={
+            'id'                : id,
+            'name'              : name,
+            'type'              : type,
+            'global'            : [],
+            'acquired'          : [],
+            'local'             : roles
+            }
 
 # TODO: process global roles
 
@@ -54,6 +54,4 @@ dec_users = [('Owner' not in a['local'], a['type'], a['name'], a) for a in resul
 dec_users.sort()
 result = [a[-1] for a in dec_users]
 return result
-
-
 
