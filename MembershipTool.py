f13a2abffa7dee5d8a1ffa6d1a74fc7f6127a92d
Hanno Schlichting <hanno@hannosch.eu>
2006-11-28 07:27:28 +0000

    Merged 2.1 branch r11409:11445 into the 2.5 branch
    svn path=/CMFPlone/branches/2.5/; revision=11446

--- MembershipTool.py
+++ MembershipTool.py
@@ -564,7 +564,7 @@ class MembershipTool(PloneBaseTool, BaseTool):
         if not self.isAnonymousUser():
             member = self.getAuthenticatedMember()
             login_time = member.getProperty('login_time', '2000/01/01')
-            if  str(login_time) == '2000/01/01':
+            if str(login_time) == '2000/01/01':
                 login_time = self.ZopeTime()
             member.setProperties(login_time=self.ZopeTime(),
                                  last_login_time=login_time)
