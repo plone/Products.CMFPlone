e82cd71e2678425ec1a35b3cf57001c01527efd3
Martin Aspeli <optilude@gmail.com>
2006-12-26 01:51:55 +0000

    Better resolver for interfaces - thanks Christoffer T. Olsen :)
    svn path=/plone.app.layout/trunk/; revision=11713

--- plone/app/layout/globals/interface.py
+++ plone/app/layout/globals/interface.py
@@ -1,4 +1,6 @@
 from zope.interface import Interface, implements
+from zope.dottedname.resolve import resolve
+
 from plone.memoize.view import memoize
 
 from Acquisition import aq_base
@@ -7,11 +9,7 @@ from Products.Five.browser import BrowserView
 from interfaces import IInterfaceInformation
 
 def resolveInterface(dotted_name):
-    parts = dotted_name.split('.')
-    m_name = '.'.join(parts[:-1])
-    k_name = parts[-1]
-    module = __import__(m_name, globals(), locals(), [k_name])
-    klass = getattr(module, k_name)
+    klass = resolve(dotted_name)
     if not issubclass(klass, Interface):
         raise ValueError, '%r is not a valid Interface.' % dotted_name
     return klass
