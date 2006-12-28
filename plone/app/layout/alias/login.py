44bcb23a78d22d824d657655e613afc0fbdc3061
Martin Aspeli <optilude@gmail.com>
2006-12-28 22:51:16 +0000

    Add a 'login' alias - you can now use /login, and you'll end up at the login_form via a redirect.
    svn path=/plone.app.layout/trunk/; revision=11763

---
+++ plone/app/layout/alias/login.py
@@ -0,0 +1,13 @@
+from zope.component import getMultiAdapter
+
+from Products.Five.browser import BrowserView
+
+class LoginAlias(BrowserView):
+    """Base class for views that perform a redirect
+    """
+    
+    def redirect(self):
+        url = self.request.get('HTTP_REFERER', None)
+        if not url:
+            url = self.context.absolute_url()
+        self.request.response.redirect('login_form?came_from=%s' % url)
