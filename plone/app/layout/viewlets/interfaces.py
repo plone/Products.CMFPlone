1068085d6aa51a8533ecf55dc22a108303fc174a
Florian Schulze <florian.schulze@gmx.net>
2007-01-07 14:28:07 +0000

    Added viewlet manager for link tags.
    svn path=/plone.app.layout/trunk/; revision=11913

--- plone/app/layout/viewlets/interfaces.py
+++ plone/app/layout/viewlets/interfaces.py
@@ -4,6 +4,12 @@ class IHtmlHead(IViewletManager):
     """A viewlet manager that sits in the <head> of the rendered page
     """
 
+class IHtmlHeadLinks(IViewletManager):
+    """ A viewlet manager that sits in the <head> and is responsible
+        for semantic navigation links, the favicon, rss links and more
+        provided by <link> tags.
+    """
+
 class IPortalHeader(IViewletManager):
     """A viewlet manager that sits at the very top of the rendered page
     """
