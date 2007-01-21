f5216218d3d2adfea46ac1c129b49bedc31d91df
Hanno Schlichting <hanno@hannosch.eu>
2007-01-21 00:11:31 +0000

    Merged 2.5 branch r11851:11994 into the trunk, merge-monkey finally retires :)
    svn path=/CMFPlone/trunk/; revision=11997

--- migrations/__init__.py
+++ migrations/__init__.py
@@ -204,10 +204,18 @@ def registerMigrations():
                                       v2_5.final_two51.final_two51)
 
     MigrationTool.registerUpgradePath('2.5.1',
-                                      '2.5.2 (SVN/UNRELEASED)',
+                                      '2.5.2-rc1',
                                       v2_5.two51_two52.two51_two52)
 
-    MigrationTool.registerUpgradePath('2.5.2 (SVN/UNRELEASED)',
+    MigrationTool.registerUpgradePath('2.5.2-rc1',
+                                      '2.5.2',
+                                      null)
+
+    MigrationTool.registerUpgradePath('2.5.2',
+                                      '2.5.3 (svn/unreleased)',
+                                      null)
+
+    MigrationTool.registerUpgradePath('2.5.3 (svn/unreleased)',
                                       '3.0-alpha1',
                                       v3_0.alphas.three0_alpha1)
 
