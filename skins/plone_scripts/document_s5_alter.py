c457105a1eeae84347899c5ad1bb084499169c8f
Andy McKay <andy@clearwind.ca>
2007-02-19 09:30:33 +0000

    Fix script
    svn path=/CMFPlone/branches/plone-s5/; revision=12660

--- skins/plone_scripts/document_s5_alter.py
+++ skins/plone_scripts/document_s5_alter.py
@@ -1,6 +1,21 @@
 ##parameters=test=False
 # ugly, ugly, ugly code, that basically changes the way the slide is put together
 # this should be a HTML parser or XSLT or even JS
+
+from Products.CMFPlone.utils import safe_hasattr
+
+msg = "For Plone to show this page as a presentation to work, it must find some header "\
+"tags (H1, h1, H2, h2) in your document. The document will be "\
+"split into slides based upon those tags. Structured text or Kupu "\
+"will all generate suitable tags for you. Further you must have the "\
+"'Enable Presentation' setting turned on in the user interface for this peice of content."
+
+if not safe_hasattr(context, "getPresentation") or not context.getPresentation():
+    if test:
+        return False
+    else:
+        raise ValueError, msg
+    
 body =  context.CookedBody()
 
 # this should be a regex too, but hey this works
@@ -12,10 +27,6 @@ for t in tags:
         break
 
 if tag is None:
-    msg = "For Plone to show this page as a presentation to work, it must find some header "\
-    "tags (H1, h1, H2, h2) in your document. The document will be "\
-    "split into slides based upon those tags. Structured text or Kupu "\
-    "will all generate suitable tags for you."
     if test:
         return False
     else:
@@ -35,6 +46,5 @@ body = body.split('<%s>' % tag)
 body = ('</div>\n<div class="slide">\n<%s>' % tag).join(body)
 body = body.split('</%s>' % tag)
 body = ('</%s>\n' % tag).join(body)
-body = '<div class="slide">' + body + '</div>'
 
 return body
