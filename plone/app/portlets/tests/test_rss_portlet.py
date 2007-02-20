e453aee8ce867d2bbcfd69fa1dcc031d20a36a2e
Christian Scholz <cs@comlounge.net>
2007-02-20 22:24:30 +0000

    added test for portlet creation etc.
    svn path=/plone.app.portlets/branches/mrtopf-6183-6182-deferred-portlets-and-rss-portlet/; revision=12913

---
+++ plone/app/portlets/tests/test_rss_portlet.py
@@ -0,0 +1,77 @@
+from zope.component import getUtility, getMultiAdapter
+from zope.app.component.hooks import setHooks, setSite
+
+from plone.portlets.interfaces import IPortletType
+from plone.portlets.interfaces import IPortletManager
+from plone.portlets.interfaces import IPortletAssignment
+from plone.portlets.interfaces import IPortletDataProvider
+from plone.portlets.interfaces import IPortletRenderer
+
+from plone.app.portlets.portlets import rss
+from plone.app.portlets.storage import PortletAssignmentMapping
+
+from plone.app.portlets.tests.base import PortletsTestCase
+
+class TestPortlet(PortletsTestCase):
+
+    def afterSetUp(self):
+        setHooks()
+        setSite(self.portal)
+
+    def testPortletTypeRegistered(self):
+        portlet = getUtility(IPortletType, name='portlets.rss')
+        self.assertEquals(portlet.addview, 'portlets.rss')
+
+    def testInterfaces(self):
+        portlet = rss.Assignment()
+        self.failUnless(IPortletAssignment.providedBy(portlet))
+        self.failUnless(IPortletDataProvider.providedBy(portlet.data))
+
+    def testInvokeAddview(self):
+        portlet = getUtility(IPortletType, name='portlets.rss')
+        mapping = PortletAssignmentMapping()
+        request = self.folder.REQUEST
+
+        adding = getMultiAdapter((mapping, request,), name='+')
+        addview = getMultiAdapter((adding, request), name=portlet.addview)
+
+        addview.createAndAdd(data={})
+
+        self.assertEquals(len(mapping), 1)
+        self.failUnless(isinstance(mapping.values()[0], rss.Assignment))
+
+    def testRenderer(self):
+        context = self.folder
+        request = self.folder.REQUEST
+        view = self.folder.restrictedTraverse('@@plone')
+        manager = getUtility(IPortletManager, name='plone.rightcolumn', context=self.portal)
+        assignment = rss.Assignment()
+
+        renderer = getMultiAdapter((context, request, view, manager, assignment), IPortletRenderer)
+        self.failUnless(isinstance(renderer, rss.Renderer))
+
+class TestRenderer(PortletsTestCase):
+
+    def afterSetUp(self):
+        setHooks()
+        setSite(self.portal)
+
+    def renderer(self, context=None, request=None, view=None, manager=None, assignment=None):
+        context = context or self.folder
+        request = request or self.folder.REQUEST
+        view = view or self.folder.restrictedTraverse('@@plone')
+        manager = manager or getUtility(IPortletManager, name='plone.rightcolumn', context=self.portal)
+        assignment = assignment or rss.Assignment()
+
+        return getMultiAdapter((context, request, view, manager, assignment), IPortletRenderer)
+
+    def test_rss_items(self):
+        r = self.renderer(assignment=rss.Assignment())
+        self.assertEquals(False, r.enabled)
+
+def test_suite():
+    from unittest import TestSuite, makeSuite
+    suite = TestSuite()
+    suite.addTest(makeSuite(TestPortlet))
+    suite.addTest(makeSuite(TestRenderer))
+    return suite
