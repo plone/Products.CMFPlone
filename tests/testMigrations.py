e6559787cbb63520d5da75b3e0387a2e51931db1
Christian Scholz <cs@comlounge.net>
2007-02-24 20:39:20 +0000

    In preparation of the merge of the Resource Registry with Caching support I changed the setup to enable debug mode of the registries before running migration tests as they are adding/removing resources a lot of times. With caching each compilation of the cache would then take several seconds slowing down tests tremendously.
    Also not that I do not reset the debug flag after the tests as the instance is deleted anyway and setting them just means that the cache get created again.
    
    
    svn path=/CMFPlone/trunk/; revision=13074

--- tests/testMigrations.py
+++ tests/testMigrations.py
@@ -120,6 +120,16 @@ class BogusMailHost(SimpleItem):
 
 class MigrationTest(PloneTestCase.PloneTestCase):
 
+    def afterSetUp(self):
+        """general test setup"""
+        # disable resource compiling as it takes a long time now
+        self.portal.portal_javascripts.setDebugMode(True)
+        self.portal.portal_css.setDebugMode(True)
+
+        # NOTE: Do not reset this in beforeTearDown as it will compile the
+        # resources then which takes quite a bit and makes tests slow
+        # there should be no need to do so anyway 
+
     def removeActionFromTool(self, action_id, category=None, action_provider='portal_actions'):
         # Removes an action from portal_actions
         tool = getattr(self.portal, action_provider)
@@ -215,6 +225,7 @@ class MigrationTest(PloneTestCase.PloneTestCase):
 class TestMigrations_v2_1_1(MigrationTest):
 
     def afterSetUp(self):
+        MigrationTest.afterSetUp(self)
         self.actions = self.portal.portal_actions
         self.icons = self.portal.portal_actionicons
         self.properties = self.portal.portal_properties
@@ -276,6 +287,7 @@ class TestMigrations_v2_1_1(MigrationTest):
 class TestMigrations_v2_1_2(MigrationTest):
 
     def afterSetUp(self):
+        MigrationTest.afterSetUp(self)
         self.actions = self.portal.portal_actions
         self.memberdata = self.portal.portal_memberdata
         self.skins = self.portal.portal_skins
@@ -560,6 +572,7 @@ class TestMigrations_v2_1_3(MigrationTest):
 class TestMigrations_v2_5(MigrationTest):
 
     def afterSetUp(self):
+        MigrationTest.afterSetUp(self)
         self.actions = self.portal.portal_actions
         self.memberdata = self.portal.portal_memberdata
         self.catalog = self.portal.portal_catalog
@@ -891,6 +904,7 @@ class TestMigrations_v2_5(MigrationTest):
 class TestMigrations_v2_5_1(MigrationTest):
 
     def afterSetUp(self):
+        MigrationTest.afterSetUp(self)
         self.actions = self.portal.portal_actions
         self.memberdata = self.portal.portal_memberdata
         self.catalog = self.portal.portal_catalog
@@ -1039,6 +1053,7 @@ class TestMigrations_v2_5_1(MigrationTest):
 class TestMigrations_v3_0_Actions(MigrationTest):
 
     def afterSetUp(self):
+        MigrationTest.afterSetUp(self)
         self.actions = self.portal.portal_actions
         self.types = self.portal.portal_types
         self.workflow = self.portal.portal_workflow
@@ -1130,6 +1145,7 @@ class TestMigrations_v3_0_Actions(MigrationTest):
 class TestMigrations_v3_0(MigrationTest):
 
     def afterSetUp(self):
+        MigrationTest.afterSetUp(self)
         self.actions = self.portal.portal_actions
         self.cp = getUtility(IControlPanel)
         self.icons = self.portal.portal_actionicons
