5765a1942ff00ac2fab07e543e8b7266e4a210ed
Hanno Schlichting <hanno@hannosch.eu>
2007-02-24 09:55:39 +0000

    Removed some old non-functional SpeedPack code.
    svn path=/CMFPlone/trunk/; revision=13027

--- SkinsTool.py
+++ SkinsTool.py
@@ -1,14 +1,9 @@
-from Products.CMFCore.SkinsTool import SkinsTool as BaseTool
-from Products.CMFPlone import ToolNames
 from AccessControl import ClassSecurityInfo
 from Globals import InitializeClass
+from Products.CMFCore.SkinsTool import SkinsTool as BaseTool
+from Products.CMFPlone import ToolNames
 from Products.CMFPlone.PloneBaseTool import PloneBaseTool
 
-_skincache = {}
-
-# __getSkinByName can be renamed getSkinByName to
-# override SkinsContainer.getSkinByName.  This
-# is about a 10% speed increase.
 
 class SkinsTool(PloneBaseTool, BaseTool):
 
@@ -21,18 +16,6 @@ class SkinsTool(PloneBaseTool, BaseTool):
 
     __implements__ = (PloneBaseTool.__implements__, BaseTool.__implements__, )
 
-    security.declarePrivate('getSkinByName')
-    def __getSkinByName(self, name):
-        path = self.getSkinPath(name)
-        if path is None:
-            return None
-        if path not in _skincache.keys():
-            _skincache[path]=None
-        if _skincache[path]:
-            return _skincache[path]
-        skinob=self.getSkinByPath(path)
-        _skincache[path]=skinob
-        return skinob
 
 SkinsTool.__doc__ = BaseTool.__doc__
 
