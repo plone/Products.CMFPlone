749d304fe10d2987e5b7b5e86f6e6a41304fc459
Christian Scholz <cs@comlounge.net>
2007-02-25 20:39:08 +0000

    exchanged webstats.js by a script which copies it's content from the site property webstats_js
    svn path=/CMFPlone/branches/mrtopf-6158-google-analytics-support/; revision=13170

---
+++ skins/plone_ecmascript/webstats.js.py
@@ -0,0 +1,17 @@
+## Script (Python) "webstats.js.py"
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
+##parameters=author,limit_per_type=5
+##title=Output the script in the webstats_js site property 
+##
+
+from Products.CMFCore.utils import getToolByName
+ptool=getToolByName(context, 'portal_properties')
+site_props = ptool.site_properties
+
+out = site_props.getProperty('webstats_js','')
+
+return out
