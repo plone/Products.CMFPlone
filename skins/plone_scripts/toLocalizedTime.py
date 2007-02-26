0a465a0d19d402d3623619ff90a9a1e8acf0bce0
Hanno Schlichting <hanno@hannosch.eu>
2007-02-26 07:26:58 +0000

    Undeprecated the toLocalizedTime script. We are probably going to change the approach to be based on Zope3 locales in the next release, so it doesn't make sense to force everybody to change their products now.
    svn path=/CMFPlone/trunk/; revision=13245

--- skins/plone_scripts/toLocalizedTime.py
+++ skins/plone_scripts/toLocalizedTime.py
@@ -10,8 +10,4 @@
 # The time parameter must be either a string that is suitable for
 # initializing a DateTime or a DateTime object.
 # Returns a localized string.
-context.plone_log("The toLocalizedTime script is deprecated and will be "
-                  "removed in plone 3.5.  Use the method of the @@plone view,"
-                  " or the method in the main_template globals.")
-
 return context.restrictedTraverse('@@plone').toLocalizedTime(time, long_format)
