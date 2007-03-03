7860718cb789a4d1c2834eedd6c3bce0f5e2cd5b
Hanno Schlichting <hanno@hannosch.eu>
2007-03-03 19:07:04 +0000

    Added version overview and debug-mode hint to the maintenance control panel.
    svn path=/plone.app.controlpanel/trunk/; revision=13418

--- plone/app/controlpanel/maintenance.py
+++ plone/app/controlpanel/maintenance.py
@@ -1,26 +1,25 @@
-import time
+from plone.fieldsets.form import FieldsetsEditForm
 from zope.component import adapts
+from zope.formlib import form
 from zope.interface import Interface
 from zope.interface import implements
 from zope.schema import Int
-from App.config import getConfiguration
 
-from zope.formlib import form
-
-from Products.Five.browser.pagetemplatefile import ZopeTwoPageTemplateFile
 from Products.CMFCore.utils import getToolByName
 from Products.CMFDefault.formlib.schema import SchemaAdapterBase
-from Products.CMFPlone.interfaces import IPloneSiteRoot
 from Products.CMFPlone import PloneMessageFactory as _
-from Products.CMFPlone.utils import safe_hasattr
-from plone.fieldsets.form import FieldsetsEditForm
+from Products.CMFPlone.interfaces import IPloneSiteRoot
+from Products.Five.browser.pagetemplatefile import ZopeTwoPageTemplateFile
+
 from plone.app.controlpanel.interfaces import IPloneControlPanelForm
 
 
 class IMaintenanceSchema(Interface):
 
-    days = Int(title=_(u'Number of days to keep'),
-        description=_(u'Select the number of days history to keep'),
+    days = Int(title=_(u"Number of days to keep"),
+        description=_(u"The Zope Database keeps deleted and previous versions "
+                       "of objects. Packing the database will actually delete "
+                       "these to a certain point in time and free diskspace."),
         default=7,
         required=True)
 
@@ -54,18 +53,13 @@ class MaintenanceControlPanel(FieldsetsEditForm):
 
     implements(IPloneControlPanelForm)
 
-    template = ZopeTwoPageTemplateFile('control-panel.pt')
+    template = ZopeTwoPageTemplateFile('maintenance.pt')
     form_fields = form.FormFields(IMaintenanceSchema)
-    label = _("Database maintenance")
-    description = _("""The Zope Database is a transactional database, which results in
-                       deleted items and previous versions of items being kept. Packing
-                       the database will actually delete the history to a certain point
-                       in time and free diskspace used.""")
-    form_name = _("Start packing")
+    label = _(u'Maintenance')
+    description = _(u"Zope server and site maintenance options.")
+    form_name = _(u'Start packing')
 
     @form.action(_(u'Pack'))
     def handle_edit_action(self, action, data):
         form.applyChanges(self.context, self.form_fields, data, self.adapters)
-        self.status = _("Packed the database.")
-    
-
+        self.status = _(u'Packed the database.')
