ae38c0cdcb143b0f1f6bc0f98f643b65eefdc6f9
Wichert Akkerman <wichert@wiggy.net>
2007-03-08 10:49:45 +0000

    More getToolByName-removal
    svn path=/plone.app.layout/trunk/; revision=13620

--- plone/app/layout/globals/tools.py
+++ plone/app/layout/globals/tools.py
@@ -6,11 +6,12 @@ from Acquisition import aq_inner
 from Products.Five.browser import BrowserView
 from Products.CMFCore.interfaces import IActionsTool
 from Products.CMFCore.interfaces import ICatalogTool
+from Products.CMFCore.interfaces import IMembershipTool
 from Products.CMFCore.interfaces import IPropertiesTool
+from Products.CMFCore.interfaces import ISyndicationtool
 from Products.CMFCore.interfaces import ITypesTool
 from Products.CMFCore.interfaces import IURLTool
 from Products.CMFCore.interfaces import IConfigurableWorkflowTool
-from Products.CMFCore.utils import getToolByName
 from Products.CMFPlone.interfaces import IInterfaceTool
 
 from interfaces import ITools
@@ -35,7 +36,7 @@ class Tools(BrowserView):
         
     @memoize_contextless
     def membership(self):
-        return getToolByName(aq_inner(self.context), 'portal_membership')
+        return getUtility(IMembershipTool)
         
     @memoize_contextless
     def properties(self):
@@ -43,7 +44,7 @@ class Tools(BrowserView):
 
     @memoize_contextless
     def syndication(self):
-        return getToolByName(aq_inner(self.context), 'portal_syndication')
+        return getUtility(ISyndicationtool)
         
     @memoize_contextless
     def url(self):
