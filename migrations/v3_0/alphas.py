2e1a7d85fe047ed37f6fc35ed67407ef53631f9b
Hanno Schlichting <hanno@hannosch.eu>
2007-03-09 23:07:41 +0000

    Registered mimetypes_registry as an utility
    svn path=/CMFPlone/trunk/; revision=13663

--- migrations/v3_0/alphas.py
+++ migrations/v3_0/alphas.py
@@ -61,6 +61,7 @@ from Products.DCWorkflow.DCWorkflow import DCWorkflowDefinition
 from Products.DCWorkflow.exportimport import WorkflowDefinitionConfigurator, _initDCWorkflow
 from Products.GenericSetup.interfaces import ISetupTool
 from Products.MailHost.interfaces import IMailHost
+from Products.MimetypesRegistry.interfaces import IMimetypesRegistryTool
 from Products.PloneLanguageTool.interfaces import ILanguageTool
 from Products.ResourceRegistries.interfaces import ICSSRegistry
 from Products.ResourceRegistries.interfaces import IJSRegistry
@@ -680,6 +681,7 @@ def registerToolsAsUtilities(portal, out):
                     (portal.portal_uidhandler, IUniqueIdHandler),
                     (portal.portal_setup, ISetupTool),
                     (portal.MailHost, IMailHost),
+                    (portal.mimetypes_registry, IMimetypesRegistryTool),
                     (portal.portal_languages, ILanguageTool),
                     (portal.portal_css, ICSSRegistry),
                     (portal.portal_javascripts, IJSRegistry),
--- tests/testMigrations.py
+++ tests/testMigrations.py
@@ -57,6 +57,7 @@ from Products.CMFUid.interfaces import IUniqueIdGenerator
 from Products.CMFUid.interfaces import IUniqueIdHandler
 from Products.GenericSetup.interfaces import ISetupTool
 from Products.MailHost.interfaces import IMailHost
+from Products.MimetypesRegistry.interfaces import IMimetypesRegistryTool
 from Products.PloneLanguageTool.interfaces import ILanguageTool
 from Products.ResourceRegistries.interfaces import ICSSRegistry
 from Products.ResourceRegistries.interfaces import IJSRegistry
@@ -1686,7 +1687,7 @@ class TestMigrations_v3_0(MigrationTest):
                       IQuickInstallerTool, ICSSRegistry, IJSRegistry, 
                       IFormControllerTool, ILanguageTool, IDiffTool,
                       IATCTTool, IArchetypeTool, IReferenceCatalog,
-                      IUIDCatalog, )
+                      IUIDCatalog, IMimetypesRegistryTool, )
         for i in interfaces:
             sm.unregisterUtility(provided=i)
         registerToolsAsUtilities(self.portal, [])
