f3f3ce6d14aca07df87a010f443cb6114b6ccf91
Martin Aspeli <optilude@gmail.com>
2007-04-28 22:36:31 +0000

    Factor the test case base class into a separate module so that other packages with controlpanels can re-use it more easily (without the trigger-on-import side effects of setupPloneSite)
    svn path=/plone.app.controlpanel/trunk/; revision=14513

---
+++ plone/app/controlpanel/tests/cptc.py
@@ -0,0 +1,32 @@
+"""Base class for control panel test cases.
+
+This is in a separate module because it's potentially useful to other
+packages which register controlpanels. They should be able to import it
+without the PloneTestCase.setupPloneSite() side effects.
+"""
+
+from Products.PloneTestCase.PloneTestCase import FunctionalTestCase
+from Products.Five.testbrowser import Browser
+from Products.CMFCore.utils import getToolByName
+
+class ControlPanelTestCase(FunctionalTestCase):
+    """base test case with convenience methods for all control panel tests"""
+
+    def afterSetUp(self):
+        super(ControlPanelTestCase, self).afterSetUp()
+                
+        self.browser = Browser()
+        
+        self.uf = self.portal.acl_users
+        self.uf.userFolderAddUser('root', 'secret', ['Manager'], [])
+        
+        self.ptool = getToolByName(self.portal, 'portal_properties')
+        self.site_props = self.ptool.site_properties
+        
+    def loginAsManager(self):
+        """points the browser to the login screen and logs in as user root with Manager role."""
+        self.browser.open('http://nohost/plone/')
+        self.browser.getLink('Log in').click()
+        self.browser.getControl('Login Name').value = 'root'
+        self.browser.getControl('Password').value = 'secret'
+        self.browser.getControl('Log in').click()
--- plone/app/controlpanel/tests/test_doctests.py
+++ plone/app/controlpanel/tests/test_doctests.py
@@ -3,38 +3,14 @@ from unittest import TestSuite
 
 from Testing.ZopeTestCase import FunctionalDocFileSuite
 from Products.PloneTestCase.PloneTestCase import PloneTestCase
-from Products.PloneTestCase.PloneTestCase import FunctionalTestCase
 from Products.PloneTestCase.PloneTestCase import setupPloneSite
 
-from Products.CMFCore.utils import getToolByName
+from plone.app.controlpanel.tests.cptc import ControlPanelTestCase
 
 setupPloneSite()
 
 OPTIONFLAGS = (doctest.ELLIPSIS |
                doctest.NORMALIZE_WHITESPACE)
-
-class ControlPanelTestCase(FunctionalTestCase):
-    """base test case with convenience methods for all control panel tests"""
-
-    def afterSetUp(self):
-        super(ControlPanelTestCase, self).afterSetUp()
-        from Products.Five.testbrowser import Browser
-        self.browser = Browser()
-        
-        self.uf = self.portal.acl_users
-        self.uf.userFolderAddUser('root', 'secret', ['Manager'], [])
-        
-        self.ptool = getToolByName(self.portal, 'portal_properties')
-        self.site_props = self.ptool.site_properties
-        
-    def loginAsManager(self):
-        """points the browser to the login screen and logs in as user root with Manager role."""
-        self.browser.open('http://nohost/plone/')
-        self.browser.getLink('Log in').click()
-        self.browser.getControl('Login Name').value = 'root'
-        self.browser.getControl('Password').value = 'secret'
-        self.browser.getControl('Log in').click()
-
 
 def test_suite():
     tests = ['calendar.txt',
