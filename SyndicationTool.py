28e67314e99b2c7a420b788d269379ccd0902a4a
Hanno Schlichting <hanno@hannosch.eu>
2007-06-24 00:29:55 +0000

    Removed old _actions from the SyndicationTool.
    svn path=/CMFPlone/trunk/; revision=15503

--- SyndicationTool.py
+++ SyndicationTool.py
@@ -1,23 +1,17 @@
 from Products.CMFDefault.SyndicationTool import SyndicationTool as BaseTool
 from Products.CMFPlone import ToolNames
-from Products.CMFCore.Expression import Expression
 from Products.CMFCore.permissions import ManageProperties
 from Products.CMFCore.utils import getToolByName
 from AccessControl import ClassSecurityInfo, Unauthorized
 from Globals import InitializeClass
 from Products.CMFPlone.PloneBaseTool import PloneBaseTool
 
-actions = tuple(BaseTool._actions)
-for a in actions:
-    if a.id == 'syndication':
-        a.condition=Expression(text='python: folder is object and portal.portal_syndication.isSiteSyndicationAllowed()')
 
 class SyndicationTool(PloneBaseTool, BaseTool):
 
     meta_type = ToolNames.SyndicationTool
     security = ClassSecurityInfo()
     toolicon = 'skins/plone_images/rss.gif'
-    _actions = actions
 
     __implements__ = (PloneBaseTool.__implements__, BaseTool.__implements__, )
 
