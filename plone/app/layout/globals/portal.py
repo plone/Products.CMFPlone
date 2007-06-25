5e17fef10d37de9401858c6edd2fcf957a58e884
Hanno Schlichting <hanno@hannosch.eu>
2007-06-25 12:04:49 +0000

    Use the url tool to get the portal, so we have a request on it
    svn path=/plone.app.layout/trunk/; revision=15542

--- plone/app/layout/globals/portal.py
+++ plone/app/layout/globals/portal.py
@@ -1,12 +1,10 @@
 from zope.interface import implements
-from zope.component import getUtility
 from zope.i18n.interfaces import IUserPreferredLanguages
 from zope.i18n.locales import locales, LoadLocaleError
 
 from plone.memoize.view import memoize, memoize_contextless
 
 from Acquisition import aq_inner
-from Products.CMFCore.interfaces import ISiteRoot
 from Products.CMFCore.utils import getToolByName
 from Products.Five.browser import BrowserView
 
@@ -26,7 +24,7 @@ class PortalState(BrowserView):
 
     @memoize_contextless
     def portal(self):
-        return getUtility(ISiteRoot)
+        return getToolByName(aq_inner(self.context), 'portal_url').getPortalObject()
     
     @memoize_contextless
     def portal_title(self):
