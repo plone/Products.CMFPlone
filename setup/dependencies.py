48b244b422915a5679756127fa01f01d9f4e1511
Hanno Schlichting <hanno@hannosch.eu>
2007-08-18 17:02:48 +0000

    Removed dependency checking code for everything contained in the Plone Core tarball. The number of dependencies is just too huge by now to check them all. We still check external dependencies like Python, Zope, PIL and elementree though.
    svn path=/CMFPlone/trunk/; revision=16791

--- setup/dependencies.py
+++ setup/dependencies.py
@@ -7,8 +7,6 @@ PREFERRED_PYTHON_VER = "2.4.4 or newer"
 MINIMUM_ZOPE_VER = (2, 10, 4)
 PREFERRED_ZOPE_VER = "2.10.4 or newer"
 
-MINIMUM_CMF_VER = (2, 1, 0)
-
 messages = []
 
 def log(message, summary='', severity=logging.ERROR, optional=0):
@@ -52,52 +50,6 @@ elif ZOPE_VER < MINIMUM_ZOPE_VER:
      ('.'.join([str(x) for x in ZOPE_VER]),
        '.'.join([str(x) for x in MINIMUM_ZOPE_VER]), PREFERRED_ZOPE_VER) ))
 
-# make sure CMF is installed
-cmfcore = 0
-try:
-    import Products.CMFCore
-    cmfcore = 1
-except ImportError:
-    log(("CMFCore not found.  Please download the CMF "
-         "from http://cmf.zope.org/download"))
-
-# check the CMF version
-if cmfcore:
-    from Products.CMFCore.utils import _wwwdir
-    from os.path import join
-
-    x = []
-    CMF_VERSION = 'Unknown'
-    try:
-        file = join(_wwwdir, '..', 'version.txt')
-        CMF_VERSION = open(file, 'r').read().strip()
-        version = CMF_VERSION.strip()
-        if version.lower().startswith('cmf-'):
-            version = version[4:]
-        filtered = ''
-        for v in version:
-            if v in ['0','1','2','3','4','5','6','7','8','9','.']:
-                filtered += v
-            else:
-                break
-        cmf_ver = [int(x) for x in filtered.split('.')]
-
-        if tuple(cmf_ver) < MINIMUM_CMF_VER:
-            log(("Plone requires CMF %s or later. "
-                 "Your version is: %s" % (MINIMUM_CMF_VER, CMF_VERSION)))
-    except IOError:
-        # couldnt find file, oh well
-        pass
-    except ValueError:
-        # couldnt make sense of the version number
-        pass
-
-try:
-    import Products.CMFQuickInstallerTool
-except ImportError:
-    log(("CMFQuickInstallerTool not found. Please download it from "
-         "http://plone.org/products/cmfquickinstallertool"))
-
 try:
     # TODO: we might want to check if the user has jpeg and zlib support, too
     import PIL.Image
@@ -113,100 +65,3 @@ except ImportError:
          "transformation. Please download it from "
          "http://effbot.org/downloads/#elementtree"),
          severity=logging.INFO)
-
-try:
-    import Products.ExternalEditor
-except ImportError:
-    log(("ExternalEditor not found. If you want "
-         "the external edit functionality, please "
-         "download it from "
-         "http://plope.com/software/ExternalEditor"),
-        severity=logging.INFO, optional=1)
-
-try:
-    import Products.kupu
-except ImportError:
-    log(("Kupu not found. If you want WYSIWYG capabilities "
-        "in Plone, you can download it from "
-        "http://kupu.oscom.org/"),
-        severity=logging.INFO, optional=1)
-
-try:
-    import Products.PlacelessTranslationService
-except ImportError:
-    log(("Placeless Translation Service not found. Plone "
-         "runs without this, but if you want multilingual "
-         "interface or access keys, you must download it from "
-         "http://plone.org/products/pts/"),
-        severity=logging.INFO, optional=1)
-
-try:
-    import Products.PloneTranslations
-except ImportError:
-    log(("PloneTranslation product with i18n files not found. Plone "
-         "runs without this, but if you want multilingual "
-         "interface or access keys, you must download it from "
-         "http://plone.org/products/plonetranslations"),
-        severity=logging.INFO, optional=1)
-
-try:
-    import Products.CMFFormController
-except ImportError:
-    log(("CMFFormController not found. Please "
-         "download it from http://plone.org/products/cmfformcontroller"))
-
-try:
-    import Products.SecureMailHost
-except ImportError:
-    log(("SecureMailHost not found. Please "
-         "http://plone.org/products/securemailhost"))
-
-try:
-    import Products.MimetypesRegistry
-except ImportError:
-    log(("MimetypesRegistry not found. Please "
-         "download it from http://plone.org/products/archetypes"))
-
-try:
-    import Products.PortalTransforms
-except ImportError:
-    log(("PortalTransforms not found. Please "
-         "download it from http://plone.org/products/archetypes"))
-
-try:
-    import Products.Archetypes
-except ImportError:
-    # TODO we might want to check the AT version
-    log(("Archetypes not found. Please "
-         "download it from http://plone.org/products/archetypes"))
-
-try:
-    import Products.ATContentTypes.content.document
-except ImportError:
-    log(("ATContentTypes not found or too old. Please "
-         "download it from http://plone.org/products/atcontenttypes"))
-
-try:
-    import Products.ExtendedPathIndex
-except ImportError:
-    log(("ExtendedPathIndex not found. "
-         "Please download it from http://plone.org/products/extendedpathindex"))
-
-try:
-    import Products.ResourceRegistries
-except ImportError:
-    log(("ResourceRegistries not found. "
-         "Please download it from http://plone.org/products/resourceregistries"))
-
-try:
-    import Products.CMFDynamicViewFTI
-except ImportError:
-    log(("CMFDynamicViewFTI not found. "
-         "Please download it from http://plone.org/products/cmfdynamicviewfti"))
-
-try:
-    import Products.PlonePAS
-except ImportError:
-    log(("PlonePAS not found."
-         "Please download it from http://plone.org/products/plonepas"))
-
