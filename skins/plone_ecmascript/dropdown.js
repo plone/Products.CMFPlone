de63028111dded95961ae341f79874e76a13b1ad
Martijn Pieters <plone.org@zopatista.com>
2007-09-27 19:23:36 +0000

    jQuerify the dropdown menus.
    svn path=/CMFPlone/branches/mj-jquery/; revision=17281

--- skins/plone_ecmascript/dropdown.js
+++ skins/plone_ecmascript/dropdown.js
@@ -34,141 +34,50 @@
  *
  */
 
-function isActionMenu(node) {
-    if (hasClassName(node, 'actionMenu')) {
-        return true;
-    }
-    return false;
-};
-
 function hideAllMenus() {
-    var menus = cssQuery('dl.actionMenu');
-    for (var i=0; i < menus.length; i++) {
-        replaceClassName(menus[i], 'activated', 'deactivated', true);
-    }
+    $('dl.actionMenu').removeClass('activated').addClass('deactivated');
 };
 
 function toggleMenuHandler(event) {
-    if (!event) var event = window.event; // IE compatibility
-
-    // terminate if we hit a non-compliant DOM implementation
-    // returning true, so the link is still followed
-    if (!W3CDOM){return true;}
-
-    var container = findContainer(this, isActionMenu);
-    if (!container) {
-        return true;
-    }
-
-    // check if the menu is visible
-    if (hasClassName(container, 'activated')) {
-        // it's visible - hide it
-        replaceClassName(container, 'activated', 'deactivated', true);
-    } else {
-        // it's invisible - make it visible
-        replaceClassName(container, 'deactivated', 'activated', true);
-    }
-
+    // swap between activated and deactivated
+    $(this).parents('.actionMenu:first')
+        .toggleClass('deactivated')
+        .toggleClass('activated');
     return false;
 };
 
-function hideMenusHandler(event) {
-    if (!event) var event = window.event; // IE compatibility
-
-    hideAllMenus();
-
-    // we want to follow this link
-    return true;
-};
-
 function actionMenuDocumentMouseDown(event) {
-    if (!event) var event = window.event; // IE compatibility
-
-    if (event.target)
-        targ = event.target;
-    else if (event.srcElement)
-        targ = event.srcElement;
-
-    var container = findContainer(targ, isActionMenu);
-    if (container) {
-        // targ is part of the menu, so just return and do the default
+    if ($(event.target).parents('.actionMenu:first').length)
+        // target is part of the menu, so just return and do the default
         return true;
-    }
 
     hideAllMenus();
-
-    return true;
 };
 
 function actionMenuMouseOver(event) {
-    if (!event) var event = window.event; // IE compatibility
+    var menu_id = $(this).parents('.actionMenu:first').attr('id');
+    if (!menu_id) return true;
 
-    if (!this.tagName && (this.tagName == 'A' || this.tagName == 'a')) {
-        return true;
-    }
+    var switch_menu = $('dl.actionMenu.activated').length > 0;
+    // hide all other menus
+    $('dl.actionMenu:not(#' + menu_id + ')')
+        .removeClass('activated').addClass('deactivated');
 
-    var container = findContainer(this, isActionMenu);
-    if (!container) {
-        return true;
-    }
-    var menu_id = container.id;
-
-    var switch_menu = false;
-    // hide all menus
-    var menus = cssQuery('dl.actionMenu');
-    for (var i=0; i < menus.length; i++) {
-        var menu = menus[i]
-        // check if the menu is visible
-        if (hasClassName(menu, 'activated')) {
-            switch_menu = true;
-        }
-        // turn off menu when it's not the current one
-        if (menu.id != menu_id) {
-            replaceClassName(menu, 'activated', 'deactivated', true);
-        }
-    }
-
-    if (switch_menu) {
-        var menu = cssQuery('#'+menu_id)[0];
-        if (menu) {
-            replaceClassName(menu, 'deactivated', 'activated', true);
-        }
-    }
-
-    return true;
+    if (switch_menu)
+        $('#' + menu_id).removeClass('deactivated').addClass('activated');
 };
 
-function initializeMenus() {
-    // terminate if we hit a non-compliant DOM implementation
-    if (!W3CDOM) {return false;}
-
-    document.onmousedown = actionMenuDocumentMouseDown;
+$(function() {
+    $(document).mousedown(actionMenuDocumentMouseDown);
 
     hideAllMenus();
 
     // add toggle function to header links
-    var menu_headers = cssQuery('dl.actionMenu dt.actionMenuHeader a');
-    if (window.kukit && kukit.log) {
-        // Only log if in kss 
-        kukit.log('Initializing ' + menu_headers.length + ' menus.');
-    } 
-    for (var i=0; i < menu_headers.length; i++) {
-        var menu_header = menu_headers[i];
-
-        menu_header.onclick = toggleMenuHandler;
-        menu_header.onmouseover = actionMenuMouseOver;
-    }
-
+    $('dl.actionMenu dt.actionMenuHeader a')
+        .click(toggleMenuHandler)
+        .mouseover(actionMenuMouseOver);
+        
     // add hide function to all links in the dropdown, so the dropdown closes
     // when any link is clicked
-    var menu_contents = cssQuery('dl.actionMenu > dd.actionMenuContent');
-    for (var i=0; i < menu_contents.length; i++) {
-        menu_contents[i].onclick = hideMenusHandler;
-    }
-
-    // uncomment to enable sorting of elements
-    //var nodes = cssQuery('#objectMenu > dd.actionMenuContent li');
-    //sortNodes(nodes, getInnerTextFast);
-};
-
-registerPloneFunction(initializeMenus);
+    $('dl.actionMenu > dd.actionMenuContent').click(hideAllMenus);
+});
