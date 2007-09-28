eb2acbdc2d0c159dbc9ee624fe70c926e727da45
Martijn Pieters <plone.org@zopatista.com>
2007-09-28 08:23:56 +0000

    When auto-focusing on the first element, skip the form tabs select box.
    svn path=/CMFPlone/branches/mj-jquery/; revision=17285

--- skins/plone_ecmascript/first_input_focus.js
+++ skins/plone_ecmascript/first_input_focus.js
@@ -1,5 +1,5 @@
 // Focus on error or first element in a form with class="enableAutoFocus"
 $(function() {
     if ($("form div.error :input:first").focus().length) return;
-    $("form.enableAutoFocus :input:visible:first").focus();
+    $("form.enableAutoFocus :input:not(.formTabs):visible:first").focus();
 });
