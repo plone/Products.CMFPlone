b0288aa04d294359ef65bfc3bfb783d2a6163455
Martijn Pieters <plone.org@zopatista.com>
2007-09-28 16:44:57 +0000

    jQuerify and clean up highlightsearchterms.
    svn path=/CMFPlone/branches/mj-jquery/; revision=17307

--- skins/plone_ecmascript/highlightsearchterms.js
+++ skins/plone_ecmascript/highlightsearchterms.js
@@ -1,39 +1,29 @@
 function highlightTermInNode(node, word) {
     var contents = node.nodeValue;
     var index = contents.toLowerCase().indexOf(word.toLowerCase());
-    if (index < 0){return false};
-
-    var parent = node.parentNode;
-    if (parent.className != "highlightedSearchTerm") {
-        // make 3 shiny new nodes
-        var hiword = document.createElement("span");
-        hiword.className = "highlightedSearchTerm";
-        hiword.appendChild(document.createTextNode(contents.substr(index, word.length)));
-        parent.insertBefore(document.createTextNode(contents.substr(0, index)), node);
-        parent.insertBefore(hiword, node);
-        parent.insertBefore(document.createTextNode(contents.substr(index+word.length)), node);
-        parent.removeChild(node);
-    }
+    if (index < 0) return;
+    if ($(node).parent().hasClass("highlightedSearchTerm")) return;
+    
+    // replace the node with [before]<span>word</span>[after]
+    $(node)
+        .before(document.createTextNode(contents.substr(0, index)))
+        .before(
+            $('<span>')
+                .addClass("highlightedSearchTerm")
+                .text(contents.substr(index, word.length))
+        )
+        .before(document.createTextNode(contents.substr(index+word.length)))
+        .remove();
 }
 
 function highlightSearchTerms(terms, startnode) {
-    // terminate if we hit a non-compliant DOM implementation
-    if (!W3CDOM){return false};
-    if (!terms){return false};
-    if (!startnode){return false};
+    if (!terms || !startnode) return;
 
-    for (var term_index=0; term_index < terms.length; term_index++) {
+    $.each(terms, function(i, term) {
         // don't highlight reserved catalog search terms
-        var term = terms[term_index];
-        if (term.length < 1)
-            continue;
-        var term_lower = term.toLowerCase();
-        if (term_lower != 'not'
-            && term_lower != 'and'
-            && term_lower != 'or') {
-            walkTextNodes(startnode, highlightTermInNode, term);
-        }
-    }
+        if (!term || /(not|and|or)/.test(term.toLowerCase())) return;
+        walkTextNodes(startnode, highlightTermInNode, term);
+    });
 }
 
 function getSearchTermsFromURI(uri) {
@@ -57,26 +47,16 @@ function getSearchTermsFromURI(uri) {
     var qq = qfinder.exec(query);
     if (qq && qq[2]) {
         var terms = qq[2].replace(/\+/g,' ').split(' ');
-        for (var i=0; i < terms.length; i++) {
-            if (terms[i] != '') {
-                result.push(terms[i]);
-            }
-        }
+        result.push.apply(result, $.grep(terms, 'a != ""'));
         return result;
     }
     return result.length == 0 ? false : result;
 }
 
-function highlightSearchTermsFromURI() {
-    // terminate if we hit a non-compliant DOM implementation
-    if (!W3CDOM){return false};
-
+$(function() {
     // search-term-highlighter function --  Geir Baekholt
     var terms = getSearchTermsFromURI(window.location.search);
     // make sure we start the right place so we don't higlight menuitems or breadcrumb
-    var contentarea = getContentArea();
-    highlightSearchTerms(terms, contentarea);
-}
-
-registerPloneFunction(highlightSearchTermsFromURI);
+    highlightSearchTerms(terms, getContentArea());
+});
 
