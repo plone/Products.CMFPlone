3ec6df3c4b283fc3d58f2fb8950f791ed4bbf5b7
Martijn Pieters <plone.org@zopatista.com>
2007-09-30 23:42:51 +0000

    Make this full-compression-able and don't pollute the global namespace.
    svn path=/CMFPlone/branches/mj-jquery/; revision=17368

--- skins/plone_ecmascript/collapsiblesections.js
+++ skins/plone_ecmascript/collapsiblesections.js
@@ -33,23 +33,25 @@
  *
  */
 
-function toggleCollapsible() {
-    var container = $(this).parents('dl.collapsible:first');
-    if (!container) return true;
+(function() {
+_toggleCollapsible = function() {
+    var $container = $(this).parents('dl.collapsible:first');
+    if (!$container) return true;
 
-    var type = container.hasClass('inline') ? 'Inline' :'Block';
+    var $type = $container.hasClass('inline') ? 'Inline' :'Block';
     // toggle between collapsed and expanded classes
-    container.toggleClass('collapsed' + type + 'Collapsible')
-             .toggleClass('expanded' + type + 'Collapsible');
+    $container.toggleClass('collapsed' + $type + 'Collapsible')
+              .toggleClass('expanded' + $type + 'Collapsible');
 };
 
 $(function() {
-    $('dl.collapsible dt.collapsibleHeader:first').click(toggleCollapsible);
+    $('dl.collapsible dt.collapsibleHeader:first').click(_toggleCollapsible);
     $('dl.collapsible').each(function() {
-        var state = $(this).hasClass('collapsedOnLoad') ?
-                    'collapsed' : 'expanded';
-        var type = $(this).hasClass('inline') ? 'Inline' :'Block';
+        var $state = $(this).hasClass('collapsedOnLoad') ?
+                     'collapsed' : 'expanded';
+        var $type = $(this).hasClass('inline') ? 'Inline' :'Block';
         $(this).removeClass('collapsedOnLoad')
-               .addClass(state + type + 'Collapsible');
+               .addClass($state + $type + 'Collapsible');
     });
 });
+})();
