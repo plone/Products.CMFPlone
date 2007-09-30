47a3f1a4c9b5d9cf965c39c2dde5f126d01f5ef8
Martijn Pieters <plone.org@zopatista.com>
2007-09-30 23:30:39 +0000

    Clean up some more and make it full-compression-able.
    svn path=/CMFPlone/branches/mj-jquery/; revision=17366

--- skins/plone_ecmascript/accessibility.js
+++ skins/plone_ecmascript/accessibility.js
@@ -1,15 +1,13 @@
-function setBaseFontSize(fontsize, reset) {
-    var body = $('body');
-    if (reset == 1) {
-        body.removeClass('smallText').removeClass('largeText');
-        createCookie("fontsize", fontsize, 365);
+function setBaseFontSize($fontsize, $reset) {
+    var $body = $('body');
+    if ($reset) {
+        $body.removeClass('smallText').removeClass('largeText');
+        createCookie("fontsize", $fontsize, 365);
     }
-    body.addClass(fontsize);
+    $body.addClass($fontsize);
 };
 
 $(function() {
-    var fontsize = readCookie("fontsize");
-    if (fontsize != null) {
-        setBaseFontSize(fontsize, 0);
-    }
+    var $fontsize = readCookie("fontsize");
+    if ($fontsize) setBaseFontSize($fontsize, 0);
 });
