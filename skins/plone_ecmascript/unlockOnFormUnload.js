10ad16e8263abd31aae311949583ea619533d771
Martijn Pieters <plone.org@zopatista.com>
2007-09-30 22:25:46 +0000

    New unlock version, jquery style, which now can be used together with any other unlock handler, provided that too uses jquery.
    svn path=/CMFPlone/branches/mj-jquery/; revision=17352

--- skins/plone_ecmascript/unlockOnFormUnload.js
+++ skins/plone_ecmascript/unlockOnFormUnload.js
@@ -1,45 +1,14 @@
 /* Unlock the object on form unload*/
-
-if (typeof(plone)=='undefined'){
-    var plone = {};
-}
-
-if (window.onunload) {
-    throw "Attempt to redefine window.unonload (not supported)";
-}
-
-plone.dummyProcessReqChange = function() {};
-plone.UnlockHandler = function() {
-    var self = this;
-
-    this.execute = function(event) {
-        var objectUrl = "";
-        var baseUrl = undefined;
-        var nodes = document.getElementsByTagName("base");
-        if (nodes.length == 0) {
-            var base = window.location.href;
-            var pieces = base.split('/');
-            pieces.pop();
-            baseUrl = pieces.join('/');
-        } else {
-            baseUrl = nodes[0].href;
-        }
-        var unlockRequest = new XMLHttpRequest();
-        unlockRequest.onreadystatechange= plone.dummyProcessReqChange;
-        unlockRequest.open("GET", baseUrl + "/@@plone_lock_operations/safe_unlock");
-        unlockRequest.send(null);
-    };
-};
-
-//var Class = UnlockHandler.prototype;
-
-registerPloneFunction(function() {
-    // Check if there are any forms
-    var forms = cssQuery('form.enableUnlockProtection');
-
+$(function() {
     // set up the handler, if there are any forms
-    if (forms.length > 0) {
-        var handler = new plone.UnlockHandler().execute;
-        window.onunload = handler;
-    }
+    if ($('form.enableUnlockProtection').length)
+        $(window).unload(function() {
+            var baseUrl = $('base').attr('href');
+            if (!baseUrl) {
+                var pieces = window.location.href.split('/');
+                pieces.pop();
+                baseUrl = pieces.join('/');
+            }
+            $.get(baseUrl + '/@@plone_lock_operations/safe_unlock');
+        });
 });
