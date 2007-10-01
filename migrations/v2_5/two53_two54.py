06e812ec26bf64683b91e7d122c40521cad996ea
Alec Mitchell <apm13@columbia.edu>
2007-10-01 07:10:01 +0000

    Oops
    svn path=/CMFPlone/branches/2.5/; revision=17372

---
+++ migrations/v2_5/two53_two54.py
@@ -0,0 +1,23 @@
+from Products.CMFCore.utils import getToolByName
+
+PROF_ID = 'CMFPlone:plone'
+
+def two53_two54(portal):
+    """2.5.3 -> 2.5.4
+    """
+    out = []
+    addGSSteps(portal, out)
+
+    return out
+
+
+def addGSSteps(portal, out):
+    """ Add import export steps in GS """
+    setup_tool = getToolByName(portal, 'portal_setup', None)
+    if setup_tool is not None:
+        # only reun when unset
+        if not setup_tool.getImportContextID():
+            # make sure the profile is there
+            if PROF_ID in [i['id'] for i in setup_tool.listProfileInfo()]:
+                setup_tool.setImportContext(PROF_ID)
+    out.append('Set plone GS profile as default')
