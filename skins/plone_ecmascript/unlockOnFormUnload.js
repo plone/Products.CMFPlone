07dbccbc67380c85347f4d5f492fb5a491d49a83
Martijn Pieters <plone.org@zopatista.com>
2007-10-03 21:23:18 +0000

    Re-instate the plone.UnlockHandler object to a certain extent so that the kupu plugins can still access it.
    svn path=/CMFPlone/branches/mj-jquery/; revision=17414

--- skins/plone_ecmascript/unlockOnFormUnload.js
+++ skins/plone_ecmascript/unlockOnFormUnload.js
@@ -1,14 +1,23 @@
 /* Unlock the object on form unload*/
-$(function() {
-    // set up the handler, if there are any forms
-    if ($('form.enableUnlockProtection').length)
-        $(window).unload(function() {
-            var baseUrl = $('base').attr('href');
-            if (!baseUrl) {
-                var pieces = window.location.href.split('/');
-                pieces.pop();
-                baseUrl = pieces.join('/');
-            }
-            $.get(baseUrl + '/@@plone_lock_operations/safe_unlock');
-        });
-});
+if (typeof(plone)=='undefined')
+    var plone = {};
+
+plone.UnlockHandler = {
+    init: function() {
+        // set up the handler, if there are any forms
+        if ($('form.enableUnlockProtection').length)
+            $(window).unload(plone.UnlockHandler.execute);
+    },
+    
+    execute: function() {
+        var baseUrl = $('base').attr('href');
+        if (!baseUrl) {
+            var pieces = window.location.href.split('/');
+            pieces.pop();
+            baseUrl = pieces.join('/');
+        }
+        $.get(baseUrl + '/@@plone_lock_operations/safe_unlock');
+    }
+};
+
+$(plone.UnlockHandler.init);
