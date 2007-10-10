38cb4d30644768fe8a6b4243e6b09784399761c1
Florian Schulze <florian.schulze@gmx.net>
2007-10-10 15:32:02 +0000

    Added dependency on plone.app.viewletmanager.
    svn path=/plone.app.layout/trunk/; revision=17555

--- setup.py
+++ setup.py
@@ -26,6 +26,7 @@ viewlets and general views.
       zip_safe=False,
       install_requires=[
         'setuptools',
+        'plone.app.viewletmanager>=1.2dev',
       ],
       entry_points="""
       # -*- Entry points: -*-
