221e361222122e5f2d2369fc357af4861cd83738
Wichert Akkerman <wichert@wiggy.net>
2007-10-14 17:14:11 +0000

    Add a language selection portlet
    svn path=/plone.app.portlets/trunk/; revision=17669

---
+++ plone/app/portlets/portlets/language.py
@@ -0,0 +1,45 @@
+from zope.interface import implements
+from plone.portlets.interfaces import IPortletDataProvider
+from plone.app.portlets.portlets import base
+from plone.app.i18n.locales.browser.selector import LanguageSelector
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from Products.CMFPlone import PloneMessageFactory as _
+
+
+class ILanguagePortlet(IPortletDataProvider):
+    """A portlet which shows the available portal languages.
+    """
+
+
+class Assignment(base.Assignment):
+    implements(ILanguagePortlet)
+
+    title = _(u'label_languages', default=u'Languages')
+
+
+class Renderer(base.Renderer):
+
+    def __init__(self, context, request, view, manager, data):
+        base.Renderer.__init__(self, context, request, view, manager, data)
+        self.selector=LanguageSelector(context, request, None, None)
+        self.languages=self.selector.languages()
+
+
+    def show(self):
+        return self.selector.available() and self.languages
+
+
+    def showFlags(self):
+        return self.selector.showFlags()
+
+
+    def update(self):
+        pass
+
+    render = ViewPageTemplateFile('language.pt')
+
+
+class AddForm(base.NullAddForm):
+
+    def create(self):
+        return Assignment()
