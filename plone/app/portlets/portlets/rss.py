c5555f489c88cd9eed97bd3d65d5a29ead766742
Christian Scholz <cs@comlounge.net>
2007-11-02 07:56:24 +0000

    hopefully fixed the RSS portlet so it now at least refreshes for non-anonymous users (fixed #6972).
    svn path=/plone.app.portlets/trunk/; revision=17781

--- plone/app/portlets/portlets/rss.py
+++ plone/app/portlets/portlets/rss.py
@@ -227,7 +227,12 @@ class Renderer(base.DeferredRenderer):
     @property
     def initializing(self):
         """should return True if deferred template should be displayed"""
-        return not self._getFeed().loaded
+	feed=self._getFeed()
+	if not feed.loaded:
+		return True
+	if feed.needs_update:
+		return True
+	return False
             
     def deferred_update(self):
         """refresh data for serving via KSS"""
