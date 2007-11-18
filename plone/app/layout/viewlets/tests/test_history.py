da95740bf856752d472ea654f00042c44693ca11
rsantos <rsantos@localhost>
2007-11-18 18:26:26 +0000

    Changed method 'workflowHistory' to handle history entries with usernames that don't exist any more (deleted users) and anonymous users. Also added test case. This fixes #7250.
    svn path=/plone.app.layout/branches/1.0/; revision=18037

---
+++ plone/app/layout/viewlets/tests/test_history.py
@@ -0,0 +1,66 @@
+import unittest
+from plone.app.layout.viewlets.tests.base import ViewletsTestCase
+from plone.app.layout.viewlets.content import WorkflowHistoryViewlet
+from DateTime import DateTime
+
+class TestWorkflowHistoryViewlet(ViewletsTestCase):
+    """
+    Test the workflow history viewlet
+    """
+    def afterSetUp(self):
+        # add document, perform transition, set history for non-existent
+        # member and also None (anonymous)
+        self.folder.invokeFactory('Document', 'd1')
+
+    def addMember(self, username, roles=('Member',)):
+        self.portal.portal_membership.addMember(username, 'secret', roles, [])
+
+    def delMember(self, username):
+        self.portal.portal_membership.deleteMembers([username])
+
+    def test_emptyHistory(self):
+        request = self.app.REQUEST
+        context = getattr(self.folder, 'd1')
+        viewlet = WorkflowHistoryViewlet(context, request, None, None)
+        viewlet.update()
+        self.assertEqual(viewlet.workflowHistory(), [])
+
+    def test_transitionHistory(self):
+        wf_tool = self.portal.portal_workflow
+        request = self.app.REQUEST
+        context = getattr(self.folder, 'd1')
+        self.loginAsPortalOwner()
+        wf_tool.doActionFor(context, 'publish')
+
+        viewlet = WorkflowHistoryViewlet(context, request, None, None)
+        viewlet.update()
+
+        history = viewlet.workflowHistory()
+        self.assertEqual(len(history), 1)
+        self.assertEqual(history[0]['action'], 'publish')
+
+        # add a temporary user to perform a transition
+        self.addMember('tempuser', roles=('Member', 'Manager'))
+        self.login('tempuser')
+        wf_tool.doActionFor(context, action='retract', actor=None)
+        self.logout()
+
+        self.loginAsPortalOwner()
+
+        # remove the user
+        self.delMember('tempuser')
+
+        # if the user that performed the transition no longer exists, the link
+        # shouldn't be included.
+        viewlet = WorkflowHistoryViewlet(context, request, None, None)
+        viewlet.update()
+        history = viewlet.workflowHistory()
+
+        self.assertEqual(len(history), 2)
+        self.assertEqual(history[0]['actor_home'], '')
+
+
+def test_suite():
+    suite = unittest.TestSuite()
+    suite.addTest(unittest.makeSuite(TestWorkflowHistoryViewlet))
+    return suite
