7b7b33bc066baa39990629536bc145a3690877b9
Hanno Schlichting <hanno@hannosch.eu>
2007-11-24 14:03:25 +0000

    Fixed description in filter control panel to be recognizable by i18ndude
    svn path=/plone.app.controlpanel/trunk/; revision=18106

--- plone/app/controlpanel/filter.py
+++ plone/app/controlpanel/filter.py
@@ -258,11 +258,13 @@ class FilterControlPanel(ControlPanelForm):
     form_fields['stripped_combinations'].custom_widget = combination_widget
 
     label = _("HTML Filter settings")
-    description = _("""Plone filters HTML tags that are considered security risks. Be aware
-of the implications before making changes below. By default only tags
-defined in XHTML are permitted. In particular, to allow 'embed' as a
-tag you must both remove it from 'Nasty tags' and add it to 'Custom
-tags'. Although the form will update immediately to show any changes
-you make, your changes are not saved until you press the 'Save' button.""")
+    description = _("Plone filters HTML tags that are considered security "
+                    "risks. Be aware of the implications before making "
+                    "changes below. By default only tags defined in XHTML "
+                    "are permitted. In particular, to allow 'embed' as a tag "
+                    "you must both remove it from 'Nasty tags' and add it to "
+                    "'Custom tags'. Although the form will update "
+                    "immediately to show any changes you make, your changes "
+                    "are not saved until you press the 'Save' button.")
     form_name = _("HTML Filter settings")
 
