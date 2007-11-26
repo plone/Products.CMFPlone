af913101a68d4b1701876b0b4623abfe75c8656a
Radim Novotny <novotny.radim@gmail.com>
2007-11-26 21:03:57 +0000

    Added tests package with one failing test to show http://dev.plone.org/plone/ticket/7359 Fix follows immediately.
    svn path=/plone.app.layout/branches/1.0/; revision=18129

---
+++ plone/app/layout/analytics/tests/base.py
@@ -0,0 +1,8 @@
+from Products.PloneTestCase import PloneTestCase as ptc
+ptc.setupPloneSite()
+
+class AnalyticsTestCase(ptc.PloneTestCase):
+    pass
+
+class AnalyticsFunctionalTestCase(ptc.FunctionalTestCase):
+    pass
---
+++ plone/app/layout/analytics/tests/test_doctests.py
@@ -0,0 +1,16 @@
+import unittest
+import doctest
+from zope import component
+from zope.component import testing
+from zope.testing import doctestunit
+from Testing import ZopeTestCase as ztc
+
+from plone.app.layout.analytics.tests import base
+
+def test_suite():
+    return unittest.TestSuite((
+        ztc.ZopeDocFileSuite(
+            'tests/analytics.txt', package='plone.app.layout.analytics',
+            test_class=base.AnalyticsFunctionalTestCase,
+            optionflags=doctest.REPORT_ONLY_FIRST_FAILURE | doctest.NORMALIZE_WHITESPACE | doctest.ELLIPSIS),
+        ))
