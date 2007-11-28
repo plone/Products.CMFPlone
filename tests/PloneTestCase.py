4f7b1ace06086bd91a16169a7b59317ef12c0d55
Andreas Zeidler <witsch@plone.org>
2007-11-28 14:20:08 +0000

    #6805:  added patch to run [source:CMFPlone/branches/3.0/tests#18201 CMFPlone tests] using the blob content type as a drop-in replacement for `ATFile`
    svn path=/CMFPlone/branches/3.0/; revision=18202

--- tests/PloneTestCase.py
+++ tests/PloneTestCase.py
@@ -4,15 +4,20 @@
 
 # $Id$
 
+from plone.app.blob import db   # needs to be imported first to set up ZODB
 from Products.PloneTestCase.ptc import *
 
 # Make the test fixture extension profile active
 from zope.interface import classImplements
 from Products.CMFPlone.Portal import PloneSite
 from Products.CMFPlone.interfaces import ITestCasePloneSiteRoot
+
 classImplements(PloneSite, ITestCasePloneSiteRoot)
 
-setupPloneSite(extension_profiles=['Products.CMFPlone:testfixture'])
+from plone.app.blob.tests import setupPackage
+setupPackage()
+
+setupPloneSite(extension_profiles=['Products.CMFPlone:testfixture', 'plone.app.blob:testing'])
 
 
 class PloneTestCase(PloneTestCase):
