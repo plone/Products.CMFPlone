099a7360320751401048b29baf51e28d630c9fdb
Hanno Schlichting <hanno@hannosch.eu>
2007-12-02 14:38:17 +0000

    Moved 'Add Plone Site' button to the right by popular demand.
    svn path=/Products.CMFPlone/trunk/; revision=18247

--- Products/CMFPlone/patches/addplonesitebutton.py
+++ Products/CMFPlone/patches/addplonesitebutton.py
@@ -3,7 +3,7 @@ from OFS.ObjectManager import ObjectManager
 ADD_PLONE_SITE_HTML = '''
 <dtml-if "_.len(this().getPhysicalPath()) == 1">
   <!-- Add Plone site button-->
-  <form action="&dtml-URL1;/" method="get" style="margin-top:0.5em; margin-bottom:0em">
+  <form action="&dtml-URL1;/" method="get" style="text-align: right; margin-top:0.5em; margin-bottom:0em">
     <input type="hidden" name=":method" value="manage_addProduct/CMFPlone/addPloneSiteForm" />
     <input class="form-element" type="submit" name="submit" value="Add Plone Site" />
   </form>
