825e27a118b2d109f7d04a940ceca4c29f9427d3
Alec Mitchell <apm13@columbia.edu>
2007-12-07 04:11:03 +0000

    Merge new migration step from 3.0 branch (18375)
    svn path=/Products.CMFPlone/trunk/; revision=18377

--- Products/CMFPlone/migrations/v3_0/final_three0x.py
+++ Products/CMFPlone/migrations/v3_0/final_three0x.py
@@ -1,4 +1,6 @@
+from Products.CMFCore.utils import getToolByName
 from Products.CMFPlone.migrations.migration_util import loadMigrationProfile
+from Products.CMFEditions.StandardModifiers import install
 
 
 def final_three01(portal):
@@ -22,6 +24,13 @@ def three03_three04(portal):
     out = []
     
     loadMigrationProfile(portal, 'profile-Products.CMFPlone.migrations:3.0.3-3.0.4')
-    
+    installNewModifiers(portal, out)
+
     return out
 
+
+def installNewModifiers(portal, out):
+    modifiers = getToolByName(portal, 'portal_modifier', None)
+    if modifiers is not None:
+        install(modifiers)
+        out.append('Added new CMFEditions modifiers')
