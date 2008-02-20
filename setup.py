524975bb05d6ba874b6d045b471b04c43eeeba1b
Wichert Akkerman <wichert@wiggy.net>
2008-02-20 22:05:38 +0000

    plone is a namespace
    svn path=/plone.app.layout/trunk/; revision=19420

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ viewlets and general views.
       url='http://svn.plone.org/svn/plone/plone.app.layout',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['plone.app'],
+      namespace_packages=['plone', 'plone.app'],
       include_package_data=True,
       zip_safe=False,
       install_requires=[
