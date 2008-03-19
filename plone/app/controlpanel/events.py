c46ea5d3ac485a331e65679ccca1137170530cdb
Hanno Schlichting <hanno@hannosch.eu>
2008-03-19 12:42:02 +0000

    Added an IConfigurationChangedEvent which is fired on each successful change of any configuration setting and a subscriber which empties all RAM caches when some configuration changed. This closes #7008.
    svn path=/plone.app.controlpanel/trunk/; revision=19953

---
+++ plone/app/controlpanel/events.py
@@ -0,0 +1,20 @@
+from zope.app.cache.interfaces.ram import IRAMCache
+from zope.component import adapter
+from zope.component import queryUtility
+from zope.interface import implements
+
+from plone.app.controlpanel.interfaces import IConfigurationChangedEvent
+
+class ConfigurationChangedEvent(object):
+    implements(IConfigurationChangedEvent)
+
+    def __init__(self, context, data):
+        self.context = context
+        self.data = data
+
+
+@adapter(IConfigurationChangedEvent)
+def handleConfigurationChangedEvent(event):
+    util = queryUtility(IRAMCache)
+    if util is not None:
+        util.invalidateAll()
--- plone/app/controlpanel/form.py
+++ plone/app/controlpanel/form.py
@@ -1,5 +1,6 @@
 from zope.interface import implements
 from zope.component import getMultiAdapter
+from zope.event import notify
 
 from plone.fieldsets.form import FieldsetsEditForm
 from zope.formlib import form
@@ -11,7 +12,9 @@ from Products.statusmessages.interfaces import IStatusMessage
 from plone.app.form import named_template_adapter
 from plone.app.form.validators import null_validator
 
+from plone.app.controlpanel.events import ConfigurationChangedEvent
 from plone.app.controlpanel.interfaces import IPloneControlPanelForm
+
 
 class ControlPanelForm(FieldsetsEditForm):
     """A simple form to be used as a basis for control panel screens."""
@@ -23,6 +26,7 @@ class ControlPanelForm(FieldsetsEditForm):
         if form.applyChanges(self.context, self.form_fields, data,
                              self.adapters):
             self.status = _("Changes saved.")
+            notify(ConfigurationChangedEvent(self, data))
             self._on_save(data)
         else:
             self.status = _("No changes made.")
