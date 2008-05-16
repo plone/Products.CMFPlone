ff6c395582c110dd13edb7b83192173c74651dd5
Wichert Akkerman <wichert@wiggy.net>
2008-05-16 10:25:11 +0000

    Reverse the right list
    svn path=/plone.app.layout/trunk/; revision=20880

--- plone/app/layout/viewlets/content.py
+++ plone/app/layout/viewlets/content.py
@@ -140,6 +140,7 @@ class WorkflowHistoryViewlet(ViewletBase):
                             # member info is not available
                             # the user was probably deleted
                             r['actor_home'] = ''
+                history = review_history
                 history.reverse()
 
             except WorkflowException:
