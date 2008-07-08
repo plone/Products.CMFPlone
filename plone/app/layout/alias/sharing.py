7d79ce856284975cfb030b0931fb06f41f5e84f7
Hanno Schlichting <hanno@hannosch.eu>
2008-07-08 15:41:16 +0000

    Adjusted deprecation warnings to point to Plone 4.0 instead of Plone 3.5 since we changed the version numbering again.
    svn path=/plone.app.layout/branches/1.1/; revision=21473

--- plone/app/layout/alias/sharing.py
+++ plone/app/layout/alias/sharing.py
@@ -8,8 +8,9 @@ class FolderLocalroleFormAlias(BrowserView):
     def redirect(self):
         
         log = getLogger('Plone')
-        log.warn("folder_localrole_form is deprecated and will "
-            "be removed in Plone 3.5. Use the /@@sharing view instead. Most likely, "
-            "if you see this message the old template is being referenced in portal_types.")
+        log.warn("folder_localrole_form is deprecated and will be removed in "
+                 "Plone 4.0. Use the /@@sharing view instead. Most likely, "
+                 "if you see this message the old template is being "
+                 "referenced in portal_types.")
         
         self.request.response.redirect('%s/@@sharing' % self.context.absolute_url())
