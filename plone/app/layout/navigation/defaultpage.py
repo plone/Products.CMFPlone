30b411c9c83fca29806b57e085a4f33d6910e7ac
Wichert Akkerman <wichert@wiggy.net>
2008-07-22 12:49:32 +0000

    Refactor default_page: move all logic out of the view to separate methods so they can be called without a request. The request is not used at all so putting the logic into a view just made it harder to access the utility functions, as witnessed by the 3 different APIs that indirectly end up calling this view.
    Deprecate parameters which were not in the interface and were never used.
    
    svn path=/plone.app.layout/branches/1.1/; revision=21791

--- plone/app/layout/navigation/defaultpage.py
+++ plone/app/layout/navigation/defaultpage.py
@@ -1,3 +1,4 @@
+import warnings
 from zope.component import queryUtility
 from zope.interface import implements
 
@@ -14,91 +15,103 @@ class DefaultPage(BrowserView):
     implements(IDefaultPage)
 
     def isDefaultPage(self, obj, context_=None):
-        """Finds out if the given obj is the default page in its parent folder.
+        if context_ is not None:
+            warnings.warn("The context_ parameter for isDefaultPage was never "
+                          "implemented and will be removed in Plone 4.",
+                          DeprecationWarning, 1)
 
-        Only considers explicitly contained objects, either set as index_html,
-        with the default_page property, or using IBrowserDefault.
-        """
-        #XXX: What is this context/obj confusion all about?
-        if context_ is None:
-            context_ = obj
-        parentDefaultPage = self.getDefaultPage(context_)
-        if parentDefaultPage is None or '/' in parentDefaultPage:
-            return False
-        return (parentDefaultPage == obj.getId())
+        return isDefaultPage(aq_inner(self.context), obj)
+
 
     def getDefaultPage(self, context_=None):
-        """Given a folderish item, find out if it has a default-page using
-        the following lookup rules:
+        if context_ is not None:
+            warnings.warn("The context_ parameter for getDefaultPage was "
+                          "never implemented and will be removed in Plone 4.",
+                          DeprecationWarning, 1)
 
-            1. A content object called 'index_html' wins
-            2. If the folder implements IBrowserDefault, query this
-            3. Else, look up the property default_page on the object
-                - Note that in this case, the returned id may *not* be of an
-                  object in the folder, since it could be acquired from a
-                  parent folder or skin layer
-            4. Else, look up the property default_page in site_properties for
-                magic ids and test these
+        return getDefaultPage(aq_inner(self.context))
 
-        The id of the first matching item is then used to lookup a translation
-        and if found, its id is returned. If no default page is set, None is
-        returned. If a non-folderish item is passed in, return None always.
-        """
-        context = aq_inner(self.context)
-        if context_ is None:
-            context_ = context
 
-        # The list of ids where we look for default
-        ids = {}
+def isDefaultPage(container, obj):
+    """Finds out if the given obj is the default page in its parent folder.
 
-        # For BTreeFolders we just use the has_key, otherwise build a dict
-        if hasattr(aq_base(context), 'has_key'):
-            ids = context
-        else:
-            for id in context.objectIds():
-                ids[id] = 1
+    Only considers explicitly contained objects, either set as index_html,
+    with the default_page property, or using IBrowserDefault.
+    """
+    parentDefaultPage = getDefaultPage(container)
+    if parentDefaultPage is None or '/' in parentDefaultPage:
+        return False
+    return (parentDefaultPage == obj.getId())
 
-        # Inline function with default argument.
-        def lookupTranslationId(obj, page):
-            return utils.lookupTranslationId(obj, page, ids)
 
-        # 1. test for contentish index_html
-        if ids.has_key('index_html'):
-            return lookupTranslationId(context, 'index_html')
+def getDefaultPage(context):
+    """Given a folderish item, find out if it has a default-page using
+    the following lookup rules:
 
-        # 2. Test for IBrowserDefault
-        browserDefault = IBrowserDefault(context, None)
-        if browserDefault is not None:
-            fti = context.getTypeInfo()
-            if fti is not None:
-                dynamicFTI = IDynamicViewTypeInformation(fti, None)
-                if dynamicFTI is not None:
-                    page = dynamicFTI.getDefaultPage(context, check_exists=True)
-                    if page is not None:
-                        return lookupTranslationId(context, page)
+        1. A content object called 'index_html' wins
+        2. If the folder implements IBrowserDefault, query this
+        3. Else, look up the property default_page on the object
+            - Note that in this case, the returned id may *not* be of an
+              object in the folder, since it could be acquired from a
+              parent folder or skin layer
+        4. Else, look up the property default_page in site_properties for
+            magic ids and test these
 
-        # 3. Test for default_page property in folder, then skins
-        pages = getattr(aq_base(context), 'default_page', [])
-        if isinstance(pages, basestring):
-            pages = [pages]
-        for page in pages:
-            if page and ids.has_key(page):
-                return lookupTranslationId(context, page)
+    The id of the first matching item is then used to lookup a translation
+    and if found, its id is returned. If no default page is set, None is
+    returned. If a non-folderish item is passed in, return None always.
+    """
+    # The list of ids where we look for default
+    ids = {}
 
-        portal = queryUtility(ISiteRoot)
-        # Might happen during portal creation
-        if portal is not None:
-            for page in pages:
-                if portal.unrestrictedTraverse(page, None):
+    # For BTreeFolders we just use the has_key, otherwise build a dict
+    if hasattr(aq_base(context), 'has_key'):
+        ids = context
+    else:
+        for id in context.objectIds():
+            ids[id] = 1
+
+    # Inline function with default argument.
+    def lookupTranslationId(obj, page):
+        return utils.lookupTranslationId(obj, page, ids)
+
+    # 1. test for contentish index_html
+    if ids.has_key('index_html'):
+        return lookupTranslationId(context, 'index_html')
+
+    # 2. Test for IBrowserDefault
+    browserDefault = IBrowserDefault(context, None)
+    if browserDefault is not None:
+        fti = context.getTypeInfo()
+        if fti is not None:
+            dynamicFTI = IDynamicViewTypeInformation(fti, None)
+            if dynamicFTI is not None:
+                page = dynamicFTI.getDefaultPage(context, check_exists=True)
+                if page is not None:
                     return lookupTranslationId(context, page)
 
-            # 4. Test for default sitewide default_page setting
-            pp = getattr(portal, 'portal_properties', None)
-            if pp is not None:
-                site_properties = getattr(pp, 'site_properties', None)
-                if site_properties is not None:
-                    for page in site_properties.getProperty('default_page', []):
-                        if ids.has_key(page):
-                            return lookupTranslationId(context, page)
+    # 3. Test for default_page property in folder, then skins
+    pages = getattr(aq_base(context), 'default_page', [])
+    if isinstance(pages, basestring):
+        pages = [pages]
+    for page in pages:
+        if page and ids.has_key(page):
+            return lookupTranslationId(context, page)
 
-        return None
+    portal = queryUtility(ISiteRoot)
+    # Might happen during portal creation
+    if portal is not None:
+        for page in pages:
+            if portal.unrestrictedTraverse(page, None):
+                return lookupTranslationId(context, page)
+
+        # 4. Test for default sitewide default_page setting
+        pp = getattr(portal, 'portal_properties', None)
+        if pp is not None:
+            site_properties = getattr(pp, 'site_properties', None)
+            if site_properties is not None:
+                for page in site_properties.getProperty('default_page', []):
+                    if ids.has_key(page):
+                        return lookupTranslationId(context, page)
+
+    return None
