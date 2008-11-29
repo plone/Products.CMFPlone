0bebaaeea03cdbca2d1705f1622b780a1f3ee000
Hanno Schlichting <hanno@hannosch.eu>
2008-11-29 20:25:36 +0000

    Adjusted dashboard to new controlpanel actions approach.
    svn path=/plone.app.layout/trunk/; revision=23491

--- plone/app/layout/dashboard/dashboard.py
+++ plone/app/layout/dashboard/dashboard.py
@@ -1,4 +1,5 @@
 from zope.component import getUtility
+from zope.i18n import translate
 
 from Products.Five.browser import BrowserView
 
@@ -12,7 +13,18 @@ from Products.CMFCore.utils import getToolByName
 class DashboardView(BrowserView):
     """Power the dasbhard
     """
-    
+
+    def actions(self):
+        portal_actions = getToolByName(self.context, 'portal_actions')
+        categories = 'controlpanel/controlpanel_user'
+        actions = portal_actions.listActionInfos(categories=categories)
+        def _title(v):
+            return translate(v.get('title'),
+                             domain='plone',
+                             context=self.request)
+        actions.sort(key=_title)
+        return actions
+
     @memoize
     def empty(self):
         dashboards = [getUtility(IPortletManager, name=name) for name in
