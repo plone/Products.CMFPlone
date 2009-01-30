f19171c6c4eeda8131a267faa1dafddf73ff9fa9
David Convent <davconvent@gmail.com>
2009-01-30 12:03:25 +0000

    added needed fles for the views
    svn path=/plone.app.users/trunk/; revision=24741

---
+++ plone/app/users/browser/profilesettings.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+accountsettings.py
+"""
+
+#import warnings
+#from zope.component import queryAdapter
+#from zope.component import queryUtility
+from zope.interface import implements
+
+#from Acquisition import aq_inner, aq_base
+#from Products.CMFPlone import utils
+from Products.Five.browser import BrowserView
+
+from plone.app.users.browser.interfaces import IPersonalPreferencesPane
+
+class ProfileSettingsPane(BrowserView):
+    implements(IPersonalPreferencesPane)
+
+    
