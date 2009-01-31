47d040827ac3e077ece7f411c9bc603945e2d4ec
David Convent <davconvent@gmail.com>
2009-01-31 15:28:04 +0000

    dumping existing tests in svn for collaborative work
    svn path=/plone.app.users/trunk/; revision=24795

--- plone/app/users/tests/aptc.py
+++ plone/app/users/tests/aptc.py
@@ -1,16 +1,17 @@
-"""Base class for control panel test cases.
+"""Base class for account panel test cases.
 
 This is in a separate module because it's potentially useful to other
-packages which register controlpanels. They should be able to import it
+packages which register accountpanels. They should be able to import it
 without the PloneTestCase.setupPloneSite() side effects.
 """
 
+from zope.component import getMultiAdapter
 from Products.PloneTestCase.PloneTestCase import FunctionalTestCase
 from Products.Five.testbrowser import Browser
 from Products.CMFCore.utils import getToolByName
 
 class AccountPanelTestCase(FunctionalTestCase):
-    """base test case with convenience methods for all control panel tests"""
+    """base test case with convenience methods for all account panel tests"""
 
     def afterSetUp(self):
         super(AccountPanelTestCase, self).afterSetUp()
@@ -18,10 +19,9 @@ class AccountPanelTestCase(FunctionalTestCase):
         self.browser = Browser()
         
         self.uf = self.portal.acl_users
-        self.uf.userFolderAddUser('user', 'secret', [], [])
-        self.mt = getToolByName(self.portal, 'portal_membership')
+        self.portal_state = self.portal.restrictedTraverse('@@plone_portal_state')
 
-    def loginAsMember(self, user='user', pwd='secret'):
+    def loginAsMember(self, user='test_user_1_', pwd='secret'):
         """points the browser to the login screen and logs in as user 'user'."""
         self.browser.open('http://nohost/plone/')
         self.browser.getLink('Log in').click()
