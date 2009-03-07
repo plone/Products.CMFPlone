c803a4618891e0b2c9311974d5ecdade2d66c9d5
Hanno Schlichting <hanno@hannosch.eu>
2009-03-07 10:30:05 +0000

    Prepare release
    svn path=/plone.app.layout/branches/1.1/; revision=25614

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(name='plone.app.layout',
       keywords='plone layout viewlet',
       author='Martin Aspeli',
       author_email='optilude@gmx.net',
-      url='http://svn.plone.org/svn/plone/plone.app.layout',
+      url='http://pypi.python.org/pypi/plone.app.layout',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['plone', 'plone.app'],
@@ -25,11 +25,8 @@ setup(name='plone.app.layout',
       zip_safe=False,
       install_requires=[
         'setuptools',
-        'plone.app.viewletmanager>=1.2dev',
+        'plone.app.viewletmanager',
         'plone.memoize',
         'plone.portlets',
       ],
-      entry_points="""
-      # -*- Entry points: -*-
-      """,
       )
