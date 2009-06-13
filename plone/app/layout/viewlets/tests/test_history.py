b4989223827719bb116b02fdf1c305b587b9ed4d
Alec Mitchell <apm13@columbia.edu>
2009-06-13 00:48:02 +0000

    Add test suite for content history viewlet and prepare 1.2.3 release.
    svn path=/plone.app.layout/branches/1.2/; revision=27403

--- plone/app/layout/viewlets/tests/test_history.py
+++ plone/app/layout/viewlets/tests/test_history.py
@@ -1,6 +1,7 @@
 import unittest
 from plone.app.layout.viewlets.tests.base import ViewletsTestCase
 from plone.app.layout.viewlets.content import WorkflowHistoryViewlet
+from plone.app.layout.viewlets.content import ContentHistoryViewlet
 from DateTime import DateTime
 
 class TestWorkflowHistoryViewlet(ViewletsTestCase):
@@ -60,7 +61,39 @@ class TestWorkflowHistoryViewlet(ViewletsTestCase):
         self.assertEqual(history[0]['actor_home'], '')
 
 
+class TestContentHistoryViewlet(ViewletsTestCase):
+    """
+    Test the workflow history viewlet
+    """
+    def afterSetUp(self):
+        # add document, perform transition, set history for non-existent
+        # member and also None (anonymous)
+        self.folder.invokeFactory('Document', 'd1')
+
+    def test_emptyHistory(self):
+        request = self.app.REQUEST
+        context = getattr(self.folder, 'd1')
+        viewlet = ContentHistoryViewlet(context, request, None, None)
+        viewlet.update()
+        self.assertEqual(viewlet.revisionHistory(), [])
+
+    def test_revisionHistory(self):
+        repo_tool = self.portal.portal_repository
+        request = self.app.REQUEST
+        context = getattr(self.folder, 'd1')
+        self.loginAsPortalOwner()
+        repo_tool.save(context, comment='Initial Revision')
+
+        viewlet = ContentHistoryViewlet(context, request, None, None)
+        viewlet.update()
+
+        history = viewlet.revisionHistory()
+        self.assertEqual(len(history), 1)
+        self.assertEqual(history[0]['comments'], 'Initial Revision')
+
+
 def test_suite():
     suite = unittest.TestSuite()
     suite.addTest(unittest.makeSuite(TestWorkflowHistoryViewlet))
+    suite.addTest(unittest.makeSuite(TestContentHistoryViewlet))
     return suite
