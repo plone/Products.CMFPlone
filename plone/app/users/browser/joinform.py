ea81574e61146f4d8138171a5dc5a3cbdd215e05
Maurits van Rees <maurits@vanrees.org>
2009-10-19 21:46:15 +0000

    pep8/pyflakes to make this code more readable, at least with 80 characters wide.
    svn path=/plone.app.users/trunk/; revision=30718

--- plone/app/users/browser/joinform.py
+++ plone/app/users/browser/joinform.py
@@ -25,51 +25,59 @@ from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 # vocab of the join fields setting in usergroupssettings controlpanel.
 JOIN_CONST = ['username', 'password', 'mail_me']
 
+
 class IJoinSchema(Interface):
 
-    username = schema.ASCIILine(title=_(u'label_user_name', default=u'User Name'),
-                               description=_(u'help_user_name_creation_casesensitive',
-                               default=u"Enter a user name, usually something "
+    username = schema.ASCIILine(
+        title=_(u'label_user_name', default=u'User Name'),
+        description=_(u'help_user_name_creation_casesensitive',
+                      default=u"Enter a user name, usually something "
                                "like 'jsmith'. "
                                "No spaces or special characters. "
                                "Usernames and passwords are case sensitive, "
                                "make sure the caps lock key is not enabled. "
                                "This is the name used to log in."))
 
-    password = schema.Password(title=_(u'label_password', default=u'Password'),
-                               description=_(u'help_password_creation',
-                                             default=u'Minimum 5 characters.'))
+    password = schema.Password(
+        title=_(u'label_password', default=u'Password'),
+        description=_(u'help_password_creation',
+                      default=u'Minimum 5 characters.'))
 
-    password_ctl = schema.Password(title=_(u'label_confirm_password',
-                                    default=u'Confirm password'),
-                               description=_(u'help_confirm_password',
-                                    default=u"Re-enter the password. "
-                                    "Make sure the passwords are identical."))
+    password_ctl = schema.Password(
+        title=_(u'label_confirm_password',
+                default=u'Confirm password'),
+        description=_(u'help_confirm_password',
+                      default=u"Re-enter the password. "
+                      "Make sure the passwords are identical."))
 
-    mail_me = schema.Bool(title=_(u'label_mail_password',
-                                    default=u"Send a mail with the password"),
-                          default=False)
+    mail_me = schema.Bool(
+        title=_(u'label_mail_password',
+                default=u"Send a mail with the password"),
+        default=False)
+
 
 def FullNameWidget(field, request):
-
-    """ Change the description of the widget """
-    
-    field.description = _(u'help_full_name_creation',
-                            default=u"Enter full name, eg. John Smith.")
+    """Widget for fullname field.
+    """
+    field.description = _(
+        u'help_full_name_creation',
+        default=u"Enter full name, eg. John Smith.")
     widget = TextWidget(field, request)
     return widget
+
 
 def EmailWidget(field, request):
-
-    """ Change the description of the widget """
-    
-    field.description = _(u'help_email_creation',
-                    default = u"Enter an email address. "
-                    "This is necessary in case the password is lost. "
-                    "We respect your privacy, and will not give the address "
-                    "away to any third parties or expose it anywhere.")
+    """Widget for email field.
+    """
+    field.description = _(
+        u'help_email_creation',
+        default = u"Enter an email address. "
+        "This is necessary in case the password is lost. "
+        "We respect your privacy, and will not give the address "
+        "away to any third parties or expose it anywhere.")
     widget = TextWidget(field, request)
     return widget
+
 
 class NoCheckBoxWidget(CheckBoxWidget):
     """ A widget used for _not_ displaying the checkbox.
@@ -79,28 +87,30 @@ class NoCheckBoxWidget(CheckBoxWidget):
         """Render the widget to HTML."""
         return ""
 
-def CantChoosePasswordWidget(field, request):
 
+def CantChoosePasswordWidget(field, request):
     """ Change the mail_me field widget so it doesn't display the checkbox """
 
     field.title = u''
     field.readonly = True
-    field.description = _(u'label_password_change_mail',
-                    default=u"A URL will be generated and e-mailed to you; "
-                    "follow the link to reach a page where you can change your "
-                    "password and complete the registration process.")
+    field.description = _(
+        u'label_password_change_mail',
+        default=u"A URL will be generated and e-mailed to you; "
+        "follow the link to reach a page where you can change your "
+        "password and complete the registration process.")
     widget = NoCheckBoxWidget(field, request)
     return widget
+
 
 class JoinForm(PageForm):
 
     """ Dynamically get fields from user data, through admin
         config settings.
     """
-    
+
     label = _(u'heading_registration_form', default=u'Registration Form')
     description = _(u"")
-    template =  ViewPageTemplateFile('pageform_no_portlets.pt')
+    template = ViewPageTemplateFile('pageform_no_portlets.pt')
 
     @property
     def form_fields(self):
@@ -110,11 +120,12 @@ class JoinForm(PageForm):
         """
 
         portal = getUtility(ISiteRoot)
-        props = getToolByName(self.context, 'portal_properties').site_properties
+        portal_props = getToolByName(self.context, 'portal_properties')
+        props = portal_props.site_properties
         join_fields = list(props.getProperty('join_form_fields'))
 
         canSetOwnPassword = not portal.getProperty('validate_email', True)
-        
+
 
         # Check on required join fields
         #
@@ -126,21 +137,21 @@ class JoinForm(PageForm):
             # Add password if needed
             #
             if not 'password' in join_fields:
-                
+
                 join_fields.insert(join_fields.index('username') + 1,
                                    'password')
 
             # Add password_ctl after password
             #
             if not 'password_ctl' in join_fields:
-                
+
                 join_fields.insert(join_fields.index('password') + 1,
                                    'password_ctl')
 
             # Add email_me after password_ctl
             #
             if not 'mail_me' in join_fields:
-                
+
                 join_fields.insert(join_fields.index('password_ctl') + 1,
                                    'mail_me')
 
@@ -171,8 +182,8 @@ class JoinForm(PageForm):
         #
         return form.Fields(*[all_fields[id] for id in join_fields])
 
+    # Actions validators
 
-    #Actions validators
     def validate_registration(self, action, data):
         """
         specific business logic for this join form
@@ -214,7 +225,8 @@ class JoinForm(PageForm):
                 password = self.widgets['password'].getInputValue()
                 if len(password) < 5:
                     err_str = _(u'Passwords must contain at least 5 letters.')
-                    errors.append(WidgetInputError('password', u'label_password', err_str))
+                    errors.append(WidgetInputError(
+                            'password', u'label_password', err_str))
                     self.widgets['password'].error = err_str
 
 
@@ -224,8 +236,10 @@ class JoinForm(PageForm):
             username = self.widgets['username'].getInputValue()
             portal = getUtility(ISiteRoot)
             if username == portal.getId():
-                err_str = _(u'This username is reserved. Please choose a different name.')
-                errors.append(WidgetInputError('username', u'label_username', err_str))
+                err_str = _(u"This username is reserved. Please choose a "
+                            "different name.")
+                errors.append(WidgetInputError(
+                        'username', u'label_username', err_str))
                 self.widgets['username'].error = err_str
 
 
@@ -233,24 +247,27 @@ class JoinForm(PageForm):
         if not 'username' in error_keys:
             username = self.widgets['username'].getInputValue()
             if not registration.isMemberIdAllowed(username):
-                err_str = _(u'The login name you selected is already in use or is not valid. Please choose another.')
-                errors.append(WidgetInputError('username', u'label_username', err_str))
+                err_str = _(u"The login name you selected is already in use "
+                            "or is not valid. Please choose another.")
+                errors.append(WidgetInputError(
+                        'username', u'label_username', err_str))
                 self.widgets['username'].error = err_str
 
-        
+
         # Skip this check if email was already in error list
         if not 'email' in error_keys:
             if 'email' in form_field_names:
                 email = self.widgets['email'].getInputValue()
                 if not registration.isValidEmail(email):
                     err_str = _(u'You must enter a valid email address.')
-                    errors.append(WidgetInputError('email', u'label_email', err_str))
+                    errors.append(WidgetInputError(
+                            'email', u'label_email', err_str))
                     self.widgets['email'].error = err_str
 
         return errors
-        
 
-    @form.action(_(u'label_register', default=u'Register'), validator='validate_registration', name=u'register')
+    @form.action(_(u'label_register', default=u'Register'),
+                 validator='validate_registration', name=u'register')
     def action_join(self, action, data):
 
 
@@ -262,7 +279,8 @@ class JoinForm(PageForm):
         password = data.get('password') or registration.generatePassword()
 
         try:
-            registration.addMember(username, password, properties=data, REQUEST=self.request)
+            registration.addMember(username, password, properties=data,
+                                   REQUEST=self.request)
         except (AttributeError, ValueError), err:
 
             IStatusMessage(self.request).addStatusMessage(_(err), type="error")
@@ -272,22 +290,29 @@ class JoinForm(PageForm):
             try:
                 registration.registeredNotify(username)
             except ConflictError:
-                IStatusMessage(self.request).addStatusMessage(_("Conflict error"),
-                                                          type="error")
+                IStatusMessage(self.request).addStatusMessage(
+                    _("Conflict error"), type="error")
                 return
             except Exception:
                 if portal.validate_email:
-                    IStatusMessage(self.request).addStatusMessage(_("Couldn't send mail"),
-                                                              type="error")
+                    IStatusMessage(self.request).addStatusMessage(
+                        _("Couldn't send mail"), type="error")
 
-                    self.context.acl_users.userFolderDelUsers([username,], REQUEST=self.request)
-                    self.status = (_(u'status_fatal_password_mail',
-                            default=u'Failed to create your account: we were unable to send your password to your email address: ${address}',
-                            mapping={u'address' : data.get('email', '')}))
+                    self.context.acl_users.userFolderDelUsers(
+                        [username], REQUEST=self.request)
+                    self.status = (
+                        _(u'status_fatal_password_mail',
+                          default=u"Failed to create your account: we were "
+                          "unable to send your password to your email "
+                          "address: ${address}",
+                          mapping={u'address': data.get('email', '')}))
                     return
                 else:
-                    self.status = (_(u'status_nonfatal_password_mail',
-                            default=u'You account has been created, but we were unable to send your password to your email address: ${address}',
-                            mapping={u'address' : data.get('email', '')}))
+                    self.status = (
+                        _(u'status_nonfatal_password_mail',
+                          default=u"You account has been created, but we were "
+                          "unable to send your password to your email "
+                          "address: ${address}",
+                          mapping={u'address': data.get('email', '')}))
 
         return self.context.unrestrictedTraverse('registered')()
