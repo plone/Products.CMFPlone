6e6a00e45ea3cfcab7390add1b80a81c201fc9cf
Hanno Schlichting <hanno@hannosch.eu>
2009-10-27 19:48:46 +0000

    Merged the ``selectedTabs`` Python script into the GlobalSectionsViewlet.
    svn path=/plone.app.layout/branches/2.0/; revision=30888

--- plone/app/layout/viewlets/common.py
+++ plone/app/layout/viewlets/common.py
@@ -152,11 +152,38 @@ class GlobalSectionsViewlet(ViewletBase):
                                            name='portal_tabs_view')
         self.portal_tabs = portal_tabs_view.topLevelTabs()
 
-        selectedTabs = context.restrictedTraverse('selectedTabs')
-        self.selected_tabs = selectedTabs('index_html',
-                                          context,
-                                          self.portal_tabs)
+        self.selected_tabs = self.selectedTabs(portal_tabs=self.portal_tabs)
         self.selected_portal_tab = self.selected_tabs['portal']
+
+    def selectedTabs(self, default_tab='index_html', portal_tabs=()):
+        plone_url = getToolByName(self.context, 'portal_url')()
+        plone_url_len = len(plone_url)
+        request = self.request
+        valid_actions = []
+
+        url = request['URL']
+        path = url[plone_url_len:]
+
+        for action in portal_tabs:
+            if not action['url'].startswith(plone_url):
+                # In this case the action url is an external link. Then, we
+                # avoid issues (bad portal_tab selection) continuing with next
+                # action.
+                continue
+            action_path = action['url'][plone_url_len:]
+            if not action_path.startswith('/'):
+                action_path = '/' + action_path
+            if path.startswith(action_path):
+                # Make a list of the action ids, along with the path length
+                # for choosing the longest (most relevant) path.
+                valid_actions.append((len(action_path), action['id']))
+
+        # Sort by path length, the longest matching path wins
+        valid_actions.sort()
+        if valid_actions:
+            return {'portal' : valid_actions[-1][1]}
+
+        return {'portal' : default_tab}
 
 
 class PersonalBarViewlet(ViewletBase):
