0729de265f14501973acf4111251bfd710993b22
Hanno Schlichting <hanno@hannosch.eu>
2009-12-24 16:28:56 +0000

    Whitespace and import cleanup
    svn path=/plone.app.layout/trunk/; revision=32621

--- plone/app/layout/viewlets/common.py
+++ plone/app/layout/viewlets/common.py
@@ -1,17 +1,18 @@
-from zope.interface import implements, alsoProvides
-from zope.component import getMultiAdapter
-from zope.viewlet.interfaces import IViewlet
-from zope.deprecation.deprecation import deprecate
+from cgi import escape
 
-from plone.app.layout.globals.interfaces import IViewView 
+from zope.component import getMultiAdapter
+from zope.deprecation.deprecation import deprecate
+from zope.interface import implements, alsoProvides
+from zope.viewlet.interfaces import IViewlet
 
 from AccessControl import getSecurityManager
 from Acquisition import aq_base, aq_inner
+from Products.CMFCore.utils import getToolByName
 from Products.CMFPlone.utils import safe_unicode
 from Products.Five.browser import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-from Products.CMFCore.utils import getToolByName
-from cgi import escape
+
+from plone.app.layout.globals.interfaces import IViewView
 
 
 class ViewletBase(BrowserView):
