b1b64d43a01aa0809fcdaaf78d11618a81589430
Eric Steele <esteele@plone.org>
2010-01-10 20:28:24 +0000

    Use MockMailHost instead as we were getting some errors attempting to use the actual one.
    svn path=/plone.app.users/trunk/; revision=32942

--- plone/app/users/tests/base.py
+++ plone/app/users/tests/base.py
@@ -8,19 +8,39 @@ without the PloneTestCase.setupPloneSite() side effects.
 from Products.PloneTestCase.PloneTestCase import FunctionalTestCase
 from Products.Five.testbrowser import Browser
 
+from Acquisition import aq_base
+from zope.component import getSiteManager
+from Products.CMFPlone.tests.utils import MockMailHost
+from Products.MailHost.interfaces import IMailHost
+from Products.CMFCore.utils import getToolByName
+
 
 class TestCase(FunctionalTestCase):
     """base test case which adds amin user"""
+
 
     def afterSetUp(self):
         super(TestCase, self).afterSetUp()
         self.browser = Browser()
         self.portal.acl_users._doAddUser('admin', 'secret', ['Manager'], [])
 
+        self.portal._original_MailHost = self.portal.MailHost
+        self.portal.MailHost = mailhost = MockMailHost('MailHost')
+        sm = getSiteManager(context=self.portal)
+        sm.unregisterUtility(provided=IMailHost)
+        sm.registerUtility(mailhost, provided=IMailHost)
+
+    def beforeTearDown(self):
+        self.portal.MailHost = self.portal._original_MailHost
+        sm = getSiteManager(context=self.portal)
+        sm.unregisterUtility(provided=IMailHost)
+        sm.registerUtility(aq_base(self.portal._original_MailHost), provided=IMailHost)
+
+
     def setMailHost(self):
         self.portal.MailHost.smtp_host = 'localhost'
         setattr(self.portal, 'email_from_address', 'admin@foo.com')
-        
+
     def unsetMailHost(self):
         self.portal.MailHost.smtp_host = ''
         setattr(self.portal, 'email_from_address', '')
