e9fa940b8c6e33095887c9d8024d71c16b4f5ff1
Hanno Schlichting <hanno@hannosch.eu>
2010-02-14 15:58:23 +0000

    Moved the prepareObjectTabs method from the @@plone view to the contentviews viewlet and introduced a class for the viewlet.
    svn path=/plone.app.layout/trunk/; revision=33984

--- plone/app/layout/viewlets/common.py
+++ plone/app/layout/viewlets/common.py
@@ -1,6 +1,8 @@
 from cgi import escape
 from datetime import date
+from urllib import unquote
 
+from plone.memoize.view import memoize
 from zope.component import getMultiAdapter
 from zope.deprecation.deprecation import deprecate
 from zope.interface import implements, alsoProvides
@@ -224,6 +226,86 @@ class PersonalBarViewlet(ViewletBase):
                 self.user_name = fullname
             else:
                 self.user_name = userid
+
+
+class ContentViewsViewlet(ViewletBase):
+    index = ViewPageTemplateFile('contentviews.pt')
+
+    @memoize
+    def prepareObjectTabs(self, default_tab='view', sort_first=['folderContents']):
+        """Prepare the object tabs by determining their order and working
+        out which tab is selected. Used in global_contentviews.pt
+        """
+        context = aq_inner(self.context)
+        context_url = context.absolute_url()
+        context_fti = context.getTypeInfo()
+
+        context_state = getMultiAdapter(
+            (context, self.request), name=u'plone_context_state'
+        )
+        actions = context_state.actions
+
+        action_list = []
+        if context_state.is_structural_folder():
+            action_list = actions('folder')
+        action_list.extend(actions('object'))
+
+        tabs = []
+        found_selected = False
+        fallback_action = None
+
+        request_url = self.request['ACTUAL_URL']
+        request_url_path = request_url[len(context_url):]
+
+        if request_url_path.startswith('/'):
+            request_url_path = request_url_path[1:]
+
+        for action in action_list:
+            item = {'title'    : action['title'],
+                    'id'       : action['id'],
+                    'url'      : '',
+                    'selected' : False}
+
+            action_url = action['url'].strip()
+            starts = action_url.startswith
+            if starts('http') or starts('javascript'):
+                item['url'] = action_url
+            else:
+                item['url'] = '%s/%s' % (context_url, action_url)
+
+            action_method = item['url'].split('/')[-1]
+
+            # Action method may be a method alias:
+            # Attempt to resolve to a template.
+            action_method = context_fti.queryMethodID(
+                action_method, default=action_method
+            )
+            if action_method:
+                request_action = unquote(request_url_path)
+                request_action = context_fti.queryMethodID(
+                    request_action, default=request_action
+                )
+                if action_method == request_action:
+                    item['selected'] = True
+                    found_selected = True
+
+            current_id = item['id']
+            if current_id == default_tab:
+                fallback_action = item
+
+            tabs.append(item)
+
+        if not found_selected and fallback_action is not None:
+            fallback_action['selected'] = True
+
+        def sortOrder(tab):
+            try:
+                return sort_first.index(tab['id'])
+            except ValueError:
+                return 255
+
+        tabs.sort(key=sortOrder)
+        return tabs
 
 
 class ManagePortletsFallbackViewlet(ViewletBase):
--- plone/app/layout/viewlets/tests/test_common.py
+++ plone/app/layout/viewlets/tests/test_common.py
@@ -1,25 +1,88 @@
 import unittest
-from zope import interface
+
+from zope.interface import alsoProvides
+from zope.interface import directlyProvides
+from zope.interface import noLongerProvides
+
+from Products.CMFPlone.interfaces import INonStructuralFolder
+
 from plone.app.layout.viewlets.tests.base import ViewletsTestCase
 from plone.app.layout.viewlets.common import ViewletBase
+from plone.app.layout.viewlets.common import ContentViewsViewlet
 from plone.app.layout.navigation.interfaces import INavigationRoot
 
+
 class TestViewletBase(ViewletsTestCase):
+    """Test the base class for the viewlets.
     """
-    Test the base class for the viewlets
-    """
+
     def test_update(self):
         request = self.app.REQUEST
         self.setRoles(['Manager', 'Member'])
         self.portal.invokeFactory('Folder', 'f1')
         context = getattr(self.portal, 'f1')
-        interface.alsoProvides(context, INavigationRoot)
+        alsoProvides(context, INavigationRoot)
         viewlet = ViewletBase(context, request, None, None)
         viewlet.update()
         self.assertEqual(viewlet.site_url, "http://nohost/plone")
         self.assertEqual(viewlet.navigation_root_url, "http://nohost/plone/f1")
 
+
+class TestContentViewsViewlet(ViewletsTestCase):
+    """Test the content views viewlet.
+    """
+
+    def afterSetUp(self):
+        self.folder.invokeFactory('Document', 'test',
+                                  title='Test default page')
+
+    def _invalidateRequestMemoizations(self):
+        try:
+            del self.app.REQUEST.__annotations__
+        except AttributeError:
+            pass
+
+    def testPrepareObjectTabsOnPortalRoot(self):
+        self._invalidateRequestMemoizations()
+        self.loginAsPortalOwner()
+        self.app.REQUEST['ACTUAL_URL'] = self.portal.absolute_url()
+        view = ContentViewsViewlet(self.portal, self.app.REQUEST, None)
+        tabs = view.prepareObjectTabs()
+        self.assertEquals(tabs[0]['id'], 'folderContents')
+        self.assertEquals(['view'], [t['id'] for t in tabs if t['selected']])
+
+    def testPrepareObjectTabsNonFolder(self):
+        self._invalidateRequestMemoizations()
+        self.loginAsPortalOwner()
+        self.app.REQUEST['ACTUAL_URL'] = self.folder.test.absolute_url()
+        view = ContentViewsViewlet(self.folder.test, self.app.REQUEST, None)
+        tabs = view.prepareObjectTabs()
+        self.assertEquals(0, len([t for t in tabs if t['id'] == 'folderContents']))
+        self.assertEquals(['view'], [t['id'] for t in tabs if t['selected']])
+
+    def testPrepareObjectTabsNonStructuralFolder(self):
+        self._invalidateRequestMemoizations()
+        self.loginAsPortalOwner()
+        self.app.REQUEST['ACTUAL_URL'] = self.folder.absolute_url()
+        directlyProvides(self.folder, INonStructuralFolder)
+        view = ContentViewsViewlet(self.folder, self.app.REQUEST, None)
+        tabs = view.prepareObjectTabs()
+        noLongerProvides(self.folder, INonStructuralFolder)
+        self.assertEquals(0, len([t for t in tabs if t['id'] == 'folderContents']))
+        self.assertEquals(['view'], [t['id'] for t in tabs if t['selected']])
+
+    def testPrepareObjectTabsDefaultView(self):
+        self._invalidateRequestMemoizations()
+        self.loginAsPortalOwner()
+        self.app.REQUEST['ACTUAL_URL'] = self.folder.test.absolute_url() + '/edit'
+        view = ContentViewsViewlet(self.folder.test, self.app.REQUEST, None)
+        tabs = view.prepareObjectTabs()
+        self.assertEquals(0, len([t for t in tabs if t['id'] == 'folderContents']))
+        self.assertEquals(['edit'], [t['id'] for t in tabs if t['selected']])
+
+
 def test_suite():
     suite = unittest.TestSuite()
     suite.addTest(unittest.makeSuite(TestViewletBase))
+    suite.addTest(unittest.makeSuite(TestContentViewsViewlet))
     return suite
