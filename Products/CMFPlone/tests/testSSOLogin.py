dd1b0acb06dd2b88b1992bb809cd82e756ca17ef
Laurence Rowe <l@lrowe.co.uk>
2010-12-29 17:45:50 +0000

    Fix sso login using iframe to respect came_from
    svn path=/Products.CMFPlone/trunk/; revision=46536

--- Products/CMFPlone/tests/testSSOLogin.py
+++ Products/CMFPlone/tests/testSSOLogin.py
@@ -78,6 +78,29 @@ class TestSSOLogin(SSOLoginTestCase):
         browser.getLink('Log out').click()
         browser.getLink('Log in')
 
+    def test_requireLogin(self):
+        browser = self.browser
+        browser.handleErrors = True # So unauthorized renders a login form
+        # Login to the central portal
+        browser.open(self.login_portal.absolute_url())
+        browser.getLink('Log in').click()
+        browser.getControl(name='__ac_name').value = ptc.default_user
+        browser.getControl(name='__ac_password').value = ptc.default_password
+        browser.getControl(name='submit').click()
+        # Check we are logged in centrally
+        browser.getLink('Log out')
+        # But not on the other portal
+        browser.open(self.portal.absolute_url())
+        browser.getLink('Log in')
+        # Now open the protected doc
+        protected_url = self.folder.absolute_url() + '/folder_contents'
+        browser.open(protected_url)
+        # Without javascript we must click through
+        self.assertEqual(browser.getControl(name='came_from').value, protected_url)
+        browser.getForm('external_login_form').submit()
+        self.assertEqual(browser.url, protected_url)
+        browser.getLink('Log out')
+
 
 class TestSSOLoginIframe(SSOLoginTestCase):
 
@@ -88,7 +111,7 @@ class TestSSOLoginIframe(SSOLoginTestCase):
             site_properties = portal.portal_properties.site_properties
             site_properties._updateProperty('external_login_iframe', True)
 
-    def test_loginAndLogout(self):
+    def test_loginAndLogoutSSO(self):
         browser = self.browser
         browser.open(self.portal.absolute_url())
         browser.getLink('Log in').click()
@@ -133,6 +156,33 @@ class TestSSOLoginIframe(SSOLoginTestCase):
         browser.open(self.another_portal.absolute_url())
         browser.getLink('Log out').click()
         browser.getLink('Log in')
+
+    def test_requireLoginSSO(self):
+        browser = self.browser
+        browser.handleErrors = True # So unauthorized renders a login form
+        # Login to the central portal
+        browser.open(self.login_portal.absolute_url())
+        browser.getLink('Log in').click()
+        browser.getControl(name='__ac_name').value = ptc.default_user
+        browser.getControl(name='__ac_password').value = ptc.default_password
+        browser.getControl(name='submit').click()
+        # Check we are logged in centrally
+        browser.getLink('Log out')
+        # But not on the other portal
+        browser.open(self.portal.absolute_url())
+        browser.getLink('Log in')
+        # Now open the protected doc
+        protected_url = self.folder.absolute_url() + '/folder_contents'
+        browser.open(protected_url)
+        # The test browser does not support iframes
+        form = browser.getForm(name='login_form')
+        self.assertEqual(browser.getControl(name='came_from').value, protected_url)
+        form.submit()
+        # Without javascript we must click through
+        self.assertEqual(browser.getControl(name='came_from').value, protected_url)
+        browser.getForm('external_login_form').submit()
+        self.assertEqual(browser.url, protected_url)
+        browser.getLink('Log out')
 
 
 def test_suite():
