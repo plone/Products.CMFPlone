9a8c4f01b59b8fff26fd330085419638ebb745fd
Hanno Schlichting <hanno@hannosch.eu>
2011-05-27 12:35:28 +0000

    Add a test that resembles the actual code being called in the batch_macros template
    svn path=/Products.CMFPlone/trunk/; revision=50066

--- Products/CMFPlone/tests/testBatch.py
+++ Products/CMFPlone/tests/testBatch.py
@@ -1,5 +1,7 @@
 import unittest
 
+from Products.ZCatalog.Lazy import LazyMap
+
 from Products.CMFPlone.PloneBatch import Batch
 
 
@@ -9,3 +11,24 @@ class TestBatch(unittest.TestCase):
         batch = Batch(range(100), size=10, start=10)
         self.assertEqual([b for b in batch],
             [10, 11, 12, 13, 14, 15, 16, 17, 18, 19])
+
+    def test_batch_lazy_map(self):
+        def get(key):
+            return key
+        sequence = LazyMap(get, range(80, 90), actual_result_count=95)
+        batch = Batch(sequence, size=10, start=80)
+        self.assertEqual([b for b in batch],
+            [80, 81, 82, 83, 84, 85, 86, 87, 88, 89])
+
+        self.assertEqual(batch.numpages, 10)
+        self.assertEqual(batch.pagenumber, 9)
+        self.assertEqual(batch.navlist, [6, 7, 8, 9, 10])
+        self.assertEqual(batch.leapback, [])
+        self.assertEqual(batch.prevlist, [6, 7, 8])
+        self.assertEqual(batch.previous.length, 10)
+        self.assertEqual(batch.next.length, 5)
+        self.assertEqual(batch.pageurl({}), 'b_start:int=80')
+        self.assertEqual(batch.prevurls({}),
+            [(6, 'b_start:int=50'), (7, 'b_start:int=60'),
+            (8, 'b_start:int=70')])
+        self.assertEqual(batch.nexturls({}), [(10, 'b_start:int=90')])
