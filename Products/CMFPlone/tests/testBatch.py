26e04e0c5dad87b8491b3ff82323673feb3e62d2
Hanno Schlichting <hanno@hannosch.eu>
2011-05-27 11:54:58 +0000

    PEP8 / style
    svn path=/Products.CMFPlone/trunk/; revision=50064

--- Products/CMFPlone/tests/testBatch.py
+++ Products/CMFPlone/tests/testBatch.py
@@ -1,27 +1,11 @@
-#
-# ActionsTool tests
-#
-
-from Products.CMFPlone.tests import PloneTestCase
-
-from traceback import format_exception
-from zope.i18nmessageid.message import Message
-
-from Acquisition import Explicit
-from OFS.SimpleItem import Item
 from Products.CMFPlone.PloneBatch import Batch
+from Products.CMFPlone.tests import PloneTestCase
 
 
 class TestBatch(PloneTestCase.PloneTestCase):
 
-    def testBatch(self):
+    def test_batch(self):
         sequence = range(1000)
         batch = Batch(sequence, size=10, start=10)
-        self.assertEqual([b for b in batch], [10, 11, 12, 13, 14, 15, 16, 17, 18, 19])
-
-
-def test_suite():
-    from unittest import TestSuite, makeSuite
-    suite = TestSuite()
-    suite.addTest(makeSuite(TestBatch))
-    return suite
+        self.assertEqual([b for b in batch],
+            [10, 11, 12, 13, 14, 15, 16, 17, 18, 19])
