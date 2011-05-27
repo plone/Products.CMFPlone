922d0411678f05540ecbd62c35a4fa80c52261e7
Hanno Schlichting <hanno@hannosch.eu>
2011-05-27 12:17:41 +0000

    Fixed PloneBatch to work for non-lazy sequences.
    svn path=/Products.CMFPlone/trunk/; revision=50065

--- Products/CMFPlone/tests/testBatch.py
+++ Products/CMFPlone/tests/testBatch.py
@@ -1,11 +1,11 @@
+import unittest
+
 from Products.CMFPlone.PloneBatch import Batch
-from Products.CMFPlone.tests import PloneTestCase
 
 
-class TestBatch(PloneTestCase.PloneTestCase):
+class TestBatch(unittest.TestCase):
 
-    def test_batch(self):
-        sequence = range(1000)
-        batch = Batch(sequence, size=10, start=10)
+    def test_batch_no_lazy(self):
+        batch = Batch(range(100), size=10, start=10)
         self.assertEqual([b for b in batch],
             [10, 11, 12, 13, 14, 15, 16, 17, 18, 19])
