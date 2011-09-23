plone/Products.CMFPlone

commit 6228c3452f7ae9ac758f199123e0cc05f57817ec

Treat aliases to the '(Default)' view of a content type also as a view template (providing IViewView). Fixes #8198
svn path=/plone.app.layout/trunk/; revision=52328

plone/app/layout/globals/context.py
@@ -121,6 +121,11 @@ class ContextState(BrowserView):
         last_part = last_part[1:]
         if last_part == 'view':
             return True
+        context = aq_inner(self.context)
+        browserDefault = IBrowserDefault(context, None)
+        fti = browserDefault.getTypeInfo()
+        if fti.getMethodAliases().get(last_part) == '(Default)':
+            return True
 
         template_id = self.view_template_id()
         if last_part == template_id:

plone/app/layout/globals/tests/test_context.py
@@ -1,6 +1,7 @@
 from plone.app.layout.globals.tests.base import GlobalsTestCase
 
 from zope.interface import directlyProvides
+from Products.CMFDynamicViewFTI.interfaces import IBrowserDefault
 from Products.CMFPlone.interfaces import INonStructuralFolder
 from Products.CMFPlone.utils import _createObjectByType
 from Products.PloneTestCase import PloneTestCase
@@ -138,6 +139,21 @@ class TestContextStateView(GlobalsTestCase):
         self.folder.setLayout('foo_view')
         self.app.REQUEST['ACTUAL_URL'] = self.folder.absolute_url() + '/bar_view'
         self.assertEquals(self.fview.is_view_template(), False)
+        self.assertEquals(self.dview.is_view_template(), False)
+
+    def test_is_view_template_edit(self):
+        self.app.REQUEST['ACTUAL_URL'] = self.folder.absolute_url() + '/edit'
+        self.assertEquals(self.fview.is_view_template(), False)
+        self.assertEquals(self.dview.is_view_template(), False)
+
+    def test_is_view_template_alias(self):
+        browserDefault = IBrowserDefault(self.folder, None)
+        fti = browserDefault.getTypeInfo()
+        aliases = fti.getMethodAliases()
+        aliases['foo_alias'] = '(Default)'
+        fti.setMethodAliases(aliases)
+        self.app.REQUEST['ACTUAL_URL'] = self.folder.absolute_url() + '/foo_alias'
+        self.assertEquals(self.fview.is_view_template(), True)
         self.assertEquals(self.dview.is_view_template(), False)
 
     def test_object_url(self):

commit bcf8876700e4cc40d7e3202d0d88ed9dee58c532

Fixed error introduced by previous commit r52328: AttributeError: 'NoneType' object has no attribute 'getTypeInfo'. Refs #8198
svn path=/plone.app.layout/trunk/; revision=52342

plone/app/layout/globals/context.py
@@ -123,9 +123,10 @@ class ContextState(BrowserView):
             return True
         context = aq_inner(self.context)
         browserDefault = IBrowserDefault(context, None)
-        fti = browserDefault.getTypeInfo()
-        if fti.getMethodAliases().get(last_part) == '(Default)':
-            return True
+        if browserDefault is not None:
+            fti = browserDefault.getTypeInfo()
+            if fti.getMethodAliases().get(last_part) == '(Default)':
+                return True
 
         template_id = self.view_template_id()
         if last_part == template_id: