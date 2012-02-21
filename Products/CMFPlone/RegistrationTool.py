4327bd447f7b22dd18150c12a0e78c0e1beca9bd
Maurits van Rees <maurits@vanrees.org>
2012-02-21 12:38:00 +0100

    Extra dot at end of sentence.

--- Products/CMFPlone/RegistrationTool.py
+++ Products/CMFPlone/RegistrationTool.py
@@ -74,7 +74,7 @@ def get_member_by_login_name(context, login_name, raise_exceptions=True):
             raise ValueError(
                 _(u'Multiple users found with the same login name.'))
     if member is None and raise_exceptions:
-        raise ValueError(_(u'The username you entered could not be found'))
+        raise ValueError(_(u'The username you entered could not be found.'))
     return member
 
 # seed the random number generator
