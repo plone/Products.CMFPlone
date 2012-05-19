05d57ccac052ea146ccb3d4deb8ee56ee0f23f34
Joel Kleier <joel.kleier@gmail.com>
2012-05-19 12:15:02 -0400

    pep8/consistency cleanup

--- Products/CMFPlone/skins/plone_scripts/getAllowedTypes.py
+++ Products/CMFPlone/skins/plone_scripts/getAllowedTypes.py
@@ -8,6 +8,6 @@
 ##title=Return a list of the content types allowed here filtered by getNotAddableTypes
 
 filterOut = context.getNotAddableTypes()
-types = context.sortObjects( context.allowedContentTypes() )
+types = context.sortObjects(context.allowedContentTypes())
 
-return [ ctype for ctype in types if ctype.getId() not in filterOut ]
+return [ctype for ctype in types if ctype.getId() not in filterOut]
