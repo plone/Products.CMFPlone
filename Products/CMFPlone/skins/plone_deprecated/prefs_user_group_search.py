925bda7387451766a23dcefb743041f0e2d27195
Joel Kleier <joel.kleier@gmail.com>
2012-05-19 16:33:34 -0400

    pep8/consistency cleanup

--- Products/CMFPlone/skins/plone_deprecated/prefs_user_group_search.py
+++ Products/CMFPlone/skins/plone_deprecated/prefs_user_group_search.py
@@ -6,7 +6,7 @@
 ##bind subpath=traverse_subpath
 ##parameters=searchstring, restrict, return_form=None, ignore=[]
 ##title=Valid Search Resriction
-##
+
 #MembershipTool.searchForMembers
 
 groups_tool = context.portal_groups
@@ -15,19 +15,25 @@ groupsList = []
 usersList = []
 
 if restrict != "users":
-    groupsList = groups_tool.searchForGroups(REQUEST=None, title_or_name=searchstring)
+    groupsList = groups_tool.searchForGroups(REQUEST=None,
+                                             title_or_name=searchstring)
     groupsList.sort(key=lambda x: x.getGroupTitleOrName().lower())
 
 if restrict != "groups":
     usersList = members_tool.searchForMembers(REQUEST=None, name=searchstring)
-    usersList.sort(key=lambda x: x is not None and x.getProperty('fullname').lower() if x.getProperty('fullname') else x.getId().lower())
+    usersList.sort(key=lambda x: x is not None
+                                 and x.getProperty('fullname').lower()
+                                 if x.getProperty('fullname')
+                                 else x.getId().lower())
 
 retlist = groupsList + usersList
 
 if ignore:
-  retlist = [r for r in retlist if r not in ignore]
+    retlist = [r for r in retlist if r not in ignore]
 
 # reorder retlist?
 if return_form:
-    context.REQUEST.RESPONSE.redirect( context.absolute_url() + '/' + return_form )
+    context.REQUEST.RESPONSE.redirect(context.absolute_url()
+                                      + '/'
+                                      + return_form)
 return retlist
