0d44b100b05d9ac3dbc2c9528a2c4ff1ad767b52
Joel Kleier <joel.kleier@gmail.com>
2012-05-19 18:05:01 -0400

    pep8/consistency cleanup

--- Products/CMFPlone/skins/plone_prefs/prefs_error_log_setProperties.py
+++ Products/CMFPlone/skins/plone_prefs/prefs_error_log_setProperties.py
@@ -6,13 +6,13 @@
 ##bind subpath=traverse_subpath
 ##parameters=keep_entries,ignored_exceptions,copy_to_zlog=0
 ##title=
-##
 
 from Products.CMFPlone import PloneMessageFactory as _
 
-request=context.REQUEST
+request = context.REQUEST
 
-context.error_log.setProperties(keep_entries,copy_to_zlog,ignored_exceptions)
+context.error_log.setProperties(keep_entries, copy_to_zlog, ignored_exceptions)
 context.plone_utils.addPortalMessage(_(u'Changes made.'))
 
-return request.RESPONSE.redirect(context.absolute_url() + '/prefs_error_log_form')
+return request.RESPONSE.redirect(
+            context.absolute_url() + '/prefs_error_log_form')
