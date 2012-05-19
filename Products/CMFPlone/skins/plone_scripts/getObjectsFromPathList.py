1a193fefa5e2005101a0364d25cb0dc3942cdd1e
Joel Kleier <joel.kleier@gmail.com>
2012-05-19 12:18:04 -0400

    pep8/consistency cleanup

--- Products/CMFPlone/skins/plone_scripts/getObjectsFromPathList.py
+++ Products/CMFPlone/skins/plone_scripts/getObjectsFromPathList.py
@@ -6,16 +6,17 @@
 ##bind subpath=traverse_subpath
 ##parameters=paths=[],batch=False,b_size=100
 ##title=method to turn a list of paths into a list of objects
-##
+
 from zExceptions import Forbidden
 if container.REQUEST.get('PUBLISHED') is script:
-   raise Forbidden('Script may not be published.')
+    raise Forbidden('Script may not be published.')
 
 contents = []
 portal = context.portal_url.getPortalObject()
 for path in paths:
     obj = portal.restrictedTraverse(str(path), None)
-    if obj is not None: contents.append(obj)
+    if obj is not None:
+        contents.append(obj)
 
 if batch:
     from Products.CMFPlone import Batch
