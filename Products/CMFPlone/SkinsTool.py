a0171f063f12638106331c4d7983e12b53ed697f
Hanno Schlichting <hanno@hannosch.eu>
2012-05-24 16:52:14 -0400

    Let theme bundles and skins be friends again.
    When adding, changing or removing skins, automatically recook all resource registries, to keep theme bundles in-sync. This mirrors the event subscriber support used in plone.app.registry for the same purpose.

--- Products/CMFPlone/SkinsTool.py
+++ Products/CMFPlone/SkinsTool.py
@@ -1,6 +1,10 @@
 from AccessControl import ClassSecurityInfo
 from App.class_init import InitializeClass
+from Products.CMFCore.permissions import ManagePortal
 from Products.CMFCore.SkinsTool import SkinsTool as BaseTool
+from Products.ResourceRegistries.tools.BaseRegistry import \
+    cookWhenChangingSettings
+
 from Products.CMFPlone.PloneBaseTool import PloneBaseTool
 
 
@@ -12,6 +16,22 @@ class SkinsTool(PloneBaseTool, BaseTool):
 
     default_skin = ''
     request_varname = 'plone_skin'
+
+    security.declareProtected(ManagePortal, 'addSkinSelection')
+    def addSkinSelection(self, skinname, skinpath, test=0, make_default=0):
+        super(SkinsTool, self).addSkinSelection(skinname, skinpath,
+            test=test, make_default=make_default)
+        # when adding a new skin, we need to tell RR about it
+        cookWhenChangingSettings(None, None)
+
+    security.declareProtected(ManagePortal, 'manage_skinLayers')
+    def manage_skinLayers(self, chosen=(), add_skin=0, del_skin=0,
+                          skinname='', skinpath='', REQUEST=None):
+        super(SkinsTool, self).manage_skinLayers(chosen=chosen,
+            add_skin=add_skin, del_skin=del_skin, skinname=skinname,
+            skinpath=skinpath, REQUEST=REQUEST)
+        # when changing skins, we need to tell RR about it
+        cookWhenChangingSettings(None, None)
 
 
 SkinsTool.__doc__ = BaseTool.__doc__
