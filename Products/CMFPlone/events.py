d28bac6340e63df394f9f7aae3638cacce6a4f5d
Elizabeth Leddy <eleddy@eastofeaton.com>
2012-09-11 18:56:37 -0700

    Add event to fix products installed with latest keyword
    Conflicts:
    	docs/CHANGES.txt

--- Products/CMFPlone/events.py
+++ Products/CMFPlone/events.py
@@ -1,9 +1,25 @@
 from zope.interface import implements
 from zope.component.interfaces import ObjectEvent
 
+from Products.CMFCore.utils import getToolByName
+
 from interfaces import ISiteManagerCreatedEvent
 
 
 class SiteManagerCreatedEvent(ObjectEvent):
 
     implements(ISiteManagerCreatedEvent)
+
+
+def profileImportedEventHandler(event):
+    """
+    When a profile is imported with the keyword "latest", it needs to
+    be reconfigured with the actual number.
+    """
+    profile_id = event.profile_id.replace('profile-', '')
+    gs = event.tool
+    qi = getToolByName(gs, 'portal_quickinstaller')
+    installed_version = gs.getLastVersionForProfile(profile_id)
+    if installed_version == (u'latest',):
+        actual_version = qi.getLatestUpgradeStep(profile_id)
+        gs.setLastVersionForProfile(profile_id, actual_version)
