8ed369c3d3120bcc1a6430024d89d1ad715ccbc4
David Glick <dglick@gmail.com>
2012-09-16 14:18:34 -0700

    make sure StringIO is importable even after a module security fix in Zope 2.13.17

--- Products/CMFPlone/__init__.py
+++ Products/CMFPlone/__init__.py
@@ -55,6 +55,7 @@ def initialize(context):
     # Make Batch available at module level
     this_module.Batch = Batch
 
+    ModuleSecurityInfo('StringIO').declarePublic('StringIO')
     from StringIO import StringIO
     allow_class(StringIO)
 
