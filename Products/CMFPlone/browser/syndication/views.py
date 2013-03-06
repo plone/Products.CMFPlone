d48aebcebc224d9253acdc9b2ed43e85f683b7d5
Nathan Van Gheem <vangheem@gmail.com>
2013-03-05 18:23:07 -0600

    get rid of the newsml view class that is not needed

--- Products/CMFPlone/browser/syndication/views.py
+++ Products/CMFPlone/browser/syndication/views.py
@@ -29,10 +29,6 @@ class FeedView(BrowserView):
             return self.index()
 
 
-class NewsMLView(FeedView):
-    pass
-
-
 class SearchFeedView(FeedView):
     def feed(self):
         return getAdapter(self.context, ISearchFeed)
