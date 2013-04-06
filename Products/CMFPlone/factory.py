6c5312e3d401b9fd2643e3c11227e7d8cbb07b0a
Philip Bauer <bauer@starzel.de>
2013-04-06 11:10:00 +0200

    force install of plone.app.collections when chosing archetypes

--- Products/CMFPlone/factory.py
+++ Products/CMFPlone/factory.py
@@ -103,6 +103,7 @@ def addPloneSite(context, site_id, title='Plone site', description='',
         if setup_content:
             if base_contenttypes_profile == _ATCONTENTTYPES_PROFILE:
                 content_profile = _AT_CONTENT_PROFILE
+                setup_tool.runAllImportStepsFromProfile('profile-plone.app.collection:default')
             else:
                 content_profile = _DEX_CONTENT_PROFILE
             setup_tool.runAllImportStepsFromProfile('profile-%s' % content_profile)
