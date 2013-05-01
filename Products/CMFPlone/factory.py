469b0365b047c75032dfb52be08af2ab8c596abf
Johannes Raggam <raggam-nl@adm.at>
2013-05-02 00:57:39 +0200

    Conditionally import plone.app.collection, if available

--- Products/CMFPlone/factory.py
+++ Products/CMFPlone/factory.py
@@ -21,6 +21,7 @@ _IMREALLYPLONE4 = True
 
 
 class HiddenProfiles(object):
+
     implements(INonInstallable)
 
     def getNonInstallableProfiles(self):
@@ -103,7 +104,12 @@ def addPloneSite(context, site_id, title='Plone site', description='',
         if setup_content:
             if base_contenttypes_profile == _ATCONTENTTYPES_PROFILE:
                 content_profile = _AT_CONTENT_PROFILE
-                setup_tool.runAllImportStepsFromProfile('profile-plone.app.collection:default')
+                try:
+                    # Conditionally import plone.app.collection, if available
+                    import plone.app.collection
+                    setup_tool.runAllImportStepsFromProfile('profile-plone.app.collection:default')
+                except ImportError:
+                    pass
             else:
                 content_profile = _DEX_CONTENT_PROFILE
             setup_tool.runAllImportStepsFromProfile('profile-%s' % content_profile)
