bcb62cb1e48f7cbb31e272bad8a8e2a8a0433fcc
tisto <tisto@plone.org>
2013-10-10 12:06:14 +0200

    Run robot tests on test level 5.

--- Products/CMFPlone/tests/test_robot.py
+++ Products/CMFPlone/tests/test_robot.py
@@ -1,9 +1,13 @@
+from Products.CMFPlone.testing import PRODUCTS_CMFPLONE_ROBOT_TESTING
+from plone.testing import layered
 import os
 import unittest
 import robotsuite
-from plone.testing import layered
 
-from Products.CMFPlone.testing import PRODUCTS_CMFPLONE_ROBOT_TESTING
+UNIT_TEST_LEVEL = 1
+INTEGRATION_TEST_LEVEL = 2
+FUNCTIONAL_TEST_LEVEL = 3
+ROBOT_TEST_LEVEL = 5
 
 
 def test_suite():
@@ -11,13 +15,16 @@ def test_suite():
     current_dir = os.path.abspath(os.path.dirname(__file__))
     robot_dir = os.path.join(current_dir, 'robot')
     robot_tests = [
-        os.path.join('robot', doc) for doc in os.listdir(robot_dir)
-        if doc.endswith('.robot') and doc.startswith('test_')
+        os.path.join('robot', doc) for doc in
+        os.listdir(robot_dir) if doc.endswith('.robot') and
+        doc.startswith('test_')
     ]
-    for test in robot_tests:
+    for robot_test in robot_tests:
+        robottestsuite = robotsuite.RobotTestSuite(robot_test)
+        robottestsuite.level = ROBOT_TEST_LEVEL
         suite.addTests([
             layered(
-                robotsuite.RobotTestSuite(test),
+                robottestsuite,
                 layer=PRODUCTS_CMFPLONE_ROBOT_TESTING
             ),
         ])
