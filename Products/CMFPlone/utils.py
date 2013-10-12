f97e49fa97432027a3e28a8109348e09095e2f4b
Philip Bauer <bauer@starzel.de>
2013-10-12 12:43:44 -0300

    Move imaging_properties and utilities to CMFPlone
    fixes https://github.com/plone/plone.app.contenttypes/issues/82

--- Products/CMFPlone/utils.py
+++ Products/CMFPlone/utils.py
@@ -24,6 +24,7 @@ from App.Common import package_home
 from App.ImageFile import ImageFile
 from DateTime import DateTime
 from DateTime.interfaces import DateTimeError
+from Products.CMFCore.interfaces import IPropertiesTool
 from Products.CMFCore.permissions import ManageUsers
 from Products.CMFCore.utils import ToolInit as CMFCoreToolInit
 from Products.CMFCore.utils import getToolByName
@@ -48,6 +49,10 @@ PACKAGE_HOME = package_home(globals())
 security.declarePrivate('PACKAGE_HOME')
 WWW_DIR = join(PACKAGE_HOME, 'www')
 security.declarePrivate('WWW_DIR')
+
+# image-scaling
+QUALITY_DEFAULT = 88
+pattern = re.compile(r'^(.*)\s+(\d+)\s*:\s*(\d+)$')
 
 # Log methods
 from log import log
@@ -665,3 +670,35 @@ def set_own_login_name(member, loginname):
     if not secman.checkPermission(ManageUsers, member):
         raise Unauthorized('You can only change your OWN login name.')
     pas.updateLoginName(member.getId(), loginname)
+
+
+def getAllowedSizes():
+    """Get image-scale sizes.
+    """
+    ptool = queryUtility(IPropertiesTool)
+    if ptool is None:
+        return None
+    props = getattr(ptool, 'imaging_properties', None)
+    if props is None:
+        return None
+    sizes = {}
+    for line in props.getProperty('allowed_sizes'):
+        line = line.strip()
+        if line:
+            name, width, height = pattern.match(line).groups()
+            name = name.strip().replace(' ', '_')
+            sizes[name] = int(width), int(height)
+    return sizes
+
+
+def getQuality():
+    """Get quality used in image-scaling
+    """
+    ptool = queryUtility(IPropertiesTool)
+    if ptool:
+        props = getattr(ptool, 'imaging_properties', None)
+        if props:
+            quality = props.getProperty('quality')
+            if quality:
+                return quality
+    return QUALITY_DEFAULT
