e25b9223aa13ead4fd98a14ca3cef6509fbc010f
JeanMichel FRANCOIS <toutpt@gmail.com>
2013-11-13 12:19:19 +0100

    refactor: provide base viewlet to set headers
    update caching headers viewlet
    add X-UA-Compatible viewlet

--- plone/app/layout/viewlets/httpheaders.py
+++ plone/app/layout/viewlets/httpheaders.py
@@ -1,48 +1,42 @@
 from plone.app.layout.viewlets.common import ViewletBase
 
 
-class HTTPCachingHeaders(ViewletBase):
-    """Replace the old global_cache_settings/macros/cacheheaders
-    """
+class HeaderViewlet(ViewletBase):
+    """a base viewlet that do not render anything, just ready to set headers"""
+
     def index(self):
         return u""
 
     def update(self):
-        super(HTTPCachingHeaders, self).update()
+        super(HeaderViewlet, self).update()
         self.setHeader = self.request.response.setHeader
-        self.set_content_type()
-        self.set_language()
-        self.set_expires()
+        for name, value in self.getHeaders():
+            self.setHeader(name, value)
+
+    def getHeaders(self):
+        return []
+
+
+class HTTPCachingHeaders(HeaderViewlet):
+    """Replace the old global_cache_settings/macros/cacheheaders
+    """
 
     def enable_compression(self):
         """Call to activate gzip"""
         self.context.enableHTTPCompression(request=request, enable=1)
 
-    def set_content_type(self):
-        self.setHeader('Content-Type', 'text/html;;charset=utf-8')
-
-    def set_language(self):
+    def getHeaders(self):
         self.lang = getattr(self.context, 'Language', None)
         self.lang = self.lang or self.portal_state.default_language()
-        self.setHeader('Content-Language', self.lang)
+        return [
+            ('Content-Type', 'text/html;;charset=utf-8'),
+            ('Expires', 'Sat, 1 Jan 2000 00:00:00 GMT'),
+            ('Content-Language', self.lang)
+        ]
 
-    def set_expires(self):
-        self.setHeader('Expires', 'Sat, 1 Jan 2000 00:00:00 GMT')
 
-"""
-The is the original macro global_cache_settings/macros/cacheheaders
-<metal:cacheheaders define-macro="cacheheaders"
-    tal:define="portal_state context/@@plone_portal_state;
-                lang context/Language|nothing;
-                lang python: lang or portal_state.default_language();
-                setHeader python:request.RESPONSE.setHeader;">
-    <metal:block tal:define="dummy python:setHeader('Content-Type', 'text/html;;charset=utf-8')" />
-    <metal:block tal:define="dummy python:setHeader('Content-Language', lang)" />
-    <metal:block tal:define="dummy python:setHeader('Expires', 'Sat, 1 Jan 2000 00:00:00 GMT')" />
-    <tal:gzip tal:replace="nothing">
-      Use the following line to enable gzip compression
-      tal:content="structure python:context.enableHTTPCompression(request=request, enable=1)"
-    </tal:gzip>
-</metal:cacheheaders>
+class XUACompatible(HeaderViewlet):
+    """set the header ('X-UA-Compatible', 'IE=edge,chrome=1');"""
 
-"""
+    def getHeaders(self):
+        return [('X-UA-Compatible', 'IE=edge,chrome=1')]
