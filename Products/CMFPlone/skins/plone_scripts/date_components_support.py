ffca69fb329fcf930b686fb26e54eba6f8828b6b
Cédric Messiant <cedricmessiant@ecreall.com>
2013-11-14 17:26:45 +0100

    Increase PLONE_CEILING for dates to 2500

--- Products/CMFPlone/skins/plone_scripts/date_components_support.py
+++ Products/CMFPlone/skins/plone_scripts/date_components_support.py
@@ -25,7 +25,7 @@ now = DateTime()
 # from CMFDefault.DublinCore
 CEILING = DateTime(9999, 0)
 FLOOR = DateTime(1970, 0)
-PLONE_CEILING = DateTime(2021, 0)  # 2020-12-31
+PLONE_CEILING = DateTime(2500, 0) # 2499-12-31
 
 
 def month_names():
