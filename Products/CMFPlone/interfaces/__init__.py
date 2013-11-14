d8f95c62a10cdf5aad7173c730d99a6b059e4714
Alessandro Pisa <alessandro.pisa@redturtle.it>
2013-11-14 22:01:40 +0100

    removed import that was causing a circular import

--- Products/CMFPlone/interfaces/__init__.py
+++ Products/CMFPlone/interfaces/__init__.py
@@ -19,15 +19,3 @@ from structure import INonStructuralFolder
 from translationservice import ITranslationServiceTool
 from breadcrumbs import IHideFromBreadcrumbs
 from workflow import IWorkflowChain
-
-import pkg_resources
-try:
-    pkg_resources.get_distribution('Products.ATContentTypes')
-except pkg_resources.DistributionNotFound:
-    class IFactoryTool(Interface):
-        ''' Replacement for a needed interface
-        '''
-else:
-    from Products.ATContentTypes.interfaces import IFactoryTool
-
-IFactoryTool  # pyflakes
