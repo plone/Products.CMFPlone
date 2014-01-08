d9c02dc94619308ab80060859f6c9dbd51a0de57
Juan Pablo Giménez <jpg@rosario.com>
2014-01-08 10:23:32 -0300

    Implement NewsML feed syndication
    This was ported from collective.syndication.
    
    Conflicts:
    	Products/CMFPlone/browser/syndication/templates/search-rss.pt

--- Products/CMFPlone/browser/syndication/views.py
+++ Products/CMFPlone/browser/syndication/views.py
@@ -1,11 +1,16 @@
+from DateTime import DateTime
+from uuid import uuid3
+from uuid import NAMESPACE_OID
 from zope.component import queryAdapter
 from zope.component import getMultiAdapter
+from zope.cachedescriptors.property import Lazy as lazy_property
 from Products.Five import BrowserView
 from zExceptions import NotFound
 
 from Products.CMFPlone.interfaces.syndication import ISearchFeed
 from Products.CMFPlone.interfaces.syndication import IFeed
 from Products.CMFPlone.interfaces.syndication import IFeedSettings
+from Products.CMFPlone.interfaces.syndication import ISyndicatable
 from Products.CMFPlone import PloneMessageFactory as _
 
 from z3c.form import form, button, field
@@ -47,6 +52,62 @@ class SearchFeedView(FeedView):
         if util.search_rss_enabled(raise404=True):
             self.request.response.setHeader('Content-Type',
                                             'application/atom+xml')
+            return self.index()
+
+
+class NewsMLFeedView(FeedView):
+
+    def context_enabled(self):
+        settings = IFeedSettings(self.context, None)
+        if settings and not settings.enabled:
+            raise NotFound
+        else:
+            return True
+
+    @lazy_property
+    def current_date(self):
+        return DateTime()
+
+    def duid(self, item, value):
+        uid = uuid3(NAMESPACE_OID, item.uid + str(value))
+        return uid.hex
+
+    def get_image(self, item):
+        scales = item.context.restrictedTraverse('@@images')
+        if scales:
+            try:
+                return scales.scale('image')
+            except AttributeError:
+                pass
+        return None
+
+    def newsml_allowed(self):
+        util = getMultiAdapter((self.context, self.request),
+                               name='syndication-util')
+        if not util.site_enabled():
+            return False
+        elif ISyndicatable.providedBy(self.context):
+            settings = IFeedSettings(self.context, None)
+            if settings.enabled:
+                return True
+        return False
+
+    def newsml_enabled(self, raise404=False):
+        if not self.newsml_allowed():
+            if raise404:
+                raise NotFound
+            else:
+                return False
+        else:
+            return True
+
+    def __call__(self):
+        if self.newsml_enabled(raise404=True):
+            settings = IFeedSettings(self.context, None)
+            if settings and self.__name__ not in settings.feed_types:
+                raise NotFound
+            self.request.response.setHeader('Content-Type',
+                                            'application/vnd.iptc.g2.newsitem+xml')
             return self.index()
 
 
