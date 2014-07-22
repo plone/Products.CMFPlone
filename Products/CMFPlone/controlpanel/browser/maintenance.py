9f21ff72bbcc323f7e05d0161ab18dfc04ffab34
Timo Stollenwerk <tisto@plone.org>
2014-07-22 13:54:14 +0200

    Revert "Revert "Move maintenance control panel from plone.app.controlpanel to Products.CMFPlone.""
    This reverts commit e6cb204923fb411ef0da999ead0cd2b9370a6548.

---
+++ Products/CMFPlone/controlpanel/browser/maintenance.py
@@ -0,0 +1,123 @@
+from z3c.form import button
+import os
+from cgi import escape
+
+from plone.app.form.validators import null_validator
+from z3c.form import form
+
+from AccessControl import getSecurityManager
+from AccessControl.Permissions import view_management_screens
+from Acquisition import aq_inner
+from Products.CMFCore.utils import getToolByName
+from Products.CMFPlone import PloneMessageFactory as _
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
+from plone.protect import CheckAuthenticator
+
+from plone.autoform.form import AutoExtensibleForm
+
+from Products.CMFPlone.interfaces import IMaintenanceSchema
+
+
+class MaintenanceControlPanel(AutoExtensibleForm, form.EditForm):
+    """A simple form to pack the databases."""
+
+    schema = IMaintenanceSchema
+    id = "maintenance-control-panel"
+    label = _(u'Maintenance')
+    description = _(u"Zope server and site maintenance options.")
+    form_name = _(u'Zope Database Packing')
+    control_panel_view = "maintenance-controlpanel"
+    template = ViewPageTemplateFile('maintenance.pt')
+
+    @button.buttonAndHandler(_(u'Pack database now'), name='pack')
+    def handle_edit_action(self, action):
+        data, errors = self.extractData()
+        if errors:
+            self.status = self.formErrorsMessage
+            return
+        CheckAuthenticator(self.request)
+        if not self.available():
+            self.status = _(
+                u'text_not_allowed_manage_server',
+                default=u'You are not allowed to manage the Zope server.'
+            )
+            return
+
+        value = data.get('days', None)
+        # skip the actual pack method in tests
+        if value is not None and isinstance(value, int) and value >= 0:
+            context = aq_inner(self.context)
+            cpanel = context.unrestrictedTraverse('/Control_Panel')
+            cpanel.manage_pack(days=value, REQUEST=None)
+        self.status = _(u'Packed the database.')
+
+    #@form.action(_(u'Shut down'), validator=null_validator, name=u'shutdown')
+
+    @button.buttonAndHandler(_(u'Shut down'), name='shutdown')
+    def handle_shutdown_action(self, action):
+        CheckAuthenticator(self.request)
+        if not self.available():
+            self.status = _(
+                u'text_not_allowed_manage_server',
+                default=u'You are not allowed to manage the Zope server.'
+            )
+            return
+        context = aq_inner(self.context)
+        cpanel = context.unrestrictedTraverse('/Control_Panel')
+        result = cpanel.manage_shutdown()
+        return result
+
+    #@form.action(_(u'Restart'), validator=null_validator)
+
+    @button.buttonAndHandler(_(u'Restart'), name='restart')
+    def handle_restart_action(self, action):
+        CheckAuthenticator(self.request)
+        if not self.available():
+            self.status = _(
+                u'text_not_allowed_manage_server',
+                default=u'You are not allowed to manage the Zope server.'
+            )
+            return
+        context = aq_inner(self.context)
+        cpanel = context.unrestrictedTraverse('/Control_Panel')
+        url = self.request.get('URL')
+        cpanel.manage_restart(url)
+        return """<html>
+        <head><meta HTTP-EQUIV=REFRESH CONTENT="30; URL=%s">
+        </head>
+        <body>
+            Zope is restarting. This page will refresh in 30 seconds...
+        </body>
+        </html>
+        """ % escape(url, 1)
+
+    def available(self):
+        root = aq_inner(self.context).getPhysicalRoot()
+        sm = getSecurityManager()
+        return sm.checkPermission(view_management_screens, root)
+
+    def isRestartable(self):
+        if 'ZMANAGED' in os.environ:
+            return True
+        return False
+
+    def isDevelopmentMode(self):
+        qi = getToolByName(self.context, 'portal_quickinstaller')
+        return qi.isDevelopmentMode()
+
+    def coreVersions(self):
+        mt = getToolByName(self.context, 'portal_migration')
+        versions = mt.coreVersions()
+        versions['Instance'] = versions['Plone Instance']
+        return versions
+
+    def processTime(self):
+        context = aq_inner(self.context)
+        cpanel = context.unrestrictedTraverse('/Control_Panel')
+        return cpanel.process_time()
+
+    def dbSize(self):
+        context = aq_inner(self.context)
+        cpanel = context.unrestrictedTraverse('/Control_Panel')
+        return cpanel.db_size()
---
+++ Products/CMFPlone/controlpanel/tests/test_controlpanel_bbb_maintenance_adapter.py
@@ -0,0 +1,51 @@
+import unittest
+from plone.app.testing import setRoles
+from Products.CMFPlone.interfaces import IMaintenanceSchema
+from zope.component import getAdapter
+from plone.app.testing import TEST_USER_ID
+from Products.CMFCore.utils import getToolByName
+from plone.registry.interfaces import IRegistry
+from zope.component import getUtility
+
+from Products.CMFPlone.testing import \
+    PRODUCTS_CMFPLONE_INTEGRATION_TESTING
+
+
+class MaintenanceControlPanelAdapterTest(unittest.TestCase):
+
+    layer = PRODUCTS_CMFPLONE_INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        self.request = self.layer['request']
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+        registry = getUtility(IRegistry)
+        self.maintenance_settings = registry.forInterface(
+            IMaintenanceSchema, prefix="plone")
+
+    def test_adapter_lookup(self):
+        self.assertTrue(
+            getAdapter(self.portal, IMaintenanceSchema)
+        )
+
+    def test_get_days(self):
+        self.assertEqual(
+            getAdapter(self.portal, IMaintenanceSchema).days,
+            7
+        )
+        self.maintenance_settings.days = 4
+        self.assertEquals(
+            getAdapter(self.portal, IMaintenanceSchema).days,
+            4
+        )
+
+    def test_set_days(self):
+        self.assertEquals(
+            self.maintenance_settings.days,
+            7
+        )
+        getAdapter(self.portal, IMaintenanceSchema).days = 4
+        self.assertEquals(
+            self.maintenance_settings.days,
+            4
+        )
