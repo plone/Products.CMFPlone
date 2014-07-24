8f42c17d8ace47d6fcba8bf6d49036700d76ec4f
Timo Stollenwerk <tisto@plone.org>
2014-07-24 13:56:26 +0200

    Properly test the site control panel registry settings.

--- Products/CMFPlone/controlpanel/tests/test_controlpanel_editing.py
+++ Products/CMFPlone/controlpanel/tests/test_controlpanel_editing.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-from plone.registry import Registry
 from Products.CMFPlone.interfaces import IEditingSchema
 import unittest2 as unittest
 
--- Products/CMFPlone/controlpanel/tests/test_controlpanel_site.py
+++ Products/CMFPlone/controlpanel/tests/test_controlpanel_site.py
@@ -1,4 +1,9 @@
 # -*- coding: utf-8 -*-
+from zope.component import getMultiAdapter
+from zope.component import getUtility
+from plone.registry.interfaces import IRegistry
+
+from Products.CMFCore.utils import getToolByName
 from Products.CMFPlone.interfaces import ISiteSchema
 from Products.CMFPlone.testing import \
     PRODUCTS_CMFPLONE_INTEGRATION_TESTING
@@ -13,14 +18,34 @@ class SiteRegistryIntegrationTest(unittest.TestCase):
 
     layer = PRODUCTS_CMFPLONE_INTEGRATION_TESTING
 
+    def setUp(self):
+        self.portal = self.layer['portal']
+        self.request = self.layer['request']
+        registry = getUtility(IRegistry)
+        self.settings = registry.forInterface(
+            ISiteSchema, prefix="plone")
+
+    def test_search_controlpanel_view(self):
+        view = getMultiAdapter((self.portal, self.portal.REQUEST),
+                               name="search-controlpanel")
+        view = view.__of__(self.portal)
+        self.assertTrue(view())
+
+    def test_search_in_controlpanel(self):
+        self.controlpanel = getToolByName(self.portal, "portal_controlpanel")
+        self.assertTrue('SearchSettings' in [
+            a.getAction(self)['id']
+            for a in self.controlpanel.listActions()
+        ])
+
     def test_site_title_setting(self):
-        self.assertTrue('site_title' in ISiteSchema.names())
+        self.assertTrue(hasattr(self.settings, 'site_title'))
 
     def test_exposeDCMetaTags_setting(self):
-        self.assertTrue('exposeDCMetaTags' in ISiteSchema.names())
+        self.assertTrue(hasattr(self.settings, 'exposeDCMetaTags'))
 
     def test_webstats_js_setting(self):
-        self.assertTrue('webstats_js' in ISiteSchema.names())
+        self.assertTrue(hasattr(self.settings, 'webstats_js'))
 
     def test_enable_sitemap_setting(self):
-        self.assertTrue('enable_sitemap' in ISiteSchema.names())
+        self.assertTrue(hasattr(self.settings, 'enable_sitemap'))
