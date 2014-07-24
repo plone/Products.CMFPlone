cf6af099201032393a809ad4c0ef8ff440bb7817
Timo Stollenwerk <tisto@plone.org>
2014-07-24 13:43:04 +0200

    Test the search registry properly. Not just the names in the schema.

--- Products/CMFPlone/controlpanel/tests/test_controlpanel_search.py
+++ Products/CMFPlone/controlpanel/tests/test_controlpanel_search.py
@@ -1,4 +1,9 @@
 # -*- coding: utf-8 -*-
+from zope.component import getMultiAdapter
+from zope.component import getUtility
+from plone.registry.interfaces import IRegistry
+
+from Products.CMFCore.utils import getToolByName
 from Products.CMFPlone.interfaces import ISearchSchema
 from Products.CMFPlone.testing import \
     PRODUCTS_CMFPLONE_INTEGRATION_TESTING
@@ -13,8 +18,28 @@ class SearchRegistryIntegrationTest(unittest.TestCase):
 
     layer = PRODUCTS_CMFPLONE_INTEGRATION_TESTING
 
-    def test_enable_livesearch_setting(self):
-        self.assertTrue('enable_livesearch' in ISearchSchema.names())
+    def setUp(self):
+        self.portal = self.layer['portal']
+        self.request = self.layer['request']
+        registry = getUtility(IRegistry)
+        self.settings = registry.forInterface(
+            ISearchSchema, prefix="plone")
+
+    def test_search_controlpanel_view(self):
+        view = getMultiAdapter((self.portal, self.portal.REQUEST),
+                               name="search-controlpanel")
+        view = view.__of__(self.portal)
+        self.assertTrue(view())
+
+    def test_search_in_controlpanel(self):
+        self.controlpanel = getToolByName(self.portal, "portal_controlpanel")
+        self.assertTrue('SearchSettings' in [
+            a.getAction(self)['id']
+            for a in self.controlpanel.listActions()
+        ])
+
+    def test_enable_livesearch_attribute_exists(self):
+        self.assertTrue(hasattr(self.settings, 'enable_livesearch'))
 
     def test_types_not_searched(self):
-        self.assertTrue('types_not_searched' in ISearchSchema.names())
+        self.assertTrue(hasattr(self.settings, 'types_not_searched'))
