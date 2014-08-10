e7ab305a86ad75efc32242b405b1f4189921071f
Ramon Navarro Bosch <ramon.nb@gmail.com>
2014-08-10 14:42:07 +0200

    Temporal making it work

--- Products/CMFPlone/patternsregistry/viewlets/viewletmanagers.py
+++
@@ -1,7 +0,0 @@
-from zope.viewlet.interfaces import IViewletManager
-
-class IHtmlHeadScripts(IViewletManager):
-    """ A viewlet manager for javascripts. """
-
-class IHtmlHeadStyles(IViewletManager):
-    """ A viewlet manager for stylesheets. """
