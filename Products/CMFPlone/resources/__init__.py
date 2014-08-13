20f92d9a5863bbcbfa4f88901a0ed648df1b18bd
Nathan Van Gheem <vangheem@gmail.com>
2014-08-13 18:03:48 +0200

    subscribe to theme enabled event

--- Products/CMFPlone/resources/__init__.py
+++ Products/CMFPlone/resources/__init__.py
@@ -1,1 +1,10 @@
-#
+from zope.component import adapter
+from plone.app.theming.interfaces import IThemeAppliedEvent
+
+
+@adapter(IThemeAppliedEvent)
+def onThemeApplied(event):
+    # check for bundles to enable or disable
+    theme = event.theme
+    # theme.enabled_bundles
+    # theme.disabled_bundles
