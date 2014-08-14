fd4ed546e630b3a675d4aaf97840de93e8916349
Victor Fernandez de Alba <sneridagh@gmail.com>
2014-08-14 12:12:32 +0200

    Delete remaining console.log

--- Products/CMFPlone/static/patterns/toolbar/src/toolbar.js
+++ Products/CMFPlone/static/patterns/toolbar/src/toolbar.js
@@ -29,7 +29,6 @@ define([
         $( "#edit-zone nav li" ).has( "a .plone-toolbar-caret" ).click(function(e) {
           e.preventDefault();
           e.stopPropagation();
-          console.log('first');
           if ($(this).hasClass("active")) {
             $( "#edit-zone" ).css("right", "0");
             $( "html" ).css("margin-left", "-120px");
@@ -109,7 +108,6 @@ define([
         $( "#edit-zone nav > ul > li" ).has( "a .plone-toolbar-caret" ).on('click', function(event) {
           event.preventDefault();
           event.stopPropagation();
-          console.log('second');
           if ($(this).hasClass("active")) {
             $( "#edit-zone nav li" ).removeClass("active");
           } else {
