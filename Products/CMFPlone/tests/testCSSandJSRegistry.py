1a823393c8d39122f77e05d37978bc18461bf6e9
Ramon Navarro Bosch <ramon.nb@gmail.com>
2014-09-23 10:40:05 +0200

    Test new Resource Registry

--- Products/CMFPlone/tests/testCSSandJSRegistry.py
+++ Products/CMFPlone/tests/testCSSandJSRegistry.py
@@ -1,55 +1,72 @@
 from plone.app.testing.bbb import PloneTestCase
 from Products.ResourceRegistries.config import CSSTOOLNAME, JSTOOLNAME
 from Products.CMFCore.utils import getToolByName
+from plone.registry.interfaces import IRegistry
+from zope.component import getUtility
+from Products.CMFPlone.interfaces import (
+    IBundleRegistry,
+    IResourceRegistry)
 
 
 class TestCSSRegistry(PloneTestCase):
 
     def afterSetUp(self):
-        self.tool = getToolByName(self.portal, CSSTOOLNAME)
-
-    def testToolExists(self):
-        self.assertTrue(CSSTOOLNAME in self.portal)
+        self.registry = getUtility(IRegistry)
 
     def testDefaultCssIsInstalled(self):
-        installedStylesheetIds = self.tool.getResourceIds()
+        installedResources = self.registry.collectionOfInterface(
+            IResourceRegistry, prefix="plone.resources")
         expected = [
-            '++resource++plone.css',
-            'RTL.css',
+            '++plone++static/plone.less',
+            ]
+        css_files = [y for x in installedResources.values() for y in x.css]
+        for e in expected:
+            self.assertTrue(e in css_files, e)
+
+    def testBundleIsInstalled(self):
+        installedBundles = self.registry.collectionOfInterface(
+            IBundleRegistry, prefix="plone.bundles")
+        expected = [
+            'plone',
+            'plone-legacy'
             ]
         for e in expected:
-            self.assertTrue(e in installedStylesheetIds, e)
+            self.assertTrue(e in installedBundles.keys(), e)
 
-    def testRTLShouldHaveHigherPrecedence(self):
-        installedStylesheetIds = self.tool.getResourceIds()
-        indexRTLStylesheet = self.tool.getResourcePosition('RTL.css')
-        comes_before = ['++resource++plone.css']
-        for cb in comes_before:
-            self.assertTrue(cb in installedStylesheetIds[:indexRTLStylesheet],
-                            cb)
+    # def testRTLShouldHaveHigherPrecedence(self):
+    #     installedStylesheetIds = self.tool.getResourceIds()
+    #     indexRTLStylesheet = self.tool.getResourcePosition('RTL.css')
+    #     comes_before = ['++resource++plone.css']
+    #     for cb in comes_before:
+    #         self.assertTrue(cb in installedStylesheetIds[:indexRTLStylesheet],
+    #                         cb)
 
+    def testJSIsInsertedInPage(self):
+        page = self.portal.index_html()
+        self.assertTrue("++plone++static/plone.less" in page)
 
 class TestJSRegistry(PloneTestCase):
 
     def afterSetUp(self):
-        self.tool = getToolByName(self.portal, JSTOOLNAME)
-
-    def testToolExists(self):
-        self.assertTrue(JSTOOLNAME in self.portal)
+        self.registry = getUtility(IRegistry)
 
     def testDefaultJSIsInstalled(self):
-        installedScriptIds = self.tool.getResourceIds()
+        installedResources = self.registry.collectionOfInterface(
+            IResourceRegistry, prefix="plone.resources")
         expected = [
             '++resource++plone.js',
             'jquery.highlightsearchterms.js',
             'mark_special_links.js',
-            'styleswitcher.js',
             'table_sorter.js',
             'cookie_functions.js',
-            'plone_javascript_variables.js']
+            'plone_javascript_variables.js',
+            ]
+        js_files = [x.js for x in installedResources.values()]
         for e in expected:
-            self.assertTrue(e in installedScriptIds, e)
+            self.assertTrue(e in js_files, e)
+
 
     def testJSIsInsertedInPage(self):
+        self.assertTrue(self.registry['plone.resources.development'])
         page = self.portal.index_html()
-        self.assertTrue("" in page)
+        self.assertTrue("++resource++plone.js" in page)
