2d6d3d0dc632c6a63db6f7a1a86d65d2c9bae6a3
Jure Cerjak <jcerjak@termitnjak.si>
2015-01-25 18:36:21 +0100

    use "enable_self_reg" in the registry instead of modifying permissions

--- Products/CMFPlone/tests/robot/robot_setup.py
+++ Products/CMFPlone/tests/robot/robot_setup.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 from plone.app.robotframework.remote import RemoteLibrary
 
-from zope.component.hooks import getSite
 from zope.component import queryUtility
 from plone.registry.interfaces import IRegistry
 from Products.CMFPlone.interfaces import IMailSchema
+from Products.CMFPlone.interfaces import ISecuritySchema
 
 
 class CMFPloneRemoteKeywords(RemoteLibrary):
@@ -22,14 +22,11 @@ class CMFPloneRemoteKeywords(RemoteLibrary):
         mail_settings.email_from_address = 'john@doe.com'
 
     def the_self_registration_enabled(self):
-        portal = getSite()
-        app_perms = portal.rolesOfPermission(permission='Add portal member')
-        reg_roles = []
-        for appperm in app_perms:
-            if appperm['selected'] == 'SELECTED':
-                reg_roles.append(appperm['name'])
-
-        reg_roles.append('Anonymous')
-
-        portal.manage_permission(
-            'Add portal member', roles=reg_roles, acquire=0)
+        registry = queryUtility(IRegistry)
+        if registry is None:
+            return
+        security_settings = registry.forInterface(
+            ISecuritySchema, prefix='plone')
+        if security_settings is None:
+            return
+        security_settings.enable_self_reg = True
