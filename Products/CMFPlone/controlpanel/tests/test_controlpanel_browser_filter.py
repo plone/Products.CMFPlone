f8be24881bf13301c9e4ef8e82da31a6372f24af
Kees Hink <keeshink@gmail.com>
2015-03-19 15:26:40 +0100

    Test disable_filtering

--- Products/CMFPlone/controlpanel/tests/test_controlpanel_browser_filter.py
+++ Products/CMFPlone/controlpanel/tests/test_controlpanel_browser_filter.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
+from Products.CMFCore.utils import getToolByName
 # from Products.CMFPlone.interfaces import IFilterSchema
 from Products.CMFPlone.testing import PRODUCTS_CMFPLONE_FUNCTIONAL_TESTING
+from Products.PortalTransforms.data import datastream
 from plone.app.testing import SITE_OWNER_NAME, SITE_OWNER_PASSWORD
 from plone.testing.z2 import Browser
 from zope.component import getMultiAdapter
@@ -25,6 +27,10 @@ class FilterControlPanelFunctionalTest(unittest.TestCase):
             'Authorization',
             'Basic %s:%s' % (SITE_OWNER_NAME, SITE_OWNER_PASSWORD,)
         )
+        self.safe_html = getattr(
+            getToolByName(self.portal, 'portal_transforms'),
+            'safe_html',
+            None)
 
     def test_filter_control_panel_link(self):
         self.browser.open(
@@ -49,3 +55,23 @@ class FilterControlPanelFunctionalTest(unittest.TestCase):
                                name="filter-controlpanel")
         view = view.__of__(self.portal)
         self.assertTrue(view())
+
+    def test_disable_filtering(self):
+        self.browser.open(
+            "%s/@@filter-controlpanel" % self.portal_url)
+        self.browser.getControl(
+            name='form.widgets.disable_filtering:list').value = "selected"
+        self.browser.getControl('Save').click()
+
+        # test that the transform is disabled
+        self.assertEqual(
+            self.safe_html._config['disable_transform'],
+            1)
+
+        # anything passes
+        nasty_html = '<script></script>'
+        ds = datastream('dummy_name')
+        self.assertEqual(
+            nasty_html,
+            str(self.safe_html.convert(nasty_html, ds))
+        )
