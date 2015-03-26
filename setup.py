d8a4a1a25cfe3ebefbc764404e5cbe46cb4c54ca
Ramon Navarro Bosch <ramon.nb@gmail.com>
2015-03-26 22:23:36 +0100

    Missed to add multilingual to CMFPlone dependencies

--- setup.py
+++ setup.py
@@ -90,6 +90,7 @@ setup(
         'plone.app.folder',
         'plone.app.form',
         'plone.app.i18n',
+        'plone.app.multilingual',
         'plone.app.jquerytools',
         'plone.app.layout >=1.1.7dev-r23744',
         'plone.app.linkintegrity >=1.0.3',
