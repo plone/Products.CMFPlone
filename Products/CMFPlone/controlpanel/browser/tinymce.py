1a706ddbe2cbd5cf789c70af4d9521857f83b78d
David Glick <david@glicksoftware.com>
2015-04-09 00:10:23 -0400

    Don't show tinymce pattern settings on the tinymce control panel
    These are advanced settings that rarely need to be customized and aren't clearly explained here. If necessary they can be found in portal_registry.
    
    Also this fixes #422 where the tinymce control panel can't be saved because there are two content_css fields

--- Products/CMFPlone/controlpanel/browser/tinymce.py
+++ Products/CMFPlone/controlpanel/browser/tinymce.py
@@ -1,18 +1,12 @@
 from Products.CMFPlone import PloneMessageFactory as _
 from Products.CMFPlone.interfaces import ITinyMCELayoutSchema
 from Products.CMFPlone.interfaces import ITinyMCELibrariesSchema
-from Products.CMFPlone.interfaces import ITinyMCEPatternSchema
 from Products.CMFPlone.interfaces import ITinyMCEResourceTypesSchema
 from Products.CMFPlone.interfaces import ITinyMCESchema
 from Products.CMFPlone.interfaces import ITinyMCEToolbarSchema
 from plone.app.registry.browser import controlpanel
 from z3c.form import field
 from z3c.form import group
-
-
-class TinyMCEPatternForm(group.GroupForm):
-    label = _(u"Pattern")
-    fields = field.Fields(ITinyMCEPatternSchema)
 
 
 class TinyMCEToolbarForm(group.GroupForm):
@@ -38,7 +32,7 @@ class TinyMCEControlPanelForm(controlpanel.RegistryEditForm):
     schema_prefix = "plone"
     fields = field.Fields(ITinyMCELayoutSchema)
     groups = (TinyMCEToolbarForm, TinyMCELibrariesForm,
-              TinyMCEResourceTypesForm, TinyMCEPatternForm)
+              TinyMCEResourceTypesForm)
 
 
 class TinyMCEControlPanel(controlpanel.ControlPanelFormWrapper):
