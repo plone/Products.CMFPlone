52e78b4115de03d13797b5eb47d109cbf6e02fa3
Ramon Navarro Bosch <ramon.nb@gmail.com>
2015-07-14 09:21:30 +0200

    Toolbar logo and position

--- plone/app/layout/viewlets/toolbar.py
+++ plone/app/layout/viewlets/toolbar.py
@@ -3,6 +3,9 @@ from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from zope.component import getMultiAdapter
 from plone.memoize.instance import memoize
 from plone.app.layout.viewlets.common import PersonalBarViewlet
+from zope.component import getUtility
+from plone.registry.interfaces import IRegistry
+from Products.CMFPlone.interfaces.controlpanel import ISiteSchema
 
 
 class ToolbarViewletManager(OrderedViewletManager):
@@ -23,3 +26,22 @@ class ToolbarViewletManager(OrderedViewletManager):
         viewlet = PersonalBarViewlet(self.context, self.request, self.__parent__, self)
         viewlet.update()
         return viewlet
+
+    def get_toolbar_logo(self):
+        registry = getUtility(IRegistry)
+        settings = registry.forInterface(ISiteSchema, prefix='plone')
+        portal_url = self.portal_state.portal_url()
+        return portal_url + settings.toolbar_logo
+
+    def get_toolbar_position(self):
+        registry = getUtility(IRegistry)
+        settings = registry.forInterface(ISiteSchema, prefix='plone')
+        if settings.toolbar_position == 'side':
+            return 'plone-toolbar-left pat-toolbar'
+        else:
+            return 'portal-toolbar-top pat-toolbar'
+
+    def show_switcher(self):
+        return False
+
+
