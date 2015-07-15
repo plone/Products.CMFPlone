0497ff4fb24ad178ff78a62629c74c8361b1dc0b
vangheem <vangheem@gmail.com>
2015-07-15 18:52:18 +0200

    always show folder contents

--- Products/CMFPlone/skins/plone_scripts/displayContentsTab.py
+++ Products/CMFPlone/skins/plone_scripts/displayContentsTab.py
@@ -9,4 +9,4 @@
 
 # We won't deprecate this just yet, because people expect it to be acquired
 # from context and frequently override it on their content classes.
-return context.restrictedTraverse('@@plone').displayContentsTab()
+return True
