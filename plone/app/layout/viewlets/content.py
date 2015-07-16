3494e4c2cb71f19320895c7aee1cee3ebbfc327b
Ramon Navarro Bosch <ramon.nb@gmail.com>
2015-07-16 12:05:59 +0200

    timeago time on history view

--- plone/app/layout/viewlets/content.py
+++ plone/app/layout/viewlets/content.py
@@ -388,9 +388,10 @@ class ContentHistoryViewlet(WorkflowHistoryViewlet):
     def toLocalizedTime(self, time, long_format=None, time_only=None):
         """Convert time to localized time
         """
-        util = getToolByName(self.context, 'translation_service')
-        return util.ulocalized_time(time, long_format, time_only, self.context,
-                                    domain='plonelocales')
+        # util = getToolByName(self.context, 'translation_service')
+        return DateTime(time).ISO()
+        # return util.ulocalized_time(time, long_format, time_only, self.context,
+        #                             domain='plonelocales')
 
 
 class ContentHistoryView(ContentHistoryViewlet):
