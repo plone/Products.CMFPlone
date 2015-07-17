f55f461a6fc2a2a4951900c95bf56d159377112a
vangheem <vangheem@gmail.com>
2015-07-17 08:37:32 +0200

    update resources

--- Products/CMFPlone/static/plone-logged-in-compiled.js
+++ Products/CMFPlone/static/plone-logged-in-compiled.js
@@ -83592,129 +83592,6 @@ define('mockup-patterns-structure-url/js/views/paging',[
   return PagingView;
 });
 
-define('mockup-patterns-structure-url/js/views/addmenu',[
-  'jquery',
-  'underscore',
-  'backbone',
-  'mockup-ui-url/views/buttongroup',
-  'mockup-ui-url/views/button',
-  'mockup-utils',
-  'bootstrap-dropdown'
-], function($, _, Backbone, ButtonGroup, ButtonView, utils) {
-  'use strict';
-
-  var AddMenu = ButtonGroup.extend({
-    title: 'Add',
-    className: 'btn-group addnew',
-    events: {
-    },
-    initialize: function(options) {
-      var self = this;
-      ButtonGroup.prototype.initialize.apply(self, [options]);
-      self.app.on('context-info-loaded', function(data) {
-        self.$items.empty();
-        _.each(data.addButtons, function(item) {
-          var view = new ButtonView({
-            id: item.id,
-            title: item.title,
-            url: item.action
-          });
-          view.render();
-          var wrap = $('<li/>');
-          // As we are reusing the whole ButtonView for render the add content
-          // list we should remove entirely the "btn btn-default" classes.
-          // This element in fact, should not have any class at all, so we
-          // remove the attribute completely
-          view.$el.removeAttr('class');
-
-          wrap.append(view.el);
-          self.$items.append(wrap);
-          view.$el.click(function(e) {
-            self.buttonClicked.apply(self, [e, view]);
-            return false;
-          });
-        });
-      });
-    },
-    buttonClicked: function(e, button) {
-      var self = this;
-      e.preventDefault();
-      self.app.loading.show();
-      window.location = button.url;
-      /* Do not launch in overlay otherwise you get overlays insides of
-         overlays potentials--nasty. Leave here if we change our mind
-         for some reason.
-      $.ajax({
-        url: button.url,
-        type: 'POST',
-        data: {
-          '_authenticator': $('[name="_authenticator"]').val(),
-        },
-        success: function(response) {
-          self.app.loading.hide();
-          var modal = new Modal(self.$el, {
-            html: utils.parseBodyTag(response),
-            content: '#content',
-            width: '80%',
-            backdropOptions: {
-              closeOnClick: false
-            },
-            automaticallyAddButtonActions: false,
-            actionOptions: {
-              displayInModal: false,
-              reloadWindowOnClose: false
-            },
-            actions: {
-              'input#form-buttons-save, .formControls input[name="form.button.save"]': {
-                onSuccess: function(modal, response, state, xhr, form) {
-                  self.app.collection.pager();
-                  if (self.$items.is(':visible')) {
-                    self.$dropdown.dropdown('toggle');
-                  }
-                  modal.hide();
-                },
-                onError: function() {
-                  window.alert('error on form');
-                }
-              },
-              'input#form-buttons-cancel, .formControls input[name="form.button.cancel"]': {
-                modalFunction: 'hide'
-              }
-            },
-          });
-          modal.show();
-        },
-        error: function() {
-          // XXX handle error
-          self.app.loading.hide();
-        }
-      });
-    */
-    },
-    render: function() {
-      var self = this;
-      self.$el.empty();
-
-      self.$el.append(
-        '<a class="btn dropdown-toggle btn-default" data-toggle="dropdown" href="#">' +
-          '<span class="glyphicon glyphicon-plus"></span>' +
-          self.title +
-          '<span class="caret"></span>' +
-        '</a>' +
-        '<ul class="dropdown-menu">' +
-        '</ul>' +
-      '</div>');
-
-      self.$items = self.$('.dropdown-menu');
-      self.$dropdown = self.$('.dropdown-toggle');
-      self.$dropdown.dropdown();
-      return this;
-    }
-  });
-
-  return AddMenu;
-});
-
 define('mockup-patterns-structure-url/js/views/columns',[
   'jquery',
   'underscore',
@@ -85245,7 +85122,6 @@ define('mockup-patterns-structure-url/js/views/app',[
   'mockup-patterns-structure-url/js/views/rearrange',
   'mockup-patterns-structure-url/js/views/selectionbutton',
   'mockup-patterns-structure-url/js/views/paging',
-  'mockup-patterns-structure-url/js/views/addmenu',
   'mockup-patterns-structure-url/js/views/columns',
   'mockup-patterns-structure-url/js/views/textfilter',
   'mockup-patterns-structure-url/js/views/upload',
@@ -85258,7 +85134,7 @@ define('mockup-patterns-structure-url/js/views/app',[
 ], function($, _, Backbone, Toolbar, ButtonGroup, ButtonView, BaseView,
             TableView, SelectionWellView,
             GenericPopover, RearrangeView, SelectionButtonView,
-            PagingView, AddMenu, ColumnsView, TextFilterView, UploadView,
+            PagingView, ColumnsView, TextFilterView, UploadView,
             ResultCollection, SelectedCollection, utils, _t, logger) {
   'use strict';
 
@@ -85584,13 +85460,6 @@ define('mockup-patterns-structure-url/js/views/app',[
           app: self
         });
         items.push(uploadButton);
-      }
-      if (self.options.contextInfoUrl) {
-        // only add menu if set
-        items.push(new AddMenu({
-          contextInfoUrl: self.options.contextInfoUrl,
-          app: self
-        }));
       }
 
       var buttons = [];
@@ -90088,5 +89957,5 @@ require([
   'use strict';
 });
 
-define("/Users/ramon/Development/buildout.coredev-toolbar/src/Products.CMFPlone/Products/CMFPlone/static/plone-logged-in.js", function(){});
+define("/Users/nathan/code/coredev5/src/Products.CMFPlone/Products/CMFPlone/static/plone-logged-in.js", function(){});
 
