16dacae3f6d7caf25d3c52f7e2e5f41d3785274c
Maurits van Rees <maurits@vanrees.org>
2015-08-24 23:41:40 +0200

    Reworked sendto permission test.
    We must look for the error message in the status message.

--- Products/CMFPlone/tests/testSecurityDeclarations.py
+++ Products/CMFPlone/tests/testSecurityDeclarations.py
@@ -399,11 +399,20 @@ class TestAllowSendtoSecurity(PloneTestCase.PloneTestCase):
         self.setRoles(['Member'])
         # get sendto script in context of folder
         sendto = self.folder.sendto
-        # should fail with the not allowed msg check if the msg
-        # contains the string
-        msg = sendto()
-        errormsg = "You%20are%20not%20allowed%20to%20send%20this%20link"
-        self.assertTrue(errormsg in str(msg), str(msg))
+        # Set the parameters on the request, so that the validation
+        # script is happy.
+        request = self.folder.REQUEST
+        request['send_to_address'] = 'target@example.org'
+        request['send_from_address'] = 'sender@example.org'
+        from Products.statusmessages.interfaces import IStatusMessage
+        # Clean out any current status messages.
+        IStatusMessage(request).show()
+        # Should fail with the not allowed msg as status message.
+        sendto()
+        status_messages = IStatusMessage(request).show()
+        self.assertEqual(len(status_messages), 1)
+        self.assertEqual(status_messages[0].message,
+                         "You are not allowed to send this link.")
 
 
 class TestSkinSecurity(PloneTestCase.PloneTestCase):
