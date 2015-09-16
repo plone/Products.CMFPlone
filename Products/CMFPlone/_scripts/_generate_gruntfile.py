0245527f7efe463db01640e2f48d9370151a966b
Ramon Navarro Bosch <ramon@iMac-de-Ramon.local>
2015-09-16 16:33:37 +0200

    Set the name of the resource as the name defined on the configuration so no define is added

--- Products/CMFPlone/_scripts/_generate_gruntfile.py
+++ Products/CMFPlone/_scripts/_generate_gruntfile.py
@@ -333,7 +333,7 @@ for bkey, bundle in bundles.items():
                         bkey=resource,
                         paths=json.dumps(paths),
                         shims=json.dumps(shims),
-                        name=main_js_path,
+                        name=resource,
                         out=target_path + '/' + resource + '-compiled.js'
                     )
                     require_configs += rc
