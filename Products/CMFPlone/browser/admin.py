6e8b1e7f3620da17a6bad053cb69153f2ecbf7bb
Jon Pentland <jon@iomedia.co.uk>
2015-10-12 16:07:54 +0300

    Sub-sort language by label, not language code when adding a site

--- Products/CMFPlone/browser/admin.py
+++ Products/CMFPlone/browser/admin.py
@@ -205,7 +205,7 @@ class AddPloneSite(BrowserView):
         # Sort list by language, next by country
         data = sorted(grouped.values(), key=lambda k: k['label'])
         for item in data:
-            item['languages'] = sorted(item['languages'], key=lambda k: k['langcode'])
+            item['languages'] = sorted(item['languages'], key=lambda k: k['label'].lower())
         return data
 
     def timezones(self):
