c62e3c4fc08bf492f60e04d55cea8b7ff460a1bd
Maurits van Rees <maurits@vanrees.org>
2016-10-22 18:37:49 -0400

    Let AddMoveAndDeleteDocument.txt use plone.app.contenttypes.
    Start moving others in test_functional.py to plone.app.testing.

--- Products/CMFPlone/tests/test_functional.py
+++ Products/CMFPlone/tests/test_functional.py
@@ -7,11 +7,17 @@ import unittest
 import pkg_resources
 from Products.CMFPlone.tests.PloneTestCase import PloneTestCase
 from Products.CMFPlone.tests.PloneTestCase import FunctionalTestCase
+from plone.app.contenttypes.testing import (
+    PLONE_APP_CONTENTTYPES_FUNCTIONAL_TESTING
+)
+from plone.app.testing import PLONE_FUNCTIONAL_TESTING
+from plone.testing import layered
 from Testing.ZopeTestCase import ZopeDocFileSuite
 
 
 UNITTESTS = ['messages.txt', 'mails.txt', 'emaillogin.txt', 'translate.txt',
              'pwreset_browser.txt']
+CONTENT_TESTS = ['AddMoveAndDeleteDocument.txt']
 OPTIONFLAGS = (doctest.ELLIPSIS | doctest.NORMALIZE_WHITESPACE)
 
 from Products.GenericSetup import EXTENSION, profile_registry
@@ -41,16 +47,33 @@ class PloneAtTestCase(PloneTestCase):
 
 
 def test_suite():
-    filenames = [
+    # Some files need to be tested with the standard functional layer.
+    ignored = UNITTESTS + CONTENT_TESTS
+    standard_filenames = [
         filename for filename in
         glob.glob(os.path.sep.join([os.path.dirname(__file__), '*.txt']))
-        if os.path.basename(filename) not in UNITTESTS]
-    suites = [ZopeDocFileSuite(
+        if os.path.basename(filename) not in ignored]
+    suites = [layered(doctest.DocFileSuite(
               os.path.basename(filename),
               optionflags=OPTIONFLAGS,
               package='Products.CMFPlone.tests',
-              test_class=FunctionalTestCase)
-              for filename in filenames]
+              ), layer=PLONE_FUNCTIONAL_TESTING)
+              for filename in standard_filenames]
+
+    # Other files need to be tested with the plone.app.contenttypes layer.
+    content_filenames = [
+        filename for filename in
+        glob.glob(os.path.sep.join([os.path.dirname(__file__), '*.txt']))
+        if os.path.basename(filename) in CONTENT_TESTS]
+    suites.extend(
+        [layered(doctest.DocFileSuite(
+            os.path.basename(filename),
+            optionflags=OPTIONFLAGS,
+            package='Products.CMFPlone.tests',
+            ), layer=PLONE_APP_CONTENTTYPES_FUNCTIONAL_TESTING)
+        for filename in content_filenames])
+
+    # And some use Archetypes.
     suites.extend(
         [ZopeDocFileSuite(
          os.path.basename(filename),
