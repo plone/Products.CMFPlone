43a4ca6259ec302c3d7c94c947e68ce188daf169
Thomas Schorr <t_schorr@gmx.de>
2017-02-01 09:17:37 +0000

    remove temporary fix to use chameleon pts

--- Products/CMFPlone/controlpanel/tests/test_controlpanel_browser_usergroups_siteadmin_role.py
+++ Products/CMFPlone/controlpanel/tests/test_controlpanel_browser_usergroups_siteadmin_role.py
@@ -16,6 +16,7 @@ from plone.app.testing.bbb import PloneTestCaseFixture
 from plone.app import testing
 from Products.CMFCore.utils import getToolByName
 
+
 class ControlPanelFixture(PloneTestCaseFixture):
 
     def setUpPloneSite(self, portal):
@@ -49,11 +50,6 @@ class UserGroupsControlPanelTestCase(FunctionalTestCase):
                 'somepassword',
                 properties=member
             )
-        from five.pt.engine import Program
-        from zope.pagetemplate.interfaces import IPageTemplateEngine
-        from zope.component import getSiteManager
-        sm = getSiteManager()
-        sm.registerUtility(Program, IPageTemplateEngine)
         transaction.commit()
 
 
@@ -102,7 +98,8 @@ class TestSiteAdministratorRoleFunctional(UserGroupsControlPanelTestCase):
     def testControlPanelOverview(self):
         # make sure we can view the Site Setup page,
         # at both old and new URLs
-        res = self.publish('/plone/@@overview-controlpanel', 'siteadmin:secret')
+        res = self.publish(
+            '/plone/@@overview-controlpanel', 'siteadmin:secret')
         self.assertEqual(200, res.status)
         res = self.publish(
             '/plone/@@overview-controlpanel', 'siteadmin:secret'
@@ -319,9 +316,10 @@ class TestSiteAdministratorRoleFunctional(UserGroupsControlPanelTestCase):
         res = self.publish('/plone/@@usergroup-userprefs',
                            basic='siteadmin:secret')
         contents = self._simplify_white_space(res.getOutput())
-        self.assertTrue('<input type="checkbox" class="noborder notify" '
-                        'name="delete:list" value="root" disabled="disabled" />'
-                        in contents)
+        self.assertTrue(
+            '<input type="checkbox" class="noborder notify" '
+            'name="delete:list" value="root" disabled="disabled" />'
+            in contents)
 
         form = {
             '_authenticator': self.siteadmin_token,
