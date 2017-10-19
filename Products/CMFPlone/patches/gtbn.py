5ebf14074264a3845ba91e7c9c73bbef3190869d
David Glick <david@glicksoftware.com>
2017-10-19 14:37:11 +0200

    rewrap tools in request container

--- Products/CMFPlone/patches/gtbn.py
+++ Products/CMFPlone/patches/gtbn.py
@@ -8,14 +8,17 @@ from Products.CMFCore import utils
 # rather than the entire function, to make sure that aliases to the function
 # that were imported prior to this patch will still run the patched code.
 code = """
+from Acquisition.interfaces import IAcquirer
+from Acquisition import aq_base, aq_chain
 from persistent.interfaces import IPersistent
 from OFS.interfaces import IItem
+from ZPublisher.BaseRequest import RequestContainer
 try:
     from Products.ATContentTypes.tool.factory import FauxArchetypeTool
 except ImportError:
     FauxArchetypeTool = type('FauxArchetypeTool')
 
-def _getToolByName(self, name, default=_marker):
+def _getToolByName(obj, name, default=_marker):
     pass
 
 def check_getToolByName(obj, name, default=_marker):
@@ -28,6 +31,15 @@ def check_getToolByName(obj, name, default=_marker):
             result.__class__.__module__ == 'mock' or \
             result is _marker or \
             result is default:
+        # Rewrap in request container
+        result_chain = aq_chain(result)
+        obj_chain = aq_chain(obj)
+        if (IAcquirer.providedBy(result) and
+                not isinstance(result_chain[-1], RequestContainer) and
+                isinstance(obj_chain[-1], RequestContainer)):
+            result = obj_chain[-2]
+            for ob in reversed(result_chain):
+                result = aq_base(ob).__of__(result)
         return result
     else:
         raise TypeError("Object found is not a portal tool (%s)" % (name,))
