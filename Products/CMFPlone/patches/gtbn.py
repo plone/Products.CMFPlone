aa5fc4bf5f3172929371605a305135f955d3f2c2
David Glick <david@glicksoftware.com>
2017-10-20 02:51:19 +0200

    Acquisition means always having to say...

--- Products/CMFPlone/patches/gtbn.py
+++ Products/CMFPlone/patches/gtbn.py
@@ -13,6 +13,7 @@ from Acquisition import aq_base, aq_chain, aq_inner
 from persistent.interfaces import IPersistent
 from OFS.interfaces import IItem
 from ZPublisher.BaseRequest import RequestContainer
+from zope.globalrequest import getRequest
 try:
     from Products.ATContentTypes.tool.factory import FauxArchetypeTool
 except ImportError:
@@ -31,15 +32,21 @@ def check_getToolByName(obj, name, default=_marker):
             result.__class__.__module__ == 'mock' or \
             result is _marker or \
             result is default:
+
         # Rewrap in request container
-        result_chain = aq_chain(result)
-        obj_chain = aq_chain(aq_inner(obj))
-        if (IAcquirer.providedBy(result) and
-                not isinstance(result_chain[-1], RequestContainer) and
-                isinstance(obj_chain[-1], RequestContainer)):
-            result = obj_chain[-2]
-            for ob in reversed(result_chain):
+        request = getattr(obj, 'REQUEST', None) or getRequest()
+        if IAcquirer.providedBy(result) and request is not None:
+            chain = []
+            parent = result
+            while 1:
+                chain.append(parent)
+                parent = aq_parent(aq_inner(parent))
+                if parent in chain or parent is None or isinstance(parent, RequestContainer):
+                    break
+            result = RequestContainer(REQUEST=request)
+            for ob in reversed(chain):
                 result = aq_base(ob).__of__(result)
+
         return result
     else:
         raise TypeError("Object found is not a portal tool (%s)" % (name,))
