ab06a6228444fd567ebc03ec2d6442edbbaab960
esteele <eric@esteele.net>
2017-10-21 17:41:23 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-version = '2.7.6.dev0'
+version = '2.8.0.dev0'
 
 long_description = \
     open("README.rst").read() + "\n" + open("CHANGES.rst").read()
