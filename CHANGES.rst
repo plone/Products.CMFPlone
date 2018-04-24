1c0410c7dfd7b0c4d5db2270924e3a4d860c492e
Maurits van Rees <maurits@vanrees.org>
2018-04-24 21:53:23 +0200

    Preparing release 2.8.0
    [ci skip]

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,7 +1,7 @@
 Changelog
 =========
 
-2.8.0 (unreleased)
+2.8.0 (2018-04-24)
 ------------------
 
 New features:
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-version = '2.8.0.dev0'
+version = '2.8.0'
 
 long_description = \
     open("README.rst").read() + "\n" + open("CHANGES.rst").read()
