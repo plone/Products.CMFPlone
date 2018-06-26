11482a2d37b9ded04d517bd1e9f50e234e3b3b46
David Glick <david@glicksoftware.com>
2018-06-26 17:13:49 +0200

    site_logo is bytes, not ascii

--- Products/CMFPlone/controlpanel/tests/test_controlpanel_browser_site.py
+++ Products/CMFPlone/controlpanel/tests/test_controlpanel_browser_site.py
@@ -11,9 +11,9 @@ from zope.component import getUtility
 import unittest
 
 # Red pixel with filename pixel.png
-SITE_LOGO_BASE64 = 'filenameb64:cGl4ZWwucG5n;datab64:iVBORw0KGgoAAAANSUhEUgAA'\
-                   'AAEAAAABCAIAAACQd1PeAAAADElEQVQI12P4z8AAAAMBAQAY3Y2wAAAAA'\
-                   'ElFTkSuQmCC'
+SITE_LOGO_BASE64 = b'filenameb64:cGl4ZWwucG5n;datab64:iVBORw0KGgoAAAANSUhEUgA'\
+                   b'AAAEAAAABCAIAAACQd1PeAAAADElEQVQI12P4z8AAAAMBAQAY3Y2wAAA'\
+                   b'AAElFTkSuQmCC'
 SITE_LOGO_HEX = b'\x89PNG\r\n\x1a\n\x00\x00\x00\rIHDR\x00\x00\x00\x01\x00\x00'\
                 b'\x00\x01\x08\x02\x00\x00\x00\x90wS\xde\x00\x00\x00\x0cIDAT'\
                 b'\x08\xd7c\xf8\xcf\xc0\x00\x00\x03\x01\x01\x00\x18\xdd\x8d'\
--- Products/CMFPlone/tests/test_utils.py
+++ Products/CMFPlone/tests/test_utils.py
@@ -16,9 +16,9 @@ from plone.subrequest.interfaces import ISubRequest
 import unittest
 
 
-SITE_LOGO_BASE64 = 'filenameb64:cGl4ZWwucG5n;datab64:iVBORw0KGgoAAAANSUhEUgAA'\
-                   'AAEAAAABCAIAAACQd1PeAAAADElEQVQI12P4z8AAAAMBAQAY3Y2wAAAAA'\
-                   'ElFTkSuQmCC'
+SITE_LOGO_BASE64 = b'filenameb64:cGl4ZWwucG5n;datab64:iVBORw0KGgoAAAANSUhEUgA'\
+                   b'AAAEAAAABCAIAAACQd1PeAAAADElEQVQI12P4z8AAAAMBAQAY3Y2wAAA'\
+                   b'AAElFTkSuQmCC'
 
 
 class DefaultUtilsTests(unittest.TestCase):
