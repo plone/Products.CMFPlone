0a9733482306bf1bcc98af63d1f0f4565026be7c
Manuel Reinhardt <reinhardt@syslab.com>
2018-10-02 10:13:11 +0200

    cherrypick getObjSize script from python3

--- Products/CMFPlone/tests/test_utils.py
+++ Products/CMFPlone/tests/test_utils.py
@@ -147,6 +147,24 @@ class DefaultUtilsTests(unittest.TestCase):
         ctx.vh_root = '/approot/PloneSite/folder/SubSite'
         self.assertEqual(get_top_site_from_url(ctx, req).id, 'SubSite')
 
+    def test_human_readable_size(self):
+        from Products.CMFPlone.utils import human_readable_size
+
+        self.assertEqual(human_readable_size(0), '0 KB')
+        self.assertEqual(human_readable_size(1), '1 KB')
+        size = 1000
+        self.assertEqual(human_readable_size(size), '1 KB')
+        size += 24
+        self.assertEqual(human_readable_size(size), '1.0 KB')
+        size += 512
+        self.assertEqual(human_readable_size(size), '1.5 KB')
+        size *= 1024
+        self.assertEqual(human_readable_size(size), '1.5 MB')
+        size *= 1024
+        self.assertEqual(human_readable_size(size), '1.5 GB')
+        size *= 1024
+        self.assertEqual(human_readable_size(size), '1536.0 GB')
+
 
 class LogoTests(PloneTestCase.PloneTestCase):
 
