df6d7041f02a9d64a1e7034daafede940f7105a8
Katja Süss <k.suess@rohberg.ch>
2018-10-28 17:43:07 +0100

    removed Products.ResourceRegistries
    see pull request plone.app.upgrade https://github.com/plone/plone.app.upgrade/pull/183/files

--- setup.py
+++ setup.py
@@ -105,7 +105,6 @@ setup(
         'Products.PluggableAuthService',
         'Products.PluginRegistry',
         'Products.PortalTransforms',
-        'Products.ResourceRegistries',
         'Products.Sessions',
         'Products.SiteErrorLog',
         'Products.statusmessages',
