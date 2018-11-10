4f0fbafe2b0b5d425bdb441c9acb23207725ec2e
Paul Roeland <paul@cleanclothes.org>
2018-11-10 16:24:49 +0900

    move robodoc files around and announce for testrunner

---
+++ Products/CMFPlone/tests/test_robodoc.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from Products.CMFPlone.testing import PRODUCTS_CMFPLONE_ROBOT_TESTING
+from plone.app.testing import ROBOT_TEST_LEVEL
+from plone.testing import layered
+import os
+import unittest
+import robotsuite
+
+
+def test_suite():
+    suite = unittest.TestSuite()
+    current_dir = os.path.abspath(os.path.dirname(__file__))
+    robot_dir = os.path.join(current_dir, 'robodoc')
+    robot_tests = [
+        os.path.join('robodoc', doc) for doc in
+        os.listdir(robot_dir) if doc.endswith('.robot') and
+        doc.startswith('test_')
+    ]
+    for robot_test in robot_tests:
+        robottestsuite = robotsuite.RobotTestSuite(
+            robot_test,
+            noncritical=['unstable'],
+        )
+        robottestsuite.level = ROBOT_TEST_LEVEL
+        suite.addTests([
+            layered(
+                robottestsuite,
+                layer=PRODUCTS_CMFPLONE_ROBOT_TESTING
+            ),
+        ])
+    return suite
