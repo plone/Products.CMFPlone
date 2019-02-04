f8b781e5a3d7fff4236299f9349027f80424a673
ale-rt <alessandro.pisa@gmail.com>
2019-02-04 23:12:51 +0100

    Add basic validators for the portal action controlpanel forms
    Fixes #2689

--- Products/CMFPlone/interfaces/controlpanel.py
+++ Products/CMFPlone/interfaces/controlpanel.py
@@ -1,13 +1,16 @@
 # -*- coding: utf-8 -*-
+from Products.CMFCore.utils import getToolByName
 from Products.CMFPlone import PloneMessageFactory as _
 from Products.CMFPlone._compat import dump_json_to_text
 from Products.CMFPlone.interfaces.basetool import IPloneBaseTool
 from zope import schema
 from zope.deferredimport import deprecated
+from zope.component.hooks import getSite
 from zope.interface import Attribute
 from zope.interface import implementer
 from zope.interface import Interface
 from zope.interface import Invalid
+from zope.interface import invariant
 from zope.schema.vocabulary import SimpleTerm
 from zope.schema.vocabulary import SimpleVocabulary
 
@@ -1559,6 +1562,20 @@ class ILinkSchema(Interface):
         required=False)
 
 
+def _check_tales_expression(value):
+    from Products.PageTemplates.Expressions import getEngine
+    try:
+        getEngine().compile(value)
+    except Exception:
+        raise Invalid(
+            _(
+                'The expression "${value}" is invalid',
+                mapping={'value': value},
+            )
+        )
+    return True
+
+
 class IActionSchema(Interface):
 
     category = schema.Choice(
@@ -1586,7 +1603,9 @@ class IActionSchema(Interface):
             default=u'An expression producing the called URL. '
             u'Example: string:${globals_view/navigationRootUrl}/page'
         ),
-        required=True)
+        required=True,
+        constraint=_check_tales_expression,
+    )
 
     available_expr = schema.ASCIILine(
         title=_(u'action_condition_heading', default=u'Condition'),
@@ -1628,6 +1647,29 @@ class INewActionSchema(Interface):
     id = schema.ASCIILine(
         title=_(u'Id'),
         required=True)
+
+    @invariant
+    def validate_category_id(data):
+        categoryid = data.category
+        pa = getToolByName(getSite(), 'portal_actions')
+        category = pa.get(categoryid, {})
+        actionid = data.id
+        if actionid in category:
+            raise Invalid(
+                _(
+                    'An action with the id "${actionid}" already exists',
+                    mapping={'actionid': actionid},
+                )
+            )
+        try:
+            category._checkId(actionid)
+        except Exception:
+            raise Invalid(
+                _(
+                    'The id "${actionid}" is invalid',
+                    mapping={'actionid': actionid},
+                )
+            )
 
 
 class IPloneControlPanelView(Interface):
