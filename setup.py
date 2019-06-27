ea5769697381c20e64a275b7b081151806437b32
esteele <eric@esteele.net>
2019-06-27 16:22:50 -0400

    Back to development: 3.2.1
    [ci skip]

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-version = '3.2.0'
+version = '3.2.1.dev0'
 
 long_description = \
     open("README.rst").read() + "\n" + open("CHANGES.rst").read()
