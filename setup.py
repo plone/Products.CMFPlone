314419864660a2a7d3e3d9665d27bd38c568b4f7
Philip Bauer <bauer@starzel.de>
2019-06-29 09:39:50 +0200

    Declare support for Python 3.8

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ setup(
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
     ],
     keywords='Plone CMF Python Zope CMS Webapplication',
