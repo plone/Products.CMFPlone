b8f7fa3e62027fe42f7431f37ad7e2a751bf97e7
Maurits van Rees <maurits@vanrees.org>
2019-10-09 21:49:20 +0200

    Added development status 5 production/stable to classifiers.
    [ci skip]

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setup(
     long_description=open("README.rst").read() + "\n" +
     open("CHANGES.rst").read(),
     classifiers=[
+        "Development Status :: 5 - Production/Stable",
         "Environment :: Web Environment",
         "Framework :: Plone",
         "Framework :: Plone :: 5.2",
