4fd19fe423e84a168a8495d137fe502f8e42a580
Érico Andrei <ericof@gmail.com>
2019-11-06 14:26:15 +0100

    Fix #1318: Always install default content types on Plone site creation (#2971)
    * Fix #1318: Always install default content types on Plone site creation
    
    * Update changelog entry

--- Products/CMFPlone/tests/test_factory.py
+++ Products/CMFPlone/tests/test_factory.py
@@ -1,9 +1,20 @@
 # -*- coding: utf-8 -*-
-from Products.CMFPlone.tests.PloneTestCase import PloneTestCase
+from plone.dexterity.interfaces import IDexterityFTI
 from Products.CMFPlone.factory import addPloneSite
+from Products.CMFPlone.utils import get_installer
+from Products.CMFPlone.testing import PRODUCTS_CMFPLONE_INTEGRATION_TESTING
+from zope.component import queryUtility
+
+import unittest
 
 
-class TestFactoryPloneSite(PloneTestCase):
+class TestFactoryPloneSite(unittest.TestCase):
+
+    layer = PRODUCTS_CMFPLONE_INTEGRATION_TESTING
+
+    def setUp(self):
+        self.app = self.layer['app']
+        self.request = self.layer['request']
 
     def testPlonesiteWithUnicodeTitle(self):
         TITLE = 'Ploné'
@@ -28,3 +39,18 @@ class TestFactoryPloneSite(PloneTestCase):
         ploneSiteTitle = ploneSite.Title()
         self.assertTrue(isinstance(ploneSiteTitle, str))
         self.assertEqual(ploneSiteTitle, TITLE)
+
+    def test_site_creation_without_content_but_with_dexterity(self):
+        """Test site creation without example content have dexterity installed."""
+        ploneSite = addPloneSite(
+            self.app, 'ploneFoo', title='Foo', setup_content=False)
+        qi = get_installer(ploneSite, self.request)
+        self.assertTrue(qi.is_product_installed('plone.app.dexterity'))
+
+    def test_site_creation_without_content_but_with_content_types(self):
+        """Test site creation without example content have content types."""
+        ploneSite = addPloneSite(
+            self.app, 'ploneFoo', title='Foo', setup_content=False)
+        # Folder
+        fti = queryUtility(IDexterityFTI, name='Folder')
+        self.assertIsNotNone(fti)
