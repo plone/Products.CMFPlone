a459c8011d3c178ccd1bd9968f7534455febbc81
ale-rt <alessandro.pisa@gmail.com>
2020-02-06 11:07:42 +0100

    Analytics viewlet: make webstats_js a property
    Analytics viewlet: make webstats_js a property, so that it does not rely on an a call to the update method to be correctly evaluated
    
    Fixes #227

--- plone/app/layout/analytics/view.py
+++ plone/app/layout/analytics/view.py
@@ -16,18 +16,20 @@ class AnalyticsViewlet(BrowserView):
     def __init__(self, context, request, view, manager):
         super(AnalyticsViewlet, self).__init__(context, request)
         self.__parent__ = view
-        self.context = context
-        self.request = request
         self.view = view
         self.manager = manager
 
-    def update(self):
-        """render the webstats snippet"""
+    @property
+    def webstats_js(self):
         registry = getUtility(IRegistry)
         site_settings = registry.forInterface(
             ISiteSchema, prefix="plone", check=False)
         try:
-            if site_settings.webstats_js:
-                self.webstats_js = site_settings.webstats_js
+            return site_settings.webstats_js or u""
         except AttributeError:
-            self.webstats_js = u""
+            return u""
+
+    def update(self):
+        """ The viewlet manager _updateViewlets requires this method
+        """
+        pass
