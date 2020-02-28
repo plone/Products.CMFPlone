902db0c7d96dd89d8a241f4c21dd54a6b84a4736
Jens W. Klein <jk@kleinundpartner.at>
2020-02-28 14:56:30 +0100

    fixes #3059 ReplaceableWrapper does not work

--- Products/CMFPlone/Portal.py
+++ Products/CMFPlone/Portal.py
@@ -1,20 +1,23 @@
 # -*- coding: utf-8 -*-
+
 from AccessControl import ClassSecurityInfo
 from AccessControl import Permissions
 from AccessControl import Unauthorized
-from Acquisition import aq_base
 from AccessControl.class_init import InitializeClass
+from Acquisition import aq_base
 from ComputedAttribute import ComputedAttribute
-from plone.i18n.locales.interfaces import IMetadataLanguageAvailability
+from OFS.ObjectManager import REPLACEABLE
 from Products.CMFCore import permissions
 from Products.CMFCore.PortalObject import PortalObjectBase
+from Products.CMFCore.utils import UniqueObject
 from Products.CMFCore.utils import _checkPermission
 from Products.CMFCore.utils import getToolByName
-from Products.CMFCore.utils import UniqueObject
 from Products.CMFDynamicViewFTI.browserdefault import BrowserDefaultMixin
-from Products.CMFPlone import bbb
 from Products.CMFPlone import PloneMessageFactory as _
+from Products.CMFPlone import bbb
 from Products.CMFPlone.DublinCore import DefaultDublinCoreImpl
+from Products.CMFPlone.PloneFolder import OrderedContainer
+from Products.CMFPlone.PloneFolder import ReplaceableWrapper
 from Products.CMFPlone.interfaces.siteroot import IPloneSiteRoot
 from Products.CMFPlone.interfaces.syndication import ISyndicatable
 from Products.CMFPlone.permissions import AddPortalContent
@@ -23,11 +26,9 @@ from Products.CMFPlone.permissions import ListPortalMembers
 from Products.CMFPlone.permissions import ModifyPortalContent
 from Products.CMFPlone.permissions import ReplyToItem
 from Products.CMFPlone.permissions import View
-from Products.CMFPlone.PloneFolder import OrderedContainer
-from Products.CMFPlone.PloneFolder import ReplaceableWrapper
+from plone.i18n.locales.interfaces import IMetadataLanguageAvailability
 from zope.component import queryUtility
 from zope.interface import implementer
-
 import six
 
 if bbb.HAS_ZSERVER:
@@ -88,20 +89,24 @@ class PloneSite(PortalObjectBase, DefaultDublinCoreImpl, OrderedContainer,
     def index_html(self):
         """ Acquire if not present. """
         request = getattr(self, 'REQUEST', None)
-        if request is not None and 'REQUEST_METHOD' in request:
-            if request.maybe_webdav_client:
-                method = request['REQUEST_METHOD']
-                if bbb.HAS_ZSERVER and method in ('PUT', ):
-                    # Very likely a WebDAV client trying to create something
-                    return ReplaceableWrapper(NullResource(self, 'index_html'))
-                elif method in ('GET', 'HEAD', 'POST'):
-                    # Do nothing, let it go and acquire.
-                    pass
-                else:
-                    raise AttributeError('index_html')
+        if (
+            request is not None
+            and 'REQUEST_METHOD' in request
+            and request.maybe_webdav_client
+        ):
+            method = request['REQUEST_METHOD']
+            if bbb.HAS_ZSERVER and method in ('PUT', ):
+                # Very likely a WebDAV client trying to create something
+                result = NullResource(self, 'index_html')
+                setattr(result, '__replaceable__', REPLACEABLE)
+                return ReplaceableWrapper()
+            elif method not in ('GET', 'HEAD', 'POST'):
+                raise AttributeError('index_html')
         # Acquire from skin.
         _target = self.__getattr__('index_html')
-        return ReplaceableWrapper(aq_base(_target).__of__(self))
+        result = aq_base(_target).__of__(self)
+        setattr(result, '__replaceable__', REPLACEABLE)
+        return result
 
     index_html = ComputedAttribute(index_html, 1)
 
