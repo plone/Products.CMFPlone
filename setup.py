80901429f7d95ef16eba022f7734db668bc25852
Maurits van Rees <maurits@vanrees.org>
2020-07-17 16:28:57 +0200

    Back to development: 3.4.5
    [ci skip]

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-version = "3.4.4"
+version = "3.4.5.dev0"
 
 long_description = open("README.rst").read() + "\n" + open("CHANGES.rst").read()
 
