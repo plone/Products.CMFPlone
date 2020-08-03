bc2f6d88c45a82090e3d2798b37096a78d536173
Vincent Fretin <vincent.fretin@gmail.com>
2020-08-03 13:03:39 +0200

    new bundles

--- Products/CMFPlone/static/resourceregistry-compiled.js
+++ Products/CMFPlone/static/resourceregistry-compiled.js
@@ -34191,5 +34191,5 @@ require([
   'use strict';
 });
 
-define("/Users/esteele/projects/plone_5.1/src/Products.CMFPlone/Products/CMFPlone/static/resourceregistry.js", function(){});
+define("/plone51/src/Products.CMFPlone/Products/CMFPlone/static/resourceregistry.js", function(){});
 
