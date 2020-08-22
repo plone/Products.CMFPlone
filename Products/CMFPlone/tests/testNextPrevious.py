4fc8503608fb7316e529a407b2b5b43d7284c448
Roel Bruggink <roel@jaroel.nl>
2020-08-22 19:20:02 +0200

    Fix 'fix tests'. If NextPrevious is disabled we, correctly, should expect view.next and view.previous to be None

--- Products/CMFPlone/tests/testNextPrevious.py
+++ Products/CMFPlone/tests/testNextPrevious.py
@@ -79,8 +79,8 @@ class TestNextPrevious(PloneTestCase.PloneTestCase):
                                                    None)
         self.assertTrue(view)
         self.assertFalse(view.enabled())
-        self.assertNotEqual(None, view.next())
-        self.assertNotEqual(None, view.previous())
+        self.assertEqual(None, view.next())
+        self.assertEqual(None, view.previous())
 
     def testNextPreviousItems(self):
         self.folder.invokeFactory('Folder', 'case3')
