8dffc765fb4bf3a612b100587b8ca7b5a24d9c3f
Jens W. Klein <jk@kleinundpartner.at>
2020-11-26 20:24:07 +0100

    add SVG modifier

--- Products/CMFPlone/browser/icons.py
+++ Products/CMFPlone/browser/icons.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from lxml import etree
 from OFS.Image import File
 from plone.registry.interfaces import IRegistry
 from Products.Five.browser import BrowserView
@@ -17,6 +18,42 @@ from zExceptions import NotFound
 import logging
 
 logger = logging.getLogger(__name__)
+
+
+SVG_MODIFER = {}
+
+def _add_aria_title(svgtree, cfg):
+    if not "title" in cfg:
+        return
+    root = svgtree.getroot()
+    ns = root.nsmap.get(None, "")
+    # set title tag
+    title = root.find(f'{{{ns}}}title')
+    if title is None:
+        title = etree.Element("title")
+        root.append(title)
+    title.text = cfg['title']
+    # add aria attr
+    root.attrib["aria-labelledby"] = "title"
+
+
+SVG_MODIFER['add_aria_title'] = _add_aria_title
+
+
+def _add_css_class(svgtree, cfg):
+    if not cfg.get('cssclass', False):
+        return
+    root = svgtree.getroot()
+    current = root.attrib.get('class', '')
+    root.attrib['class'] = f"{cfg['cssclass']} {current}"
+
+SVG_MODIFER['add_css_class'] = _add_css_class
+
+def _strip_id(svgtree, cfg):
+    for el in svgtree.getroot().xpath("//*[@id]"):
+        del el.attrib["id"]
+
+SVG_MODIFER['strip_id'] = _strip_id
 
 
 @implementer(IPublishTraverse)
@@ -75,5 +112,24 @@ class IconsView(BrowserView):
             raise NotImplementedError(
                 "Resolve icons stored in database is not yet implemented."
             )
-        with open(iconfile.path, "rb") as fh:
-            return fh.read()
+        try:
+            with open(iconfile.path, "rb") as fh:
+                svgtree = etree.parse(fh)
+        except etree.XMLSyntaxError:
+            logger.exception(f"SVG File: {iconfile.path}")
+            with open(iconfile.path, "rb") as fh:
+                return fh.read()
+        if svgtree.docinfo.root_name.lower() != 'svg':
+            raise ValueError(f"SVG file content root tag mismatch (not svg but {svgtree.docinfo.root_name}): {iconfile.path}")
+        modifier_cfg = {
+            'cssclass': tag_class,
+            'title': tag_alt,
+        }
+        for name, modifier in SVG_MODIFER.items():
+            __traceback_info__ = name
+            modifier(svgtree, modifier_cfg)
+        return etree.tostring(svgtree)
+
+
+
+
