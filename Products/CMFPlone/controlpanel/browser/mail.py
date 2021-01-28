66b08fc95ccd89977b0af0c97eed244613fbed95
Jens W. Klein <jk@kleinundpartner.at>
2021-01-28 10:06:01 +0100

    try to fix test using @mauritsvanrees hint

--- Products/CMFPlone/controlpanel/browser/mail.py
+++ Products/CMFPlone/controlpanel/browser/mail.py
@@ -39,8 +39,9 @@ class MailControlPanelForm(controlpanel.RegistryEditForm):
         # keep password field
         smtp_user_id = (data.get('smtp_userid') or '').strip()
         smtp_pass = (data.get('smtp_pass') or '').strip()
-        if not (smtp_user_id or smtp_pass):
+        if smtp_user_id and not smtp_pass:
             del data['smtp_pass']
+
         self.applyChanges(data)
         return True
 
