cb8477f2c0089f4d86bf3d7b18cde7968a9570e5
Johannes Raggam <thetetet@gmail.com>
2021-02-18 01:30:21 +0100

    Code style: Apply black in Products.CMFPlone.patterns.

--- Products/CMFPlone/patterns/utils.py
+++ Products/CMFPlone/patterns/utils.py
@@ -5,31 +5,29 @@ import warnings
 
 
 def format_pattern_settings(option, config):
-    if option.startswith('json:'):
+    if option.startswith("json:"):
         try:
-            return json.loads(option.lstrip('json:') % config)
+            return json.loads(option.lstrip("json:") % config)
         except:
             return {}
     return option % config
 
 
 def get_portal():
-    """DEPRECATED
-    """
+    """DEPRECATED"""
     warnings.warn(
-        'Instead of Products.CMFPlone.patterns.get_portal: '
-        'use Products.CMFPlone.utils.get_portal',
-        DeprecationWarning
+        "Instead of Products.CMFPlone.patterns.get_portal: "
+        "use Products.CMFPlone.utils.get_portal",
+        DeprecationWarning,
     )
     return utils.get_portal()
 
 
 def get_portal_url(context):
-    """DEPRECATED
-    """
+    """DEPRECATED"""
     warnings.warn(
-        'Instead of Products.CMFPlone.patterns.get_portal_url: '
-        'Use Products.CMFPlone.utils.portal.absolute_url()',
-        DeprecationWarning
+        "Instead of Products.CMFPlone.patterns.get_portal_url: "
+        "Use Products.CMFPlone.utils.portal.absolute_url()",
+        DeprecationWarning,
     )
     return utils.get_portal().absolute_url()
