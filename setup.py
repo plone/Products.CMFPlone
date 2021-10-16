3da6dcc0ae420b1c01d8bebe9dff2d6ec252556b
Maurits van Rees <maurits@vanrees.org>
2021-10-16 15:14:54 +0200

    Back to development: 4.0.0a5
    [ci skip]

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-version = "4.0.0a4"
+version = "4.0.0a5.dev0"
 
 long_description = open("README.rst").read() + "\n" + open("CHANGES.rst").read()
 
