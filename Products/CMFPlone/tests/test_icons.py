e6e3d70910fa9b822b2625a467353b0381143971
Maurits van Rees <maurits@vanrees.org>
2022-01-18 22:53:15 +0100

    Fixed icon traverser.
    It could fail when visiting a page that would traverse to an icon.
    Happens at least in plone.i18n tests, and this fixes 13 failures there.
    Added a few tests in CMFPlone as well.

--- Products/CMFPlone/tests/test_icons.py
+++ Products/CMFPlone/tests/test_icons.py
@@ -37,3 +37,38 @@ class IconsTest(unittest.TestCase):
         page = self.app
         view = page.restrictedTraverse('@@iconresolver')
         self.assertIn(b'bi bi-bug', view.tag('bug'))
+
+
+
+class IconTraverserTest(unittest.TestCase):
+    """Test the icon traverser or PloneBundlesTraverser."""
+
+    layer = PRODUCTS_CMFPLONE_INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        self.request = self.layer['request']
+
+    def test_default_icon(self):
+        self.portal.restrictedTraverse("++plone++icons/plone.svg")
+
+    def test_bootstrap_icon(self):
+        self.portal.restrictedTraverse("++plone++bootstrap-icons/clock.svg")
+
+    def test_bootstrap_icon_with_path_info(self):
+        """Get bootstrap icon while request has a PATH_INFO.
+
+        When the request has PATH_INFO, which it normally has,
+        the code originally ignored the remaining name (clock.svg)
+        and tried to traverse based on this PATH_INFO.
+        This works fine when the url of the request is for an icon.
+        But when the url is for a normal page which tries to load an icon
+        in the template, it fails.
+
+        I don't know why most of the time it goes right.
+        But I have occasionally seen failures.
+        And it happens at least in plone.i18n tests.
+        https://github.com/plone/plone.i18n/pull/41
+        """
+        self.request.environ["PATH_INFO"] = "plone"
+        self.portal.restrictedTraverse("++plone++bootstrap-icons/clock.svg")
