652497b84c77122e27642997e9afb675c73638b6
Manuel Amador (Rudd-O) <rudd-o@rudd-o.com>
2022-01-29 04:27:13 +0000

    Handle default case, and correct the mimetype with the right default.

--- Products/CMFPlone/browser/favicon.py
+++ Products/CMFPlone/browser/favicon.py
@@ -1,10 +1,12 @@
+import os
+
 from Products.CMFPlone.interfaces import ISiteSchema
 from plone.formwidget.namedfile.converter import b64decode_file
+from plone.memoize import ram
 from plone.namedfile.browser import Download
 from plone.namedfile.file import NamedImage
 from plone.registry.interfaces import IRegistry
 from zope.component import getUtility
-from plone.memoize import ram
 
 
 class SiteFavicon(Download):
@@ -18,9 +20,16 @@ class SiteFavicon(Download):
         settings = registry.forInterface(ISiteSchema, prefix="plone")
         if getattr(settings, 'site_favicon', False):
             filename, data = b64decode_file(settings.site_favicon)
-            data = NamedImage(data=data, filename=filename)
+            mimetype = getattr(settings, 'site_favicon_mimetype', "image/vnd.microsoft.icon")
+            data = NamedImage(data=data, contentType=mimetype, filename=filename)
             self.data = data
             self.filename = filename
+        else:
+            basedir = os.path.dirname(os.path.dirname(__file__))
+            with open(os.path.join(basedir, "skins", "plone_images", "favicon.ico"), "rb") as icon:
+                data = NamedImage(data=icon.read(), contentType="image/vnd.microsoft.icon", filename="favicon.ico")
+            self.data = data
+            self.filename = "favicon.ico"
 
     def _getFile(self):
         return self.data
