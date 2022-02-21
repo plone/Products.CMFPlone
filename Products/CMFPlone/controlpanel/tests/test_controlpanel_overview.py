280ab0a520bd6bd14d258619033fbf08632af0b7
Benoit Suttor <benoit.suttor@imio.be>
2022-02-21 16:56:48 +0100

    become black compliant

--- Products/CMFPlone/controlpanel/tests/test_controlpanel_overview.py
+++ Products/CMFPlone/controlpanel/tests/test_controlpanel_overview.py
@@ -4,6 +4,7 @@ from plone.app.testing import setRoles
 from plone.registry.interfaces import IRegistry
 from zope.component import getUtility
 from unittest import mock
+import os
 import unittest
 
 
@@ -12,15 +13,15 @@ def mock_getUtility1(iface):
 
 
 def mock_getUtility2(iface):
-    return {'plone.portal_timezone': None}
+    return {"plone.portal_timezone": None}
 
 
 def mock_getUtility3(iface):
-    return {'plone.portal_timezone': 'Europe/Amsterdam'}
+    return {"plone.portal_timezone": "Europe/Amsterdam"}
 
 
 def mock_getUtility4(iface):
-    return {'plone.app.event.portal_timezone': 'Europe/Amsterdam'}
+    return {"plone.app.event.portal_timezone": "Europe/Amsterdam"}
 
 
 class TestControlPanel(unittest.TestCase):
@@ -28,34 +29,40 @@ class TestControlPanel(unittest.TestCase):
     layer = PLONE_INTEGRATION_TESTING
 
     def setUp(self):
-        self.portal = self.layer['portal']
-        self.request = self.layer['request']
-        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+        self.portal = self.layer["portal"]
+        self.request = self.layer["request"]
+        setRoles(self.portal, TEST_USER_ID, ["Manager"])
 
-    @mock.patch('Products.CMFPlone.controlpanel.browser.overview.getUtility',
-                new=mock_getUtility1)
+    @mock.patch(
+        "Products.CMFPlone.controlpanel.browser.overview.getUtility",
+        new=mock_getUtility1,
+    )
     def test_timezone_warning__noreg(self):
         # If no registry key is available, return True
         registry = getUtility(IRegistry)
         reg_key = "plone.portal_timezone"
         del registry.records[reg_key]
         self.assertFalse(reg_key in registry)
-        view = self.portal.restrictedTraverse('@@overview-controlpanel')
+        view = self.portal.restrictedTraverse("@@overview-controlpanel")
         self.assertTrue(view.timezone_warning())
 
-    @mock.patch('Products.CMFPlone.controlpanel.browser.overview.getUtility',
-                new=mock_getUtility2)
+    @mock.patch(
+        "Products.CMFPlone.controlpanel.browser.overview.getUtility",
+        new=mock_getUtility2,
+    )
     def test_timezone_warning__emptyreg(self):
         # If registry key value is empty, return True
         registry = getUtility(IRegistry)
         reg_key = "plone.portal_timezone"
         registry[reg_key] = None
-        view = self.portal.restrictedTraverse('@@overview-controlpanel')
+        view = self.portal.restrictedTraverse("@@overview-controlpanel")
         self.assertTrue(view.timezone_warning())
 
-    @mock.patch('Products.CMFPlone.controlpanel.browser.overview.getUtility',
-                new=mock_getUtility3)
+    @mock.patch(
+        "Products.CMFPlone.controlpanel.browser.overview.getUtility",
+        new=mock_getUtility3,
+    )
     def test_timezone_warning__set(self):
         # If new plone.portal_timezone is set, return False
-        view = self.portal.restrictedTraverse('@@overview-controlpanel')
+        view = self.portal.restrictedTraverse("@@overview-controlpanel")
         self.assertFalse(view.timezone_warning())
