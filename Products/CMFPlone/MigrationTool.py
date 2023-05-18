a9ce5b99d451597e59a9b92dee1572e08677fd61
Roman <roman.kysil@redturtle.it>
2023-05-18 12:32:29 +0200

    Remove plone.app.multilingual dependency

--- Products/CMFPlone/MigrationTool.py
+++ Products/CMFPlone/MigrationTool.py
@@ -107,7 +107,10 @@ ADDON_LIST = AddonList([
         profile_id='plone.app.iterate:default',
         check_module='plone.app.iterate'
     ),
-    Addon(profile_id='plone.app.multilingual:default'),
+    Addon(
+        profile_id='plone.app.multilingual:default',
+        check_module="plone.app.multilingual"
+    ),
     Addon(profile_id='plone.app.querystring:default'),
     Addon(profile_id='plone.app.theming:default'),
     Addon(profile_id='plone.app.users:default'),
