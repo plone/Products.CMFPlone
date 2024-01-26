e77b4ad69ace280f54cada4308c99f3fd6a155bc
Maurits van Rees <maurits@vanrees.org>
2024-01-26 01:51:44 +0100

    Added Python 3.12 to classifiers in setup.py.
    [ci skip]

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
     ],
     python_requires=">=3.8",
     keywords="Plone CMF Python Zope CMS Webapplication",
