78221eae27d6c9b93e08fcfc3c54cbe194344dba
David Glick <david@glicksoftware.com>
2025-01-14 14:54:06 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
         "Programming Language :: Python :: 3.12",
         "Programming Language :: Python :: 3.13",
     ],
-    python_requires=">=3.9",
+    python_requires=">=3.8",
     keywords="Plone CMF Python Zope CMS Webapplication",
     author="Plone Foundation",
     author_email="releasemanager@plone.org",
