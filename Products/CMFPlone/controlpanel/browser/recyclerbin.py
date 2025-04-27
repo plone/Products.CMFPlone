c80f1fa58a2b1b0cdf0a68fb5b679708952abd90
Rohan Shaw <rohnsha0@gmail.com>
2025-04-27 14:41:02 +0530

    lint

--- Products/CMFPlone/controlpanel/browser/recyclerbin.py
+++ Products/CMFPlone/controlpanel/browser/recyclerbin.py
@@ -1,40 +1,46 @@
 # testcp.py
+from plone.app.registry.browser.controlpanel import ControlPanelFormWrapper
+from plone.app.registry.browser.controlpanel import RegistryEditForm
+from plone.z3cform import layout
 from zope import schema
 from zope.interface import Interface
-from plone.app.registry.browser.controlpanel import RegistryEditForm, ControlPanelFormWrapper
-from plone.z3cform import layout
+
 
 class IRecycleBinControlPanelSettings(Interface):
     recycling_enabled = schema.Bool(
-        title=u"Enable the Recycle Bin",
-        description=u"Enable or disable the Recycle Bin feature.",
+        title="Enable the Recycle Bin",
+        description="Enable or disable the Recycle Bin feature.",
         default=True,
     )
 
     retention_period = schema.Int(
-        title=u"Retention Period",
-        description=u"Number of days to keep deleted items in the Recycle Bin.",
+        title="Retention Period",
+        description="Number of days to keep deleted items in the Recycle Bin.",
         default=30,
         min=1,
     )
 
     maximum_size = schema.Int(
-        title=u"Maximum Size",
-        description=u"Maximum size of the Recycle Bin in MB.",
+        title="Maximum Size",
+        description="Maximum size of the Recycle Bin in MB.",
         default=100,
         min=10,
     )
-    
+
     auto_purge = schema.Bool(
-        title=u"Auto Purge",
-        description=u"Automatically purge items older than the retention period.",
+        title="Auto Purge",
+        description="Automatically purge items older than the retention period.",
         default=True,
     )
+
 
 class RecyclebinControlPanelForm(RegistryEditForm):
     schema = IRecycleBinControlPanelSettings
     schema_prefix = "plone-recyclebin"
-    label = u"Recycle Bin Settings"
-    description = u"Settings for the Plone Recycle Bin functionality"
+    label = "Recycle Bin Settings"
+    description = "Settings for the Plone Recycle Bin functionality"
 
-RecyclebinControlPanelView = layout.wrap_form(RecyclebinControlPanelForm, ControlPanelFormWrapper)
+
+RecyclebinControlPanelView = layout.wrap_form(
+    RecyclebinControlPanelForm, ControlPanelFormWrapper
+)
