5f7c305828d939c81b3b39e5df2c016a75df70c2
Rohan Shaw <rohnsha0@gmail.com>
2025-04-27 14:28:31 +0530

    Implement Recycle Bin functionality with management views and settings

--- Products/CMFPlone/events/recyclebin.py
+++ Products/CMFPlone/events/recyclebin.py
@@ -1,46 +1,7 @@
-from plone.base.interfaces import IReorderedEvent
-from plone.base.interfaces import ISiteManagerCreatedEvent
-from plone.base.utils import get_installer
-from zope.interface import implementer
-from zope.interface.interfaces import ObjectEvent
 from zope.component import adapter, queryUtility
 from zope.lifecycleevent.interfaces import IObjectRemovedEvent
 from Products.CMFCore.interfaces import IContentish
 from Products.CMFPlone.interfaces.recyclebin import IRecycleBin
-
-
-@implementer(ISiteManagerCreatedEvent)
-class SiteManagerCreatedEvent(ObjectEvent):
-    pass
-
-
-@implementer(IReorderedEvent)
-class ReorderedEvent(ObjectEvent):
-    pass
-
-
-def profileImportedEventHandler(event):
-    """
-    When a profile is imported with the keyword "latest", it needs to
-    be reconfigured with the actual number.
-    """
-    profile_id = event.profile_id
-    if profile_id is None:
-        return
-    profile_id = profile_id.replace("profile-", "")
-    gs = event.tool
-    installed_version = gs.getLastVersionForProfile(profile_id)
-    if installed_version == ("latest",):
-        qi = get_installer(gs, gs.REQUEST)
-        actual_version = qi.get_latest_upgrade_step(profile_id)
-        gs.setLastVersionForProfile(profile_id, actual_version)
-
-
-def removeBase(event):
-    """Make Zope not to inject a <base> tag into the returned HTML
-    https://dev.plone.org/ticket/13705
-    """
-    event.request.response.base = None
 
 
 @adapter(IContentish, IObjectRemovedEvent)
@@ -75,4 +36,3 @@ def handle_content_removal(obj, event):
         import logging
         logger = logging.getLogger("Products.CMFPlone.RecycleBin")
         logger.exception(f"Error adding item to recycle bin: {e}")
-
