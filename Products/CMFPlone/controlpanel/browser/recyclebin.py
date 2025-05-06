d1d0ab086a9f6b294b061457d8c9ab1aec664255
Rohan Shaw <rohnsha0@gmail.com>
2025-05-06 14:40:10 +0530

    feat(recyclebin): enhance internationalization support in recycle bin views and templates

--- Products/CMFPlone/controlpanel/browser/recyclebin.py
+++ Products/CMFPlone/controlpanel/browser/recyclebin.py
@@ -1,14 +1,16 @@
 from plone.app.registry.browser.controlpanel import ControlPanelFormWrapper
 from plone.app.registry.browser.controlpanel import RegistryEditForm
+from plone.base import PloneMessageFactory as _
 from plone.base.interfaces.recyclebin import IRecycleBinControlPanelSettings
 from plone.z3cform import layout
+from zope.i18n import translate
 
 
 class RecyclebinControlPanelForm(RegistryEditForm):
     schema = IRecycleBinControlPanelSettings
     schema_prefix = "plone-recyclebin"
-    label = "Recycle bin settings"
-    description = "Settings for the Plone recycle bin"
+    label = _("Recycle bin settings")
+    description = _("Settings for the Plone recycle bin")
 
 
 RecyclebinControlPanelView = layout.wrap_form(
