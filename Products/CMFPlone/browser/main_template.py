bc3b419e9ac242f7cbd65e46767bca535d511e96
Johannes Raggam <thetetet@gmail.com>
2025-06-24 01:32:54 +0200

    Automatically use the AJAX main template.
    Use the ajax main template if:
    
    - ajax_load translate to True
    - or if we have an xhr request and plone.use_ajax (TBD) is set to True.
    
    Don't use the ajax main template if ajax_load is set to a falsy value or we're
    not in an xhr request.

--- Products/CMFPlone/browser/main_template.py
+++ Products/CMFPlone/browser/main_template.py
@@ -1,7 +1,9 @@
 from plone.base.utils import is_truthy
+from plone.registry.interfaces import IRegistry
 from Products.CMFPlone.browser.interfaces import IMainTemplate
 from Products.Five import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from zope.component import getUtility
 from zope.interface import implementer
 
 
@@ -13,10 +15,26 @@ class MainTemplate(BrowserView):
     def __call__(self):
         return self.template()
 
+    def is_xhr(self):
+        """Check if the current request is an XHR request."""
+        return self.request.get("HTTP_X_REQUESTED_WITH") == "XMLHttpRequest"
+
+    def use_ajax(self):
+        """Check if the current request should use the ajax main template."""
+
+        # if ajax_load is not None:
+        if ajax_load := self.request.get("ajax_load", None) is not None:
+            # Explicitly set ajax_load takes precedence.
+            return is_truthy(ajax_load)
+
+        # Use the ajax main template if enabled and if we have an XHR request.
+        registry = getUtility(IRegistry)
+        use_ajax = self.is_xhr() and registry.get("plone.use_ajax", False)
+        return use_ajax
+
     @property
     def template(self):
-        # Directly query the request object, which also includes the form.
-        if is_truthy(self.request.get("ajax_load")):
+        if self.use_ajax():
             return self.ajax_template
         else:
             return self.main_template
