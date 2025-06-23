3eb8313ed962d4f6dc49d4ab6b348ca817bfa2b3
Johannes Raggam <thetetet@gmail.com>
2025-06-24 01:35:53 +0200

    Cleanup and remove set_ajax event subscriber.

--- Products/CMFPlone/events.py
+++ Products/CMFPlone/events.py
@@ -39,26 +39,3 @@ def removeBase(event):
     https://dev.plone.org/ticket/13705
     """
     event.request.response.base = None
-
-
-def set_ajax(event):
-    """Set the ajax_load parameter automatically for AJAX requests."""
-
-    registry = queryUtility(IRegistry)
-    if registry is None:
-        return
-
-    if not registry.get("plone.ajax_marker"):
-        # Autmatic ajax_load request marking disabled.
-        return
-
-    request = event.request
-
-    # If ajax_load was already set to a true-ish or false-ish value, don't set
-    # it again.
-    if (
-        request.getHeader("HTTP_X_REQUESTED_WITH") == "XMLHttpRequest"
-        and "ajax_load" not in request
-    ):
-        # Directly set on the request object
-        request.set("ajax_load", True)
