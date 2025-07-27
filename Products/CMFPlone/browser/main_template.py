68e386ee370ba17334486940857266ec77211075
Johannes Raggam <thetetet@gmail.com>
2025-07-27 13:54:09 +0200

    fixup! use plone_layout utils.

--- Products/CMFPlone/browser/main_template.py
+++ Products/CMFPlone/browser/main_template.py
@@ -1,9 +1,8 @@
-from plone.base.utils import is_truthy
-from plone.registry.interfaces import IRegistry
+from plone.memoize.view import memoize
 from Products.CMFPlone.browser.interfaces import IMainTemplate
 from Products.Five import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-from zope.component import getUtility
+from zope.component import getMultiAdapter
 from zope.interface import implementer
 
 
@@ -15,26 +14,14 @@ class MainTemplate(BrowserView):
     def __call__(self):
         return self.template()
 
-    def is_xhr(self):
-        """Check if the current request is an XHR request."""
-        return self.request.get("HTTP_X_REQUESTED_WITH") == "XMLHttpRequest"
-
-    def use_ajax(self):
-        """Check if the current request should use the ajax main template."""
-
-        # if ajax_load is not None:
-        if ajax_load := self.request.get("ajax_load", None) is not None:
-            # Explicitly set ajax_load takes precedence.
-            return is_truthy(ajax_load)
-
-        # Use the ajax main template if enabled and if we have an XHR request.
-        registry = getUtility(IRegistry)
-        use_ajax = self.is_xhr() and registry.get("plone.use_ajax", False)
-        return use_ajax
+    @property
+    @memoize
+    def plone_layout(self):
+        return getMultiAdapter((self.context, self.request), name="plone_layout")
 
     @property
     def template(self):
-        if self.use_ajax():
+        if self.plone_layout.use_ajax():
             return self.ajax_template
         else:
             return self.main_template
