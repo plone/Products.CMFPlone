fb4cb04bc763b804dce754006a0a0b474ac7ca11
Johannes Raggam <thetetet@gmail.com>
2025-07-27 13:54:17 +0200

    fixup! tst fixes

--- src/Products/CMFPlone/tests/test_main_template.py
+++ src/Products/CMFPlone/tests/test_main_template.py
@@ -1,7 +1,7 @@
 from Products.CMFPlone.testing import PRODUCTS_CMFPLONE_INTEGRATION_TESTING
+from plone.registry.interfaces import IRegistry
 from zope.component import getMultiAdapter
-from zope.event import notify
-from ZPublisher.pubevents import PubAfterTraversal
+from zope.component import getUtility
 
 import unittest
 
@@ -11,7 +11,7 @@ class TestMainTemplate(unittest.TestCase):
 
     def setUp(self):
         self.portal = self.layer["portal"]
-        self.request = self.layer["request"]
+        self.request = self.layer["request"].clone()
 
     def test_request_containment(self):
         """Very basic test to show that the following works:
@@ -19,10 +19,9 @@ class TestMainTemplate(unittest.TestCase):
         instead of:
         `request.get("ajax_load", MARKER) is not MARKER1`
         """
-        request = self.request.clone()
-        request.form["ajax_load"] = True
-        self.assertTrue("ajax_load" in request.form)
-        self.assertTrue("ajax_load" in request)
+        self.request.form["ajax_load"] = True
+        self.assertTrue("ajax_load" in self.request.form)
+        self.assertTrue("ajax_load" in self.request)
 
     def test_main_template_standard(self):
         """Test the standard case to use the main_template.pt."""
@@ -30,61 +29,63 @@ class TestMainTemplate(unittest.TestCase):
         self.assertIn("/main_template.pt", view.template.filename)
 
     def test_main_template_ajax_parameter(self):
-        """Test an explicitly set `ajax_load` URL parameter leads to use the
-        ajax_main_template.pt.
+        """Test, if an explicitly set `ajax_load` URL parameter leads to use
+        the ajax_main_template.pt.
         """
-        request = self.request.clone()
-        request.form["ajax_load"] = True
-        view = getMultiAdapter((self.portal, request), name="main_template")
+        self.request.form["ajax_load"] = True
+        view = getMultiAdapter((self.portal, self.request), name="main_template")
         self.assertIn("/ajax_main_template.pt", view.template.filename)
 
     def test_main_template_ajax_manually(self):
-        """Test an explicitly set `ajax_load` request parameter leads to use the
-        ajax_main_template.pt.
+        """Test, if an explicitly set `ajax_load` request parameter leads to
+        use the ajax_main_template.pt.
         """
-        request = self.request.clone()
-        request.set("ajax_load", True)
-        view = getMultiAdapter((self.portal, request), name="main_template")
+        self.request.set("ajax_load", True)
+        view = getMultiAdapter((self.portal, self.request), name="main_template")
         self.assertIn("/ajax_main_template.pt", view.template.filename)
 
     def test_main_template_auto(self):
-        """Test an AJAX request leads to the ajax_main_template.pt."""
-        request = self.request.clone()
-        request.environ["HTTP_X_REQUESTED_WITH"] = "XMLHttpRequest"
-        # Manually trigger the PubAfterTraversal event to set the ajax_load parameter
-        notify(PubAfterTraversal(request))
-        view = getMultiAdapter((self.portal, request), name="main_template")
+        """Test, if an AJAX request leads to the ajax_main_template.pt."""
+        registry = getUtility(IRegistry)
+        registry["plone.use_ajax"] = True
+        self.request.environ["HTTP_X_REQUESTED_WITH"] = "XMLHttpRequest"
+        view = getMultiAdapter((self.portal, self.request), name="main_template")
         self.assertIn("/ajax_main_template.pt", view.template.filename)
 
     def test_main_template_noauto_if_set(self):
         """Test a falsy ajax_load value and a AJAX request forces to use the
         normal main_template and not the ajax_main_template.pt.
         """
-        request = self.request.clone()
-        request.form["ajax_load"] = "False"
-        request.environ["HTTP_X_REQUESTED_WITH"] = "XMLHttpRequest"
-        # Manually trigger the PubAfterTraversal event to set the ajax_load parameter
-        notify(PubAfterTraversal(request))
-        view = getMultiAdapter((self.portal, request), name="main_template")
+        registry = getUtility(IRegistry)
+        registry["plone.use_ajax"] = True
+        self.request.environ["HTTP_X_REQUESTED_WITH"] = "XMLHttpRequest"
+        self.request.form["ajax_load"] = "False"
+        view = getMultiAdapter((self.portal, self.request), name="main_template")
         self.assertIn("/main_template.pt", view.template.filename)
 
     def test_main_template_no_ajax_1(self):
         """Test a falsy ajax_load value does not lead to ajax_main_template.pt."""
-        request = self.request.clone()
-        request.form["ajax_load"] = False
-        view = getMultiAdapter((self.portal, request), name="main_template")
+        registry = getUtility(IRegistry)
+        registry["plone.use_ajax"] = True
+        self.request.environ["HTTP_X_REQUESTED_WITH"] = "XMLHttpRequest"
+        self.request.form["ajax_load"] = False
+        view = getMultiAdapter((self.portal, self.request), name="main_template")
         self.assertIn("/main_template.pt", view.template.filename)
 
     def test_main_template_no_ajax_2(self):
         """Test a falsy ajax_load value does not lead to ajax_main_template.pt."""
-        request = self.request.clone()
-        request.form["ajax_load"] = "0"
-        view = getMultiAdapter((self.portal, request), name="main_template")
+        registry = getUtility(IRegistry)
+        registry["plone.use_ajax"] = True
+        self.request.environ["HTTP_X_REQUESTED_WITH"] = "XMLHttpRequest"
+        self.request.form["ajax_load"] = "0"
+        view = getMultiAdapter((self.portal, self.request), name="main_template")
         self.assertIn("/main_template.pt", view.template.filename)
 
     def test_main_template_no_ajax_3(self):
         """Test a falsy ajax_load value does not lead to ajax_main_template.pt."""
-        request = self.request.clone()
-        request.form["ajax_load"] = "off"
-        view = getMultiAdapter((self.portal, request), name="main_template")
+        registry = getUtility(IRegistry)
+        registry["plone.use_ajax"] = True
+        self.request.environ["HTTP_X_REQUESTED_WITH"] = "XMLHttpRequest"
+        self.request.form["ajax_load"] = "off"
+        view = getMultiAdapter((self.portal, self.request), name="main_template")
         self.assertIn("/main_template.pt", view.template.filename)
