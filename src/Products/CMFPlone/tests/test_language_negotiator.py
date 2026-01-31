1673766e1d6a4ee14b0149f2074a70455444b25a
SHIVAANSH0610_LUFFY <shivaansh0610@gmail.com>
2026-02-01 00:22:52 +0530

    Register plone.i18n negotiator as Zope INegotiator utility

---
+++ src/Products/CMFPlone/tests/test_language_negotiator.py
@@ -0,0 +1,7 @@
+from zope.component import getUtility
+from zope.i18n.interfaces import INegotiator
+
+ # It will Ensure we are not using a Zope default or fallback negotiator
+def test_plone_registers_i18n_negotiator():
+    negotiator = getUtility(INegotiator)
+    assert negotiator.__class__.__module__.startswith("plone.i18n")
